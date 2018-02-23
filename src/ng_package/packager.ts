51de61aed68c75e6b3553ce00c31db9812290e26
Victor Berchet <victor@suumit.com>
2018-02-23 21:21:11 +0000

    3a809cb build: add support for the "merge-assistance" label in merge-pr (#22414)

--- src/ng_package/packager.ts
+++ src/ng_package/packager.ts
@@ -32,7 +32,7 @@ function main(args: string[]): number {
   function replaceVersionPlaceholders(filePath: string) {
     if (stampData) {
       const version = shx.grep('BUILD_SCM_VERSION', stampData).split(' ')[1].trim();
-      return shx.sed(/6.0.0-beta.5-b43b164/, version, filePath);
+      return shx.sed(/6.0.0-beta.5-3a809cb/, version, filePath);
     }
     return shx.cat(filePath);
   }
