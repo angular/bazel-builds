8859ef322228349dcf454292af08eca4e17c51d7
Alex Eagle <alexeagle@google.com>
2018-02-26 21:59:36 +0000

    c593d69 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- src/ng_package/packager.ts
+++ src/ng_package/packager.ts
@@ -32,7 +32,7 @@ function main(args: string[]): number {
   function replaceVersionPlaceholders(filePath: string) {
     if (stampData) {
       const version = shx.grep('BUILD_SCM_VERSION', stampData).split(' ')[1].trim();
-      return shx.sed(/6.0.0-beta.5-5a32d7e/, version, filePath);
+      return shx.sed(/6.0.0-beta.5-c593d69/, version, filePath);
     }
     return shx.cat(filePath);
   }
