35c641edaa473c2c02b072ad02cd6b9663973981
Alex Eagle <alexeagle@google.com>
2018-02-27 19:45:07 +0000

    79656e7 docs(aio): add Observable and Rx docs (#21423)

--- src/ng_package/packager.ts
+++ src/ng_package/packager.ts
@@ -32,7 +32,7 @@ function main(args: string[]): number {
   function replaceVersionPlaceholders(filePath: string) {
     if (stampData) {
       const version = shx.grep('BUILD_SCM_VERSION', stampData).split(' ')[1].trim();
-      return shx.sed(/6.0.0-beta.5-4bd3e5f/, version, filePath);
+      return shx.sed(/6.0.0-beta.5-79656e7/, version, filePath);
     }
     return shx.cat(filePath);
   }
