0fbd882d7cdf85bbf81db96170b91a19e52f569a
Alex Eagle <alexeagle@google.com>
2018-02-28 23:19:18 +0000

    288851c release: add ng update package group metadata to angular (#22482)

--- src/ng_package/packager.ts
+++ src/ng_package/packager.ts
@@ -32,7 +32,7 @@ function main(args: string[]): number {
   function replaceVersionPlaceholders(filePath: string, content: string) {
     if (stampData) {
       const version = shx.grep('BUILD_SCM_VERSION', stampData).split(' ')[1].trim();
-      return content.replace(/6.0.0-beta.5-9eaf1bb/g, version);
+      return content.replace(/6.0.0-beta.5-288851c/g, version);
     }
     return content;
   }
