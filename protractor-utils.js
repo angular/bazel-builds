234b109d7305c8d672f1114ce0a11b22366a2cc1
Igor Minar <igor@angular.io>
2018-10-21 04:25:35 +0000

    100c7ef build: fix ivy package-dist generation (#26629)

--- protractor-utils.js
+++ protractor-utils.js
@@ -4,13 +4,6 @@
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
- *
- * @fileoverview Extracts i18n messages.
  */
 
-// Entry point
-if (require.main === module) {
-  const args = process.argv.slice(2);
-  console.error('>>> now yet implemented!');
-  process.exitCode = 1;
-}
+module.exports = require('./src/protractor/utils');
