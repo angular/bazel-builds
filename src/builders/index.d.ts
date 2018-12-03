ec6161bc46858c4433136ba2e0b482d6ee7a39e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:10:52 +0000

    8eb102a fix(ivy): avoid counting style/class bindings in component/directive `hostBindings` (#27388)

--- src/builders/index.d.ts
+++ src/builders/index.d.ts
@@ -5,15 +5,14 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  *
- * @fileoverview Bazel builder
+ * @fileoverview Bazel bundle builder
  */
 /// <amd-module name="angular/packages/bazel/src/builders/index" />
-import { BuildEvent, Builder, BuilderConfiguration, BuilderContext } from '@angular-devkit/architect';
+import { BuildEvent, Builder as BuilderInterface, BuilderConfiguration, BuilderContext } from '@angular-devkit/architect';
 import { Observable } from 'rxjs';
 import { Schema } from './schema';
-declare class BazelBuilder implements Builder<Schema> {
+export declare class Builder implements BuilderInterface<Schema> {
     private context;
     constructor(context: BuilderContext);
     run(builderConfig: BuilderConfiguration<Partial<Schema>>): Observable<BuildEvent>;
 }
-export default BazelBuilder;
