7aca7b00db0af229931a9678491903b305731363
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:08:51 +0000

    06d4a0c feat(bazel): ng-new schematics with Bazel (#27277)

--- src/builders/index.d.ts
+++ src/builders/index.d.ts
@@ -5,14 +5,15 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  *
- * @fileoverview Bazel bundle builder
+ * @fileoverview Bazel builder
  */
 /// <amd-module name="angular/packages/bazel/src/builders/index" />
-import { BuildEvent, Builder as BuilderInterface, BuilderConfiguration, BuilderContext } from '@angular-devkit/architect';
+import { BuildEvent, Builder, BuilderConfiguration, BuilderContext } from '@angular-devkit/architect';
 import { Observable } from 'rxjs';
 import { Schema } from './schema';
-export declare class Builder implements BuilderInterface<Schema> {
+declare class BazelBuilder implements Builder<Schema> {
     private context;
     constructor(context: BuilderContext);
     run(builderConfig: BuilderConfiguration<Partial<Schema>>): Observable<BuildEvent>;
 }
+export default BazelBuilder;
