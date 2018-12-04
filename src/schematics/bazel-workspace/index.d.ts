6392d668884a6dd3c9123c82bf5203a5e89da6ea
Miško Hevery <misko@hevery.com>
2018-12-04 22:36:45 +0000

    9133935 fixup! fix(core): More precise return type for `InjectableDecorator`

---
+++ src/schematics/bazel-workspace/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ *
+ * @fileoverview Schematics for bazel-workspace
+ */
+/// <amd-module name="angular/packages/bazel/src/schematics/bazel-workspace/index" />
+import { Rule } from '@angular-devkit/schematics';
+import { Schema as BazelWorkspaceOptions } from './schema';
+export default function (options: BazelWorkspaceOptions): Rule;
