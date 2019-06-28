cec48f48a0f0c4efd1b7da0d1aee286182e46c48
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:36:08 +0000

    a29dc961a feat(bazel): allow passing a custom bazel compiler host to ngc compile (#31341)

--- src/ngc-wrapped/index.d.ts
+++ src/ngc-wrapped/index.d.ts
@@ -7,14 +7,14 @@
  */
 /// <amd-module name="@angular/bazel" />
 import * as ng from '@angular/compiler-cli';
-import { BazelOptions } from '@bazel/typescript';
+import { BazelOptions, CompilerHost } from '@bazel/typescript';
 import * as ts from 'typescript';
 export declare function main(args: any): 1 | 0;
 export declare function runOneBuild(args: string[], inputs?: {
     [path: string]: string;
 }): boolean;
 export declare function relativeToRootDirs(filePath: string, rootDirs: string[]): string;
-export declare function compile({ allDepsCompiledWithBazel, compilerOpts, tsHost, bazelOpts, files, inputs, expectedOuts, gatherDiagnostics }: {
+export declare function compile({ allDepsCompiledWithBazel, compilerOpts, tsHost, bazelOpts, files, inputs, expectedOuts, gatherDiagnostics, bazelHost }: {
     allDepsCompiledWithBazel?: boolean;
     compilerOpts: ng.CompilerOptions;
     tsHost: ts.CompilerHost;
@@ -25,6 +25,7 @@ export declare function compile({ allDepsCompiledWithBazel, compilerOpts, tsHost
     files: string[];
     expectedOuts: string[];
     gatherDiagnostics?: (program: ng.Program) => ng.Diagnostics;
+    bazelHost?: CompilerHost;
 }): {
     diagnostics: ng.Diagnostics;
     program: ng.Program;
