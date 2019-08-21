73a3e843e0530b970428808eb7b65799f41edc79
Andrew Kushnir <akushnir@google.com>
2019-08-21 16:17:23 +0000

    64770571b perf: don't create holey arrays (#32155)

--- src/ng_package/packager.d.ts
+++ src/ng_package/packager.d.ts
@@ -6,4 +6,5 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="npm_angular_bazel/src/ng_package/packager" />
-export {};
+export declare function newArray<T = any>(size: number): T[];
+export declare function newArray<T>(size: number, value: T): T[];
