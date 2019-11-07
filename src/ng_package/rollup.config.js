392965d00000ea788eb140e35e493eb9ed982189
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:11:58 +0000

    8d72a37f3e test(ivy): introduce a benchmark for duplicate style/class bindings (#33600)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+10.sha-becc820.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+11.sha-8d72a37.with-local-changes/, version);
     }
   }
 }
