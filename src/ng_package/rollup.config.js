39ab129f12c68604c7d9271a45a7028c21215e7c
Kara Erickson <karakara@google.com>
2019-11-08 01:09:44 +0000

    f63e5d9319 fix(ivy): properly determine the first native node of a view (#33627)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+21.sha-a33162b.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+23.sha-f63e5d9.with-local-changes/, version);
     }
   }
 }
