68bb1acbcc7357759eb1747d888bb1d177744da4
Kara Erickson <karakara@google.com>
2019-11-08 23:57:57 +0000

    1d429b2165 docs: add AOT mode default to version 9 upgrade doc (#33703)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+31.sha-b97577b.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+33.sha-1d429b2.with-local-changes/, version);
     }
   }
 }
