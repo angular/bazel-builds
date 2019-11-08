4a7068429baba6c5dbd4d3aaa10279f61e82490e
Kara Erickson <karakara@google.com>
2019-11-08 23:01:37 +0000

    b789c891bb docs: update jiali's info (#33698)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+30.sha-290ae43.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+33.sha-b789c89.with-local-changes/, version);
     }
   }
 }
