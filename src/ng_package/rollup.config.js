c0b7f80bae54601de14b1126524c1bb7edc370cb
Kara Erickson <karakara@google.com>
2019-11-08 19:13:04 +0000

    c4fbd0bef7 docs: update ivy compatibility guide with latest changes (#33675)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+23.sha-f63e5d9.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+24.sha-c4fbd0b.with-local-changes/, version);
     }
   }
 }
