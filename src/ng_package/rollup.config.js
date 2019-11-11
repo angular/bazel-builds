305c8efc81db0a4091ed9cc28ca849398f42237a
Kara Erickson <karakara@google.com>
2019-11-11 22:25:11 +0000

    e511bfcab5 fix(ivy): ensure that the  correct `document` is available (#33712)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+50.sha-b3c3000.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+54.sha-e511bfc.with-local-changes/, version);
     }
   }
 }
