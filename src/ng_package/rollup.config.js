248432d72d1aa0061e9792c5f2be3c1029cd176a
Kara Erickson <karakara@google.com>
2019-11-11 21:23:07 +0000

    b3c3000004 fix(ngcc): ensure that adjacent statements go after helper calls (#33689)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+50.sha-b3c3000.with-local-changes/, version);
     }
   }
 }
