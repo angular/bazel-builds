f4d864a327ab44fe49e0911b3b7ce329f9c49585
Kara Erickson <karakara@google.com>
2019-11-11 18:14:36 +0000

    a8f9ff52f3 build: add @angular/localize to ng-update packageGroup (#33721)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+44.sha-a1e9956.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+45.sha-a8f9ff5.with-local-changes/, version);
     }
   }
 }
