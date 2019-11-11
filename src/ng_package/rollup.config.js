f11755104123a268d9a9c70187e458e70e5483e8
Kara Erickson <karakara@google.com>
2019-11-11 21:06:19 +0000

    c6ebcd1eb9 docs: fix numbering (#33736)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+47.sha-eece138.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes/, version);
     }
   }
 }
