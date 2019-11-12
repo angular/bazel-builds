1802fac3b56955ce1299a6c5e471863f9a9ad8bf
Kara Erickson <karakara@google.com>
2019-11-12 20:06:45 +0000

    2f0b8bc541 build(common): generate correct "global" locale files (#33662)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+64.sha-ccee818.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+67.sha-2f0b8bc.with-local-changes/, version);
     }
   }
 }
