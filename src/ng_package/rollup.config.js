8c9c645c72599bbbe8e0d831715e8f01f01b626d
Kara Erickson <karakara@google.com>
2019-11-12 22:29:22 +0000

    84a0105625 test(ivy): view insertion before ng-container with a ViewContainerRef (#33755)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+68.sha-641c671.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+77.sha-84a0105.with-local-changes/, version);
     }
   }
 }
