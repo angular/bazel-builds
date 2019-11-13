d2230c0ee47cd42a9f5fc11fd2ada0711d57803f
Kara Erickson <karakara@google.com>
2019-11-13 19:57:14 +0000

    1d563a7acd build: set up all packages to publish via wombot proxy (#33747)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+86.sha-1e1e242.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+87.sha-1d563a7.with-local-changes/, version);
     }
   }
 }
