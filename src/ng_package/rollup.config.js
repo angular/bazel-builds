db8fac331c384586ec6954102f31b19bd2bc48e5
Kara Erickson <karakara@google.com>
2019-11-13 22:16:43 +0000

    b30bb8dd91 fix(router): make routerLinkActive work with query params which contain arrays (#22666)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+89.sha-f1b3284.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+100.sha-b30bb8d.with-local-changes/, version);
     }
   }
 }
