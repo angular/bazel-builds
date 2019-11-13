dd75101f571d6ec3e386a0afba8942254a70b260
Kara Erickson <karakara@google.com>
2019-11-13 05:54:18 +0000

    cf10b336e7 fix(ivy): ComponentFactory.create should clear host element content (#33487)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+80.sha-2be0a1d.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+79.sha-cf10b33.with-local-changes/, version);
     }
   }
 }
