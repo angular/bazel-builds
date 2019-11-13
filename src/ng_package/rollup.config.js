0c5867e0d9d9ca3567d1052d8a4823a7d3ce4bc2
Kara Erickson <karakara@google.com>
2019-11-13 19:29:53 +0000

    49e517d049 fix(ngcc): support minified ES5 scenarios (#33777)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+80.sha-8ed32e5.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+86.sha-49e517d.with-local-changes/, version);
     }
   }
 }
