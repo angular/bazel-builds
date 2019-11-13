74c9546c78bfd43195cf70a5afa8b03735196265
Kara Erickson <karakara@google.com>
2019-11-13 00:29:58 +0000

    f750f187d5 docs: dix typo in `DEVELOPER.md` (#33743)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+77.sha-4e027fe.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+78.sha-f750f18.with-local-changes/, version);
     }
   }
 }
