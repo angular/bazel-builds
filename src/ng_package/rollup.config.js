6dd54ca9a5cda7e69bbe5d8553d2b77b8c6f63cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 00:20:02 +0000

    84524583e9 fix(ivy): extend assertion in `directiveInject` function to support IcuContainers (#33832)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.2+20.sha-a61fb76.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.2+21.sha-8452458.with-local-changes/, version);
     }
   }
 }
