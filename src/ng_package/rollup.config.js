d3ffd57a172611a9950bb31902be2b0b5734dd1e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 19:10:24 +0000

    9d21065e25 build: fix zone.js version stamping after updating to new rollup_bundle (#33329)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+110.sha-e51ec67.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+125.sha-9d21065.with-local-changes/, version);
     }
   }
 }
