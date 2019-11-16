3f51ac50366a39cd30a926e6799570b6ea5c5417
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-16 00:44:31 +0000

    7eb3e3bce6 build: fix build scripts on macOS (#33854)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+135.sha-a48573e.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+128.sha-7eb3e3b.with-local-changes/, version);
     }
   }
 }
