cfb9b535e26ab3f3f6006d798bf99a8be4edc023
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-18 17:34:49 +0000

    1218ce4f5f docs: add xrefs to update app and deprecations policy (#33767)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.2+45.sha-7567f2b.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.2+61.sha-1218ce4.with-local-changes/, version);
     }
   }
 }
