59b56c1766844a5d4956c5c4b2002e0c078f4714
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-19 20:44:16 +0000

    8555d51bc7 perf(ivy): extract template's instruction first create pass processing (#33856)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+172.sha-f69c6e2.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+171.sha-8555d51.with-local-changes/, version);
     }
   }
 }
