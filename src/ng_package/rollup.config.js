d2152b251800941b4d88195a7094fef946b16c10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-19 17:56:41 +0000

    c68200cf42 build: remove rollup-rule temp settings (#33908)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+151.sha-341d584.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+164.sha-c68200c.with-local-changes/, version);
     }
   }
 }
