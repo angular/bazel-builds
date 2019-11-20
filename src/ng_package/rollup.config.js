5760cb59b74f38fc3e81bd61711364045e8ee16a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:27:39 +0000

    b07b6f1d40 fix(ivy): avoid infinite recursion when evaluation source files (#33772)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+199.sha-fd83d94.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+201.sha-b07b6f1.with-local-changes/, version);
     }
   }
 }
