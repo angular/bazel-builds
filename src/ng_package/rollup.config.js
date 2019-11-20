55df015bc11eb82c42170bb882db56ef2f699a8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 20:15:28 +0000

    595375f7a3 fix(ivy): don't infer template context types when in full mode (#33537)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.2+92.sha-a7e2327.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.2+96.sha-595375f.with-local-changes/, version);
     }
   }
 }
