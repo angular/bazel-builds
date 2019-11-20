bf927d73a0b1dbb516d38394f953882c862b2303
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:29:54 +0000

    fc6ad19089 fix(ivy): wrap functions from "providers" in parentheses in Closure mode (#33609)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.2+98.sha-213e3c3.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.3+14.sha-fc6ad19.with-local-changes/, version);
     }
   }
 }
