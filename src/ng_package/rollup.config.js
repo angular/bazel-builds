7b4088a16e3bdd90f80c7c5bed364d853716d3b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 20:15:31 +0000

    97fbdab3b8 fix(ivy): report watch mode diagnostics correctly (#33862)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+187.sha-eb6975a.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+186.sha-97fbdab.with-local-changes/, version);
     }
   }
 }
