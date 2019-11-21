412187038f437d232e87120e3f0ca470b8ab55e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:16:55 +0000

    bf1bcd1e08 fix(ngcc): render localized strings when in ES5 format (#33857)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+209.sha-346e1c1.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes/, version);
     }
   }
 }
