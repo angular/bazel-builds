01ea874da73847d3f6de14863445615a3aff733a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:02:56 +0000

    715d02aa14 fix(ngcc): report errors from `analyze` and `resolve` processing (#33964)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+206.sha-e53375b.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+207.sha-715d02a.with-local-changes/, version);
     }
   }
 }
