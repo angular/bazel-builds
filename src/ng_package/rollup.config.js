59038190a3d77880a7ac35bd7225ddf6f22281e8
Matias Niemelä <matias@yearofmoo.com>
2019-11-21 22:16:34 +0000

    fd5f101bab docs: add CSRF to XSRF section of http guide (#32933)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+214.sha-9d7b939.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+216.sha-fd5f101.with-local-changes/, version);
     }
   }
 }
