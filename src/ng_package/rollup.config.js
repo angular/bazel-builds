861da341e4e1a328f5597e69be634118b4633206
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:53:53 +0000

    588823b437 refactor(elements): ng-add schematics (#33723)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+237.sha-de043a4.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+238.sha-588823b.with-local-changes/, version);
     }
   }
 }
