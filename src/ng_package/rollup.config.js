d641d4423e6c584b4d5257e8a8d15819e6613439
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 01:05:12 +0000

    7d579b913c build: create bazel define flag, `angular_ivy_enabled`, to build using ivy (#33975)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+217.sha-3d69693.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+218.sha-7d579b9.with-local-changes/, version);
     }
   }
 }
