494a602a5cd7203f76363017fc0d2279d014760f
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:46:56 +0000

    fe91700dc6 build: make ng_module.bzl is_ivy_enabled function public (#33992)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+240.sha-a4c3cee.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+243.sha-fe91700.with-local-changes/, version);
     }
   }
 }
