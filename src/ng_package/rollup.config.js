8790adf7b748e6a4bf2fe6ad51b592fa74868709
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 18:24:38 +0000

    a37bf9a516 docs: add examples for DI change in Ivy (#33643)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+224.sha-bb79018.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+226.sha-a37bf9a.with-local-changes/, version);
     }
   }
 }
