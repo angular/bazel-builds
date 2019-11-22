2bddcdcd60e3dc0e3b1a4a391c3553abeaee2e8e
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 17:55:39 +0000

    2dd572b15c docs(http): fix typo (#33978)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+220.sha-828e5c1.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+221.sha-2dd572b.with-local-changes/, version);
     }
   }
 }
