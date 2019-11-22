e2e2323f12c19b162267d99b8e7901980c26b2cb
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 18:03:28 +0000

    ad9b9a3534 docs: add santosh yadav as GDE resource (#33818)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.3+30.sha-f1bf5b2.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.3+35.sha-ad9b9a3.with-local-changes/, version);
     }
   }
 }
