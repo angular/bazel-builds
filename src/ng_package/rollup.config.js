22024d02c460e5e9d993bf32d1610801b69ba2fa
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:11:39 +0000

    de043a4bcb refactor(bazel): use getattr instead of hasattr in ng_module.bzl (#33765)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+232.sha-485ce64.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+237.sha-de043a4.with-local-changes/, version);
     }
   }
 }
