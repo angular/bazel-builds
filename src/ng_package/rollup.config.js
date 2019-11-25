cdd94ed378ca185c2701f08c1ddc9891a4222a0d
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:54:32 +0000

    1217c6b7cc docs: replace class hero to interface wherever class properties not used (#34022)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+244.sha-5129bcc.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+246.sha-1217c6b.with-local-changes/, version);
     }
   }
 }
