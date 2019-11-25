189a736457d7c03d6bdd82067f227776583e7a1a
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 18:04:49 +0000

    31e2bdffa8 perf(ivy): fix creation time micro-benchmarks (#34031)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+253.sha-d80eeb6.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+254.sha-31e2bdf.with-local-changes/, version);
     }
   }
 }
