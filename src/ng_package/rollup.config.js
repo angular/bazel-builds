9b8b8df59cd22c4a1b7c56ed88b69a08f4febc98
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 02:39:05 +0000

    7cd16b9e2c fix(language-service): function.bind() should not be an error (#34041)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+264.sha-d25de63.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+265.sha-7cd16b9.with-local-changes/, version);
     }
   }
 }
