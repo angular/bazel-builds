860ffc7900ce10257beee1830abc00ed330086e4
Miško Hevery <misko@hevery.com>
2019-11-27 00:41:10 +0000

    c291c8e2cb ci: do not run ng_benchmarks on CI (#34057)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+288.sha-005c4a1.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+289.sha-c291c8e.with-local-changes/, version);
     }
   }
 }
