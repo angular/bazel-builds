59dc69983621d6c3ef525c5c8a08b3b5a301fa19
Miško Hevery <misko@hevery.com>
2019-11-27 00:35:45 +0000

    49804fe017 fix(language-service): determine index types accessed using dot notation (#33884)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+286.sha-3f68377.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+282.sha-49804fe.with-local-changes/, version);
     }
   }
 }
