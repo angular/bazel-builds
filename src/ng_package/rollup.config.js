08fa305863ac285725d9fd82e3357d292257ed0d
Miško Hevery <misko@hevery.com>
2019-11-27 00:40:48 +0000

    005c4a1cc5 build: set default ts_devserver serving_path to /app_bundle.js to match g3 (#33996)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+282.sha-49804fe.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+288.sha-005c4a1.with-local-changes/, version);
     }
   }
 }
