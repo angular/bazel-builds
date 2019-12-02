2d1d0a8db218f5d8d52b15064380f60602f3d425
Miško Hevery <misko@hevery.com>
2019-12-02 19:59:36 +0000

    e7cc1d606a refactor(core): update missing-injectable migration description to match guide (#34125)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+321.sha-5a7f334.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+320.sha-e7cc1d6.with-local-changes/, version);
     }
   }
 }
