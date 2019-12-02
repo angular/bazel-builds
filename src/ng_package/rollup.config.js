e816cbe0908564d5d531c0361623c167cd2dee57
Miško Hevery <misko@hevery.com>
2019-12-02 20:03:40 +0000

    b8eb2f2f23 build: support yarn 1.19.x (#34143)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+325.sha-180a41a.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+322.sha-b8eb2f2.with-local-changes/, version);
     }
   }
 }
