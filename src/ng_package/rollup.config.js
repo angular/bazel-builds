fecd0817d725f5f19b816922d05fc535eb1c79a4
Miško Hevery <misko@hevery.com>
2019-12-03 18:43:50 +0000

    f16f6a290b fix(ngcc): render legacy i18n message ids by default (#34135)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+336.sha-67eac73.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+344.sha-f16f6a2.with-local-changes/, version);
     }
   }
 }
