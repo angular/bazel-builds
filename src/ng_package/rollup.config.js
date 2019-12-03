a3f704687261416c392e1fe14f2fdea908205955
Miško Hevery <misko@hevery.com>
2019-12-03 18:39:24 +0000

    67eac733d2 refactor(ivy): do not generate providedIn: null (#34116)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+335.sha-56f4e56.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+336.sha-67eac73.with-local-changes/, version);
     }
   }
 }
