dcb41289d077033c18ca3677e5680fe0fde3f74d
Miško Hevery <misko@hevery.com>
2019-12-03 18:28:23 +0000

    d4b83681f1 build: disable buildifier print warning for compile=aot deprecation (#34199)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+331.sha-0cd8431.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+332.sha-d4b8368.with-local-changes/, version);
     }
   }
 }
