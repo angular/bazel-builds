1749cc200f98698a46648769a45ed958fe6c2667
Misko Hevery <misko@hevery.com>
2019-12-04 07:18:52 +0000

    634887c0e7 test(ivy): update `ngI18nClosureMode` flag usage in tests (#34224)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+365.sha-9b6a1b8.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+370.sha-634887c.with-local-changes/, version);
     }
   }
 }
