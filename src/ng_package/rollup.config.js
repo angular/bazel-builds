6565b54b37f4b863b70afd24ccaa3cd8830c8f48
Miško Hevery <misko@hevery.com>
2019-12-04 23:22:03 +0000

    f9e959e098 fix(common): update closure locales to include directionality data (#34240)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+370.sha-634887c.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+371.sha-f9e959e.with-local-changes/, version);
     }
   }
 }
