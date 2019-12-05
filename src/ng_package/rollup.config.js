ba1741af12d48da09b6d621f53a1790840a29c80
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:25:57 +0000

    de62d8ebdb docs: fix migrations formatting (#33267)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+385.sha-0341f32.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+383.sha-de62d8e.with-local-changes/, version);
     }
   }
 }
