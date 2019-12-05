114ed0a03d51de240287f3da250cf08910a20c0e
Andrew Kushnir <akushnir@google.com>
2019-12-05 19:40:31 +0000

    02134172dd docs: add David Shevitz to about page (#34222)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+380.sha-ecb29de.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+382.sha-0213417.with-local-changes/, version);
     }
   }
 }
