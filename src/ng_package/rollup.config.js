f2ea6c3efeb1a0f2e84eef6c073d9a301d964abb
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:28:38 +0000

    e71d151571 docs: edit copy of getting started step 2 (#32776)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+371.sha-f9e959e.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+372.sha-e71d151.with-local-changes/, version);
     }
   }
 }
