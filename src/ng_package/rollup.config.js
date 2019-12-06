05ea4d9d776651b85ba0d2c1d59c12178b1cd71a
Andrew Kushnir <akushnir@google.com>
2019-12-06 19:09:09 +0000

    7a6e326ec6 docs: update CHANGELOG to add v9.0.0-rc.5 release notes (#34282)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+394.sha-613dc11.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+395.sha-7a6e326.with-local-changes/, version);
     }
   }
 }
