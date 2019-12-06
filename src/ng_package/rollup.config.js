d320c2af0e16e00914b190fa46b10298cdc96066
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:25:49 +0000

    a12b5f930a fix(compiler): ensure localized strings are ES5 compatible for JIT mode (#34265)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+400.sha-b342a69.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+401.sha-a12b5f9.with-local-changes/, version);
     }
   }
 }
