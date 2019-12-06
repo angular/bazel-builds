6bc315c662ce53c51c5cc6f50088ee3d352013ea
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:35:51 +0000

    10a33efc3e fix(ivy): align TestBed.overrideProvider with what happens with providers in TestBed providers array (#33769)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.5+28.sha-6e944ac.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.5+32.sha-10a33ef.with-local-changes/, version);
     }
   }
 }
