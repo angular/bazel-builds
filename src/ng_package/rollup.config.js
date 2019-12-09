1cfea72590fcd68501dfb5db3f76d82dee97c80d
Andrew Kushnir <akushnir@google.com>
2019-12-09 20:34:38 +0000

    60051ebb59 fix(compiler): switch to modern diagnostic formatting (#34234)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.5+35.sha-1347064.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.5+37.sha-60051eb.with-local-changes/, version);
     }
   }
 }
