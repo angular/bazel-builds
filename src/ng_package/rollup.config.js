cdeeb7e2cd1742f2c0e65351ec89c2e7987b0706
Andrew Kushnir <akushnir@google.com>
2019-12-09 19:03:04 +0000

    2f3d41f081 docs(core): add new example for HostListener (#34228)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+403.sha-f98aeca.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+405.sha-2f3d41f.with-local-changes/, version);
     }
   }
 }
