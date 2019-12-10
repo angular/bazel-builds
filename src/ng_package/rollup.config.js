eecae6012b1e1fe61130fc7737bb490245e071c3
Andrew Kushnir <akushnir@google.com>
2019-12-10 18:57:21 +0000

    c8447d21bd refactor(ivy): pull property read into a constant (#34290)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+417.sha-bb52fb7.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+419.sha-c8447d2.with-local-changes/, version);
     }
   }
 }
