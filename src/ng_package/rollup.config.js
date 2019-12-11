6dfa06bb7e5fb9f357ef7f181bfc2cc35d114440
Andrew Kushnir <akushnir@google.com>
2019-12-11 00:49:53 +0000

    c9df9afddd docs: add change detection to glossary (#34316)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+420.sha-c8b5b81.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+421.sha-c9df9af.with-local-changes/, version);
     }
   }
 }
