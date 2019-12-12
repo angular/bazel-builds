a41c0741920bedcf84aff023edfa80deff91343b
Kara Erickson <karakara@google.com>
2019-12-12 00:18:26 +0000

    ad987021ce Revert "build: update integration/bazel to rules_nodejs 0.42.1" (#34360)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+435.sha-6ce9403.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+442.sha-ad98702.with-local-changes/, version);
     }
   }
 }
