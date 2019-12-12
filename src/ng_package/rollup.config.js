a6b261e2bb7d101370cb834dac36782c59ce5793
Kara Erickson <karakara@google.com>
2019-12-12 19:24:51 +0000

    c13a4b8c03 style(common): remove unnecessary jsdoc type (#34369)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+443.sha-1583293.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+445.sha-c13a4b8.with-local-changes/, version);
     }
   }
 }
