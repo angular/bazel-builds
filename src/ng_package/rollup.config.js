750800252a1dc95e2fb5763de5bc62c390835e4c
Kara Erickson <karakara@google.com>
2019-12-12 21:42:46 +0000

    6ba5fdc208 fix(ivy): generate a better error for template var writes (#34339)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+450.sha-6e7ca05.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes/, version);
     }
   }
 }
