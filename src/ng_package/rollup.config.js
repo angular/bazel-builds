4ab11d042405cdd27692e2a1da1c9b084fbc4191
Kara Erickson <karakara@google.com>
2019-12-12 17:27:51 +0000

    c26738df12 fix(ngcc): render UMD imports even if no prior imports (#34353)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.6+13.sha-de71f91.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.6+15.sha-c26738d.with-local-changes/, version);
     }
   }
 }
