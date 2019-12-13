222dae7188f90f663dafa846574569c9a709faab
Kara Erickson <karakara@google.com>
2019-12-13 20:42:33 +0000

    ea57587b7c docs(ivy): document breaking changes for DebugElement classes and attributes (#34328)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+461.sha-b405942/, version);
+      banner = banner.replace(/9.0.0-rc.1+463.sha-ea57587/, version);
     }
   }
 }
