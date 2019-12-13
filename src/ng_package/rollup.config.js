75daa44274b47268451284e967bb36c51fe394b5
Kara Erickson <karakara@google.com>
2019-12-13 00:14:42 +0000

    a9e3cbd534 docs: add docs about inputs and strictNullChecks to template typecheck (#34194)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+458.sha-af95ddd.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+459.sha-a9e3cbd.with-local-changes/, version);
     }
   }
 }
