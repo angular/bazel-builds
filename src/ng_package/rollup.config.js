97e08de9628a5c380dacff98656a665220b103b6
Kara Erickson <karakara@google.com>
2019-12-16 23:54:36 +0000

    5cecd97493 feat(forms): expand NgModel disabled type to work with strict template type checking (#34438)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+489.sha-5df8a3b/, version);
+      banner = banner.replace(/9.0.0-rc.1+490.sha-5cecd97/, version);
     }
   }
 }
