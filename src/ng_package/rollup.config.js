63d046ef74096c0c6e3141ea77e95a0fd6a2a3ff
Kara Erickson <karakara@google.com>
2019-12-16 16:02:21 +0000

    3bbd12d560 build: update to yarn@1.21.1 (#34384)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+472.sha-f79110c.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+476.sha-3bbd12d/, version);
     }
   }
 }
