b31072028bd67295f1823f0ac45f8182b1c62311
Kara Erickson <karakara@google.com>
2019-12-16 16:07:24 +0000

    04ab03664d ci: ensure saucelabs test output is human readable (#34277)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+476.sha-3bbd12d/, version);
+      banner = banner.replace(/9.0.0-rc.1+478.sha-04ab036/, version);
     }
   }
 }
