99384e37cbff45aa842c5567dd802deb5d471c88
Miško Hevery <misko@hevery.com>
2019-12-16 20:42:10 +0000

    a71965671c refactor(ivy): add `insertTStylingBinding` to keep track of bindings.

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+421.sha-7cd465a.with-local-changes/, version);
+      banner = banner.replace(/9.0.0-rc.1+422.sha-a719656.with-local-changes/, version);
     }
   }
 }
