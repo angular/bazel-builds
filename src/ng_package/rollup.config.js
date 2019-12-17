6adb553b9a8fd14497201895dd532cb6a8f64726
Kara Erickson <karakara@google.com>
2019-12-17 23:02:18 +0000

    3e201181bb fix(ivy): correctly associate output bound events with directives (#31938)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+497.sha-7938ff3/, version);
+      banner = banner.replace(/9.0.0-rc.1+498.sha-3e20118/, version);
     }
   }
 }
