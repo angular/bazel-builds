81767a4bb9a0e2427c1c5c88bd4d2c61c7862a84
Kara Erickson <karakara@google.com>
2019-12-18 23:29:32 +0000

    f563c7c656 feat(ivy): error in ivy when inheriting a ctor from an undecorated base (#34460)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.6+81.sha-76219f6/, version);
+      banner = banner.replace(/9.0.0-rc.6+89.sha-f563c7c/, version);
     }
   }
 }
