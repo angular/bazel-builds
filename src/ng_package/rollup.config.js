cf682131dad4967b7c0b2bace8d7fa8e518c39cf
Kara Erickson <karakara@google.com>
2019-12-18 19:57:41 +0000

    76219f6e4b build: use vendored yarn version for bazel node toolchain (#34472)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.6+73.sha-5ec4fb2/, version);
+      banner = banner.replace(/9.0.0-rc.6+81.sha-76219f6/, version);
     }
   }
 }
