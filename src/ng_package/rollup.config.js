d9d8631641ebf70f16baf19dafe4b3a23e7e527a
Kara Erickson <karakara@google.com>
2019-12-18 17:37:37 +0000

    9d1175e2b2 fix(ivy): improve ExpressionChangedAfterChecked error (#34381)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+500.sha-a04f7c0/, version);
+      banner = banner.replace(/9.0.0-rc.1+499.sha-9d1175e/, version);
     }
   }
 }
