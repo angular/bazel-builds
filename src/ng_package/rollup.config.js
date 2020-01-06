bef42a757a379a7c605362147b8dd0a3d0a66a94
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:54:13 +0000

    ba2fd31e62 fix(language-service): The pipe method should not include parentheses (#34485)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+529.sha-eab7f9f/, version);
+      banner = banner.replace(/9.0.0-rc.1+530.sha-ba2fd31/, version);
     }
   }
 }
