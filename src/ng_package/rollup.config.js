e2e2f040f32cf037b87325a0ddeb43d9b37de0dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:31:16 +0000

    19944c2424 feat(compiler): record end of expression Token (#33549)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+522.sha-8410278/, version);
+      banner = banner.replace(/9.0.0-rc.1+523.sha-19944c2/, version);
     }
   }
 }
