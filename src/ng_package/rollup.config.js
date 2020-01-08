e3f89a056a642f9e4ae6bf725b082748f57d19c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 00:13:38 +0000

    4f42de9704 fix(ngcc): capture entry-point dependencies from typings as well as source (#34494)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+554.sha-18d89c9/, version);
+      banner = banner.replace(/9.0.0-rc.1+541.sha-4f42de9/, version);
     }
   }
 }
