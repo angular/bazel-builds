269b661a84fcd2a5ce9b96d35b8f5584af6bb935
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 23:05:04 +0000

    02e4921ac9 docs: release notes for the v9.0.0-rc.8 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+556.sha-39bf104/, version);
+      banner = banner.replace(/9.0.0-rc.1+557.sha-02e4921/, version);
     }
   }
 }
