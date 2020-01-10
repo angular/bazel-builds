8641f3b66eaa0cb3bc0f77d993f2233ab89eceb6
atscott <atscott@google.com>
2020-01-10 00:29:38 +0000

    f312848c2e refactor(core): remove duplicate InjectableType declaration (#34679)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.8+29.sha-f7a2ed7/, version);
+      banner = banner.replace(/9.0.0-rc.8+30.sha-f312848/, version);
     }
   }
 }
