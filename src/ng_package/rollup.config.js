df9ac5b26ea5a7b4e41787f096dc09b0f79d1747
atscott <atscott@google.com>
2020-01-11 00:14:00 +0000

    6d534f10e6 fix(ivy): don't run decorator handlers against declaration files (#34557)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+631.sha-6d28a20/, version);
+      banner = banner.replace(/9.0.0-rc.1+632.sha-6d534f1/, version);
     }
   }
 }
