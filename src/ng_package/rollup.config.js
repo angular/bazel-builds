1d13eff0122aa677ac5e7f2e2aa141d7a7edf330
Misko Hevery <misko@hevery.com>
2020-01-11 00:00:11 +0000

    142363a95f refactor(ivy): delete `ɵɵallocHostVars` instruction (#34708)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/0.0.0/, version);
+      banner = banner.replace(/9.0.0-rc.1+636.sha-142363a/, version);
     }
   }
 }
