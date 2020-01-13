e3341ef5f2ff50aa30374b986fa979a12d52e42b
atscott <atscott@google.com>
2020-01-13 18:29:49 +0000

    7d401853b5 fix(ivy): Prevent errors when querying DebugElement roots that were outside angular context (#34687)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+649.sha-58f1002/, version);
+      banner = banner.replace(/9.0.0-rc.1+650.sha-7d40185/, version);
     }
   }
 }
