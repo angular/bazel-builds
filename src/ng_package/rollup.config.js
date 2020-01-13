e2a667031f95ce269867ea45501b298929b077d0
atscott <atscott@google.com>
2020-01-13 23:34:02 +0000

    7305b02023 refactor(language-service): adds Symbol#typeArguments and does cleanup (#34571)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.8+89.sha-f1cdb8f/, version);
+      banner = banner.replace(/9.0.0-rc.8+91.sha-7305b02/, version);
     }
   }
 }
