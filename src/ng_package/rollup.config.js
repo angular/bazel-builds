666351cfbfb769fa23b9cde34ee5e79590f1487c
atscott <atscott@google.com>
2020-01-13 15:44:25 +0000

    a0eb57fb81 docs: replace from class hero to interface with accordance with # 344022 (#34294) Fixes #21186

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.8+76.sha-08c2581/, version);
+      banner = banner.replace(/9.0.0-rc.8+85.sha-a0eb57f/, version);
     }
   }
 }
