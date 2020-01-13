31f78cae09b955262a6d386c2bfb3618fb8dea73
atscott <atscott@google.com>
2020-01-13 18:03:03 +0000

    8de3c20e50 fix(ivy): do not reset view dirty state in check no changes mode (#34495)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+641.sha-ebcd59a/, version);
+      banner = banner.replace(/9.0.0-rc.1+642.sha-8de3c20/, version);
     }
   }
 }
