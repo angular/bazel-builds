e1afe28cf6e17c4533caa37097826b21002dc9e7
atscott <atscott@google.com>
2020-01-13 18:07:16 +0000

    ff02ddfd9b fix(ngcc): correctly detect dependencies in CommonJS (#34528)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.8+85.sha-a0eb57f/, version);
+      banner = banner.replace(/9.0.0-rc.8+88.sha-ff02ddf/, version);
     }
   }
 }
