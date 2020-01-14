3c6a9fb3976d947b23c641c9e2cd01d624804fc0
atscott <atscott@google.com>
2020-01-14 23:49:13 +0000

    cb142b6df9 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- src/ngc-wrapped/index.d.ts
+++ src/ngc-wrapped/index.d.ts
@@ -7,14 +7,14 @@
  */
 /// <amd-module name="@angular/bazel" />
 import * as ng from '@angular/compiler-cli';
-import { BazelOptions, CompilerHost } from '@bazel/typescript';
+import { BazelOptions, CompilerHost as BazelHost, FileLoader } from '@bazel/typescript';
 import * as ts from 'typescript';
 export declare function main(args: any): 1 | 0;
 export declare function runOneBuild(args: string[], inputs?: {
     [path: string]: string;
 }): boolean;
 export declare function relativeToRootDirs(filePath: string, rootDirs: string[]): string;
-export declare function compile({ allDepsCompiledWithBazel, compilerOpts, tsHost, bazelOpts, files, inputs, expectedOuts, gatherDiagnostics, bazelHost }: {
+export declare function compile({ allDepsCompiledWithBazel, compilerOpts, tsHost, bazelOpts, files, inputs, expectedOuts, gatherDiagnostics, bazelHost, }: {
     allDepsCompiledWithBazel?: boolean;
     compilerOpts: ng.CompilerOptions;
     tsHost: ts.CompilerHost;
@@ -25,8 +25,14 @@ export declare function compile({ allDepsCompiledWithBazel, compilerOpts, tsHost
     files: string[];
     expectedOuts: string[];
     gatherDiagnostics?: (program: ng.Program) => ng.Diagnostics;
-    bazelHost?: CompilerHost;
+    bazelHost?: BazelHost;
 }): {
     diagnostics: ng.Diagnostics;
     program: ng.Program;
 };
+/** A module resolver for handling generated files in Bazel. */
+export declare function generatedFileModuleResolver(moduleName: string, containingFile: string, compilerOptions: ts.CompilerOptions, host: ts.ModuleResolutionHost): ts.ResolvedModuleWithFailedLookupLocations;
+/** Creates a {@link FileLoader} to cache Bazel inputs.*/
+export declare function createFileLoader(inputs: {
+    [key: string]: string;
+} | undefined, bazelOpts: BazelOptions): FileLoader;
