2f88f27a72850e9163ccf153747f7be01d4248d0
atscott <atscott@google.com>
2020-01-14 22:00:12 +0000

    e100bbc696 docs: replace non-breaking with regular spaces in `resources.json` (#34772)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.8+112.sha-b1d213b/, version);
+      banner = banner.replace(/9.0.0-rc.8+115.sha-e100bbc/, version);
     }
   }
 }
