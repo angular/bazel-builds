8e7e2a558ba10dbe38abc7ecb4ba9bf0bba5c561
atscott <atscott@google.com>
2020-01-14 18:51:52 +0000

    ec147bd839 docs: remove old contributors (#34486)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+659.sha-280aae6/, version);
+      banner = banner.replace(/9.0.0-rc.1+668.sha-ec147bd/, version);
     }
   }
 }
