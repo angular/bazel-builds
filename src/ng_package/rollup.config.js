4a8dae321380e24382099025bf0bc8bc53701119
atscott <atscott@google.com>
2020-01-14 18:06:15 +0000

    277681096d fix(ivy): properly bootstrap components with attribute selectors (#34450)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+657.sha-1a45387/, version);
+      banner = banner.replace(/9.0.0-rc.1+658.sha-2776810/, version);
     }
   }
 }
