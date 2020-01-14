39fd6a1df9910c86001e6b69e9bda87f27c79b79
atscott <atscott@google.com>
2020-01-14 23:14:19 +0000

    3113bb7ad2 fix(forms): accept number length in length validators (#32057)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.8+115.sha-e100bbc/, version);
+      banner = banner.replace(/9.0.0-rc.8+116.sha-3113bb7/, version);
     }
   }
 }
