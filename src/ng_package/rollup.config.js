3eb7b3d07837bc848136a5ee9caab045b7b29908
GitHub <noreply@github.com>
2020-01-15 12:13:24 +0000

    50e52c8e1b fixup! docs: fix public api tool name

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+689.sha-ae9c061/, version);
+      banner = banner.replace(/9.0.0-rc.1+691.sha-50e52c8/, version);
     }
   }
 }
