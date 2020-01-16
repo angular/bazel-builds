66e7d6317fc67e9e76f99cde089265cf82a4e6e3
GitHub <noreply@github.com>
2020-01-16 11:56:30 +0000

    32e1dc7284 fixup! build: disable link to PUBLIC_API docs when running ts-api-guardian in angular repo

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+695.sha-9d66345/, version);
+      banner = banner.replace(/9.0.0-rc.1+696.sha-32e1dc7/, version);
     }
   }
 }
