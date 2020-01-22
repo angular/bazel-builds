959948dbf862373c48b2db1c46c750ed0a8bff3d
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:50:51 +0000

    a4cbb86360 docs: fix typo on router guide (#34911)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.10+14.sha-34c30ae/, version);
+      banner = banner.replace(/9.0.0-rc.10+15.sha-a4cbb86/, version);
     }
   }
 }
