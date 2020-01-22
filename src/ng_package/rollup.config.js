088e5277dfdda23c04e5355f894031502d6242f0
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:25:48 +0000

    34c30ae0aa build(docs-infra): always specify a landing file for StackBlitz projects (#34553)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.10+9.sha-20dc436/, version);
+      banner = banner.replace(/9.0.0-rc.10+14.sha-34c30ae/, version);
     }
   }
 }
