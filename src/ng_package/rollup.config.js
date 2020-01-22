e51c7b2963c4a54a5436a3cef835f2171c5a7e0f
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:09:08 +0000

    e1fc44f5d6 build: remove @angular/http from modules (#34892)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+749.sha-13ce375/, version);
+      banner = banner.replace(/9.0.0-rc.1+750.sha-e1fc44f/, version);
     }
   }
 }
