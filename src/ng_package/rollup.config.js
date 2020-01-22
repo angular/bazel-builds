5c678cfacc6b3746341e07ea5ffc29c2aacd82f7
Andrew Kushnir <akushnir@google.com>
2020-01-22 23:18:00 +0000

    92c411f86d docs: update payload size limits for angular.io application (#34913)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+750.sha-e1fc44f/, version);
+      banner = banner.replace(/9.0.0-rc.1+755.sha-92c411f/, version);
     }
   }
 }
