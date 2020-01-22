48c39722fc08ee9b04236e0b98a0e3be1490e96d
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 21:18:55 +0000

    32489c7426 revert: refactor(ivy): remove styleSanitizer instruction in favor of an inline param (#34480) (#34910)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+747.sha-61ad50d/, version);
+      banner = banner.replace(/9.0.0-rc.1+748.sha-32489c7/, version);
     }
   }
 }
