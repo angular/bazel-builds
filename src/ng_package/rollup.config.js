ea46f375d198a124dc1ae806bd67e1265a08199a
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:43:44 +0000

    055533b6a7 feat(common): expose getLocaleCurrencyCode publicly (#34810)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+777.sha-4ba4782/, version);
+      banner = banner.replace(/9.0.0-rc.1+778.sha-055533b/, version);
     }
   }
 }
