78524e270975956a2a678c59cb5b8483d383fe49
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:58:41 +0000

    5aa0507f6a docs(ivy): move incremental package README file to the correct location (#34912)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+780.sha-fa4ea34/, version);
+      banner = banner.replace(/9.0.0-rc.1+782.sha-5aa0507/, version);
     }
   }
 }
