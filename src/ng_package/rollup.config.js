1c330da1ae58db0eaf07f338756a78ce3578bb9b
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:42:44 +0000

    cc7fca4dc1 feat(language-service): specific suggestions for template context diags (#34751)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.10+37.sha-6a4186c/, version);
+      banner = banner.replace(/9.0.0-rc.10+38.sha-cc7fca4/, version);
     }
   }
 }
