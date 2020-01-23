a007cb08107791b18fd781dc8854d6fb433e8f80
Andrew Kushnir <akushnir@google.com>
2020-01-23 22:25:11 +0000

    c80392bcd2 fix(ngcc): do not collect private declarations from external packages (#34811)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.10+42.sha-43e2fd2/, version);
+      banner = banner.replace(/9.0.0-rc.10+45.sha-c80392b/, version);
     }
   }
 }
