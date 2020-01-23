75b87dd8d950d8ec5d5b3e10bbd561f3b479bcbc
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:50:17 +0000

    fa4ea34401 ci: rename `material-unit-tests` job to `components-repo-unit-tests` (#34898)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+779.sha-3049878/, version);
+      banner = banner.replace(/9.0.0-rc.1+780.sha-fa4ea34/, version);
     }
   }
 }
