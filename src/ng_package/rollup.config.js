49272ef5210a0e945b36150887db4cccb9437d01
Andrew Kushnir <akushnir@google.com>
2020-01-23 23:11:22 +0000

    7659f2e24b fix(ngcc): do not attempt compilation when analysis fails (#34889)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+785.sha-5b42084/, version);
+      banner = banner.replace(/9.0.0-rc.1+787.sha-7659f2e/, version);
     }
   }
 }
