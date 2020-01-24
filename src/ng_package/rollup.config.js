058d81ad73c2146756dcc84f04840358ad83539a
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:16:04 +0000

    0223aa6121 fix(language-service): Diagnostic span should point to class name (#34932)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+788.sha-b50ed5c/, version);
+      banner = banner.replace(/9.0.0-rc.1+789.sha-0223aa6/, version);
     }
   }
 }
