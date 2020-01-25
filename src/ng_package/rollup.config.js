9a4df62b362791ddb07d823bceab2dd0abbf9eaf
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:14:04 +0000

    8fca9b6530 refactor(platform-browser): avoid mutable exports. (#34207) (#34960)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.10+67.sha-2e1a16b/, version);
+      banner = banner.replace(/9.0.0-rc.11+4.sha-8fca9b6/, version);
     }
   }
 }
