71998d12e4d8080c174f013610074d0c412e14bd
Andrew Kushnir <akushnir@google.com>
2020-01-27 17:52:38 +0000

    c349cb2d16 build: update CLI to 9.0.0-rc.10 (#34965)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+817.sha-724ce8c/, version);
+      banner = banner.replace(/9.0.0-rc.1+815.sha-c349cb2/, version);
     }
   }
 }
