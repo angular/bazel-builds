58d0620c2eb9fa2ba621df2dc9334a35bf0c4d0f
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:08:31 +0000

    09869af90f fix(language-service): remove repeated symbol definitions for structural directive (#34847)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+819.sha-b3bd6ca/, version);
+      banner = banner.replace(/9.0.0-rc.1+823.sha-09869af/, version);
     }
   }
 }
