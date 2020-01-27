9483f779e14f80b42d6e68288cd91abbd68d836c
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:25:51 +0000

    6e5cfd2cd2 fix(ivy): catch FatalDiagnosticError thrown from preanalysis phase (#34801)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+824.sha-549ab2e/, version);
+      banner = banner.replace(/9.0.0-rc.1+827.sha-6e5cfd2/, version);
     }
   }
 }
