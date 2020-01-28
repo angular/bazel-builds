13c3bfef06af6d32a898f35fb1c1ad281d509fc2
Andrew Kushnir <akushnir@google.com>
2020-01-28 22:10:59 +0000

    fd03709e9c ci(docs-infra): ignore node version in `aio_monitoring_stable` CI job (#35004)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.11+33.sha-ce5afcc/, version);
+      banner = banner.replace(/9.0.0-rc.11+35.sha-fd03709/, version);
     }
   }
 }
