af66dfb6c1059854cea22a9e8b25fac24d433742
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 17:38:55 +0000

    296dc0622f ci: Migrate from CODEOWNERS to PullApprove for PR review management (#34814)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+832.sha-e7c74cb/, version);
+      banner = banner.replace(/9.0.0-rc.1+834.sha-296dc06/, version);
     }
   }
 }
