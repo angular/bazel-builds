c733e92cfc7ad2b1d021974fff2bb9f38a773cbe
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 18:48:23 +0000

    1cee9b32c3 ci: remove draft condition from pullapprove (#35012)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+835.sha-d15d6b7/, version);
+      banner = banner.replace(/9.0.0-rc.1+836.sha-1cee9b3/, version);
     }
   }
 }
