1a1a6c501829495b1a77ec0589af3ae921315a4e
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:17:32 +0000

    669df70da5 fix(ivy): ensure `DebugNode`/`DebugElement` are tree-shakeable in Ivy (#35003)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+840.sha-bd61cd0/, version);
+      banner = banner.replace(/9.0.0-rc.1+844.sha-669df70/, version);
     }
   }
 }
