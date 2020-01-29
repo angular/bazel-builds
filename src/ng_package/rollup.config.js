3a67b276153c35e88cedce9ae2b55e33f194df4f
Andrew Kushnir <akushnir@google.com>
2020-01-29 20:42:45 +0000

    519e9e1ae8 ci: minor PullApprove fixes/improvements (#35015)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+850.sha-8fd2a97/, version);
+      banner = banner.replace(/9.0.0-rc.1+855.sha-519e9e1/, version);
     }
   }
 }
