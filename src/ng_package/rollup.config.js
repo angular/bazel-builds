5c25d24ee348d685fbbef90270384f41697ba123
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:04:09 +0000

    ee8b8f52aa feat(ivy): Change static priority resolution to be same level as directive it belongs to (#34938)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+859.sha-a3de110/, version);
+      banner = banner.replace(/9.0.0-rc.1+864.sha-ee8b8f5/, version);
     }
   }
 }
