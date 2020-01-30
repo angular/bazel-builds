40e8dd4f0d0bfb59afcc1bdecf6879b9e025d033
Andrew Kushnir <akushnir@google.com>
2020-01-30 01:37:55 +0000

    c1cf46c5c3 refactor(ivy): clean of  #34804 from previous merge (#35022)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+862.sha-1765052/, version);
+      banner = banner.replace(/9.0.0-rc.1+865.sha-c1cf46c/, version);
     }
   }
 }
