69f7519a39a192fca486ed7356245a34b159707c
Andrew Kushnir <akushnir@google.com>
2020-01-30 19:51:11 +0000

    cd9ae66b35 refactor(ivy): remove markDirty from global utilities (#34453)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+867.sha-d72cfc9/, version);
+      banner = banner.replace(/9.0.0-rc.1+869.sha-cd9ae66/, version);
     }
   }
 }
