1bedb38f6544fcbb0e9a62ebc3255f9f506ed570
Misko Hevery <misko@hevery.com>
2020-01-31 19:46:17 +0000

    c070037357 refactor(compiler): rename diagnostics/src/code.ts to diagnostics/src/error_code.ts (#35067)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+874.sha-69b5290/, version);
+      banner = banner.replace(/9.0.0-rc.1+876.sha-c070037/, version);
     }
   }
 }
