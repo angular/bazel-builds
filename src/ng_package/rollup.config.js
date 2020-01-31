8c2065146d22d48082ce8ad5b432c2aa81b58ba7
Misko Hevery <misko@hevery.com>
2020-01-31 19:39:04 +0000

    69b52903f0 build: update to @bazel/protractor@1.2.0 (#35068)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+873.sha-68f51e0/, version);
+      banner = banner.replace(/9.0.0-rc.1+874.sha-69b5290/, version);
     }
   }
 }
