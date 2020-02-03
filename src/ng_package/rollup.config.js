088804604c2a90297ae7aa846e9c3c11900eaefb
Miško Hevery <misko@hevery.com>
2020-02-03 16:59:45 +0000

    a3c7ab99b7 feat: performance improvement for eventListeners (#34613)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+896.sha-4d36b2f/, version);
+      banner = banner.replace(/9.0.0-rc.1+897.sha-a3c7ab9/, version);
     }
   }
 }
