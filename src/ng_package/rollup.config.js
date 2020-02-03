596e65675e6e1cf266c9f67fb45a0ddb13c79a41
Miško Hevery <misko@hevery.com>
2020-02-03 22:44:36 +0000

    523c785e8f fix(ngcc): correctly invalidate cache when moving/removing files/directories (#35106)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+905.sha-3c30474/, version);
+      banner = banner.replace(/9.0.0-rc.1+906.sha-523c785/, version);
     }
   }
 }
