469f02533baf872a7d5c1d7956aa97a9521b1425
Miško Hevery <misko@hevery.com>
2020-02-04 18:47:32 +0000

    f7d4cc9cab ci: properly validate commit messages locally (#35035)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+910.sha-e6cb60e/, version);
+      banner = banner.replace(/9.0.0-rc.1+914.sha-f7d4cc9/, version);
     }
   }
 }
