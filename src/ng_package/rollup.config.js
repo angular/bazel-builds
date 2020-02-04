fe60004c7eeb5eea2f579a5a9e8d33ed09230c0b
Miško Hevery <misko@hevery.com>
2020-02-04 21:20:32 +0000

    8e3d2460d5 refactor(ivy): Explicitly pass in `TView` (#35069)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.14+12.sha-1a23c79/, version);
+      banner = banner.replace(/9.0.0-rc.14+14.sha-8e3d246/, version);
     }
   }
 }
