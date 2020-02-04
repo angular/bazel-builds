a1c9e89935d11c3ddd163a4f221b690a0e270728
Miško Hevery <misko@hevery.com>
2020-02-04 19:07:15 +0000

    1a23c79835 docs: changes AoT to AOT for consistency (#35112)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.14+2.sha-a320dc9/, version);
+      banner = banner.replace(/9.0.0-rc.14+12.sha-1a23c79/, version);
     }
   }
 }
