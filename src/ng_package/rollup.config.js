0565acfd488ca375c92b3498015d99326c35c145
Miško Hevery <misko@hevery.com>
2020-02-04 17:19:52 +0000

    0e3eeb4e71 docs: restructure nav for beginner concepts (#34681)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+909.sha-7369dd6/, version);
+      banner = banner.replace(/9.0.0-rc.1+908.sha-0e3eeb4/, version);
     }
   }
 }
