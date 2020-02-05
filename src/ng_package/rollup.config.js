7ef1b2be8735c96c445b96c20d7a4b3c774fefaa
Miško Hevery <misko@hevery.com>
2020-02-05 00:20:41 +0000

    1f6da8c7d5 docs: fix typo in router animation (#35100)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+922.sha-aed5c7c/, version);
+      banner = banner.replace(/9.0.0-rc.1+926.sha-1f6da8c/, version);
     }
   }
 }
