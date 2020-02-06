c69955c768537f09d7213b705851ce7a5a56395d
Misko Hevery <misko@hevery.com>
2020-02-06 23:54:18 +0000

    3c69442dbd feat(compiler-cli): implement NgTscPlugin on top of the NgCompiler API (#34792)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+942.sha-49dd7b6/, version);
+      banner = banner.replace(/9.0.0-rc.1+944.sha-3c69442/, version);
     }
   }
 }
