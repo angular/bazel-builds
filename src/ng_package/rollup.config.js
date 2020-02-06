aba97d94506e508435ae86e600d6ad2d2f515cdc
Misko Hevery <misko@hevery.com>
2020-02-06 23:58:43 +0000

    6fb538ed1d fix(bazel): update ibazel to 0.11.1 (#35158)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+944.sha-3c69442/, version);
+      banner = banner.replace(/9.0.0-rc.1+948.sha-6fb538e/, version);
     }
   }
 }
