8f46568a7974c3a3acbf6961cbc88217c9a4f723
Misko Hevery <misko@hevery.com>
2020-02-06 22:07:10 +0000

    053dabb7ed docs: consolidate and remove rc/next notes from the v9.0.0 release (#35195)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+937.sha-22acdc2/, version);
+      banner = banner.replace(/9.0.0-rc.1+938.sha-053dabb/, version);
     }
   }
 }
