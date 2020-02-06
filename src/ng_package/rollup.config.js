3651393387e34f072f1d066bd7ee63e9bae65c09
Misko Hevery <misko@hevery.com>
2020-02-06 01:46:15 +0000

    c73d7181ae build: update @angular/cli to rc 14 (#35162)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+928.sha-9f99c99/, version);
+      banner = banner.replace(/9.0.0-rc.1+929.sha-c73d718/, version);
     }
   }
 }
