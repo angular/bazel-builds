b61f120d5bc9587f08f95b32fee12a960518f892
Misko Hevery <misko@hevery.com>
2020-02-06 23:17:57 +0000

    60b887090c docs: consilidate release notes for the v8.0.0 release (#35197)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0+9.sha-bcb539f/, version);
+      banner = banner.replace(/9.0.0+10.sha-60b8870/, version);
     }
   }
 }
