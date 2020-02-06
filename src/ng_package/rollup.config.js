351e23dad031814ba8788d16e356818b913c1a0e
Misko Hevery <misko@hevery.com>
2020-02-06 02:40:04 +0000

    4fc2678438 docs(ivy): document breaking change with host attribute priority (#35175)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+929.sha-c73d718/, version);
+      banner = banner.replace(/9.0.0-rc.1+930.sha-4fc2678/, version);
     }
   }
 }
