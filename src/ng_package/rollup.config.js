cd8b467f3e91ea239457627ed8291708c583339e
Misko Hevery <misko@hevery.com>
2020-02-06 17:53:59 +0000

    7526ef77b7 docs(ivy): add docs for styling priority order (#35066)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+930.sha-4fc2678/, version);
+      banner = banner.replace(/9.0.0-rc.1+933.sha-7526ef7/, version);
     }
   }
 }
