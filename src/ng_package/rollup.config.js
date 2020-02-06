bfe5fe7320d1d76535b336af1e621e9ae6bf061f
Misko Hevery <misko@hevery.com>
2020-02-06 01:12:23 +0000

    7de87728e2 ci: temporary disable the Android 7 SauceLab due to timeout (#35174)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.14+20.sha-cf3071f/, version);
+      banner = banner.replace(/9.0.0-rc.14+21.sha-7de8772/, version);
     }
   }
 }
