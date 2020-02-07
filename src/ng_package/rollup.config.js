693b3e36aace681d3bc9d8c6be473d1dd165b26f
Kara Erickson <karakara@google.com>
2020-02-07 22:15:17 +0000

    5b5dc811c1 docs: add asset info to library guide (#34217)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0+31.sha-dea1b96/, version);
+      banner = banner.replace(/9.0.0+32.sha-5b5dc81/, version);
     }
   }
 }
