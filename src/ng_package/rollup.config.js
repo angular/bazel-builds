a4622bc641f75c4bf5c0f6c1f086d124b9241bf0
Misko Hevery <misko@hevery.com>
2020-02-07 19:42:52 +0000

    1c85c24ed3 docs(changelog): formatting fix (#35224) PR Close #35224

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+959.sha-0cbdd54/, version);
+      banner = banner.replace(/9.0.0-rc.1+961.sha-1c85c24/, version);
     }
   }
 }
