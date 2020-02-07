4878dfcff7007c5218781076e417e542a1d52a0b
Kara Erickson <karakara@google.com>
2020-02-07 20:03:20 +0000

    daac33cdc8 feat: add basic jest support (#35080)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+965.sha-f7e1c21/, version);
+      banner = banner.replace(/9.0.0-rc.1+966.sha-daac33c/, version);
     }
   }
 }
