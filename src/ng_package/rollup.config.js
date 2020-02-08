534e690af9be75a434d78539fc73af8a1ffca831
Kara Erickson <karakara@google.com>
2020-02-08 00:37:38 +0000

    18ed9dcd83 refactor(benchpress): delete outdated/unused folder (#35147)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0+32.sha-5b5dc81/, version);
+      banner = banner.replace(/9.0.0+36.sha-18ed9dc/, version);
     }
   }
 }
