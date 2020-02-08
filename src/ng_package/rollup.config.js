2748e2dde46fcfd5adb54c2236fbb15db5c46172
Kara Erickson <karakara@google.com>
2020-02-08 01:42:58 +0000

    ae0253f34a fix(ivy): set namespace for host elements of dynamically created components (#35136)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+972.sha-d5d9971/, version);
+      banner = banner.replace(/9.0.0-rc.1+973.sha-ae0253f/, version);
     }
   }
 }
