991cb0d21d5f8a21c46df452dc0bad7ccf90b812
Kara Erickson <karakara@google.com>
2020-02-10 19:12:10 +0000

    76b77ec3c2 docs(forms): update the ngForm deprecation notice (#35263)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+976.sha-2d5bb26/, version);
+      banner = banner.replace(/9.0.0-rc.1+977.sha-76b77ec/, version);
     }
   }
 }
