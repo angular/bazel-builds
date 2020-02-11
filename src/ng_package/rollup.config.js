884ea98d9cd493be0bf595334149dd53d3efccbe
Kara Erickson <karakara@google.com>
2020-02-11 00:53:59 +0000

    a92d97cda7 fix(compiler): report errors for missing binding names (#34595)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+981.sha-e1160f1/, version);
+      banner = banner.replace(/9.0.0-rc.1+982.sha-a92d97c/, version);
     }
   }
 }
