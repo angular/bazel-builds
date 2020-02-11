5e20021f73516aedebfcd627134f0de8069b216a
Kara Erickson <karakara@google.com>
2020-02-11 20:12:49 +0000

    7c9735a995 fix(elements): schematics fails with schema.json not found error (#35211)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+984.sha-332937e/, version);
+      banner = banner.replace(/9.0.0-rc.1+985.sha-7c9735a/, version);
     }
   }
 }
