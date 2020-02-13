52a050a6200c96157fbed3ca322b3354839e7ef2
Kara Erickson <karakara@google.com>
2020-02-13 18:35:59 +0000

    b2222e7e02 docs: minor typo fix (#35423) PR Close #35423

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+8.sha-dc63ceb/, version);
+      banner = banner.replace(/9.1.0-next.0+16.sha-b2222e7/, version);
     }
   }
 }
