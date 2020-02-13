c4f1f64a5c06e0c6294da251ab81862e3810e2e8
Kara Erickson <karakara@google.com>
2020-02-13 01:10:00 +0000

    e8ea3be843 docs: remove service from region where it was added before it was created (#35354)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.0-rc.1+991.sha-4aeb6c4/, version);
+      banner = banner.replace(/9.1.0-next.0+6.sha-e8ea3be/, version);
     }
   }
 }
