554cc094c1d78e9cf44b10e483eec0c39ea34376
Kara Erickson <karakara@google.com>
2020-02-13 01:35:35 +0000

    d6bc63fa38 fix(ivy): error if directive with synthetic property binding is on same node as directive that injects ViewContainerRef (#35343)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+5.sha-a6aa35e/, version);
+      banner = banner.replace(/9.1.0-next.0+7.sha-d6bc63f/, version);
     }
   }
 }
