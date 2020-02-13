e3386f0dc6a1a94b7630a95a180a64cebc826dab
Kara Erickson <karakara@google.com>
2020-02-13 18:21:11 +0000

    dc63ceb5a3 docs: update release.md and rename 'beta' releases to 'next' (#35276)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+7.sha-d6bc63f/, version);
+      banner = banner.replace(/9.1.0-next.0+8.sha-dc63ceb/, version);
     }
   }
 }
