f5c9b2940e30c6992e09dc394fc578c03f69b6e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:43:16 +0000

    be5e75c804 docs: change `item` to `items` to match the code (#35433)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+23.sha-2ca7984/, version);
+      banner = banner.replace(/9.1.0-next.0+24.sha-be5e75c/, version);
     }
   }
 }
