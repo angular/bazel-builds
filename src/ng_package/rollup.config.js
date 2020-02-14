5201696e47c61c3a578ab9194724e4cd30a5972e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:38:05 +0000

    b9b512f729 fix(ivy): `LFrame` needs to release memory on `leaveView()` (#35156)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+20.sha-27cd01f/, version);
+      banner = banner.replace(/9.1.0-next.0+21.sha-b9b512f/, version);
     }
   }
 }
