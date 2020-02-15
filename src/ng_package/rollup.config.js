e3fdd427ba86366cb211b2d98aba1b49a4be3758
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-15 00:03:22 +0000

    939e2f76f6 docs(forms): remove outdated ngForm selector deprecation notice (#35435)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+25.sha-8c75f21/, version);
+      banner = banner.replace(/9.1.0-next.0+27.sha-939e2f7/, version);
     }
   }
 }
