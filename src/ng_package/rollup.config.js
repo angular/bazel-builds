b5fc21e501a3001aad69f22278313b32fa24dbbf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:09:26 +0000

    d7c4f40171 docs(upgrade): separate AngularJS Material typings to its own block (#35514)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+26.sha-d05629b/, version);
+      banner = banner.replace(/9.1.0-next.0+29.sha-d7c4f40/, version);
     }
   }
 }
