f30cf777f0bc7643ded2706a62a4aeec6831fdfe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 22:16:12 +0000

    183a8629a4 fix(docs-infra): set `th`/`td` to proper width (#35437)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+36.sha-2f18e85/, version);
+      banner = banner.replace(/9.1.0-next.0+37.sha-183a862/, version);
     }
   }
 }
