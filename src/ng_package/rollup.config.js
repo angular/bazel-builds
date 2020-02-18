8f4c4bdf3e04848339d0f805cf4e01a23322408f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:09:34 +0000

    7b56daffe6 fix(docs-infra): add fallback to web fonts so downloading does not block rendering (#35352)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+29.sha-d7c4f40/, version);
+      banner = banner.replace(/9.1.0-next.0+33.sha-7b56daf/, version);
     }
   }
 }
