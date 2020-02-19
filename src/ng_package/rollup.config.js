dd2f982c36411b238b90baaf181999692931251c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 17:31:19 +0000

    d0d36a5ebf ci: increase AIO payload size limit (#35538)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+39.sha-3f4e02b/, version);
+      banner = banner.replace(/9.1.0-next.0+43.sha-d0d36a5/, version);
     }
   }
 }
