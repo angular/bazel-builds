fac253e077399f9dda252acf9f30c9402784de15
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 01:46:22 +0000

    3f4e02b8c7 fix(ivy): queries should match elements inside ng-container with the descendants: false option (#35384)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.0+38.sha-5fbfe69/, version);
+      banner = banner.replace(/9.1.0-next.0+39.sha-3f4e02b/, version);
     }
   }
 }
