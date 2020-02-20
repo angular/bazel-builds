9e402be904e29881181c04dd287669e49c7f4c29
Miško Hevery <misko@hevery.com>
2020-02-20 21:02:59 +0000

    646655d09a fix(compiler): use FatalDiagnosticError to generate better error messages (#35244)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+4.sha-4c693ea/, version);
+      banner = banner.replace(/9.1.0-next.1+11.sha-646655d/, version);
     }
   }
 }
