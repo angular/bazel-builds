8b9bc1ef8c652d603fa11e45c46a2cd7557362d5
Miško Hevery <misko@hevery.com>
2020-02-21 21:05:51 +0000

    bb09cd0e41 fix(ivy): error in AOT when pipe inherits constructor from injectable that uses DI (#35468)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.2+23.sha-a491f7e/, version);
+      banner = banner.replace(/9.0.2+27.sha-bb09cd0/, version);
     }
   }
 }
