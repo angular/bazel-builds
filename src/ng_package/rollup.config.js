f12d45476e8c4246034af928febdd98fcb1ad294
Miško Hevery <misko@hevery.com>
2020-02-24 17:32:19 +0000

    53f059ee8f fix(localize): improve placeholder mismatch error message (#35593)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+45.sha-71b5970/, version);
+      banner = banner.replace(/9.1.0-next.1+44.sha-53f059e/, version);
     }
   }
 }
