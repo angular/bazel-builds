fa4a566d664ac951752fa1b1f7e5af2b622edf9c
Miško Hevery <misko@hevery.com>
2020-02-25 21:30:50 +0000

    96cdf035d8 fix(service-worker): treat 503 as offline (#35595)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+52.sha-f1ffa7a/, version);
+      banner = banner.replace(/9.1.0-next.1+56.sha-96cdf03/, version);
     }
   }
 }
