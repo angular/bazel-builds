b2882fddf3ebe9449729348db8d98623a4ec48b5
Miško Hevery <misko@hevery.com>
2020-02-25 21:37:02 +0000

    1e20b2ca36 build(packaging): add repository.directory field to package.jsons (#27544)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+58.sha-0a1a989/, version);
+      banner = banner.replace(/9.1.0-next.1+61.sha-1e20b2c/, version);
     }
   }
 }
