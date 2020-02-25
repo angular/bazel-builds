0d1fcabf92bb85a64764c20d3e3fa8ba51559075
Miško Hevery <misko@hevery.com>
2020-02-25 21:32:26 +0000

    c414f45ddf docs: update getting started topics to avoid duplicate topic names (#35457)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+56.sha-96cdf03/, version);
+      banner = banner.replace(/9.1.0-next.1+57.sha-c414f45/, version);
     }
   }
 }
