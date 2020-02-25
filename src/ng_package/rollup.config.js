65be7faab4d5ca2118ab889ae55a8885fa014c63
Miško Hevery <misko@hevery.com>
2020-02-25 01:51:57 +0000

    f9d483e76e feat: add a temp solution to support passive event listeners. (#34503)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+53.sha-af76651/, version);
+      banner = banner.replace(/9.1.0-next.1+54.sha-f9d483e/, version);
     }
   }
 }
