449495c5fac9ec2c3b9e9b9743ca80f912b33a2d
Miško Hevery <misko@hevery.com>
2020-02-26 23:52:01 +0000

    79aaaa3254 fix(ivy): injecting incorrect provider when re-providing injectable with useClass (#34574)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.2+55.sha-406ce8c/, version);
+      banner = banner.replace(/9.0.2+73.sha-79aaaa3/, version);
     }
   }
 }
