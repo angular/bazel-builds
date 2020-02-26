505e2f178c1d6ef2e3173f3d89796f4cfc33c960
Miško Hevery <misko@hevery.com>
2020-02-26 21:13:44 +0000

    8fbb966ca7 docs(core): describe your change... (#34513)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+64.sha-6f68eae/, version);
+      banner = banner.replace(/9.1.0-next.1+65.sha-8fbb966/, version);
     }
   }
 }
