359355381c02f227f176801441586d5228dbaa9a
Miško Hevery <misko@hevery.com>
2020-02-26 21:20:11 +0000

    d41988437e docs(common): update ngForTrackBy error URL (#34761)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+68.sha-2a8dd47/, version);
+      banner = banner.replace(/9.1.0-next.1+67.sha-d419884/, version);
     }
   }
 }
