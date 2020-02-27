2bcfd708fb7dd3aaf3d11160ce027df4c063edf3
atscott <atscott@google.com>
2020-02-27 19:09:47 +0000

    0b97d07ad6 test(docs-infra): fix tests with new `topics` property (#35539)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+87.sha-d3c0d92/, version);
+      banner = banner.replace(/9.1.0-next.2+2.sha-0b97d07/, version);
     }
   }
 }
