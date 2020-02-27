6caa9f398816bbe816104cfaededda482dc37f2a
atscott <atscott@google.com>
2020-02-27 19:11:52 +0000

    30f29ee8e7 docs: correct spelling of 'detection' (#35723)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+2.sha-0b97d07/, version);
+      banner = banner.replace(/9.1.0-next.2+4.sha-30f29ee/, version);
     }
   }
 }
