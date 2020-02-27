577b634b9a897fbc63610feb997438940f104bbc
Misko Hevery <misko@hevery.com>
2020-02-27 06:30:18 +0000

    d3c0d92d28 release: cut the v9.1.0-next.2 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.1+85.sha-91934eb/, version);
+      banner = banner.replace(/9.1.0-next.1+87.sha-d3c0d92/, version);
     }
   }
 }
