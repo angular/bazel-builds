a72922ab0601bebbc8eefc7f49ae7c264be21bd2
atscott <atscott@google.com>
2020-02-27 22:28:10 +0000

    287d841486 build: allow auto-discover all typings files in npm package by ts-api-guardian (#35691)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.3+9.sha-9b53054/, version);
+      banner = banner.replace(/9.0.4+3.sha-287d841/, version);
     }
   }
 }
