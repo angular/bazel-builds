6e3e9806f90d2785c49c067493903c54ba39078e
Matias Niemelä <matias@yearofmoo.com>
2020-02-27 21:30:18 +0000

    9b53054ea8 refactor(ngcc): guard against a crash if source-map flattening fails (#35718)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.3+6.sha-bf6fbf5/, version);
+      banner = banner.replace(/9.0.3+9.sha-9b53054/, version);
     }
   }
 }
