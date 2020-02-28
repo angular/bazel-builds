9498fa0552d9fe02a8c6845baa22fde5213009c3
atscott <atscott@google.com>
2020-02-28 15:35:51 +0000

    73ee57a6e3 test(language-service): differentiate feature and internal infra tests (#35688)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+17.sha-5e38056/, version);
+      banner = banner.replace(/9.1.0-next.2+18.sha-73ee57a/, version);
     }
   }
 }
