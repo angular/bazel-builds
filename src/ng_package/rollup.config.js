d08dd6686d7ec939a14b7ae597d1f96a5bc08767
atscott <atscott@google.com>
2020-02-28 15:33:32 +0000

    5e38056459 ci: bump CircleCI cache version prefix (#35711)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+19.sha-de4e17b/, version);
+      banner = banner.replace(/9.1.0-next.2+17.sha-5e38056/, version);
     }
   }
 }
