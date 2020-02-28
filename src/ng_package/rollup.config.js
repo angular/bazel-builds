5650ddff31f0e137138c8f4b7b1b29029bec7aac
atscott <atscott@google.com>
2020-02-28 15:41:16 +0000

    208ef7bd62 refactor(compiler): Remove NullAstVisitor and visitAstChildren (#35619)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+18.sha-73ee57a/, version);
+      banner = banner.replace(/9.1.0-next.2+20.sha-208ef7b/, version);
     }
   }
 }
