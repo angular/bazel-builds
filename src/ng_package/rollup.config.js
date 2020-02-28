2c1dfa0ab8112d51504157722e9aae89f98bbb22
atscott <atscott@google.com>
2020-02-28 16:03:20 +0000

    fcad075ade fix(ivy): narrow `NgIf` context variables in template type checker (#35125)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.4+4.sha-52fc087/, version);
+      banner = banner.replace(/9.0.4+10.sha-fcad075/, version);
     }
   }
 }
