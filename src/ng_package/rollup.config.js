00189cd8b1cd66b834ade504cccd7aa1219dad80
atscott <atscott@google.com>
2020-02-28 20:46:39 +0000

    40035380e7 fix(core): Remove `debugger` statement when assert is thrown (#35763)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+21.sha-40039d8/, version);
+      banner = banner.replace(/9.1.0-next.2+23.sha-4003538/, version);
     }
   }
 }
