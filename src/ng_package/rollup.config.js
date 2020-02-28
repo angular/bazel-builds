4b701ef7b7f3ed6e6f682c94cb5de1479223dc99
atscott <atscott@google.com>
2020-02-28 15:31:49 +0000

    de4e17b76e fix(docs-infra): fix redirect in angular version selector (#35632)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+15.sha-3e3a1ef/, version);
+      banner = banner.replace(/9.1.0-next.2+19.sha-de4e17b/, version);
     }
   }
 }
