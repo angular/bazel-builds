9a98b060312329823490a4b2dd4117767b1fe878
atscott <atscott@google.com>
2020-02-28 00:28:17 +0000

    52fc08754d fix(ivy): support dynamic query tokens in AOT mode (#35307)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.4+3.sha-287d841/, version);
+      banner = banner.replace(/9.0.4+4.sha-52fc087/, version);
     }
   }
 }
