e8344dc778307b5746a4c55e8454e6178cb59367
atscott <atscott@google.com>
2020-03-02 17:49:47 +0000

    1503408a3d docs: reword explanation for using singleton services decorative to be more understandable (#35002)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.4+22.sha-48025eb/, version);
+      banner = banner.replace(/9.0.4+23.sha-1503408/, version);
     }
   }
 }
