78695021a857c384f24f48b92b192f02771908e0
atscott <atscott@google.com>
2020-03-02 16:42:03 +0000

    35c9f0dc2f fix(animations): process shorthand `margin` and `padding` styles correctly (#35701)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+28.sha-746e313/, version);
+      banner = banner.replace(/9.1.0-next.2+32.sha-35c9f0d/, version);
     }
   }
 }
