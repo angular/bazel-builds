016bb1cbca7ac6889b3f6c47efa8426d82dc650c
atscott <atscott@google.com>
2020-03-02 16:38:14 +0000

    d7efc45c04 perf(ngcc): only create tasks for non-processed formats (#35719)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+25.sha-5bc39f8/, version);
+      banner = banner.replace(/9.1.0-next.2+31.sha-d7efc45/, version);
     }
   }
 }
