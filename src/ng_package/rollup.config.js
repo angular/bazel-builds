574aeaa68330488221ece626800e8130cc91fc27
atscott <atscott@google.com>
2020-03-02 21:54:54 +0000

    8eb4a9d395 docs: fix docs and associated code snippets for enabling more macro tasks in `fakeAsync()` (#35778)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+38.sha-4ba4d90/, version);
+      banner = banner.replace(/9.1.0-next.2+40.sha-8eb4a9d/, version);
     }
   }
 }
