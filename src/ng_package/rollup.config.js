734e453bd4ea3eab2a599813ada0a3df8f672aa5
atscott <atscott@google.com>
2020-03-02 21:55:34 +0000

    ef2721b903 build: fix flakiness in integration/bazel by disabling symlinked_node_modules (#35804)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+40.sha-8eb4a9d/, version);
+      banner = banner.replace(/9.1.0-next.2+39.sha-ef2721b/, version);
     }
   }
 }
