4b705e8f9e8be683e091221fe7577d8eda935baa
atscott <atscott@google.com>
2020-03-03 17:12:04 +0000

    47a1811e0b feat(language-service): modularize error messages (#35678)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+39.sha-ef2721b/, version);
+      banner = banner.replace(/9.1.0-next.2+41.sha-47a1811/, version);
     }
   }
 }
