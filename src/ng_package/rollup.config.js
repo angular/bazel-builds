3f7f2e2fe3fdaf84a74a77deb55ea7f6e63eba97
atscott <atscott@google.com>
2020-03-03 22:14:50 +0000

    1f8a243b67 fix(core): unable to NgModuleRef.injector in module constructor (#35731)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+56.sha-17cf04e/, version);
+      banner = banner.replace(/9.1.0-next.2+57.sha-1f8a243/, version);
     }
   }
 }
