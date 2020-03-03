4c7f8668c09be3f4904a56b5d76a492d8835b63c
atscott <atscott@google.com>
2020-03-03 22:16:31 +0000

    ed44073a58 build(zone.js): sets rollup_bundle to use the --silent flag (#35835)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+58.sha-2c41bb8/, version);
+      banner = banner.replace(/9.1.0-next.2+59.sha-ed44073/, version);
     }
   }
 }
