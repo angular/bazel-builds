a93a500202bf7f92d899702429b5bf59677506c4
atscott <atscott@google.com>
2020-03-03 17:30:15 +0000

    89b5b97368 build: avoid running duplicate tests in language service (#35816)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+48.sha-0653db1/, version);
+      banner = banner.replace(/9.1.0-next.2+51.sha-89b5b97/, version);
     }
   }
 }
