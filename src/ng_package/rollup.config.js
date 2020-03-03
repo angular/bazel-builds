fdb8f12bc7b1beb20412944fd593b7e9c9e498d8
atscott <atscott@google.com>
2020-03-03 17:21:26 +0000

    bf42807aa6 docs(common): switch HTTP guide examples to `TestBed.inject()` (#35777)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+46.sha-c195d22/, version);
+      banner = banner.replace(/9.1.0-next.2+47.sha-bf42807/, version);
     }
   }
 }
