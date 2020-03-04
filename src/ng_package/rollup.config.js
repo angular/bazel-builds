159daf2d615a1686efc2c95d7bddf9d0a3791e21
Matias Niemelä <matias@yearofmoo.com>
2020-03-04 22:12:07 +0000

    de67978a7d fix(router): state data missing in routerLink (#33203)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+73.sha-b57be3b/, version);
+      banner = banner.replace(/9.1.0-next.2+74.sha-de67978/, version);
     }
   }
 }
