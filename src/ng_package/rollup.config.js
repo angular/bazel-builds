fb686cff0094b45bf6d38fc513597c7060bb3e83
atscott <atscott@google.com>
2020-03-04 17:03:03 +0000

    2e728f7fff docs: remove `ivy` and mention `ve` label in docs (#35809)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+61.sha-d543b13/, version);
+      banner = banner.replace(/9.1.0-next.2+62.sha-2e728f7/, version);
     }
   }
 }
