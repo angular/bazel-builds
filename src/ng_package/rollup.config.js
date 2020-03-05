f1d50ebd47467dde79fbe851293ea69b8c0fd2eb
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:23:21 +0000

    e0a35e13d5 perf(ngcc): reduce directory traversing (#35756)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+75.sha-a1d0396/, version);
+      banner = banner.replace(/9.1.0-next.2+77.sha-e0a35e1/, version);
     }
   }
 }
