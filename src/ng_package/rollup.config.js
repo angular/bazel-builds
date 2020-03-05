2d0bcad562a7a136c0587abbb14155a74daa192f
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:28:04 +0000

    716d50aa21 refactor(compiler): Break up parseTemplateBindings() for microsyntax (#35812)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+76.sha-8ef29b6/, version);
+      banner = banner.replace(/9.1.0-next.2+79.sha-716d50a/, version);
     }
   }
 }
