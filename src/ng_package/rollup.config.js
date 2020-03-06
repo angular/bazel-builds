496a0ee59afec60c380c0d9554b5e2b04f623135
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:23:24 +0000

    86a45ff561 docs(zone.js): update coments of zone configuration API definition (#35849)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+94.sha-84c2458/, version);
+      banner = banner.replace(/9.1.0-next.2+95.sha-86a45ff/, version);
     }
   }
 }
