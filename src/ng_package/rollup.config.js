7b05d5126ebaea2e0dc534dd1327cfe9bfcd2e7b
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:13:10 +0000

    c06f9a1ce3 docs(zone.js): update comment and chapter format of ngzone doc. (#35738)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+92.sha-d912d4f/, version);
+      banner = banner.replace(/9.1.0-next.2+93.sha-c06f9a1/, version);
     }
   }
 }
