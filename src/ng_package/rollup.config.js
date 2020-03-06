a9aff93178a3d5f083152b068880c00bfe68eac4
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:06:05 +0000

    bef14cf424 refactor(compiler): rename _ParseAST.optionalCharacter TemplateBinding.expression (#35886)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+88.sha-4e1d780/, version);
+      banner = banner.replace(/9.1.0-next.2+90.sha-bef14cf/, version);
     }
   }
 }
