8189677a7e793ca4335835ea6145ee7be5d9a5b3
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:00:21 +0000

    a1b90619ba test(zone.js): sets rollup_bundle to use the --silent flag in karma tests (#35881)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+87.sha-44f9fa6/, version);
+      banner = banner.replace(/9.1.0-next.2+86.sha-a1b9061/, version);
     }
   }
 }
