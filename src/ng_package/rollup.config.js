ec6d656ac146730ba186f4ed7361a2cff253e0aa
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:56:30 +0000

    55b3f97be0 fix(zone.js): `tickOptions`'s `processNewMacroTasksSynchronously` should default to true when flag omitted (#35814)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+95.sha-86a45ff/, version);
+      banner = banner.replace(/9.1.0-next.2+99.sha-55b3f97/, version);
     }
   }
 }
