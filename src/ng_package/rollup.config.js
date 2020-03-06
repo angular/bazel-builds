af32caafd1407424909cc45e0de58f93b1ee287c
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:01:24 +0000

    44f9fa6ce5 docs: add missing closing bracket (#35890)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+85.sha-b42e2e0/, version);
+      banner = banner.replace(/9.1.0-next.2+87.sha-44f9fa6/, version);
     }
   }
 }
