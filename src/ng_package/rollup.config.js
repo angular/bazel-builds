502ba75400fcb5735437fc4d995227d3a47be6ad
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 00:58:46 +0000

    b42e2e0dff build: create dev-infra-private npm package (#35862)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+84.sha-c55f900/, version);
+      banner = banner.replace(/9.1.0-next.2+85.sha-b42e2e0/, version);
     }
   }
 }
