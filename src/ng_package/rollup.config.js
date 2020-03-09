9b17a520e2acabc9193962991473f38fb183384e
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 16:44:02 +0000

    11f7e275e6 feat(compiler): type TemplateAst values as ASTWithSource (#35892)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+109.sha-1882451/, version);
+      banner = banner.replace(/9.1.0-next.2+106.sha-11f7e27/, version);
     }
   }
 }
