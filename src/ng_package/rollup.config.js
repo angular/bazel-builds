46ad348ec6064b60b29761702f7ae5ba25351ddb
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 21:26:13 +0000

    15482e7367 Revert "feat(bazel): transform generated shims (in Ivy) with tsickle (#35848)" (#35970)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+110.sha-fc4c3c3/, version);
+      banner = banner.replace(/9.1.0-next.2+112.sha-15482e7/, version);
     }
   }
 }
