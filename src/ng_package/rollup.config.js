8be30329709488f1c4816446b0b7ef5bcdf9ad48
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 21:52:41 +0000

    71309d223d Revert "feat: Monkey patches MessagePort onproperties (onmessage/onmessageerror) (#34610)" (#35973)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+112.sha-15482e7/, version);
+      banner = banner.replace(/9.1.0-next.2+113.sha-71309d2/, version);
     }
   }
 }
