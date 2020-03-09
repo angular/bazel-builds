d7104e90bff27f28390027adee81a1d6254cf6ed
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 16:48:41 +0000

    2b8edbbb1d refactor(benchpress): remove benchmarks_external from ..pullapprove.yml (#35670)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.2+106.sha-11f7e27/, version);
+      banner = banner.replace(/9.1.0-next.2+108.sha-2b8edbb/, version);
     }
   }
 }
