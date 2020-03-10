63d0c59d2d8e971fe2118d3cdbd4f73775fd2ac6
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:01:45 +0000

    20dd0ac4f2 build: Add pullapprove verification tool to dev-infra-private package (#35911)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.5+30.sha-83d7819/, version);
+      banner = banner.replace(/9.0.5+43.sha-20dd0ac/, version);
     }
   }
 }
