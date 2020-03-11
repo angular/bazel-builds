d59c1bf8399695c20c62c0cd9fd1b61fe152615f
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:11:15 +0000

    5e3a898f10 fix(bazel): update typescript peer dependency range (#36013)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+13.sha-49478f5/, version);
+      banner = banner.replace(/9.1.0-next.4+14.sha-5e3a898/, version);
     }
   }
 }
