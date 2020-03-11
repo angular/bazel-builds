2463a3bd17c1d8a27302d37537e0ead965ad4097
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:30:12 +0000

    06779cfe24 feat(compiler): Add sourceSpan and keySpan to TemplateBinding (#35897)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+19.sha-146ef48/, version);
+      banner = banner.replace(/9.1.0-next.4+26.sha-06779cf/, version);
     }
   }
 }
