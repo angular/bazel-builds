0fb0b3d4656f7b103c60e354f822f778fd5e46fd
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:33:41 +0000

    13d85e10a2 docs: add developer survey 2020 (#35655)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.6+24.sha-7af0648/, version);
+      banner = banner.replace(/9.0.6+25.sha-13d85e1/, version);
     }
   }
 }
