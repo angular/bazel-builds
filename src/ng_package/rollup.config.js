631bb0b81d34f844a6af59d72fef43cc6ed03099
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:32:59 +0000

    18b1bd4415 fix(language-service): infer $implicit value for ngIf template contexts (#35941)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+26.sha-06779cf/, version);
+      banner = banner.replace(/9.1.0-next.4+27.sha-18b1bd4/, version);
     }
   }
 }
