42e5d9b466ab58bc4078c77e4809206776a43f59
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:07:17 +0000

    2b6028b643 build: remove legacy integration test runner (#35985)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+34.sha-99ea5d7/, version);
+      banner = banner.replace(/9.1.0-next.4+35.sha-2b6028b/, version);
     }
   }
 }
