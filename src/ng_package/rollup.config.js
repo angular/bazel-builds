43eecaf9d055cbdd7e65305b2c6e91539747f3e0
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 18:51:36 +0000

    42375c4b3a ci: prevent api golden tests on windows CI (#36025)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+5.sha-15f8afa/, version);
+      banner = banner.replace(/9.1.0-next.4+8.sha-42375c4/, version);
     }
   }
 }
