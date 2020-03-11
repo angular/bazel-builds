10f7cc8a60e4ed6f5a15b6dc1f84ccd97769d963
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:41:56 +0000

    15f8afa4bf ci: move public-api goldens to goldens directory (#35768)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+7.sha-13495c6/, version);
+      banner = banner.replace(/9.1.0-next.4+5.sha-15f8afa/, version);
     }
   }
 }
