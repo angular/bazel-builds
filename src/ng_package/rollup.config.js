ddfd0d0b7f0d0c840cddcfa0b39029453495d07b
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:36:50 +0000

    6f95bc915d build: update to rules_nodejs 1.4.1 (#35999)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+4.sha-19cfaf7/, version);
+      banner = banner.replace(/9.1.0-next.4+3.sha-6f95bc9/, version);
     }
   }
 }
