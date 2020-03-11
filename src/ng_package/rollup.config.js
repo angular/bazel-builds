f8d02fb182adb80e4cdb8af4d35e57c62a9deba1
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:13:58 +0000

    10f1e94f18 build(bazel): remove hacky fake @angular/bazel npm package under tools/npm/@angular_bazel (#36009)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+15.sha-5f7d066/, version);
+      banner = banner.replace(/9.1.0-next.4+12.sha-10f1e94/, version);
     }
   }
 }
