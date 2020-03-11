5545797b6a13b0f6276b9e465b2093187b8f4f8c
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:18:07 +0000

    20f01163af fix(dev-infra): sort contributors alphabetically in `get-data.ts` script (#35997)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+16.sha-77da688/, version);
+      banner = banner.replace(/9.1.0-next.4+23.sha-20f0116/, version);
     }
   }
 }
