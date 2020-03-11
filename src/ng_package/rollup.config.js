19807b8cbcf1df5721600bf493932fb9faf198d6
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:25:55 +0000

    146ef48a6e docs: change grammar in architecture guide (#35541)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+25.sha-32f099a/, version);
+      banner = banner.replace(/9.1.0-next.4+19.sha-146ef48/, version);
     }
   }
 }
