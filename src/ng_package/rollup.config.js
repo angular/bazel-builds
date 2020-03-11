8f4c5c1c70b56a5c6eeffbdf381aa213df61ade0
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:16:56 +0000

    77da6887f7 build(dev-infra): handle async failures in `get-data.ts` (#36007)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+22.sha-d8f9ec5/, version);
+      banner = banner.replace(/9.1.0-next.4+16.sha-77da688/, version);
     }
   }
 }
