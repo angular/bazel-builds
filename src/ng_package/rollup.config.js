de1908a775610ddc1f73f53c5b0f99a2fe87275a
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:09:06 +0000

    dec80b0e35 revert: "ci: prevent api golden tests on windows CI (#36025)" (#36034)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.6+25.sha-13d85e1/, version);
+      banner = banner.replace(/9.0.6+27.sha-dec80b0/, version);
     }
   }
 }
