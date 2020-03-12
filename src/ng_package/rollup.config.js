830bad16aeca87dc2bed9e2b01de48d55f7bd41f
Andrew Kushnir <akushnir@google.com>
2020-03-12 19:38:27 +0000

    63815b54f3 test(core): Add benchpress test for transplanted view change detection (#36001)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+40.sha-372a56a/, version);
+      banner = banner.replace(/9.1.0-next.4+41.sha-63815b5/, version);
     }
   }
 }
