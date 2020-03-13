e0148a6041ae3f7acf9899db7952b1a05edebe16
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:24:51 +0000

    db74f2074b build: enable forms tests on IE (#35971)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+49.sha-772bb5e/, version);
+      banner = banner.replace(/9.1.0-next.4+50.sha-db74f20/, version);
     }
   }
 }
