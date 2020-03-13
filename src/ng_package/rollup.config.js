463a59de0c509e63ebb885dafa1010ec18c838a4
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:45:35 +0000

    9e698b7cca fix(docs-infra): remove `routerLink` from `top-bar` (#35951)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.6+36.sha-a8cc6a2/, version);
+      banner = banner.replace(/9.0.6+37.sha-9e698b7/, version);
     }
   }
 }
