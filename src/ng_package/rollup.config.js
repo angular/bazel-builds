694f3115986b49d2a30f1ae8560c5c49f30fa7cd
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:25:16 +0000

    1342668fd1 fix(zone.js): zone.js patches rxjs should check null for unsubscribe (#35990)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.6+38.sha-a24eddb/, version);
+      banner = banner.replace(/9.0.6+39.sha-1342668/, version);
     }
   }
 }
