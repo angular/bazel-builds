388c6132f1f88a52bb13dd8726c72e8d703ad28d
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:13:48 +0000

    79659ee5aa fix(compiler): support directive inputs with interpolations on `<ng-template>`s (#35984)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+56.sha-3fa8952/, version);
+      banner = banner.replace(/9.1.0-next.4+57.sha-79659ee/, version);
     }
   }
 }
