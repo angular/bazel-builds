c498123222ebdfed2fb24ff11bdc977ec13a1ab9
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:17:38 +0000

    a52b5680cb fix(compiler): do not recurse to find static symbols of same module (#35262)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.6+39.sha-1342668/, version);
+      banner = banner.replace(/9.0.6+41.sha-a52b5680/, version);
     }
   }
 }
