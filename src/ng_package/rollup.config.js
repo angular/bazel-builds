5c7563ba7f0035ef384833fe7b68a62f6a90da7a
Andrew Kushnir <akushnir@google.com>
2020-03-17 19:51:50 +0000

    a1cae28283 build: fix saucelabs jobs that were broken by bazelisk change (#36109)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+75.sha-22be967/, version);
+      banner = banner.replace(/9.1.0-next.4+77.sha-a1cae28/, version);
     }
   }
 }
