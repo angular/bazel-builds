cfbd84a52a0fa01f5011423b51fec578f6eadc8f
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:51:36 +0000

    acc483e2eb feat(language-service): improve non-callable error message (#35271)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+64.sha-61c3795/, version);
+      banner = banner.replace(/9.1.0-next.4+63.sha-acc483e/, version);
     }
   }
 }
