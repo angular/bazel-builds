186de9292edad4606845e7d56d04d91e05504e37
Andrew Kushnir <akushnir@google.com>
2020-03-17 00:05:55 +0000

    ec750caff0 refactor(core): misc refactoring to support upcoming changes to improve synthetic host bindings (#35884)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.6+41.sha-a52b5680/, version);
+      banner = banner.replace(/9.0.6+42.sha-ec750ca/, version);
     }
   }
 }
