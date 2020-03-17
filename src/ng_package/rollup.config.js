5ed15f9d26fa424ac38de336492cb19bb373a02c
Andrew Kushnir <akushnir@google.com>
2020-03-17 17:38:10 +0000

    e3ecdc6a63 feat(bazel): transform generated shims (in Ivy) with tsickle (#35975)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+66.sha-4af2a06/, version);
+      banner = banner.replace(/9.1.0-next.4+67.sha-e3ecdc6/, version);
     }
   }
 }
