48a7b9d02d1e88f81383193f83a3c7e724242062
Andrew Kushnir <akushnir@google.com>
2020-03-17 18:37:05 +0000

    34a17f3699 Revert "build: fix ts-api-guardian golden approval not working on windows (#36096)" (#36113)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+68.sha-afc9839/, version);
+      banner = banner.replace(/9.1.0-next.4+70.sha-34a17f3/, version);
     }
   }
 }
