ef753d9e702538fdff1d63b6353504ecda554c23
Andrew Kushnir <akushnir@google.com>
2020-03-17 20:57:53 +0000

    76d7188872 fix(ngcc): consistently delegate to TypeScript host for typing files (#36089)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.0.6+56.sha-3c19a5d/, version);
+      banner = banner.replace(/9.0.6+58.sha-76d7188/, version);
     }
   }
 }
