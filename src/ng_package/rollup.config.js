da1ab08b1a5947e8e0ea4672e38ad011554f4fef
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:21:10 +0000

    67b9adfd1a docs(upgrade): remove outdated/non-applicable AOT section from upgrade guide (#36015)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.4+80.sha-9e70bcb/, version);
+      banner = banner.replace(/9.1.0-next.4+101.sha-67b9adf/, version);
     }
   }
 }
