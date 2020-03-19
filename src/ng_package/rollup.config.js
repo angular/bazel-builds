96a2e5275cf3d39f9e64f1bb5328305ed56c09f9
Andrew Kushnir <akushnir@google.com>
2020-03-19 16:26:04 +0000

    e342ffd855 fix(core): adhere to bootstrap options for JIT compiled components (#35534)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.5+9.sha-fb92f5d/, version);
+      banner = banner.replace(/9.1.0-next.5+8.sha-e342ffd/, version);
     }
   }
 }
