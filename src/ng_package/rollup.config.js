a9dd5755fee9f8da43aed9034202c0fba81d777c
Andrew Kushnir <akushnir@google.com>
2020-03-19 20:25:29 +0000

    b4972fa165 release: cut the v9.1.0-rc.0 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-next.5+8.sha-e342ffd/, version);
+      banner = banner.replace(/9.1.0-rc.0/, version);
     }
   }
 }
