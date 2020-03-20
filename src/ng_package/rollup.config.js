7e083872104483c5972512e2dae01f0f96a2ab81
Misko Hevery <misko@hevery.com>
2020-03-20 17:45:18 +0000

    989dea7083 refactor(benchpress): delete broken code (#35922)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+3.sha-8968b20/, version);
+      banner = banner.replace(/9.1.0-rc.0+4.sha-989dea7/, version);
     }
   }
 }
