7b054f03d3bc7e005a67b7e42558bff124c342e6
Misko Hevery <misko@hevery.com>
2020-03-20 20:11:43 +0000

    05eeb7d279 test(compiler): remove whitespace in spans (#36169)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0/, version);
+      banner = banner.replace(/9.1.0-rc.0+8.sha-05eeb7d/, version);
     }
   }
 }
