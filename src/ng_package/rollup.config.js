519e2308f49a302fdd002ef9b37af4407db77580
Misko Hevery <misko@hevery.com>
2020-03-23 18:52:35 +0000

    0ce8ad3493 fix(core): workaround Terser inlining bug (#36200)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+12.sha-528e25a/, version);
+      banner = banner.replace(/9.1.0-rc.0+13.sha-0ce8ad3/, version);
     }
   }
 }
