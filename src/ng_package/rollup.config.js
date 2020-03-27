fea8b1f654177f580a87a0ec1e80b61d9ca058b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 21:35:36 +0000

    6ea232eb3c fix(ngcc): do not spawn more processes than intended in parallel mode (#36280)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0+14.sha-0baab0f/, version);
+      banner = banner.replace(/9.1.0+15.sha-6ea232e/, version);
     }
   }
 }
