a82dacaf85aa17578e59b62bf91beee6b0a54122
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:21:42 +0000

    1c385a1c22 build(docs-infra): do not include CI-specific config in docs examples ZIP archives (#36018)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+38.sha-fc3e5cb/, version);
+      banner = banner.replace(/9.1.0-rc.0+44.sha-1c385a1/, version);
     }
   }
 }
