3c7c381cdf97fca45000b3baf346f591b74e8dc0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 19:20:03 +0000

    15cffa210c build: update to rules_nodejs 1.5.0 (#36307)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0+16.sha-c3b297a/, version);
+      banner = banner.replace(/9.1.0+20.sha-15cffa2/, version);
     }
   }
 }
