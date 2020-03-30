ed8cfd4e7d4b3356814f1919eb7fbd0ecf1b713b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 18:56:59 +0000

    5ac308060d refactor(ngcc): rename `workerCount` to `maxWorkerCount` (#36298)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+58.sha-c6dd900/, version);
+      banner = banner.replace(/9.1.0-rc.0+57.sha-5ac3080/, version);
     }
   }
 }
