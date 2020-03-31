b77f3ab808e9c643b1fc24e0e3a5e870bd44a572
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:33:26 +0000

    e8f3c47b03 build(docs-infra): rename duplicate test name (#36348)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0+22.sha-0ab244f/, version);
+      banner = banner.replace(/9.1.0+26.sha-e8f3c47/, version);
     }
   }
 }
