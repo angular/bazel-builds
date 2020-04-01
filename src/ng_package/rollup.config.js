3b8999067861fbeadd2e9c8159397976d6aa4b90
Kara Erickson <karakara@google.com>
2020-04-01 20:33:07 +0000

    4d1d0fa03b docs: update ng-conf announcement to remove livestream link (#36382)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+67.sha-ec8bae1/, version);
+      banner = banner.replace(/9.1.0-rc.0+68.sha-4d1d0fa/, version);
     }
   }
 }
