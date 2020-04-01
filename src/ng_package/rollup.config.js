f1df1a0c4cd4287376b62d291809cf50064a7569
Kara Erickson <karakara@google.com>
2020-04-01 18:34:43 +0000

    ec8bae1b27 docs: update end date of survey to middle of April (#36339)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+66.sha-08348fc/, version);
+      banner = banner.replace(/9.1.0-rc.0+67.sha-ec8bae1/, version);
     }
   }
 }
