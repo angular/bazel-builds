057828d82ff5df208b8c31579a466e4fafd1a2d4
Kara Erickson <karakara@google.com>
2020-04-01 22:58:21 +0000

    c3ce1903b6 build: enable service-worker tests on saucelabs (#36129)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0+43.sha-cb0a2a0/, version);
+      banner = banner.replace(/9.1.0+49.sha-c3ce190/, version);
     }
   }
 }
