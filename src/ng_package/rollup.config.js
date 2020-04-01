5fff52c91f5fb002fa641d48bf377e5f4ed9bf12
Kara Erickson <karakara@google.com>
2020-04-01 22:46:18 +0000

    4d458db1b5 docs: update and edit web-worker page (#36383)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+84.sha-326240e/, version);
+      banner = banner.replace(/9.1.0-rc.0+87.sha-4d458db/, version);
     }
   }
 }
