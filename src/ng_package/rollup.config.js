5eb7b6844a6263102ce938b0a5996f19f841a6f6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:53:44 +0000

    326240eb91 fix(ngcc): allow ngcc configuration to match pre-release versions of packages (#36370)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+81.sha-cc4b813/, version);
+      banner = banner.replace(/9.1.0-rc.0+84.sha-326240e/, version);
     }
   }
 }
