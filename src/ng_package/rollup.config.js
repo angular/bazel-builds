4d036224bbcdd7b64d97298b570ed9857ab26b74
Kara Erickson <karakara@google.com>
2020-04-03 18:33:35 +0000

    776f991c1a build: update pullapprove config to better handle global approvals (#36384)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0+56.sha-9f486c3/, version);
+      banner = banner.replace(/9.1.0+67.sha-776f991/, version);
     }
   }
 }
