7627351d5748bdd989571c4270bde399aac8d39d
Kara Erickson <karakara@google.com>
2020-04-06 20:45:51 +0000

    d43c30688a fix(core): avoid migration error when non-existent symbol is imported (#36367)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+121.sha-ee70a18/, version);
+      banner = banner.replace(/9.1.0-rc.0+122.sha-d43c306/, version);
     }
   }
 }
