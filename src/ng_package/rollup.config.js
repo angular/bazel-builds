d9a63f1b8f3466a8c092db6b4e6e96d3e188ea2e
Kara Erickson <karakara@google.com>
2020-04-06 18:55:51 +0000

    f9fb8338f5 fix(ngcc): support ignoring deep-imports via package config (#36423)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+114.sha-1b4df64/, version);
+      banner = banner.replace(/9.1.0-rc.0+118.sha-f9fb833/, version);
     }
   }
 }
