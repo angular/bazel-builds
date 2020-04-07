aff939bca1ced99674c11f9c6ddcee532e4c70b9
Kara Erickson <karakara@google.com>
2020-04-07 22:58:06 +0000

    0075017b43 docs: update reactive forms page (#35969)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+143.sha-96a3de6/, version);
+      banner = banner.replace(/9.1.0-rc.0+149.sha-0075017/, version);
     }
   }
 }
