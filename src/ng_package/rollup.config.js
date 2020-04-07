6f1d4ce7d7099afd190f91c4a4942e8ec9591c33
Kara Erickson <karakara@google.com>
2020-04-07 17:06:43 +0000

    7d0af179e3 style(forms): reformat of the `forms` package after clang update (#36466)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+129.sha-93302b7/, version);
+      banner = banner.replace(/9.1.0-rc.0+131.sha-7d0af17/, version);
     }
   }
 }
