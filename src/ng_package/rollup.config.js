20cacb2d2b1f2cef1091ee3f66949b40ba8dbcfb
Kara Erickson <karakara@google.com>
2020-04-07 18:57:53 +0000

    7f28845f58 test(language-service): remove ng-for-cases.ts (#36470)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.0-rc.0+138.sha-e0415db/, version);
+      banner = banner.replace(/9.1.0-rc.0+140.sha-7f28845/, version);
     }
   }
 }
