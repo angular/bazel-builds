c8c8b8e1e0f11d13061ffa4bd61dc2799bfeeee2
atscott <atscott@google.com>
2020-04-08 23:25:52 +0000

    92c4f3d508 refactor(bazel): use runfiles helper in ts-api-guardian (#36471)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.1+4.sha-e92fce1/, version);
+      banner = banner.replace(/10.0.0-next.1+5.sha-92c4f3d/, version);
     }
   }
 }
