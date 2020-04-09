a82344a44ba6ef8df6d5342a4852d8f580828930
atscott <atscott01@gmail.com>
2020-04-09 01:23:12 +0000

    1bfa908ab3 style: typescript lint fix (#36531)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.1+13.sha-333b867/, version);
+      banner = banner.replace(/9.1.1+15.sha-1bfa908/, version);
     }
   }
 }
