39875f120cd70a53910710eb9e3c7d60ebbcd736
atscott <atscott@google.com>
2020-04-13 15:39:12 +0000

    fd7698253e docs: move ng-conf 2020 to the already presented section (#36413)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.1+29.sha-3992341/, version);
+      banner = banner.replace(/9.1.1+34.sha-fd76982/, version);
     }
   }
 }
