c359ba637cc34c2c3a09efca596243b775bc8ea7
atscott <atscott@google.com>
2020-04-13 15:37:54 +0000

    9181b73e5a docs: fix typo in Tests guide (#36592)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.1+21.sha-ca67748/, version);
+      banner = banner.replace(/10.0.0-next.1+23.sha-9181b73/, version);
     }
   }
 }
