65a0fce66b97a1bed44eb03a5543ab825d1eca81
atscott <atscott@google.com>
2020-04-14 20:04:37 +0000

    93993864b1 build: update to latest version of yarn (#36464)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.1+40.sha-26f4915/, version);
+      banner = banner.replace(/9.1.1+41.sha-9399386/, version);
     }
   }
 }
