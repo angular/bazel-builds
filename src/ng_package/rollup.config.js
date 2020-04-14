23c312b8fec14a28888f58fefd9e5a5f0831c742
atscott <atscott@google.com>
2020-04-14 19:32:19 +0000

    698b0288be build: reformat repo to new clang@1.4.0 (#36613)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.1+32.sha-5e80e7e/, version);
+      banner = banner.replace(/10.0.0-next.1+33.sha-698b028/, version);
     }
   }
 }
