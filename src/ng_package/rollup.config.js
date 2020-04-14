ded0962ea47b80925cfca53b156279dd7479c406
atscott <atscott@google.com>
2020-04-14 21:20:27 +0000

    0147de9044 build: fix circular deps failure (#36629)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.1+34.sha-b010849/, version);
+      banner = banner.replace(/10.0.0-next.1+35.sha-0147de9/, version);
     }
   }
 }
