810392a0cd45b2a7d96ddbf0be369473e0a1853f
atscott <atscott@google.com>
2020-04-15 16:43:31 +0000

    12266b2042 fix(ngcc): display output from the unlocker process on Windows (#36569)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.1+42.sha-c5e7251/, version);
+      banner = banner.replace(/9.1.1+45.sha-12266b2/, version);
     }
   }
 }
