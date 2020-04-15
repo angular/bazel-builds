718003fa19c4276b7f47a495c99775031896d546
atscott <atscott@google.com>
2020-04-15 23:38:27 +0000

    5c3774cfe6 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#36648)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.1+45.sha-12266b2/, version);
+      banner = banner.replace(/9.1.1+46.sha-5c3774c/, version);
     }
   }
 }
