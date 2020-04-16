3404b0356fe0e60e876768989b97adf0c2d3605f
atscott <atscott@google.com>
2020-04-16 17:07:48 +0000

    4a9f0bebc3 fix(core): prevent unknown property check for AOT-compiled components (#36072)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+3.sha-b1f1d3f/, version);
+      banner = banner.replace(/10.0.0-next.2+14.sha-4a9f0be/, version);
     }
   }
 }
