036b29eb63efa886c5340a0f3c1a81249e8eca0f
atscott <atscott@google.com>
2020-04-16 17:55:26 +0000

    305b059809 docs: refactor routing doc (#35566)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+17.sha-297b7da/, version);
+      banner = banner.replace(/10.0.0-next.2+18.sha-305b059/, version);
     }
   }
 }
