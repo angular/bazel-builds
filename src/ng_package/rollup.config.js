33991f4a6d277a10ab10ae9fa88fba4fd2ea23ba
atscott <atscott@google.com>
2020-04-16 17:04:49 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+1.sha-c6e5fc4/, version);
+      banner = banner.replace(/10.0.0-next.2+12.sha-1e3b8a1/, version);
     }
   }
 }
