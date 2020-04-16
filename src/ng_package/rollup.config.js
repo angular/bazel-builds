870595e225c2b00d8a1e8bdb9271b39ae16a0479
atscott <atscott@google.com>
2020-04-16 17:57:02 +0000

    297b7da166 docs: add string-union type note (#35859)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+13.sha-88b0985/, version);
+      banner = banner.replace(/10.0.0-next.2+17.sha-297b7da/, version);
     }
   }
 }
