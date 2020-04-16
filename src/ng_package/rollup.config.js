99a9d6a4be886c1efba75e2226ccb61889145097
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:00:44 +0000

    5e79799b89 chore: update saucelabs README with some more details on local testing (#36625)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+30.sha-1fa1dd5/, version);
+      banner = banner.replace(/10.0.0-next.2+31.sha-5e79799/, version);
     }
   }
 }
