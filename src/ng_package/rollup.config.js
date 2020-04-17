7e5c20e910673bdb4bebf32a1383c4c1b2a3882b
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 16:53:44 +0000

    bb150c2704 fixup!: build(docs-infra): Ensures that only member docs are linked (#36316)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+33.sha-39b0436/, version);
+      banner = banner.replace(/10.0.0-next.2+40.sha-bb150c2/, version);
     }
   }
 }
