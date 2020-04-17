2d6dfa6234c5fc441011e72bae12d203317bdb69
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 20:38:08 +0000

    81d23b33ef fix(core): pipes injecting viewProviders when used on a component host node (#36512)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+40.sha-bb150c2/, version);
+      banner = banner.replace(/10.0.0-next.2+41.sha-81d23b3/, version);
     }
   }
 }
