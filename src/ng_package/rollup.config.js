85afba92aeaf846fb2b58eaca374bcebc1f1cc7e
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 20:58:36 +0000

    351759906b refactor(compiler): remove unused CachedFileSystem (#36687)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+41.sha-81d23b3/, version);
+      banner = banner.replace(/10.0.0-next.2+44.sha-3517599/, version);
     }
   }
 }
