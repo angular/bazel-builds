9c8293560ce211f4f00a784106581d95af9ef53e
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 17:55:09 +0000

    f4ef8a92f8 docs: fix typo (#36665) PR Close #36665

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+45.sha-d7e9d87/, version);
+      banner = banner.replace(/10.0.0-next.2+46.sha-f4ef8a9/, version);
     }
   }
 }
