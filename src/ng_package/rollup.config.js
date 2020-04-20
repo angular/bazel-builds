ae74465f3dc082ac5647f405ea422657fc34ae38
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:03:38 +0000

    722d9397b0 Revert "fix(router): pass correct component to canDeactivate checks when using two or more sibling router-outlets (#36302)" (#36697)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.2+41.sha-95f45e8/, version);
+      banner = banner.replace(/9.1.2+45.sha-722d939/, version);
     }
   }
 }
