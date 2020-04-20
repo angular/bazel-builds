51240f6cb00db97d18eb844aef2a570436036937
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 20:42:00 +0000

    458dc10ddc docs: place download section in accessibility to the top (#36561)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+49.sha-4341743/, version);
+      banner = banner.replace(/10.0.0-next.2+50.sha-458dc10/, version);
     }
   }
 }
