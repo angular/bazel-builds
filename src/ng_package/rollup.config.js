9fcc79da55a5c3381b0b9896604447e79fc6061e
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:13:43 +0000

    51b09244ff fix(dev-infra): extract commit headers before checking commit message validity (#36733)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.2+53.sha-f186c32/, version);
+      banner = banner.replace(/10.0.0-next.2+51.sha-51b0924/, version);
     }
   }
 }
