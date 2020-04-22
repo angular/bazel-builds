9588479b911c5f82afe9d30855e1d94ab4578a0c
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:30:57 +0000

    8306da5ce1 release(benchpress): bump version of benchpress to 0.2.0 (#36457)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+2.sha-47f9867/, version);
+      banner = banner.replace(/10.0.0-next.3+5.sha-8306da5/, version);
     }
   }
 }
