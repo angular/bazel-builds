d1373a4a5be95f7da2e76dd35c5e87bd401ba4e3
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:31:32 +0000

    fc1b557d98 build: run pre-check before publishing (#36527)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+5.sha-8306da5/, version);
+      banner = banner.replace(/10.0.0-next.3+3.sha-fc1b557/, version);
     }
   }
 }
