93df4920fe78ebc71b0c971d83bc21063dc80237
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 20:54:33 +0000

    47f98673c7 fix(localize): include legacy ids when describing messages (#36761)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+1.sha-781f561/, version);
+      banner = banner.replace(/10.0.0-next.3+2.sha-47f9867/, version);
     }
   }
 }
