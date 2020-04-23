5e12903ce754adabca46ab8c1c271c2b765ad1f9
Andrew Kushnir <akushnir@google.com>
2020-04-23 20:53:35 +0000

    e6161ca307 build(core): use dev-infra's component_benchmark to show PoC (#36434)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+21.sha-e37f266/, version);
+      banner = banner.replace(/10.0.0-next.3+24.sha-e6161ca/, version);
     }
   }
 }
