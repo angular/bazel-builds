b2cb0e4090ea1c8b754bf7ef1de677e2f43d1ef0
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:35:51 +0000

    f0ec4ae941 ci: fix pullapprove incorrectly skipping fw-compiler as owner (#36661)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+16.sha-e5d2853/, version);
+      banner = banner.replace(/10.0.0-next.3+19.sha-f0ec4ae/, version);
     }
   }
 }
