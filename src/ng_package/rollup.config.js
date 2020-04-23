d13d14d5ffb4409ab9578558ae64a429e870b7c1
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:31:47 +0000

    152293d3cf test(router): fix router test failing on IE (#36742)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+14.sha-ceb61d1/, version);
+      banner = banner.replace(/10.0.0-next.3+15.sha-152293d/, version);
     }
   }
 }
