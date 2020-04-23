e20e59ad008a3fc1e7dd05cc8b257ad058f12057
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:34:36 +0000

    e5d2853070 docs: correct ngSwitch definition (#35489)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+13.sha-dd806b0/, version);
+      banner = banner.replace(/10.0.0-next.3+16.sha-e5d2853/, version);
     }
   }
 }
