33e17884c62e40b549de1a5d2187d6a3c20dee55
Andrew Kushnir <akushnir@google.com>
2020-04-24 18:23:47 +0000

    ad8c4cdd75 revert: "feat(dev-infra): exposed new rule 'component_benchmark' via dev_infra (#36434)" (#36798)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+27.sha-76190c7/, version);
+      banner = banner.replace(/10.0.0-next.3+30.sha-ad8c4cd/, version);
     }
   }
 }
