646b6f2388e390e1962565de746ed72f87954199
Andrew Kushnir <akushnir@google.com>
2020-04-24 19:56:18 +0000

    180a32894e build: migrate from gulp to ng-dev for running formatting (#36726)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.3+24.sha-cc71116/, version);
+      banner = banner.replace(/9.1.3+26.sha-180a328/, version);
     }
   }
 }
