052efa5e0df93a5a85cd86bc6acf138ae6f6d9be
Andrew Kushnir <akushnir@google.com>
2020-04-27 21:05:26 +0000

    00e6cb1d62 feat(router): allow CanLoad guard to return UrlTree (#36610)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+33.sha-b34fb04/, version);
+      banner = banner.replace(/10.0.0-next.3+35.sha-00e6cb1/, version);
     }
   }
 }
