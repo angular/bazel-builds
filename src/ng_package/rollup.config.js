fb3ccceb3b42517b57e3faf95f8e12b2732073e0
Andrew Kushnir <akushnir@google.com>
2020-04-27 22:18:06 +0000

    0f389fa5ae fix(core): handle synthetic props in Directive host bindings correctly (#35568)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.3+27.sha-2a27f69/, version);
+      banner = banner.replace(/9.1.3+28.sha-0f389fa/, version);
     }
   }
 }
