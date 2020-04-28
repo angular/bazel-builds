4e5708041292be23911f59cfd6e8080ea3d44a0c
Andrew Kushnir <akushnir@google.com>
2020-04-28 19:45:38 +0000

    70dd27ffd8 fix(compiler): normalize line endings in ICU expansions (#36741)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+38.sha-7c8c413/, version);
+      banner = banner.replace(/10.0.0-next.3+43.sha-70dd27f/, version);
     }
   }
 }
