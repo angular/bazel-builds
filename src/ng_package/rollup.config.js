624a031c836a5666ac390559c06bf63315a23b20
Andrew Kushnir <akushnir@google.com>
2020-04-28 22:31:02 +0000

    a6a7e1bb99 build: enable platform-browser tests on Saucelabs (#36797)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+43.sha-70dd27f/, version);
+      banner = banner.replace(/10.0.0-next.3+44.sha-a6a7e1b/, version);
     }
   }
 }
