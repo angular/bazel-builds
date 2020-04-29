2e0d2eebd12487169da617d2dc6c8030ba253177
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:21:20 +0000

    c89421bfa8 docs: release notes for the v9.1.4 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.3+47.sha-cebd4fa/, version);
+      banner = banner.replace(/10.0.0-next.3+48.sha-c89421b/, version);
     }
   }
 }
