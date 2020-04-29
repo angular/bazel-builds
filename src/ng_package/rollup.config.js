06dc21629652a7b994e8d0fbed1517e73f61ac8c
Andrew Kushnir <akushnir@google.com>
2020-04-29 15:57:13 +0000

    d7433316b0 fix(core): attempt to recover from user errors during creation (#36381)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.3+35.sha-c440165/, version);
+      banner = banner.replace(/9.1.3+36.sha-d743331/, version);
     }
   }
 }
