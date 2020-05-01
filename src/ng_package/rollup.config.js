3f97efaf764a3122cd3943d6e5be79cdd454ccde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:23:30 +0000

    12fcc7cafe build(language-service): make test project a filegroup (#36865)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.4+15.sha-49be32c/, version);
+      banner = banner.replace(/10.0.0-next.4+31.sha-12fcc7c/, version);
     }
   }
 }
