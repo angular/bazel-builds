aebe42f1f6ff36c8b6b8320ef4464bb00e7c456e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:04:30 +0000

    0f3831b105 build: create macro for transpiling javascript file to es5 (#36802)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.4+33.sha-9d9d46f/, version);
+      banner = banner.replace(/10.0.0-next.4+34.sha-0f3831b/, version);
     }
   }
 }
