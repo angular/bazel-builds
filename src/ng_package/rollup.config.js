952874936350d5858d888e5ebfc4471154581736
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:10:42 +0000

    58ea040570 test(language-service): add new mock host for testing ivy (#36879)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.4+41.sha-e037840/, version);
+      banner = banner.replace(/10.0.0-next.4+38.sha-58ea040/, version);
     }
   }
 }
