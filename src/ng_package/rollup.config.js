0152eb10cab8b60f3af08053fe93700c7690c0ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:16:44 +0000

    306f46ce92 build(docs-infra): upgrade docs examples to latest Angular CLI and framework (#36145)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.5+9.sha-70b25a3/, version);
+      banner = banner.replace(/10.0.0-next.5+7.sha-306f46c/, version);
     }
   }
 }
