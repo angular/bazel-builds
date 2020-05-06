1c9347449cdc8b4307fab067caaeaa597499ed3b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 21:13:33 +0000

    3c6c00d1d4 docs: remove the note about @angular/http from the deprecations guide (#36944)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.5+45.sha-fafa50d/, version);
+      banner = banner.replace(/10.0.0-next.5+53.sha-3c6c00d/, version);
     }
   }
 }
