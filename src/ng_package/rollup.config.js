23f842471335a8d347b0f9a8220334a40b6d75bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 21:02:38 +0000

    9d13ee0052 fix(ngcc): support ModuleWithProviders functions that delegate (#36948)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.4+55.sha-0be3792/, version);
+      banner = banner.replace(/9.1.4+57.sha-9d13ee0/, version);
     }
   }
 }
