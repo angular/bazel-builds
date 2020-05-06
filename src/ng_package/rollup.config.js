bc1a5c9e848a9c597520773456b5b77307d76c39
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:27:53 +0000

    f930e75a80 ci: update components-repo-unit-tests job commit (#36921)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.5+55.sha-20cc3ab/, version);
+      banner = banner.replace(/10.0.0-next.5+61.sha-f930e75/, version);
     }
   }
 }
