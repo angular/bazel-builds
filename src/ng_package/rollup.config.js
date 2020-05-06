5a4be2f61ab54b7f7c07e16c52f82b547b0ec992
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 01:11:20 +0000

    da79e0433f test(language-service): [ivy] Add mock service to overwrite files (#36923)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.5+37.sha-1b8752e/, version);
+      banner = banner.replace(/10.0.0-next.5+38.sha-da79e04/, version);
     }
   }
 }
