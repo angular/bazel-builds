f2cbbe572fa5b2def36e9cf6782c6152454f413f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 01:06:43 +0000

    0be3792b5c refactor(docs-infra): take advantage of latest Jasmine features in preview server tests (#36837)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.4+43.sha-9e755f3/, version);
+      banner = banner.replace(/9.1.4+55.sha-0be3792/, version);
     }
   }
 }
