5917daac2bd1828ea26b6e0a521057aefd1ba2b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 17:53:01 +0000

    dcf1dcb757 test(compiler-cli): ensure reflection tests are not brittle to case-sensitivity (#36968)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.4+57.sha-9d13ee0/, version);
+      banner = banner.replace(/9.1.4+67.sha-dcf1dcb/, version);
     }
   }
 }
