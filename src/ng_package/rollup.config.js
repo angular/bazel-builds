3535e81bc23d5f4f72357917fc92f452a556f9bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:08:18 +0000

    42d1091d6a fix(compiler-cli): don't try to tag non-ts files as shims (#36987)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.5+73.sha-d0280a0/, version);
+      banner = banner.replace(/10.0.0-next.5+74.sha-42d1091/, version);
     }
   }
 }
