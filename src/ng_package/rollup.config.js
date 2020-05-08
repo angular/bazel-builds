9c82c2e3b3cb74db58aa00ba5f2014e4620bb05e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:02:06 +0000

    fdd61152c5 docs: replace into with in (#36778)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.6+3.sha-7cc332ef/, version);
+      banner = banner.replace(/10.0.0-next.6+4.sha-fdd6115/, version);
     }
   }
 }
