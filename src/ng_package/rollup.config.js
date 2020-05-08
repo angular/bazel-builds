43983239c715712fbfbd237c5071aa0c23cda8ef
Misko Hevery <misko@hevery.com>
2020-05-08 22:03:17 +0000

    1142c378fd feat(language-service): [ivy] wrap ngtsc to handle typecheck files (#36930)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.6+14.sha-82a3bd5/, version);
+      banner = banner.replace(/10.0.0-next.6+15.sha-1142c37/, version);
     }
   }
 }
