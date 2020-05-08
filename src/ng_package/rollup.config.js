00e1773a5ea4ee880a80204eb7aba98850629f08
Misko Hevery <misko@hevery.com>
2020-05-08 22:06:07 +0000

    ed1b4a8f19 docs: add migration guides to sidenav in updating to Version 9 (#34979)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.6+17.sha-bd30c37/, version);
+      banner = banner.replace(/10.0.0-next.6+18.sha-ed1b4a8/, version);
     }
   }
 }
