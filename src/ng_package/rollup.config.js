5675cdfab64959b9fcb4c5cc5fa5e551b89cd0b9
Misko Hevery <misko@hevery.com>
2020-05-11 18:28:03 +0000

    40025f55ad refactor(language-service): move TS utils to separate file (#36984)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.6+23.sha-286fbf4/, version);
+      banner = banner.replace(/10.0.0-next.6+22.sha-40025f5/, version);
     }
   }
 }
