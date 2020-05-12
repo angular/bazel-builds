2b9d2bc988b82c2b482849b2f043b9d8f16e3394
Misko Hevery <misko@hevery.com>
2020-05-12 22:00:39 +0000

    4f3ac1d983 docs(common): fix CLDR website link in FormStyle doc (#37069)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.6+30.sha-7addc9f/, version);
+      banner = banner.replace(/10.0.0-next.6+33.sha-4f3ac1d/, version);
     }
   }
 }
