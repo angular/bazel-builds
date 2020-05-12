1763b66192588c6677f32b380fbdf078e90f81ab
Misko Hevery <misko@hevery.com>
2020-05-12 18:09:49 +0000

    8fe532e593 build(dev-infra): cleanup package dependencies for shared package (#36980)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.6+25.sha-8d8e419/, version);
+      banner = banner.replace(/10.0.0-next.6+27.sha-8fe532e/, version);
     }
   }
 }
