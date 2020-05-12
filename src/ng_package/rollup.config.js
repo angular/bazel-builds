1936ec29fe110e918906833770e57c6e6fc68084
Misko Hevery <misko@hevery.com>
2020-05-12 21:58:12 +0000

    7addc9f1a9 docs: add doc for template type guards to stuctural-directives.md (#34549)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.6+29.sha-cb6ddfc/, version);
+      banner = banner.replace(/10.0.0-next.6+30.sha-7addc9f/, version);
     }
   }
 }
