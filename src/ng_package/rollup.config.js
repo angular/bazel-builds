6abbb60c2e486254c24b94ba0827b62c6faa5b26
Misko Hevery <misko@hevery.com>
2020-05-13 23:12:45 +0000

    141fcb95a4 refactor(localize): use the `FileSystem` from ngtsc (#36843)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.7+2.sha-019835c/, version);
+      banner = banner.replace(/10.0.0-next.7+6.sha-141fcb9/, version);
     }
   }
 }
