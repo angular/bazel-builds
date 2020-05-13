242f3da002cd3b355d22742450afa601b58462fb
Misko Hevery <misko@hevery.com>
2020-05-13 23:26:07 +0000

    ce39755937 docs: Remove duplicate `and` (#37067)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.7+14.sha-2cacab9/, version);
+      banner = banner.replace(/10.0.0-next.7+10.sha-ce39755/, version);
     }
   }
 }
