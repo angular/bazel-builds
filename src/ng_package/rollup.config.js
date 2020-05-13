45fb7d92cc58a0168e8a88151808de33f39a0930
Misko Hevery <misko@hevery.com>
2020-05-13 23:23:04 +0000

    57a868627f test(docs-infra): re-enable upgrade-phonecat-2-hybrid example e2e tests (#36969)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.7+7.sha-45f4a47/, version);
+      banner = banner.replace(/10.0.0-next.7+15.sha-57a8686/, version);
     }
   }
 }
