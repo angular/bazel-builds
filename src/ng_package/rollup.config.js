0a7928ce2262d9fe6fa576e3b1130a19317e1c72
Misko Hevery <misko@hevery.com>
2020-05-13 23:07:39 +0000

    255c2b2ff0 docs: refactor lifecycle hooks doc topic for task orientation (#36353)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.7/, version);
+      banner = banner.replace(/10.0.0-next.7+1.sha-255c2b2/, version);
     }
   }
 }
