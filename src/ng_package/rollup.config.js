362bdf0e6ca14998cd45976b2fd91c618dd0bee5
Kara Erickson <karakara@google.com>
2020-05-14 18:21:15 +0000

    ef9da2f619 refactor(dev-infra): add atscott for fw-core approvers (#37020)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.6+31.sha-92b97f7/, version);
+      banner = banner.replace(/9.1.6+33.sha-ef9da2f/, version);
     }
   }
 }
