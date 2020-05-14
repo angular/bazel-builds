4d3e312f3e459b3d8ea8754dc63cc3a56ffd6c04
Kara Erickson <karakara@google.com>
2020-05-14 20:26:56 +0000

    7acd33007d fix(dev-infra): do not require a commit body for release commits (#37110)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.7+48.sha-8a56c99/, version);
+      banner = banner.replace(/10.0.0-next.7+49.sha-7acd330/, version);
     }
   }
 }
