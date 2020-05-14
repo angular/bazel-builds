60bdfa4a0a2b446d3d2d4f48ceee2b5967870b8f
Kara Erickson <karakara@google.com>
2020-05-14 18:22:00 +0000

    b2a3325258 docs: remove the note about @angular/http from the deprecations guide (#37070)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.7+43.sha-f16ca1c/, version);
+      banner = banner.replace(/10.0.0-next.7+45.sha-b2a3325/, version);
     }
   }
 }
