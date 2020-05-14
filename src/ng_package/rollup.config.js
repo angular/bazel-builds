a7c652d71f612a2215c71740afa1feac3508541f
Kara Erickson <karakara@google.com>
2020-05-14 18:08:15 +0000

    2418c6aca1 refactor(docs-infra): refactors `addImageDimensionsImpl` (#37046)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.7+16.sha-9020507/, version);
+      banner = banner.replace(/10.0.0-next.7+17.sha-2418c6a/, version);
     }
   }
 }
