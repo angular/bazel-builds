54eaf6b56c38b7a4b05ad8bd9fb98b68d3653774
Kara Erickson <karakara@google.com>
2020-05-15 02:39:07 +0000

    d808333047 ci: increase AIO payload size limit (#37124)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.6+37.sha-4de546e/, version);
+      banner = banner.replace(/9.1.6+41.sha-d808333/, version);
     }
   }
 }
