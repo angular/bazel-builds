9cdd4b3c51653dafb4bb5d185b395c64b8f1e8f6
Kara Erickson <karakara@google.com>
2020-05-15 18:06:51 +0000

    9548c7cb94 release: cut the v9.1.7 release (#37116)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.6+47.sha-5ffa4f1/, version);
+      banner = banner.replace(/9.1.6+49.sha-9548c7c/, version);
     }
   }
 }
