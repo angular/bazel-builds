b68eadb7bfe91da010bdff6c76a96933cd1689bb
Kara Erickson <karakara@google.com>
2020-05-18 22:16:18 +0000

    178a78f9ed docs(router): add docs for RouterLink inputs (#37018)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.6+67.sha-ba9e63a/, version);
+      banner = banner.replace(/9.1.6+70.sha-178a78f/, version);
     }
   }
 }
