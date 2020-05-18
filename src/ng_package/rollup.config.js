2e4fcb4b827b3bdf0994c643178c97dc86d5195b
Kara Erickson <karakara@google.com>
2020-05-18 17:48:25 +0000

    593e07bd21 build(docs-infra): upgrade cli command docs sources to dbde51375 (#37154)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.8+7.sha-2ca2c8f/, version);
+      banner = banner.replace(/10.0.0-next.8+9.sha-593e07b/, version);
     }
   }
 }
