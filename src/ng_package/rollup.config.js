b66457b1423855d8d95fb47fc9fcf01e6381a549
Kara Erickson <karakara@google.com>
2020-05-18 17:45:05 +0000

    2ca2c8fe0f build: complete removal of gulp format (#37147)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.8/, version);
+      banner = banner.replace(/10.0.0-next.8+7.sha-2ca2c8f/, version);
     }
   }
 }
