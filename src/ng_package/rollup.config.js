afc8af7571df894c9f6b19f37d7d5a91cc01e11b
Kara Erickson <karakara@google.com>
2020-05-19 22:27:56 +0000

    772c5b8f64 refactor: update to tslib 2.0 and move to direct dependencies (#37198)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.8+28.sha-4e96cdc/, version);
+      banner = banner.replace(/10.0.0-next.8+27.sha-772c5b8/, version);
     }
   }
 }
