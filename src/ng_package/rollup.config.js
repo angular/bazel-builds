e23e63d2b8da11b23565cbcbcc0a7b731f5b5197
Kara Erickson <karakara@google.com>
2020-05-19 17:41:00 +0000

    deac994517 refactor(docs-infra): remove `rev` property from `Resource` interface and data (#37181)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.8+24.sha-b754ab2/, version);
+      banner = banner.replace(/10.0.0-next.8+25.sha-deac994/, version);
     }
   }
 }
