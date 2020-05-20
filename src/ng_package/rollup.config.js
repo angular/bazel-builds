12f4d7cb2b1ccc61e7162df2c3138ad52f93eabb
Kara Erickson <karakara@google.com>
2020-05-20 22:10:48 +0000

    1465372a0e fix(elements): do not break when the constructor of an Angular Element is not called (#36114) (#37226)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.8+12.sha-3eee536/, version);
+      banner = banner.replace(/9.1.8+19.sha-1465372/, version);
     }
   }
 }
