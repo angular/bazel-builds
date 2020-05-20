2521b83d2254544c9fe8506214ad4d7682b556d2
Kara Erickson <karakara@google.com>
2020-05-20 18:01:00 +0000

    78b8d0e1ef docs(changelog): fix broken link in CHANGELOG

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.9+12.sha-aedac71/, version);
+      banner = banner.replace(/10.0.0-next.9+13.sha-78b8d0e/, version);
     }
   }
 }
