c25c543dfd9e726f48a2d6c70fe141a39c17d88a
Kara Erickson <karakara@google.com>
2020-05-20 23:05:52 +0000

    6ca3fb19fd docs: release notes for the v${releaseVersion} release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-next.9+21.sha-267f844/, version);
+      banner = banner.replace(/10.0.0-next.9+22.sha-6ca3fb1/, version);
     }
   }
 }
