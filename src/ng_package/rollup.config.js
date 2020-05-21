208042bd062b4c505a85f18c3d7f587592bd94e5
Kara Erickson <karakara@google.com>
2020-05-21 17:57:03 +0000

    cdb5d07606 feat(dev-infra): expose script for determining merge branches (#37217)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0/, version);
+      banner = banner.replace(/10.0.0-rc.0+2.sha-cdb5d07/, version);
     }
   }
 }
