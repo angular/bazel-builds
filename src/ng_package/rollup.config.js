fc447a108d6cff64ca596eb460ac962252f33d54
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:58:54 +0000

    540c29cd6b feat(dev-infra): migrate release tool to use new logging system (#37232)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+3.sha-93be1f1/, version);
+      banner = banner.replace(/10.0.0-rc.0+14.sha-540c29c/, version);
     }
   }
 }
