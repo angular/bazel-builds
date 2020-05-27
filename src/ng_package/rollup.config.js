866fb904b936fdee6ced2f15ef297de2c7b38dcd
Matias Niemelä <matias@yearofmoo.com>
2020-05-27 21:45:46 +0000

    64fc8b8ee5 docs: add IE mobile to deprecated browsers (#37313)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+19.sha-a62c1c4/, version);
+      banner = banner.replace(/10.0.0-rc.0+20.sha-64fc8b8/, version);
     }
   }
 }
