49593ed893e253ec87df305cfe8c283f45f5aa88
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 18:43:31 +0000

    8bb741198b docs: release notes for the v10.0.0-rc.2 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+35.sha-4d0e175/, version);
+      banner = banner.replace(/10.0.0-rc.0+37.sha-8bb7411/, version);
     }
   }
 }
