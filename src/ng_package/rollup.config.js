52297aa43f0fd84b0cbcf7dec8b4fbb167883c4e
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 18:16:33 +0000

    1b55da10b1 build: fix integration payload sizes

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+39.sha-c3651ce/, version);
+      banner = banner.replace(/10.0.0-rc.0+44.sha-1b55da1/, version);
     }
   }
 }
