c41ea4ad08cffed638c3192560578cb0e3e7cc7f
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:50:22 +0000

    561c0f81a0 perf(ngcc): allow immediately reporting a stale lock file (#37250)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+44.sha-1b55da1/, version);
+      banner = banner.replace(/10.0.0-rc.0+46.sha-561c0f8/, version);
     }
   }
 }
