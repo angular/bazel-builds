56c8b54168c5da16a45bf14f5049390c2582d2c3
atscott <atscott@google.com>
2020-06-03 23:16:22 +0000

    2c1f35e794 fix(language-service): Recover from error in analyzing Ng Modules (#37108)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.2+22.sha-e35269d/, version);
+      banner = banner.replace(/10.0.0-rc.2+24.sha-2c1f35e/, version);
     }
   }
 }
