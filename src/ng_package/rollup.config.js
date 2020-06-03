9ae9a42e10d6cc0a3483e5ecc2524d189c29d0e4
atscott <atscott@google.com>
2020-06-03 20:37:12 +0000

    40f3bb5638 refactor(dev-infra): small changes and fixes (#36800)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+50.sha-06a8ad2/, version);
+      banner = banner.replace(/10.0.0-rc.0+54.sha-40f3bb5/, version);
     }
   }
 }
