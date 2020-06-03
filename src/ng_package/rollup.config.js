05e3528f8dd36225beee18873cf09a5aa95f52d4
Igor Minar <igor@angular.io>
2020-06-03 00:55:09 +0000

    6855396449 docs: update aio in support for #BlackLivesMatter

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.9+13.sha-0b96908/, version);
+      banner = banner.replace(/9.1.9+14.sha-6855396/, version);
     }
   }
 }
