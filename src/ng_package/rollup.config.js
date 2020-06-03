83913528e94ee905e88a8e451ad41b5af78bde23
atscott <atscott@google.com>
2020-06-03 19:55:06 +0000

    c7c0c1f626 refactor(forms): use a type guard to get rid of casts (#32541)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.2+12.sha-2d86dbb/, version);
+      banner = banner.replace(/10.0.0-rc.2+14.sha-c7c0c1f/, version);
     }
   }
 }
