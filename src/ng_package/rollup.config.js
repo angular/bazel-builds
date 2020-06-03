23b198c7ca6e0b2b8bea44a69a747ca38f7e07bd
atscott <atscott@google.com>
2020-06-03 22:49:16 +0000

    e35269dd87 docs: update file header to be correct (#37425)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.2+21.sha-60a03b7/, version);
+      banner = banner.replace(/10.0.0-rc.2+22.sha-e35269d/, version);
     }
   }
 }
