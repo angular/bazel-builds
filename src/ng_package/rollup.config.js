92d9b48ecaf29a5f7d326adfa053c00cf888861c
atscott <atscott@google.com>
2020-06-04 17:01:32 +0000

    c025357fb8 feat(dev-infra): Add oauth scope check to ensure necessary permissions for merge tooling (#37421)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+67.sha-57411c8/, version);
+      banner = banner.replace(/10.0.0-rc.0+68.sha-c025357/, version);
     }
   }
 }
