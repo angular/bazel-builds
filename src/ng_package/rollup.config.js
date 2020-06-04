d43c718a65440dd4232a2fbf62f98fcc7e311232
atscott <atscott@google.com>
2020-06-04 18:07:21 +0000

    b2bd38699b fix(dev-infra): properly determine oauth scopes for git client token (#37439)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+70.sha-c8f7fc2/, version);
+      banner = banner.replace(/10.0.0-rc.0+69.sha-b2bd386/, version);
     }
   }
 }
