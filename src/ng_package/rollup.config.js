9e256d31335f8cb40a847584a08dc07dab6f4af7
atscott <atscott@google.com>
2020-06-04 18:03:49 +0000

    9ec25ea036 refactor(dev-infra): change required base commit sha (#37424)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.2+43.sha-5f0be3c/, version);
+      banner = banner.replace(/10.0.0-rc.2+45.sha-9ec25ea/, version);
     }
   }
 }
