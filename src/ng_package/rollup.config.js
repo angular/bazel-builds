6049ae1c044edd68e4ba9f88c88b842d6c0be3fc
atscott <atscott@google.com>
2020-06-05 17:06:10 +0000

    53e1fb3554 refactor(dev-infra): move GitClient to common util (#37318)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.2+53.sha-2cb3b66/, version);
+      banner = banner.replace(/10.0.0-rc.2+54.sha-53e1fb3/, version);
     }
   }
 }
