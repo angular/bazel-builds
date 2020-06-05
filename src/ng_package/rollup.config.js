7a26e1ada2bc671cd264dea66617261fd3bdfeb7
atscott <atscott@google.com>
2020-06-05 16:40:15 +0000

    e31208beb1 docs: fix minor error in the "Structural directives" guide (#37452)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+75.sha-3569fdf/, version);
+      banner = banner.replace(/10.0.0-rc.0+76.sha-e31208b/, version);
     }
   }
 }
