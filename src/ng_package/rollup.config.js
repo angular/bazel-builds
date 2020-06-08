370c43d74d6b3f6c812db861d78ed9183bf668cf
atscott <atscott@google.com>
2020-06-08 19:04:00 +0000

    0cb0f66c17 docs: place download section in toh to the top (#36567)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+93.sha-7793440/, version);
+      banner = banner.replace(/10.0.0-rc.0+95.sha-0cb0f66/, version);
     }
   }
 }
