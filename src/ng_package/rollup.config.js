5b198c600a163bb7cbcf0f4b76ce7a80c7535236
atscott <atscott@google.com>
2020-06-08 17:45:42 +0000

    650974e1b3 test(language-service): Remove all markers from test project (#37475)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+91.sha-ed3c549/, version);
+      banner = banner.replace(/10.0.0-rc.0+92.sha-650974e/, version);
     }
   }
 }
