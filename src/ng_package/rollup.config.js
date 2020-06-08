82c9df0ad5503e1a939e9cf881eceabaf866129d
atscott <atscott@google.com>
2020-06-08 16:51:50 +0000

    31b3888a2f style(ngcc): post-merge review tidy up (#37461)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.2+59.sha-75294e7/, version);
+      banner = banner.replace(/10.0.0-rc.2+64.sha-31b3888/, version);
     }
   }
 }
