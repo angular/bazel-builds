ab7d4a565551652d1b5dac880bc434fc42960298
atscott <atscott@google.com>
2020-06-08 22:04:13 +0000

    713fb3b587 docs(docs-infra): fix small typo (#37258)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+94.sha-db11a0d/, version);
+      banner = banner.replace(/10.0.0-rc.0+96.sha-713fb3b/, version);
     }
   }
 }
