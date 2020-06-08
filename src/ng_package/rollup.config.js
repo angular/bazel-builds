13ae6e6bdd40652d53e59a4156238c5486b21d97
atscott <atscott@google.com>
2020-06-08 18:41:32 +0000

    fdbe9f5d9f refactor(core): assert TNode is not a container when setting attribute on element (#37111)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.2+66.sha-8bead6b/, version);
+      banner = banner.replace(/10.0.0-rc.2+67.sha-fdbe9f5/, version);
     }
   }
 }
