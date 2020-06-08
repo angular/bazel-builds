e1f15d2f8bd8fd0d0b973ced75714306dbf99af0
atscott <atscott@google.com>
2020-06-08 16:38:58 +0000

    0c75a06b1e build: upgrade to bazel 3.2.0 and rules_nodejs 1.7.0 (#37358)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+84.sha-aaa2009/, version);
+      banner = banner.replace(/10.0.0-rc.0+85.sha-0c75a06/, version);
     }
   }
 }
