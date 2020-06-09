59f180b45a0b118ff1222db7e685d04e32f735c4
atscott <atscott@google.com>
2020-06-09 22:49:46 +0000

    35502c739c docs: release notes for the v9.1.10 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+111.sha-b2ccc34/, version);
+      banner = banner.replace(/10.0.0-rc.0+112.sha-35502c7/, version);
     }
   }
 }
