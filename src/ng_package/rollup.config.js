ec8d653747ef46044cc3700b4e7a7158961ff243
atscott <atscott@google.com>
2020-06-09 15:52:38 +0000

    f6ee911cbb docs(core): fix path referenced in comments of both compiler facade interface files (#37370)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+103.sha-7edb026/, version);
+      banner = banner.replace(/10.0.0-rc.0+108.sha-f6ee911/, version);
     }
   }
 }
