101c061032eeba0b466b7420e8ff9100f2594759
atscott <atscott@google.com>
2020-06-09 20:58:46 +0000

    b2ccc34f9c perf(core): avoid pulling in jit-specific code in aot bundles (#37372)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+110.sha-eaa38a5/, version);
+      banner = banner.replace(/10.0.0-rc.0+111.sha-b2ccc34/, version);
     }
   }
 }
