47837309db57aeb2f4c07a79f98bf6e713572287
atscott <atscott@google.com>
2020-06-09 00:44:31 +0000

    55979fe0ae feat(language-service): TS references from template items (#37437)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+100.sha-6280cf9/, version);
+      banner = banner.replace(/10.0.0-rc.0+101.sha-55979fe/, version);
     }
   }
 }
