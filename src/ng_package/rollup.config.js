f88ec548bdfa4cc9aa33e8a455964e9f0067d5c4
atscott <atscott@google.com>
2020-06-09 20:53:53 +0000

    d494f7bd5e docs(dev-infra): add comment about what the requiredBaseCommit is (#37509)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.3+11.sha-ec6a7ab/, version);
+      banner = banner.replace(/10.0.0-rc.3+12.sha-d494f7b/, version);
     }
   }
 }
