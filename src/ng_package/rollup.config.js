5f1d0cafa000d3fa17a27c307e3162e3e08cd55f
atscott <atscott@google.com>
2020-06-09 18:42:18 +0000

    ec6a7ab721 docs: wrong links in lifecycle hooks api documentaion (#36557)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.3+10.sha-ad6d2b4/, version);
+      banner = banner.replace(/10.0.0-rc.3+11.sha-ec6a7ab/, version);
     }
   }
 }
