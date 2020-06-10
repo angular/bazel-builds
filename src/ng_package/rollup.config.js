fe010d9d8e82d55333b36fcdd6741e0f3b0a875b
atscott <atscott@google.com>
2020-06-10 20:20:58 +0000

    20346ff192 build: remove ngcontainer Docker (#36421)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+123.sha-bd9c1e6/, version);
+      banner = banner.replace(/10.0.0-rc.0+124.sha-20346ff/, version);
     }
   }
 }
