d8f3e7434cf26ccd01d528cf07216be9af52d00f
atscott <atscott@google.com>
2020-06-10 19:16:25 +0000

    e836eaf448 fix(docs-infra): preconnect to analytics origins (#37400)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+121.sha-a398d6a/, version);
+      banner = banner.replace(/10.0.0-rc.0+120.sha-e836eaf/, version);
     }
   }
 }
