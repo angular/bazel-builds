2e73f19b58e0ec881225544077bedbb458371a14
atscott <atscott01@gmail.com>
2020-06-10 17:50:48 +0000

    7b005bb9cf Revert "fix(elements): fire custom element output events during component initialization (#36161)" (#37524)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+114.sha-401ef71/, version);
+      banner = banner.replace(/10.0.0-rc.0+115.sha-7b005bb/, version);
     }
   }
 }
