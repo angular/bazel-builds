a02dc06fb316defb2a04d0e97400f866ef23d47b
atscott <atscott@google.com>
2020-06-10 19:14:09 +0000

    ae364864f6 fix(ngcc): do not scan import expressions in d.ts files (#37503)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+122.sha-d76a872/, version);
+      banner = banner.replace(/10.0.0-rc.0+119.sha-ae36486/, version);
     }
   }
 }
