63602d69c61037ba25cfbd7bde3771f0f8d93678
Misko Hevery <misko@hevery.com>
2020-06-11 18:25:11 +0000

    39f973430a docs: remove out of date GDEs (#36467)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+124.sha-20346ff/, version);
+      banner = banner.replace(/10.0.0-rc.0+125.sha-39f9734/, version);
     }
   }
 }
