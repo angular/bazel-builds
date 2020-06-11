e32d069ff26e76606dab3d2c62607ba143d23b79
Misko Hevery <misko@hevery.com>
2020-06-11 18:37:32 +0000

    8c682c52b1 fix(ngcc): use annotateForClosureCompiler option (#36652)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+129.sha-e5b09cc/, version);
+      banner = banner.replace(/10.0.0-rc.0+130.sha-8c682c5/, version);
     }
   }
 }
