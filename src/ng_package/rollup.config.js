8d8ef04354485ba776e97627f2039e89644a2e90
Misko Hevery <misko@hevery.com>
2020-06-11 18:30:14 +0000

    b199ef6bfc feat(zone.js): move all zone optional bundles to plugins folders (#36540)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+126.sha-1fce6d6/, version);
+      banner = banner.replace(/10.0.0-rc.0+128.sha-b199ef6/, version);
     }
   }
 }
