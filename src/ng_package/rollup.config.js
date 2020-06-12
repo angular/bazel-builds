227f7f76596855d01242c9509156b733afb8f634
Misko Hevery <misko@hevery.com>
2020-06-12 22:34:50 +0000

    1091ddbb8e build(bazel): check for renderer flag in `ng_module()` (#37529)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+168.sha-62e4acc/, version);
+      banner = banner.replace(/10.0.0-rc.0+167.sha-1091ddb/, version);
     }
   }
 }
