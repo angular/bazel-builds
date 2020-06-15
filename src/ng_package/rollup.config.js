cc82e7cf02affd7d00fe0e1dc21ec2cd85513a59
Misko Hevery <misko@hevery.com>
2020-06-15 21:44:52 +0000

    a7359d494a docs: release notes for the v10.0.0-rc.6 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+180.sha-66e6b93/, version);
+      banner = banner.replace(/10.0.0-rc.0+181.sha-a7359d4/, version);
     }
   }
 }
