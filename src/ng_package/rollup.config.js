a738e7b74de9e3ce0b339a77cacde6f35c43cfa8
Misko Hevery <misko@hevery.com>
2020-06-15 16:56:52 +0000

    7ac166ff08 feat(bazel): expose explicit mapping from closure to devmode files (#36262)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+170.sha-ae4f81c/, version);
+      banner = banner.replace(/10.0.0-rc.0+171.sha-7ac166f/, version);
     }
   }
 }
