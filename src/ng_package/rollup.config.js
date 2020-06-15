00e807f7ac551c0ddbaa3e3b790a3b4dabd3752b
Misko Hevery <misko@hevery.com>
2020-06-15 16:26:34 +0000

    c1fdfd4f14 docs: update CONTRIBUTING.md with info about major docs changes (#35238)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+167.sha-1091ddb/, version);
+      banner = banner.replace(/10.0.0-rc.0+169.sha-c1fdfd4/, version);
     }
   }
 }
