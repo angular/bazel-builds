7de9260384f6789a84ff22ad89170eaba0f03a79
Misko Hevery <misko@hevery.com>
2020-06-15 16:59:24 +0000

    d330cd85ac refactor: move `hover` into a proper `@media` hover (#37320)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+172.sha-09d6dc5/, version);
+      banner = banner.replace(/10.0.0-rc.0+173.sha-d330cd8/, version);
     }
   }
 }
