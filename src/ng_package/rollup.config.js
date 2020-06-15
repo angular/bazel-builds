5216a40b46b28c94ec329b2ef0bca35147f41d8d
Misko Hevery <misko@hevery.com>
2020-06-15 21:53:39 +0000

    2a145f2463 perf(forms): optimize internal method _anyControls in FormGroup (#32534)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+184.sha-e0dfa42/, version);
+      banner = banner.replace(/10.0.0-rc.0+185.sha-2a145f2/, version);
     }
   }
 }
