c9ee05d4b2981283be869bf8ae62d925b313a556
Misko Hevery <misko@hevery.com>
2020-06-15 16:55:11 +0000

    09d6dc50ef docs: Update documentation to reflect addition of `tsconfig.base.json`. (#37222)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+171.sha-7ac166f/, version);
+      banner = banner.replace(/10.0.0-rc.0+172.sha-09d6dc5/, version);
     }
   }
 }
