3fb326b03c5198c76a856d8ff042ad8a2ab185eb
Misko Hevery <misko@hevery.com>
2020-06-16 17:04:38 +0000

    1185aa07a3 docs: wrong example in routerLink (#37590)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+186.sha-ceca5dd/, version);
+      banner = banner.replace(/10.0.0-rc.0+187.sha-1185aa0/, version);
     }
   }
 }
