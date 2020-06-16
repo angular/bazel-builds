9e84630af994c15a5dbcc314111f9060a4573131
Misko Hevery <misko@hevery.com>
2020-06-16 19:20:32 +0000

    8154bbd538 feat(dev-infra): support for caretaker note label in merge script (#37595)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+187.sha-1185aa0/, version);
+      banner = banner.replace(/10.0.0-rc.0+191.sha-8154bbd/, version);
     }
   }
 }
