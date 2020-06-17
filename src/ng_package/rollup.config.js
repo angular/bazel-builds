b50165cf0c22bf22638732ec96b2184ac7a8b56b
Misko Hevery <misko@hevery.com>
2020-06-17 18:42:51 +0000

    0d29259d9b docs: move ng-vikings 2020 to the already presented section (#37466)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.6+7.sha-26b0f3d/, version);
+      banner = banner.replace(/10.0.0-rc.6+8.sha-0d29259/, version);
     }
   }
 }
