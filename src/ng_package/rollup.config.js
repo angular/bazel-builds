4d59afae300d92a54dd89be3aa685e5d575a054e
Misko Hevery <misko@hevery.com>
2020-06-18 23:28:51 +0000

    1d844b9bd8 docs(core): correct type for opts.read (#37626)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+195.sha-53e95233/, version);
+      banner = banner.replace(/10.0.0-rc.0+196.sha-1d844b9/, version);
     }
   }
 }
