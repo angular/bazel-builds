ef6a70e4fb77cbd686303ed9eb8a6be4930337bc
Misko Hevery <misko@hevery.com>
2020-06-18 23:23:51 +0000

    bd7f440357 perf(ngcc): shortcircuit tokenizing in ESM dependency host (#37639)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+193.sha-71f008f/, version);
+      banner = banner.replace(/10.0.0-rc.0+198.sha-bd7f440/, version);
     }
   }
 }
