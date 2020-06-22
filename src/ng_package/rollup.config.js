1af39e7a3c9edbb019a1075301abbb55b2d83e5b
Misko Hevery <misko@hevery.com>
2020-06-22 18:11:48 +0000

    2a970a0af8 Revert "fix(router): fix navigation ignoring logic to compare to the browser url (#37408)" (#37650)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+196.sha-1d844b9/, version);
+      banner = banner.replace(/10.0.0-rc.0+199.sha-2a970a0/, version);
     }
   }
 }
