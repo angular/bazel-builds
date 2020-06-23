5df41046aa21e3759921307d11c1c8005ee9ef48
Misko Hevery <misko@hevery.com>
2020-06-23 20:33:40 +0000

    36a00a255b ci(docs-infra): disable flaky tests (#37673)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.6+17.sha-82c8b44/, version);
+      banner = banner.replace(/10.0.0-rc.6+20.sha-36a00a2/, version);
     }
   }
 }
