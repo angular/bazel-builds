c4f48d4867b8260e9d4791cb68244ad7e22bb83b
Misko Hevery <misko@hevery.com>
2020-06-23 20:06:45 +0000

    e03adaf9b0 docs: add module/target compiler option migration docs (#37429)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+221.sha-38dfbc7/, version);
+      banner = banner.replace(/10.0.0-rc.0+222.sha-e03adaf/, version);
     }
   }
 }
