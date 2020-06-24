eb9eadaf876366c9bf6f707e7390ec7f00c93dbf
Misko Hevery <misko@hevery.com>
2020-06-24 23:22:39 +0000

    96891a076f release: sort the v10.0.0 release in `CHANGELOG.md`

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.6+23.sha-c49507b/, version);
+      banner = banner.replace(/10.0.0+2.sha-96891a0/, version);
     }
   }
 }
