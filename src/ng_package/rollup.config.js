ebd3370769d5b5ee158e3d01741467f57883f9d2
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:57:04 +0000

    64f2ffa166 fix(core): cleanup DOM elements when root view is removed (#37600)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0+17.sha-2cbe53a/, version);
+      banner = banner.replace(/10.0.0+20.sha-64f2ffa/, version);
     }
   }
 }
