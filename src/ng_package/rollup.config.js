ada3f96489d8780764b71442ccd24c98a46760f9
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:21:30 +0000

    2170ea270a fix(router): fix error when calling ParamMap.get function (#31599)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+239.sha-80d0067/, version);
+      banner = banner.replace(/10.0.0-rc.0+240.sha-2170ea2/, version);
     }
   }
 }
