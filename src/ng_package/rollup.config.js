3196dafb784c63eb8bc96469d8136ed67d76fc17
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:20:55 +0000

    80d0067048 fix(router): `RouterLinkActive` should run CD when setting `isActive` (#21411)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+238.sha-5218916/, version);
+      banner = banner.replace(/10.0.0-rc.0+239.sha-80d0067/, version);
     }
   }
 }
