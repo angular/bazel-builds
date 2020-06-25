aa87fe30382ecb811404d342a7293826574adf85
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:12:39 +0000

    1c1eb3045c docs: Add support schedule for v10 (#37745)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+235.sha-666e6d1/, version);
+      banner = banner.replace(/10.0.0-rc.0+237.sha-1c1eb30/, version);
     }
   }
 }
