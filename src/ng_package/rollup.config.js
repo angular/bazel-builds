cfa2797c62b5bd7574f5f5887ff695e065252ba7
Andrew Kushnir <akushnir@google.com>
2020-06-25 23:04:10 +0000

    8572e885b4 test(compiler-cli): fix assertion of diagnostic filename on Windows (#37758)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+263.sha-0e3e9cd/, version);
+      banner = banner.replace(/10.0.0-rc.0+264.sha-8572e88/, version);
     }
   }
 }
