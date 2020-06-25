76d2181a10227f0a0d51252f4fff4fc330ea7a3a
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:05:01 +0000

    666e6d15fa docs: Replace $emdash; with an actual em dash (#37723)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+234.sha-f3ec37e/, version);
+      banner = banner.replace(/10.0.0-rc.0+235.sha-666e6d1/, version);
     }
   }
 }
