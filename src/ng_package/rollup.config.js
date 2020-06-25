d0584eab1038282e1fd90c5fb8bc22d628a236fb
Andrew Kushnir <akushnir@google.com>
2020-06-25 22:44:06 +0000

    a9a4edebe2 fix(docs-infra): fix typo in the deploy-to-firebase.sh script (#37754)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0+20.sha-64f2ffa/, version);
+      banner = banner.replace(/10.0.0+21.sha-a9a4ede/, version);
     }
   }
 }
