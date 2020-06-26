efa5a756780aa5a2f445509511187bbf950a46da
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:27:02 +0000

    b2816a1536 docs: typo fixes for schematics-for-libraries.md (#37753)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+264.sha-8572e88/, version);
+      banner = banner.replace(/10.0.0-rc.0+267.sha-b2816a1/, version);
     }
   }
 }
