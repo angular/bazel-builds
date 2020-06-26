b80915484a4028c90dcf2dfe58c830a1da826ec3
Igor Minar <igor@angular.io>
2020-06-26 01:40:45 +0000

    a86a41210f fix(docs-infra): fix deploy-to-firebase.sh for master and v10.0.x branches (#37762)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.11+4.sha-20c44a6/, version);
+      banner = banner.replace(/9.1.11+5.sha-a86a412/, version);
     }
   }
 }
