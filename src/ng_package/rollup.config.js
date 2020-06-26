72f50a224cc8480089ab5fa1b45b9513301f4889
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:50:59 +0000

    ad6680f602 fix(language-service): reinstate getExternalFiles() (#37750)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0+25.sha-67cfc4c/, version);
+      banner = banner.replace(/10.0.0+29.sha-ad6680f/, version);
     }
   }
 }
