6b01734c02ba2af89f3c76bfab68e1f7772357f6
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:39:12 +0000

    acf3cff9ee ci: exclude "docs" commit type from minBodyLength commit message validation (#37764)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+276.sha-e0eeb4a/, version);
+      banner = banner.replace(/10.0.0-rc.0+281.sha-acf3cff/, version);
     }
   }
 }
