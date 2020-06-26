ec6a25b08e081f7ff754edba99db09afa4e53d00
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:40:31 +0000

    0879d2e85d refactor(core): throw more descriptive error message in case of invalid host element (#35916)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+281.sha-acf3cff/, version);
+      banner = banner.replace(/10.0.0-rc.0+278.sha-0879d2e/, version);
     }
   }
 }
