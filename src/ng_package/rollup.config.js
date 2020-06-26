2e4ffe6c7359e19d6962c1dd0e36afe68d734b5f
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:34:35 +0000

    67cfc4c9bc build: add wombot proxy for publish config for @angular/benchpress (#37752)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0+21.sha-a9a4ede/, version);
+      banner = banner.replace(/10.0.0+25.sha-67cfc4c/, version);
     }
   }
 }
