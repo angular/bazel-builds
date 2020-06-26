3884b0db4decace7769a394d99a6b1b0cc886540
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:25:56 +0000

    4c7f32f28c fix(docs-infra): fix deploy-to-firebase.sh for master and v10.0.x branches (#37762)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+266.sha-2cbc429/, version);
+      banner = banner.replace(/10.0.0-rc.0+265.sha-4c7f32f/, version);
     }
   }
 }
