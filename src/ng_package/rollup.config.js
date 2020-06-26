074a75a2d30f1249a4e61d7e260cfa2284e12d9a
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:18:00 +0000

    f954ab6f10 docs: add note about the month being zero-based in the Date constructor (#37770)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+272.sha-3ee6665/, version);
+      banner = banner.replace(/10.0.0-rc.0+273.sha-f954ab6/, version);
     }
   }
 }
