35e103d7492422e4addd6772b1d76b1fbe8f484c
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:21:09 +0000

    873330b82c docs: correct the spelling mistake in observables error handling code (#36437)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+300.sha-435a28e/, version);
+      banner = banner.replace(/10.0.0-rc.0+302.sha-873330b/, version);
     }
   }
 }
