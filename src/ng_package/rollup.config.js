e1bd12f09515dfd5ba1283a0fc0d98037da248a4
Andrew Kushnir <akushnir@google.com>
2020-06-29 17:59:47 +0000

    89e16ed6a5 fix(elements): fire custom element output events during component initialization (#37570)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.1+4.sha-78460c1/, version);
+      banner = banner.replace(/10.0.1+10.sha-89e16ed/, version);
     }
   }
 }
