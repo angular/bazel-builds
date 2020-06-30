091c36ca922188b67a2ed509991bf670e83faab4
Andrew Kushnir <akushnir@google.com>
2020-06-30 00:35:03 +0000

    71956250dd perf(compiler-cli): fix memory leak in retained incremental state (#37835)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+301.sha-a177b1b/, version);
+      banner = banner.replace(/10.0.0-rc.0+304.sha-7195625/, version);
     }
   }
 }
