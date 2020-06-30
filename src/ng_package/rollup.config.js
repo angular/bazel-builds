9c8d0b5f705ea8e60c8d114431ef389483c0e3f7
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:33:29 +0000

    11dd2bef2d docs: add missing single quote (#37854)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+307.sha-6fc8069/, version);
+      banner = banner.replace(/10.0.0-rc.0+308.sha-11dd2be/, version);
     }
   }
 }
