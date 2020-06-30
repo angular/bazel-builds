407a7725f6fdaebe250a862e2af8ac4c540f7d79
Andrew Kushnir <akushnir@google.com>
2020-06-30 18:09:51 +0000

    f9f2ba6faf docs: add Sonu Kapoor to the collaborator list (#37777)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.1+21.sha-57a518a/, version);
+      banner = banner.replace(/10.0.1+23.sha-f9f2ba6/, version);
     }
   }
 }
