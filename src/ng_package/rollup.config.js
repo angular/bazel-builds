10e539d6412ee789d56564d6b33ac2aa1fd23615
Andrew Kushnir <akushnir@google.com>
2020-07-01 16:50:30 +0000

    1d5ca55aab docs(changelog): remove empty sections (#36264)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+309.sha-9206a26/, version);
+      banner = banner.replace(/10.0.0-rc.0+311.sha-1d5ca55/, version);
     }
   }
 }
