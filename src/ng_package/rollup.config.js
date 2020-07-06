e97b58b101607f21ecb301c41c0be5feb6541f58
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:17:41 +0000

    d148fdccf2 fix(dev-infra): fix typo in ng-dev config (#37862)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+323.sha-7985098/, version);
+      banner = banner.replace(/10.0.0-rc.0+317.sha-d148fdc/, version);
     }
   }
 }
