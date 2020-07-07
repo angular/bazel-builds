5eeb9e1d2be660d9974189d57f39013aef97124b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:42:06 +0000

    f296fea112 docs: minor fixes to docs related to updating to v10 (#37897)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.2+9.sha-9d54b3a/, version);
+      banner = banner.replace(/10.0.2+11.sha-f296fea/, version);
     }
   }
 }
