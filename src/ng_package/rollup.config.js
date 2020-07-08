43dbc7d78f79d9834e1199be4a76990e29178ba7
atscott <atscott@google.com>
2020-07-08 23:32:52 +0000

    e50c7dcc4c docs(router): get rid of unnecessary line in wildcard route example and fixing wildcard docregions (#37127)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+3.sha-bdbbff6/, version);
+      banner = banner.replace(/10.1.0-next.0+10.sha-e50c7dc/, version);
     }
   }
 }
