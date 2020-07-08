3d7a3f99df964d869140ab9eea434934b971e939
atscott <atscott@google.com>
2020-07-08 23:00:50 +0000

    879b2273c1 ci: decrease the minBodyLength commit message limit to 20 chars (#37949)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -139,7 +139,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.3/, version);
+      banner = banner.replace(/10.0.3+1.sha-879b227/, version);
     }
   }
 }
