b75d6805b5f97f8995a716b7dc30c530c08c93af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 19:28:40 +0000

    82a2207f4a build: filter out duplicate cherry-picked commits in changelog (#37956)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.0-rc.0+325.sha-289a96b/, version);
+      banner = banner.replace(/10.0.0-rc.0+326.sha-82a2207/, version);
     }
   }
 }
