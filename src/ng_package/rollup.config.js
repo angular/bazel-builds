3b80aa8c210fc68f52273261bb14a0dd27e2c5e1
atscott <atscott01@gmail.com>
2020-07-09 22:57:15 +0000

    1550663b9e fix(bazel): ng_module rule does not expose flat module information in Ivy (#36971)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+18.sha-c40cbec/, version);
+      banner = banner.replace(/10.1.0-next.0+19.sha-1550663/, version);
     }
   }
 }
