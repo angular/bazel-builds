76208afdf08a926466e59929b3dfa3573755b212
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:18:32 +0000

    9bd4b74b06 feat(dev-infra): commit message validation should skip lines consisting of URLs (#37890)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+20.sha-4b4b745/, version);
+      banner = banner.replace(/10.1.0-next.0+22.sha-9bd4b74/, version);
     }
   }
 }
