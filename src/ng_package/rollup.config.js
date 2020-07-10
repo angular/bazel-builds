15c7cf4598299ebae8464ed2ab9cc96815f05499
Andrew Scott <atscott01@gmail.com>
2020-07-10 22:28:06 +0000

    88d68d2d21 docs: Move router tutorial (toh) from router.md to new file (#37979)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+24.sha-4783a07/, version);
+      banner = banner.replace(/10.1.0-next.0+25.sha-88d68d2/, version);
     }
   }
 }
