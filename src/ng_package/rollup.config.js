6d8ddbc545723ea44f37343b13d38d95aaffd175
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:52:49 +0000

    4783a071f3 docs(router): fix routerLink docs (#37997) The current content for the routerLink commands input does not make it to aio. fixes #35414

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+23.sha-9322b9a/, version);
+      banner = banner.replace(/10.1.0-next.0+24.sha-4783a07/, version);
     }
   }
 }
