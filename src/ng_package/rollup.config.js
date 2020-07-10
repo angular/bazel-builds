6634489515eaf3ad4b0ae220ed085a9453d14f97
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:23:45 +0000

    a94383f168 fix(compiler): check more cases for pipe usage inside host bindings (#37883)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.3+33.sha-c776825/, version);
+      banner = banner.replace(/10.0.3+36.sha-a94383f/, version);
     }
   }
 }
