d82f6c47a0dfb967e003f6f6558fd8ce53102b33
Andrew Scott <atscott01@gmail.com>
2020-07-10 17:28:18 +0000

    4b4b74548d fix(dev-infra): include `bazel` utility files in npm package (#37891)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+19.sha-1550663/, version);
+      banner = banner.replace(/10.1.0-next.0+20.sha-4b4b745/, version);
     }
   }
 }
