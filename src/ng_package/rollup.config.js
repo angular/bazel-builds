904038ed1f75e2da39610e2611d443560bfa2cbf
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:44:02 +0000

    72d6032fa4 docs: reformat and update CONTRIBUTING.md (#37951)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+25.sha-88d68d2/, version);
+      banner = banner.replace(/10.1.0-next.0+28.sha-72d6032/, version);
     }
   }
 }
