0d73d489e58f3d122c9de9c56c856fa03a6233fd
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:38:29 +0000

    69472a1ed0 refactor(dev-infra): allow for consumption with rules_nodejs v2.0.0 (#37968)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+38.sha-10aba15/, version);
+      banner = banner.replace(/10.1.0-next.0+39.sha-69472a1/, version);
     }
   }
 }
