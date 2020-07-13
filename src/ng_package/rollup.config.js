d1333e233500a9d4e6e01a2c95d1e8c7077a7792
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:34:08 +0000

    62e19c8813 docs: Add Scully to resources.json (#37678)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+34.sha-95669df/, version);
+      banner = banner.replace(/10.1.0-next.0+35.sha-62e19c8/, version);
     }
   }
 }
