106fba18f39eb559e73f9aef5a6ad5fc3ff4dc0a
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:49:41 +0000

    53e4ff72d2 docs: fix typo in router.md (#37227)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.3+38.sha-0d38288/, version);
+      banner = banner.replace(/10.0.3+45.sha-53e4ff7/, version);
     }
   }
 }
