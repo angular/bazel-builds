86b52f808280b8845ee38099b170bd3511e258bd
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:51:04 +0000

    a259a84d3f docs(http): Remove extra semicolons in the http guide (#37228)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+28.sha-72d6032/, version);
+      banner = banner.replace(/10.1.0-next.0+31.sha-a259a84/, version);
     }
   }
 }
