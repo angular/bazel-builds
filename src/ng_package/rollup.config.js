877dda8ccfae82285b9059e883fcbba2b0659988
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:32:55 +0000

    9021d3f5e1 docs: fix live examples in testing guides (#38038)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+41.sha-81542b3/, version);
+      banner = banner.replace(/10.1.0-next.0+42.sha-9021d3f/, version);
     }
   }
 }
