e681652cad4ab29300891d76a6961e93653a8318
Andrew Scott <atscott01@gmail.com>
2020-07-14 18:25:04 +0000

    396033da80 refactor(forms): remove unnecessary `!` operators from validators (#36805)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.3+55.sha-d690eec/, version);
+      banner = banner.replace(/10.0.3+58.sha-396033d/, version);
     }
   }
 }
