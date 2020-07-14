dce53f133ab5d1476631757bd75cc924f94ffffe
Sonu Kapoor <sonukapoor@gmail.com>
2020-07-14 18:39:44 +0000

    e52ac490c8 docs(core): fixes minor typo in initNgDevMode function docs

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+42.sha-2dd22b7/, version);
+      banner = banner.replace(/10.1.0-next.0+42.sha-e52ac49/, version);
     }
   }
 }
