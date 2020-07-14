590e853d75f0506505b60233f526f8bbc62b8305
Andrew Scott <atscott01@gmail.com>
2020-07-14 20:48:42 +0000

    b358495a6c fix(ngcc): report a warning if ngcc tries to use a solution-style tsconfig (#38003)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+48.sha-f4fac40/, version);
+      banner = banner.replace(/10.1.0-next.0+49.sha-b358495/, version);
     }
   }
 }
