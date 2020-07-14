1bb9ea1af3fa61dda53df4f36f56e0d65cadb207
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:40:20 +0000

    d690eec88f docs(router): fix typo in "spotlight on pathmatch" (#38039)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.3+52.sha-9cf78d5/, version);
+      banner = banner.replace(/10.0.3+55.sha-d690eec/, version);
     }
   }
 }
