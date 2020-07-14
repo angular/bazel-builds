a3cdfa7ce7d51f0be949cf4d99432238cc044307
Andrew Scott <atscott01@gmail.com>
2020-07-14 20:38:09 +0000

    f4fac406b9 docs(core): fixes minor typo in initNgDevMode function docs (#38042)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+46.sha-421e807/, version);
+      banner = banner.replace(/10.1.0-next.0+48.sha-f4fac40/, version);
     }
   }
 }
