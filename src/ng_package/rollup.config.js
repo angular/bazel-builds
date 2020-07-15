d50c3e01b3441bae1266d5644c1ddb4afd761a27
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:08:06 +0000

    bf641e1b4b fix(core): incorrectly validating properties on ng-content and ng-container (#37773)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+4.sha-545f1db/, version);
+      banner = banner.replace(/10.1.0-next.1+10.sha-bf641e1/, version);
     }
   }
 }
