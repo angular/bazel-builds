b253daa37ee4a549d94ff2bb2436a6b9b37b8dbc
Andrew Kushnir <akushnir@google.com>
2020-07-15 21:15:50 +0000

    469aba0140 docs: add Emma Twersky to DevRel Contributor page (#38084)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+12.sha-d8c47b9/, version);
+      banner = banner.replace(/10.1.0-next.1+13.sha-469aba0/, version);
     }
   }
 }
