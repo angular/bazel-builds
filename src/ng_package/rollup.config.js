3d0d31fb4c6b39740711cdc5f9dd15ff9bee8a1e
atscott <atscott01@gmail.com>
2020-07-15 18:46:55 +0000

    13d176302b release: cut the v10.0.4 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.3+60.sha-e3b8010/, version);
+      banner = banner.replace(/10.0.4/, version);
     }
   }
 }
