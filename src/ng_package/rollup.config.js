c7f5745a823ec37fdb3c250d5b6d389e8696f408
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:02:58 +0000

    545f1dbac9 feat(dev-infra): add bazel firefox browser with RBE compatibility (#38029)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+2.sha-45d79ef/, version);
+      banner = banner.replace(/10.1.0-next.1+4.sha-545f1db/, version);
     }
   }
 }
