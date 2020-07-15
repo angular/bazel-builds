8b5609296454bbabbbe2fbd73ddfba7862cb2951
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:08:13 +0000

    406f801b70 build: remove obsolete Bazel function ctx.new_file (#38080)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+8.sha-c8e77d8/, version);
+      banner = banner.replace(/10.1.0-next.1+9.sha-406f801/, version);
     }
   }
 }
