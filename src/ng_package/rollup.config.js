471c11aa969e36625a29ec076d85d6767e133785
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:29:55 +0000

    9ac3383d01 build(language-service): remove typescript from ivy bundle (#38088)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+12.sha-6e32972/, version);
+      banner = banner.replace(/10.0.4+15.sha-9ac3383/, version);
     }
   }
 }
