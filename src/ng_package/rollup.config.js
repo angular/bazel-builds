5a4ca242973d27a1c3a45e85082c646bd811114a
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:21:43 +0000

    e382632473 build: fix symbol extractor not dealing with ES2015 classes (#38093)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+16.sha-e0b35bf/, version);
+      banner = banner.replace(/10.1.0-next.1+23.sha-e382632/, version);
     }
   }
 }
