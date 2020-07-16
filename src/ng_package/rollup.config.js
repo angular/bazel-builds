8ae90e1a37aee5f27871e1026ad387c3ccb5c5e0
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:24:42 +0000

    99960a98d2 docs: update router api documentation (#37980)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+15.sha-9ac3383/, version);
+      banner = banner.replace(/10.0.4+16.sha-99960a9/, version);
     }
   }
 }
