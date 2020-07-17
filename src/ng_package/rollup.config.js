75c1176fdc69d3c738c9b308345c6bfc4d4a96df
Andrew Kushnir <akushnir@google.com>
2020-07-17 20:28:22 +0000

    38a7021d5e fix(core): error due to integer overflow when there are too many host bindings (#38014)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+26.sha-b3b03c3/, version);
+      banner = banner.replace(/10.1.0-next.1+27.sha-38a7021/, version);
     }
   }
 }
