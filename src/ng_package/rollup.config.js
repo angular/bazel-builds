a557068d11af6d32124de9112b58ec664c3bca2b
Doug Parker <dgp1130@users.noreply.github.com>
2020-07-17 22:59:04 +0000

    e056ca3575 docs: correct flag default values in `--strict`

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.0+11.sha-7b7e3d9/, version);
+      banner = banner.replace(/10.1.0-next.1+29.sha-e056ca3/, version);
     }
   }
 }
