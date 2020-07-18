66df8bb196e9e2cc8f76b41330f0a72efa14ad68
Andrew Kushnir <akushnir@google.com>
2020-07-18 00:18:35 +0000

    8e305e7099 docs: correct flag default values in `--strict` (#37982)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+22.sha-481df83/, version);
+      banner = banner.replace(/10.0.4+23.sha-8e305e7/, version);
     }
   }
 }
