f7a9b8214bbc63d560ad86ed6c7a0119451ba0b4
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:35:00 +0000

    c8c74153b1 refactor(dev-infra): Update triage labels documentation (#38081)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+30.sha-72f1eec/, version);
+      banner = banner.replace(/10.1.0-next.1+34.sha-c8c7415/, version);
     }
   }
 }
