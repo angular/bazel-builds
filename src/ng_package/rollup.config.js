fb6b651ba074a9c7ad190845e26db9b400564b41
Andrew Kushnir <akushnir@google.com>
2020-07-20 21:33:59 +0000

    3373453736 feat(common): add ReadonlyMap in place of Map in keyValuePipe (#37311)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+38.sha-7c3c8d6/, version);
+      banner = banner.replace(/10.1.0-next.1+40.sha-3373453/, version);
     }
   }
 }
