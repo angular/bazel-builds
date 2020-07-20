33605366ebf72df5dad29b490f018c22468a8c78
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:39:00 +0000

    5b31a0a294 docs: separate template syntax into multiple docs (#36954)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+35.sha-5d3d8b8/, version);
+      banner = banner.replace(/10.1.0-next.1+36.sha-5b31a0a/, version);
     }
   }
 }
