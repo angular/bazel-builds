af343c3295d9592459b85a8d6f22b247bdf6c03e
Andrew Kushnir <akushnir@google.com>
2020-07-20 20:56:03 +0000

    655da32837 build: update pullapprove to remove @matsko from reviewer lists (#38146)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+36.sha-5b31a0a/, version);
+      banner = banner.replace(/10.1.0-next.1+37.sha-655da32/, version);
     }
   }
 }
