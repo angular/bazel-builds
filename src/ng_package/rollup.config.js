4d656a266951556bd1c0abab383626c177bd703d
Andrew Kushnir <akushnir@google.com>
2020-07-20 20:58:22 +0000

    d6d3984524 docs(common): fix selector field in NgIfAs example component (#35854)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+31.sha-9cb318f/, version);
+      banner = banner.replace(/10.0.4+33.sha-d6d3984/, version);
     }
   }
 }
