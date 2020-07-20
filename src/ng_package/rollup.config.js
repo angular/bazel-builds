800b9b35abd54892f3099a7f25ccde8ad6714b82
Andrew Kushnir <akushnir@google.com>
2020-07-20 21:38:54 +0000

    4a6dcd006b docs(router): fix typos (#38132)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+40.sha-3373453/, version);
+      banner = banner.replace(/10.1.0-next.1+39.sha-4a6dcd0/, version);
     }
   }
 }
