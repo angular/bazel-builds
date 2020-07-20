e01f2142a98c8c474fbe6900e9d5dedba2b90bac
Andrew Kushnir <akushnir@google.com>
2020-07-20 17:23:55 +0000

    e80278cf02 fix(compiler): properly associate source spans for implicitly closed elements (#38126)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+23.sha-8e305e7/, version);
+      banner = banner.replace(/10.0.4+27.sha-e80278c/, version);
     }
   }
 }
