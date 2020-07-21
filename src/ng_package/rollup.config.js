8da1cacf52859198922c188a318710b3f97902ce
Andrew Kushnir <akushnir@google.com>
2020-07-21 17:07:46 +0000

    2af3d9c040 refactor(core): rename synthetic host property and listener instructions (#38150)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+35.sha-4664acc/, version);
+      banner = banner.replace(/10.0.4+36.sha-2af3d9c/, version);
     }
   }
 }
