00c8f06b922158b094f05a1cf36c15ef708ba9bf
Andrew Kushnir <akushnir@google.com>
2020-07-21 21:55:12 +0000

    7f82a2c1c8 docs: fix type in lazy-load callout (#38153)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.1+44.sha-a9910f5/, version);
+      banner = banner.replace(/10.1.0-next.1+45.sha-7f82a2c/, version);
     }
   }
 }
