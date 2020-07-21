1546049d837ac11a09fe5e9e51393b1ca333c42c
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:31:29 +0000

    4664acce50 docs: add routing terms to glossary (#38053)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+34.sha-3797861/, version);
+      banner = banner.replace(/10.0.4+35.sha-4664acc/, version);
     }
   }
 }
