a68a6f79f628bab713366f93feab1fa4b22d043b
Andrew Kushnir <akushnir@google.com>
2020-07-21 18:55:49 +0000

    f5eeb1a714 docs: fix DOCUMENT import path (#38158) (#38159)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.4+36.sha-2af3d9c/, version);
+      banner = banner.replace(/10.0.4+37.sha-f5eeb1a/, version);
     }
   }
 }
