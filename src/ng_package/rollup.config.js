3667e2262ee6ddde3fad6833140ee3525087fcfd
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:39:54 +0000

    6ec7297e43 build(docs-infra): remove boilerplate file listings in `example-boilerplate.js` (#38173)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.5/, version);
+      banner = banner.replace(/10.0.5+7.sha-6ec7297/, version);
     }
   }
 }
