6db730b18a6f40c4bbab49f4aa235b1087f80579
Misko Hevery <misko@hevery.com>
2020-07-23 18:26:35 +0000

    51aa4aa1c3 docs: update dynamic-component loading guide (#36959)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+13.sha-8bb8f98/, version);
+      banner = banner.replace(/10.1.0-next.2+14.sha-51aa4aa/, version);
     }
   }
 }
