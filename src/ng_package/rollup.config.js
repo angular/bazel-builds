ccf098ba4414a55d2acad0da35ced3237afc54e0
Misko Hevery <misko@hevery.com>
2020-07-23 18:31:17 +0000

    88d4b269b5 build(docs-infra): simplify `ExampleZipper` by removing `PackageJsonCustomizer` (#38192)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+18.sha-dd09fb5/, version);
+      banner = banner.replace(/10.1.0-next.2+20.sha-88d4b26/, version);
     }
   }
 }
