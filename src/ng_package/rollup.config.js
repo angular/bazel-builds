67ec87c91ae009c660c66b2014accb9b23b8dc73
Misko Hevery <misko@hevery.com>
2020-07-23 18:28:36 +0000

    b518b30dae docs: add Kevin Kreuzer to GDE page (#37929)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+16.sha-7b2e2f5/, version);
+      banner = banner.replace(/10.1.0-next.2+15.sha-b518b30/, version);
     }
   }
 }
