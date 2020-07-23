c96d35d58996b91e4118251d0c0fb4b96281f25a
Misko Hevery <misko@hevery.com>
2020-07-23 04:07:23 +0000

    062b8d90af refactor(forms): refactor common validators used in unit tests (#38020)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+10.sha-8df888d/, version);
+      banner = banner.replace(/10.1.0-next.2+11.sha-062b8d9/, version);
     }
   }
 }
