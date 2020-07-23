e80f50fda62c2775617a710a3268c9eed2c3c8b8
Misko Hevery <misko@hevery.com>
2020-07-23 18:19:32 +0000

    8bb8f98c28 docs: remove duplicate `https://` (#38199)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+12.sha-3c1866b/, version);
+      banner = banner.replace(/10.1.0-next.2+13.sha-8bb8f98/, version);
     }
   }
 }
