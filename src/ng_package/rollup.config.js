947301e0b2e091d81b784b81dfdb996858132737
Misko Hevery <misko@hevery.com>
2020-07-23 03:52:16 +0000

    778ad3776a docs: create coding standards doc (#37700)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+2.sha-d88711c/, version);
+      banner = banner.replace(/10.1.0-next.2+9.sha-778ad37/, version);
     }
   }
 }
