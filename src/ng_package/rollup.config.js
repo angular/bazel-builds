70db60e4bbfe56f538024ae1063d406d35fba91e
Misko Hevery <misko@hevery.com>
2020-07-24 17:15:14 +0000

    6e2db228af docs: Fix link by removing a space (#38214) PR Close #38214

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+22.sha-cce5583/, version);
+      banner = banner.replace(/10.1.0-next.2+23.sha-6e2db22/, version);
     }
   }
 }
