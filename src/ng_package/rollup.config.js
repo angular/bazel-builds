66acda060ac6911b2a799441c1fe2145d9458ab9
Misko Hevery <misko@hevery.com>
2020-07-24 23:06:26 +0000

    1822cbcd46 fix(zone.js): patch nodejs EventEmtter.prototype.off (#37863)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+25.sha-a71f114/, version);
+      banner = banner.replace(/10.1.0-next.2+26.sha-1822cbc/, version);
     }
   }
 }
