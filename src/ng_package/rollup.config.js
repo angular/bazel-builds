fc5dbc5058cbd190e9fd8b0f1c466eeb43a60c94
Misko Hevery <misko@hevery.com>
2020-07-27 17:46:48 +0000

    f74cfadf64 docs: clarify the description of pipes (#37950)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+35.sha-8e5969b/, version);
+      banner = banner.replace(/10.1.0-next.2+36.sha-f74cfad/, version);
     }
   }
 }
