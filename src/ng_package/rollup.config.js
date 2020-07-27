ca68ac322833256a37a93c119f18639b1611f2bd
Misko Hevery <misko@hevery.com>
2020-07-27 22:47:45 +0000

    b1e7775a8a fix(compiler-cli): Add support for string literal class members (#38226)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.5+32.sha-776067c/, version);
+      banner = banner.replace(/10.0.5+38.sha-b1e7775/, version);
     }
   }
 }
