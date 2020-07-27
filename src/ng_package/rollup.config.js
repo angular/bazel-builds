0cb1d183c111bf9e668dce42ca63520be03fb9b4
Misko Hevery <misko@hevery.com>
2020-07-27 17:27:24 +0000

    8e5969bb52 fix(compiler): share identical stylesheets between components in the same file (#38213)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+31.sha-4dfc3fe/, version);
+      banner = banner.replace(/10.1.0-next.2+35.sha-8e5969b/, version);
     }
   }
 }
