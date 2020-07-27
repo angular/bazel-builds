59a1cba3b55fb5b64bc83ada38b49e3e87fa74c1
Misko Hevery <misko@hevery.com>
2020-07-27 17:12:04 +0000

    4dfc3fe6a2 refactor(router): extract Router config utils to a separate file (#38229)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+32.sha-1296003/, version);
+      banner = banner.replace(/10.1.0-next.2+31.sha-4dfc3fe/, version);
     }
   }
 }
