c47c8ccdc8400fc6bd3c32a804f2d7ccabb3c387
Misko Hevery <misko@hevery.com>
2020-07-27 17:12:46 +0000

    1296003445 docs: add ng-add save option (#38198)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+30.sha-67e3ecc/, version);
+      banner = banner.replace(/10.1.0-next.2+32.sha-1296003/, version);
     }
   }
 }
