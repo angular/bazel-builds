1298db1b5b4513681fc0ddd1fb5bc049103a04b7
Misko Hevery <misko@hevery.com>
2020-07-27 22:45:39 +0000

    174aac68f7 refactor(platform-browser): specify return type of parseEventName (#38089)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+42.sha-5d3ba8d/, version);
+      banner = banner.replace(/10.1.0-next.2+43.sha-174aac6/, version);
     }
   }
 }
