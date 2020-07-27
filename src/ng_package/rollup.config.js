355c77d123d97ad5207cbf505eccbc36aab448fb
Misko Hevery <misko@hevery.com>
2020-07-27 18:32:14 +0000

    b65b6163f7 docs: fix breaking URL for RxJS marble testing (#38209)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+36.sha-f74cfad/, version);
+      banner = banner.replace(/10.1.0-next.2+37.sha-b65b616/, version);
     }
   }
 }
