103908cd7757f123ecba96d9d68cb0fb5baf4b83
Misko Hevery <misko@hevery.com>
2020-07-28 22:27:29 +0000

    cdda60a430 release: cut the v10.0.6 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.5+51.sha-7570356/, version);
+      banner = banner.replace(/10.0.6/, version);
     }
   }
 }
