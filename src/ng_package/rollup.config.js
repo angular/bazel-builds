165e415232e59a59e64dbb1d62c2d2afe4469e51
Misko Hevery <misko@hevery.com>
2020-07-28 19:57:33 +0000

    2a45b932e2 build: fix broken build (#38274)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.2+45.sha-65cc0c8/, version);
+      banner = banner.replace(/10.1.0-next.2+58.sha-2a45b93/, version);
     }
   }
 }
