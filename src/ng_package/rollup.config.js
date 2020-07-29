98ad9c753dcc4568952fa7681a646972e4fd5f76
Misko Hevery <misko@hevery.com>
2020-07-29 17:58:57 +0000

    b280d54470 refactor(docs-infra): simplify/improve `CopierService` hidden textarea creation (#38244)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+13.sha-1b17722/, version);
+      banner = banner.replace(/10.1.0-next.3+12.sha-b280d54/, version);
     }
   }
 }
