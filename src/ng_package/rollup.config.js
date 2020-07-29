2ac13f210b6ca2daba8241e540645286761ae2dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 20:59:44 +0000

    8effc83b92 docs(router): clarify how base href is used to construct targets (#38123)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+15.sha-887c350/, version);
+      banner = banner.replace(/10.1.0-next.3+17.sha-8effc83/, version);
     }
   }
 }
