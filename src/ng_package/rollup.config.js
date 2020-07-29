eb9735ca5f31dbfd81b0a4f8e3a890dac2b4f367
Misko Hevery <misko@hevery.com>
2020-07-29 17:58:46 +0000

    1b17722091 refactor(docs-infra): Lazy-loads SVG icons (#38268)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+2.sha-9c8bc4a/, version);
+      banner = banner.replace(/10.1.0-next.3+13.sha-1b17722/, version);
     }
   }
 }
