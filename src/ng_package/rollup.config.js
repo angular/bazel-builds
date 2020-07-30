ec0cd31c72445c20dc8e19e52ab88f39a26d1ad7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:43:43 +0000

    fd51e01335 fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+19.sha-3a525d1/, version);
+      banner = banner.replace(/10.1.0-next.3+20.sha-fd51e01/, version);
     }
   }
 }
