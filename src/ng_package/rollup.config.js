ea4dbf4fcf8e9823673403569018358eac2b77a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:10:56 +0000

    1eebb7f189 test(compiler-cli): disable one TypeChecker test on Windows due to path sensitivity issue (#38294)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+16.sha-7f8c222/, version);
+      banner = banner.replace(/10.1.0-next.3+18.sha-1eebb7f/, version);
     }
   }
 }
