4a245afcfb121846ab5083ecc807f96709c43733
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 19:43:46 +0000

    3a525d196b Revert "fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)" (#38303)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+18.sha-1eebb7f/, version);
+      banner = banner.replace(/10.1.0-next.3+19.sha-3a525d1/, version);
     }
   }
 }
