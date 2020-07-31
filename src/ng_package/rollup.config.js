4512feb79c366359b488198fa44cd49c2571f8be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:56:03 +0000

    ff9f4de4f1 fix(compiler): update unparsable character reference entity error messages (#38319)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+38.sha-3a46c2d/, version);
+      banner = banner.replace(/10.1.0-next.3+39.sha-ff9f4de/, version);
     }
   }
 }
