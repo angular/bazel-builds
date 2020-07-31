6205c24f77e54faf913a721c28d0baddb14f80d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 18:26:36 +0000

    9c5fc1693a refactor(docs-infra): update docs examples `tslint.json` to match CLI and fix failures (#38143)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.7+2.sha-7ff5ef2/, version);
+      banner = banner.replace(/10.0.7+17.sha-9c5fc16/, version);
     }
   }
 }
