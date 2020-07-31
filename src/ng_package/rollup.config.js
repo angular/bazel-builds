4d741dc6cc93aade444dfeb1f0c6dddf6480c2f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:20:56 +0000

    7ff5ef27c7 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.6+21.sha-879ff08f/, version);
+      banner = banner.replace(/10.0.7+2.sha-7ff5ef2/, version);
     }
   }
 }
