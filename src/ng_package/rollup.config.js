ba94be2206b07a5cb56829943f518bf474c840fb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:16:54 +0000

    e49b053dac build: cleanup .bazelrc file to no longer set unused flags (#38124)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+39.sha-ff9f4de/, version);
+      banner = banner.replace(/10.1.0-next.3+41.sha-e49b053/, version);
     }
   }
 }
