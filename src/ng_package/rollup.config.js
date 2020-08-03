2114fdeff52540b0c84ce371439c832cfcd3ae8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:22:55 +0000

    8fbf40bf40 feat(core): update reference and doc to change `async` to `waitAsync`. (#37583)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+40.sha-87baa06/, version);
+      banner = banner.replace(/10.1.0-next.3+43.sha-8fbf40b/, version);
     }
   }
 }
