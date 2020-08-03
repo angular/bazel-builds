2f4a309fe5c61f6485202fb7a0c5aa620e5ed8a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:14:58 +0000

    87baa06cc6 revert: docs(core): correct SomeService to SomeComponent (#38325)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.3+41.sha-e49b053/, version);
+      banner = banner.replace(/10.1.0-next.3+40.sha-87baa06/, version);
     }
   }
 }
