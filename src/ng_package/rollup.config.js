dc0e608ee546a1c2e75cb15df8c62da2b2b100b4
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:13:03 +0000

    585ef2bdd7 docs: fix typo in glossary (#38318)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4/, version);
+      banner = banner.replace(/10.1.0-next.4+1.sha-585ef2b/, version);
     }
   }
 }
