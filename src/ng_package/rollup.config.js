90fa137b2801ebcf74a1391d3f78d86e21ff83f6
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:36:10 +0000

    db897f4f7a docs: add a page with the Angular roadmap (#38356)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.8+6.sha-4151314/, version);
+      banner = banner.replace(/10.0.8+8.sha-db897f4/, version);
     }
   }
 }
