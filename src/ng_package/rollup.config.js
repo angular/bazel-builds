508af278b78d9dc2e3f298b5aff65542f83e2aa3
Andrew Kushnir <akushnir@google.com>
2020-08-06 19:01:41 +0000

    0551fbdf88 fix(docs-infra): correctly generate CLI commands docs when the overview page moves (#38365)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+17.sha-18cd1a9/, version);
+      banner = banner.replace(/10.1.0-next.4+16.sha-0551fbd/, version);
     }
   }
 }
