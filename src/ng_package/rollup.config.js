7ad5c75bf8ef216ccc0bd2ad44ad614c29f88877
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:29:34 +0000

    0c2490368e refactor(platform-browser): specify return type of parseEventName (#38089)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+9.sha-bb88c9f/, version);
+      banner = banner.replace(/10.1.0-next.4+10.sha-0c24903/, version);
     }
   }
 }
