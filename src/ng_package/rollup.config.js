32191b15e28200778e28802e45d8998a1b478ddb
Andrew Kushnir <akushnir@google.com>
2020-08-06 23:50:11 +0000

    deb290bc2c refactor(core): extract `icuSwitchCase`, `icuUpdateCase`, `removeNestedIcu` (#38154) (#38370)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.8+17.sha-f5d5bac/, version);
+      banner = banner.replace(/10.0.8+19.sha-deb290b/, version);
     }
   }
 }
