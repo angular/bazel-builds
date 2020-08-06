d72125351c1e5b0ab48f783ec50703a522d5ebc4
Andrew Kushnir <akushnir@google.com>
2020-08-06 22:45:53 +0000

    7525f3afc1 fix(compiler-cli): type-check inputs that include undefined when there's coercion members (#38273)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+19.sha-26be5b4/, version);
+      banner = banner.replace(/10.1.0-next.4+21.sha-7525f3a/, version);
     }
   }
 }
