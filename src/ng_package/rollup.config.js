94d8bfb6966d9f9ea19d38a12771fb2113bb7349
Andrew Kushnir <akushnir@google.com>
2020-08-06 01:13:22 +0000

    58f4b3ad80 fix(common): ensure scrollRestoration is writable (#30630) (#38357)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.8+8.sha-db897f4/, version);
+      banner = banner.replace(/10.0.8+10.sha-58f4b3a/, version);
     }
   }
 }
