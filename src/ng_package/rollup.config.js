f7252b3b4931d41610d449bff6bbc45cdba17055
Andrew Kushnir <akushnir@google.com>
2020-08-07 00:20:46 +0000

    ef7b70a375 refactor(core): add debug ranges to `LViewDebug` with matchers (#38359)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.8+19.sha-deb290b/, version);
+      banner = banner.replace(/10.0.8+20.sha-ef7b70a/, version);
     }
   }
 }
