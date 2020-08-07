ba542d78806a22622216c086ce8c6348c4b4e89c
Andrew Kushnir <akushnir@google.com>
2020-08-07 18:36:31 +0000

    354e66efad refactor(common): use getElementById in ViewportScroller.scrollToAnchor (#30143)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+23.sha-702958e/, version);
+      banner = banner.replace(/10.1.0-next.4+24.sha-354e66e/, version);
     }
   }
 }
