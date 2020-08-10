6c63b5a55fd19ac80c6e42f0674a0f3171f2f888
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:07:33 +0000

    6d8c73a4d6 fix(core): Store the currently selected ICU in `LView` (#38345)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+27.sha-0de93fd/, version);
+      banner = banner.replace(/10.1.0-next.4+29.sha-6d8c73a/, version);
     }
   }
 }
