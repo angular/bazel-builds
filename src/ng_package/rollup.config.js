916f10950db6430fac9f8da5a46e671f18a186f1
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:13:25 +0000

    3dfaf56fdd docs: Remove redundant sentence from Router (#38398) PR Close #38398

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.8+24.sha-42a649d/, version);
+      banner = banner.replace(/10.0.8+26.sha-3dfaf56/, version);
     }
   }
 }
