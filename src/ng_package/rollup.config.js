4b096177416cffe05a8d9b19edb13befa06b05d9
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:33:28 +0000

    7f111491a8 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.8+27.sha-ee5123f/, version);
+      banner = banner.replace(/10.0.8+28.sha-7f11149/, version);
     }
   }
 }
