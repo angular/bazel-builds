9454d5b53c207bb1faafeed688ce6001880905f3
Andrew Kushnir <akushnir@google.com>
2020-08-11 16:03:20 +0000

    df76a2048b fix(router): restore 'history.state' object for navigations coming from Angular router (#28108) (#28176)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+35.sha-3d15616/, version);
+      banner = banner.replace(/10.1.0-next.4+36.sha-df76a20/, version);
     }
   }
 }
