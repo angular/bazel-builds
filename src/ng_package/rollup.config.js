482d7206f8f0810cc811a002b9a6675b9b44dce6
Andrew Kushnir <akushnir@google.com>
2020-08-11 00:48:04 +0000

    3d156162af fix(dev-infra): update i18n-related file locations in PullApprove config (#38403)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+34.sha-5dc8d28/, version);
+      banner = banner.replace(/10.1.0-next.4+35.sha-3d15616/, version);
     }
   }
 }
