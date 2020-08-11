b0c783d0696d5662a1b5fa38908e9c773395cb74
Andrew Kushnir <akushnir@google.com>
2020-08-11 23:53:28 +0000

    a2e069fdda build: run formatting automatically on pre-commit hook (#38402)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+43.sha-e2e5f83/, version);
+      banner = banner.replace(/10.1.0-next.4+46.sha-a2e069f/, version);
     }
   }
 }
