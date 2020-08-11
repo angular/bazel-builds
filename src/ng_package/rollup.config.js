1bf5a638847cfb82221388d6873b2a3e7668995c
Andrew Kushnir <akushnir@google.com>
2020-08-11 23:55:54 +0000

    a6292faa97 docs: remove solution style tsconfig (#38394)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.4+46.sha-a2e069f/, version);
+      banner = banner.replace(/10.1.0-next.4+44.sha-a6292fa/, version);
     }
   }
 }
