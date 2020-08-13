142cbf92158dfa5d936e2823bf45c444fecec797
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:02:18 +0000

    b071495f92 fix(core): fix multiple nested views removal from ViewContainerRef (#38317)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.5+17.sha-d5f819e/, version);
+      banner = banner.replace(/10.1.0-next.5+18.sha-b071495/, version);
     }
   }
 }
