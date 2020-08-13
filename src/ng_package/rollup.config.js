c9ccb1ea471e0e56311aa8f9e0c801b453b3a4c9
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:16:06 +0000

    692e34d4a2 test(docs-infra): remove deprecated `ReflectiveInjector` (#38408)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.9+11.sha-aa816d3/, version);
+      banner = banner.replace(/10.0.9+14.sha-692e34d/, version);
     }
   }
 }
