d312ed569c0a32d1c4bba9893e4ac610b18540d9
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:02:41 +0000

    aa816d3887 ci: disable closure size tracking test (#38449)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.9+9.sha-cb83b8a/, version);
+      banner = banner.replace(/10.0.9+11.sha-aa816d3/, version);
     }
   }
 }
