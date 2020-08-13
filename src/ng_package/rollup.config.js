58fcaea49634edfab6a67e2d185943c8ab078898
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:04:16 +0000

    ffd1691ba9 feat(dev-infra): save invalid commit message attempts to be restored on next commit attempt (#38304)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.9+4.sha-afd4417/, version);
+      banner = banner.replace(/10.0.9+5.sha-ffd1691/, version);
     }
   }
 }
