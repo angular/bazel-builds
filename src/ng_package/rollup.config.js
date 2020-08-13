7ee454c4d956b9c99b83f420c8524440d48afb22
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:31:57 +0000

    8763d8201c build: update ng-dev config file for new commit message configuration (#38430)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.5+6.sha-773f790/, version);
+      banner = banner.replace(/10.1.0-next.5+8.sha-8763d82/, version);
     }
   }
 }
