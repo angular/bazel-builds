71312ff10880334e0f00d47de1f8ce60f6550ec6
Andrew Scott <atscott01@gmail.com>
2020-08-13 17:59:30 +0000

    a80f654af9 fix(core): error if CSS custom property in host binding has number in name (#38432)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.5+9.sha-aa847cb/, version);
+      banner = banner.replace(/10.1.0-next.5+10.sha-a80f654/, version);
     }
   }
 }
