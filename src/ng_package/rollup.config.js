ee623e0a810754107716793604bd5cf1ef76cebe
Andrew Scott <atscott01@gmail.com>
2020-08-14 19:03:58 +0000

    71079ce47e fix(common): Allow scrolling when browser supports scrollTo (#38468)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.5+19.sha-ca79880/, version);
+      banner = banner.replace(/10.1.0-next.5+20.sha-71079ce/, version);
     }
   }
 }
