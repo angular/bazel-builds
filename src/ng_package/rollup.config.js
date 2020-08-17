d0efc92709bf0f3b9af90df6be63ef6f81849299
Andrew Scott <atscott@google.com>
2020-08-17 17:39:44 +0000

    cb05c0102f fix(core): move generated i18n statements to the `consts` field of ComponentDef (#38404)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.5+20.sha-71079ce/, version);
+      banner = banner.replace(/10.1.0-next.5+22.sha-cb05c01/, version);
     }
   }
 }
