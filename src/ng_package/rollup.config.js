208eba9babced47f73619355d115857101ad9de9
Andrew Scott <atscott@google.com>
2020-08-17 19:58:11 +0000

    e0e5c9f195 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38349)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.5+27.sha-cfe424e/, version);
+      banner = banner.replace(/10.1.0-next.5+28.sha-e0e5c9f/, version);
     }
   }
 }
