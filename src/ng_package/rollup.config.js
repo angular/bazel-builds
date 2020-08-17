9851cc791731c34e4f595147a8d73494b87abae3
Andrew Scott <atscott@google.com>
2020-08-17 18:50:26 +0000

    cfe424e875 refactor(language-service): [Ivy] remove temporary compiler (#38310)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.5+26.sha-3b9c802/, version);
+      banner = banner.replace(/10.1.0-next.5+27.sha-cfe424e/, version);
     }
   }
 }
