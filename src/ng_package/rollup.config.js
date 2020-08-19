803b840e0848053d97b4afb98af57a45fc4ce42a
Misko Hevery <misko@hevery.com>
2020-08-19 19:01:33 +0000

    7ad32649c0 fix(router): support lazy loading for empty path named outlets (#38379)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.6+12.sha-0270020/, version);
+      banner = banner.replace(/10.1.0-next.7+2.sha-7ad3264/, version);
     }
   }
 }
