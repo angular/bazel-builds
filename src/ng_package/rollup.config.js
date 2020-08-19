3f5877aec5d90a2592deced3eb56d9281316e49a
Andrew Scott <atscott@google.com>
2020-08-19 15:51:58 +0000

    55fd725e74 docs: Fix typo in the inputs and outputs guide (#38524)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.6+9.sha-f77fd5e/, version);
+      banner = banner.replace(/10.1.0-next.6+10.sha-55fd725/, version);
     }
   }
 }
