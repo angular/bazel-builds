8b4c891a503822f1b0f6a01cc8b74ec8640d5241
Misko Hevery <misko@hevery.com>
2020-08-19 21:35:44 +0000

    0b54c0c6b4 refactor(compiler-cli): add getTemplateOfComponent to TemplateTypeChecker (#38355)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+3.sha-1ec6099/, version);
+      banner = banner.replace(/10.1.0-next.7+4.sha-0b54c0c/, version);
     }
   }
 }
