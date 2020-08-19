8e465c44a2fa827f2dba89a0fde79b3392c3ec27
Misko Hevery <misko@hevery.com>
2020-08-19 21:38:09 +0000

    68a9a01a64 fix(localize): parse all parts of a translation with nested HTML (#38452)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+4.sha-0b54c0c/, version);
+      banner = banner.replace(/10.1.0-next.7+6.sha-68a9a01/, version);
     }
   }
 }
