7e1b0e63bc59d9089394c47ce2fb4dca14d2155c
Misko Hevery <misko@hevery.com>
2020-08-20 04:25:21 +0000

    87bbf69ce8 Revert "fix(core): remove closing body tag from inert DOM builder (#38454)"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.11+5.sha-5528536/, version);
+      banner = banner.replace(/10.0.11+6.sha-87bbf69/, version);
     }
   }
 }
