3eb86d766c77515630ea14a5318e60963f8dda15
Misko Hevery <misko@hevery.com>
2020-08-20 20:28:16 +0000

    7f2a3e4b6b docs: apply code styling in template reference variables guide (#38522)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.11+10.sha-1775f35/, version);
+      banner = banner.replace(/10.0.11+11.sha-7f2a3e4/, version);
     }
   }
 }
