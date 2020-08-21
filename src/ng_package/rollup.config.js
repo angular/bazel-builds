d38f555d690d4b2b45f2c8704f4f2abbc93cedeb
Misko Hevery <misko@hevery.com>
2020-08-21 19:47:40 +0000

    e7da4040d6 fix(compiler-cli): adding references to const enums in runtime code (#38542)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+16.sha-2a643e1/, version);
+      banner = banner.replace(/10.1.0-next.7+17.sha-e7da404/, version);
     }
   }
 }
