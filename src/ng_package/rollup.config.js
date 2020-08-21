95fc2aa62efbbbe0f9a751bf106da1031a03a5d9
Misko Hevery <misko@hevery.com>
2020-08-21 19:39:14 +0000

    2a643e1ab6 docs: change function name from async -> waitForAsync (#38548)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+14.sha-956b25a/, version);
+      banner = banner.replace(/10.1.0-next.7+16.sha-2a643e1/, version);
     }
   }
 }
