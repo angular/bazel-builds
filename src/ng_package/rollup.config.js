4eddc9dee8ee8d7d960cf70a0957a157aa50d754
Misko Hevery <misko@hevery.com>
2020-08-24 16:54:09 +0000

    b48cc6ead5 feat(language-service): introduce hybrid visitor to locate AST node (#38540)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+22.sha-201a546/, version);
+      banner = banner.replace(/10.1.0-next.7+19.sha-b48cc6e/, version);
     }
   }
 }
