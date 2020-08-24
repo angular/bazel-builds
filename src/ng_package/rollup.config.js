47761dc0a8a9771dd2fc7b9ae682320197b4521a
Misko Hevery <misko@hevery.com>
2020-08-24 23:16:05 +0000

    cb3db0d31b release: clean up changelog

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+26.sha-f18e2d5/, version);
+      banner = banner.replace(/10.1.0-next.7+28.sha-cb3db0d/, version);
     }
   }
 }
