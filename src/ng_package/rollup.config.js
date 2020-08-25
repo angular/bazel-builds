0a5daf80cb1d78ac9c6f40edb0aa404507853fc9
Misko Hevery <misko@hevery.com>
2020-08-25 17:17:50 +0000

    18e474f522 fix(zone.js): zone.js toString patch should check typeof Promise is function (#38350)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+28.sha-cb3db0d/, version);
+      banner = banner.replace(/10.1.0-next.7+29.sha-18e474f/, version);
     }
   }
 }
