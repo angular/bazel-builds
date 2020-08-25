6fdeae59b10ea9a0fb6ea868ed104f4b84c4d7b7
Misko Hevery <misko@hevery.com>
2020-08-25 17:19:09 +0000

    9cbde86534 docs: fix typo in the testing component basics guide (#38573)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-next.7+29.sha-18e474f/, version);
+      banner = banner.replace(/10.1.0-next.7+30.sha-9cbde86/, version);
     }
   }
 }
