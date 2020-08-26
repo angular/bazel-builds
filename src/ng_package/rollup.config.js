c5507ebf0906e764a88ce1ba5e9a93edac242012
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:16:48 +0000

    89ff4467fd docs: add Sam Vloeberghs to GDE list (#37970)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.14/, version);
+      banner = banner.replace(/10.0.14+3.sha-89ff446/, version);
     }
   }
 }
