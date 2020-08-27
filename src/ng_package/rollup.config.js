606a9559bc6124e625eff66c92cdb818d812df83
Joey Perrott <josephperrott@gmail.com>
2020-08-27 18:21:11 +0000

    43ddfbaf31 build: bump to lastest sha of angular/dev-infra's lock-closed action (#38615)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.14+5.sha-94fd5c7/, version);
+      banner = banner.replace(/10.0.14+6.sha-43ddfba/, version);
     }
   }
 }
