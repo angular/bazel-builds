4d43bc593b18919edbe8f85b86df828f31aa6160
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:11:01 +0000

    c1bc070b40 refactor(dev-infra): remove `style` type from commit style guide (#38639)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+18.sha-b2857b4/, version);
+      banner = banner.replace(/10.1.0-rc.0+16.sha-c1bc070/, version);
     }
   }
 }
