e3fe2b4fefde504a88b7a93fb9cd2f253c1b44c2
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:55:23 +0000

    7d5ea554e9 refactor(dev-infra): use a mixin to require a `github-token` for an ng-dev command (#38630)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.0.14+22.sha-b060837/, version);
+      banner = banner.replace(/10.0.14+23.sha-7d5ea55/, version);
     }
   }
 }
