e324ee3d52e22a71bc672fe269ea75ebe2801b54
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:06:57 +0000

    0fc2bef0cd docs(service-worker): add links to service worker communication guide (#36847)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+24.sha-0a7a5e3/, version);
+      banner = banner.replace(/10.1.0-rc.0+29.sha-0fc2bef/, version);
     }
   }
 }
