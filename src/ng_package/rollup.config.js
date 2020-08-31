dd76b49851b25dbf08e4d153273493c676450abe
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:56:38 +0000

    d5fabc303d refactor(forms): remove extra space in error message (#38637)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+21.sha-3487b54/, version);
+      banner = banner.replace(/10.1.0-rc.0+23.sha-d5fabc3/, version);
     }
   }
 }
