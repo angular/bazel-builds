335d01e4b24eb065124cc47025f615d6d84748cd
Joey Perrott <josephperrott@gmail.com>
2020-08-31 20:55:50 +0000

    f5a148b1b7 fix(compiler): incorrectly inferring namespace for HTML nodes inside SVG (#38477)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+30.sha-4f28192/, version);
+      banner = banner.replace(/10.1.0-rc.0+31.sha-f5a148b/, version);
     }
   }
 }
