eb9af10b8f6d3df561c9d749c312e3787cf39bfb
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:49:10 +0000

    cbbf8b542f docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+14.sha-91dfb18/, version);
+      banner = banner.replace(/10.1.0-rc.0+15.sha-cbbf8b5/, version);
     }
   }
 }
