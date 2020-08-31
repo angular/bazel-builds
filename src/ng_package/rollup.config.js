07d2c85fa35d24fe44cfbd43c37fe196f56c51f8
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:05:21 +0000

    827ba05914 docs: remove first person and space in CircleCI in the testing guide. (#38631)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+17.sha-5d5caf2/, version);
+      banner = banner.replace(/10.1.0-rc.0+19.sha-827ba05/, version);
     }
   }
 }
