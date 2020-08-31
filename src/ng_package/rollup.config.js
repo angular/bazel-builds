9963b3e04e359c33e8c1b2e8344d7c9e14895d48
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:08:37 +0000

    6b1a505566 feat(dev-infra): write outputs of command runs to ng-dev log file (#38599)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+6.sha-dd82f2f/, version);
+      banner = banner.replace(/10.1.0-rc.0+12.sha-6b1a505/, version);
     }
   }
 }
