0fa1a29ee9bd3c24e2aae13fb4f6f75ff588c973
Joey Perrott <josephperrott@gmail.com>
2020-09-01 18:01:05 +0000

    d9fea857db fix(forms): ensure to emit `statusChanges` on subsequent value update/validations (#38354)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+34.sha-03dbcc7/, version);
+      banner = banner.replace(/10.1.0-rc.0+35.sha-d9fea85/, version);
     }
   }
 }
