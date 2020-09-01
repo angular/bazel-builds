afc4e4a1b73fbd89f771e89cf6314bd9aabc6539
Joey Perrott <josephperrott@gmail.com>
2020-09-01 20:30:08 +0000

    5588324802 build: add configuration for the caretaker command (#38601)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0-rc.0+17.sha-0dda97e/, version);
+      banner = banner.replace(/10.1.0-rc.0+19.sha-5588324/, version);
     }
   }
 }
