6e1b4016c8370589aec73d43b5cbd06e56c3deb1
Andrew Scott <atscott@google.com>
2020-09-02 22:12:31 +0000

    1d8c5d88cd refactor(compiler): `element.sourceSpan` should span the `outerHTML` (#38581)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+1.sha-86e7cd8/, version);
+      banner = banner.replace(/11.0.0-next.0+5.sha-1d8c5d8/, version);
     }
   }
 }
