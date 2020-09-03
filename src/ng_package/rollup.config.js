6f6565dbe8798a65f9c6ad993d489a20d1b5c356
Andrew Scott <atscott@google.com>
2020-09-03 21:27:47 +0000

    c90eb5450d refactor(compiler-cli): make template parsing errors into diagnostics (#38576)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+10.sha-1c7e5ce/, version);
+      banner = banner.replace(/11.0.0-next.0+12.sha-c90eb54/, version);
     }
   }
 }
