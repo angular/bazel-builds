f1a80835766c17d3d7169a5356b0151b1c0fd9ab
atscott <atscott01@gmail.com>
2020-09-03 17:04:36 +0000

    29c89c9297 docs(dev-infra): fix typo in comment (arguements --> arguments) (#38653)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0+3.sha-6aac499/, version);
+      banner = banner.replace(/10.1.0+6.sha-29c89c9/, version);
     }
   }
 }
