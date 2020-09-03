a69c2837dd27a77fd824ab50d5976e09ef3bf4b1
atscott <atscott01@gmail.com>
2020-09-03 00:09:31 +0000

    fdea1804d6 fix(core): remove CollectionChangeRecord symbol (#38668)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+2.sha-5da1934/, version);
+      banner = banner.replace(/11.0.0-next.0+6.sha-fdea180/, version);
     }
   }
 }
