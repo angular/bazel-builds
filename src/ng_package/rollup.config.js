1e41d8a640e543210d4087d71cf1254974c4dfb8
atscott <atscott01@gmail.com>
2020-09-08 21:48:44 +0000

    4360eed9b7 fix(localize): enable whitespace preservation marker in XLIFF files (#38737)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+42.sha-c880e39/, version);
+      banner = banner.replace(/11.0.0-next.0+43.sha-4360eed/, version);
     }
   }
 }
