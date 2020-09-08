3697c338e6fbb0a24cecf1b83f7fff4c7dd56666
atscott <atscott01@gmail.com>
2020-09-08 17:25:42 +0000

    3e57ca1d98 docs: fix typos in deployment guide (#38727)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+18.sha-79d8795/, version);
+      banner = banner.replace(/11.0.0-next.0+17.sha-3e57ca1/, version);
     }
   }
 }
