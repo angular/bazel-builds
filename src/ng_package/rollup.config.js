8feaf8a83a7acbd0a7bdeb28fb715239a75dc3ff
Andrew Scott <atscott@google.com>
2020-09-08 16:33:27 +0000

    0a277c6c40 docs: remove reverted bug fix from 10.1 change log (#38718)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0+9.sha-9bf32c4/, version);
+      banner = banner.replace(/10.1.0+10.sha-0a277c6/, version);
     }
   }
 }
