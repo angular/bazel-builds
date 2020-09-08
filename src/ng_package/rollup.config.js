0fbcf7c36b483c15c67fdc0d992a81ca9e0f8568
atscott <atscott01@gmail.com>
2020-09-08 17:32:05 +0000

    d9da7e5a18 docs: fix result of sanitization example (#38724)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+24.sha-97475d7/, version);
+      banner = banner.replace(/11.0.0-next.0+19.sha-d9da7e5/, version);
     }
   }
 }
