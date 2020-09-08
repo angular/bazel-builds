632876f83d7dd389a1e4d3fdb78f71cc275fbded
atscott <atscott01@gmail.com>
2020-09-08 17:28:57 +0000

    a29f9a9fb3 fix(dev-infra): change logging of commit message restoration to debug (#38704)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+17.sha-3e57ca1/, version);
+      banner = banner.replace(/11.0.0-next.0+23.sha-a29f9a9/, version);
     }
   }
 }
