71d54badeed36be84bd1224f7847d9935e1e8f8a
atscott <atscott01@gmail.com>
2020-09-08 17:30:32 +0000

    9f28f82598 docs: add space between icon and text in issue template (#38712)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+16.sha-c2d017d/, version);
+      banner = banner.replace(/11.0.0-next.0+22.sha-9f28f82/, version);
     }
   }
 }
