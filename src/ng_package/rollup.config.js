1a2223d22858a30f426eed353ebc2bc77b27c825
atscott <atscott01@gmail.com>
2020-09-08 17:29:18 +0000

    c2d017de83 docs: word correction (#38729) PR Close #38729

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+23.sha-a29f9a9/, version);
+      banner = banner.replace(/11.0.0-next.0+16.sha-c2d017d/, version);
     }
   }
 }
