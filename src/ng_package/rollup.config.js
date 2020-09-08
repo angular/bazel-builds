a3d4971ee107c3600fb1de21b77d69234e93282c
atscott <atscott01@gmail.com>
2020-09-08 17:44:04 +0000

    926ffcd8ac fix(router): support lazy loading for empty path named outlets (#38379)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+21.sha-261f689/, version);
+      banner = banner.replace(/11.0.0-next.0+25.sha-926ffcd/, version);
     }
   }
 }
