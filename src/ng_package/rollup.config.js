715dd531da2a59eecfb67e34251642cccf4c5923
atscott <atscott01@gmail.com>
2020-09-08 19:02:24 +0000

    2c4a98a285 fix(localize): do not expose NodeJS typings in $localize runtime code (#38700)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+25.sha-926ffcd/, version);
+      banner = banner.replace(/11.0.0-next.0+28.sha-2c4a98a/, version);
     }
   }
 }
