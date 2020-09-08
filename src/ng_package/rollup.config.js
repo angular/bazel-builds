bf90227fbb4f3c9b70911ab3ac06d0cf1c0f40a4
atscott <atscott01@gmail.com>
2020-09-08 21:19:56 +0000

    bfb7eec698 ci: update payload size limit for integration tests (#38746)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+26.sha-83ace4e/, version);
+      banner = banner.replace(/11.0.0-next.0+39.sha-bfb7eec/, version);
     }
   }
 }
