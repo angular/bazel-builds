5d03d72f9f639f32e9d48f0dabad5c6a9d5b2202
atscott <atscott01@gmail.com>
2020-09-08 22:59:32 +0000

    e156e29edd perf(compiler-cli): optimize computation of type-check scope information (#38749)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0+34.sha-190dca0/, version);
+      banner = banner.replace(/10.1.0+36.sha-e156e29/, version);
     }
   }
 }
