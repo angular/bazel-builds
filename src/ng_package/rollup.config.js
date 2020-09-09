d7b6dee4dc6aba4dd88dc50d5dbcf6883ee2c638
atscott <atscott01@gmail.com>
2020-09-09 23:46:49 +0000

    c6ebb77cec test(language-service): [ivy] remove all markers from test (#38777)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+2.sha-f942118/, version);
+      banner = banner.replace(/11.0.0-next.1+1.sha-c6ebb77/, version);
     }
   }
 }
