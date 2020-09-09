d42648563314850d1ecb67b1e0796b0be89ca069
atscott <atscott01@gmail.com>
2020-09-09 16:36:48 +0000

    7997fc5f97 docs: mark the `entryComponents` array as deprecated (#38616)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+48.sha-d896c33/, version);
+      banner = banner.replace(/11.0.0-next.0+50.sha-7997fc5/, version);
     }
   }
 }
