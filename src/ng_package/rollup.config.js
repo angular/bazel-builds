96eea4524dd0053616922aceba55e941a45c78aa
atscott <atscott01@gmail.com>
2020-09-09 16:33:33 +0000

    d896c33b0e fix(dev-infra): set build commit message type to allow an optional scope (#38745)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+52.sha-f645d26/, version);
+      banner = banner.replace(/11.0.0-next.0+48.sha-d896c33/, version);
     }
   }
 }
