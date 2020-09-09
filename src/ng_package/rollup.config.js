216e77e2ac8063436f608f082fa78e083a7be120
atscott <atscott01@gmail.com>
2020-09-09 16:32:20 +0000

    f645d26e3f docs(docs-infra): add The Deep Dive podcast, update Angular inDepth URL (#37621)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+46.sha-ded075a/, version);
+      banner = banner.replace(/11.0.0-next.0+52.sha-f645d26/, version);
     }
   }
 }
