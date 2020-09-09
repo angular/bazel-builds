e2719e29c0ad12d90d96654d8db0c7703a4a9741
atscott <atscott01@gmail.com>
2020-09-09 16:29:07 +0000

    6d9bfb8368 docs(router): fixed PreloadAllModules comment typo (#38758) PR Close #38758

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+47.sha-19a4843/, version);
+      banner = banner.replace(/11.0.0-next.0+51.sha-6d9bfb8/, version);
     }
   }
 }
