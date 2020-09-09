0936af0343fcce5b71f9d7d342f61bec3fef0858
atscott <atscott01@gmail.com>
2020-09-09 17:06:30 +0000

    3d77b64fc3 docs: add Andrew Grekov to GDE resources (#36690)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+50.sha-7997fc5/, version);
+      banner = banner.replace(/11.0.0-next.0+53.sha-3d77b64/, version);
     }
   }
 }
