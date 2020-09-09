a7cf3b47f0db1950c836aa7e07a35eaa5b5c1a5e
atscott <atscott01@gmail.com>
2020-09-09 16:31:57 +0000

    ded075a79c docs: add missing space (#38759)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+51.sha-6d9bfb8/, version);
+      banner = banner.replace(/11.0.0-next.0+46.sha-ded075a/, version);
     }
   }
 }
