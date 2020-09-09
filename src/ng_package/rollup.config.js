3fbdb37b0898e96926567cba15a4aae149d92cd0
atscott <atscott01@gmail.com>
2020-09-09 18:05:03 +0000

    f1b355b54f docs: Restructure table of contents to provide a more streamlined experience (#38763)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.0+43.sha-b1398d1/, version);
+      banner = banner.replace(/10.1.0+46.sha-f1b355b/, version);
     }
   }
 }
