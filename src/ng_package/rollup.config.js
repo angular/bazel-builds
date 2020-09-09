2c80e4780348a87564bb316d7ddf479c4ef054cd
atscott <atscott01@gmail.com>
2020-09-09 16:28:25 +0000

    19a484302d docs: Fix component decorator closing brackets (#38754) PR Close #38754

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.0+49.sha-3cb2a79/, version);
+      banner = banner.replace(/11.0.0-next.0+47.sha-19a4843/, version);
     }
   }
 }
