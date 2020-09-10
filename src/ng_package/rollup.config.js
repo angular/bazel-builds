d877f5d618cdc0a164425162730c2da3b2e9d7ac
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:16:52 +0000

    ce1efc1af2 fix(localize): ensure that `formatOptions` is optional (#38787)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+11.sha-d141516/, version);
+      banner = banner.replace(/11.0.0-next.1+12.sha-ce1efc1/, version);
     }
   }
 }
