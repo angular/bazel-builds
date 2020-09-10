1ec13c302b19c08a6238ee3bb1b9e611c8fafafa
Andrew Kushnir <akushnir@google.com>
2020-09-10 16:35:43 +0000

    d1415162cb fix(core): clear the `RefreshTransplantedView` when detached (#38768)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+9.sha-281865b/, version);
+      banner = banner.replace(/11.0.0-next.1+11.sha-d141516/, version);
     }
   }
 }
