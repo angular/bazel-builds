132520993a7a4d42efc20046c20698f540bd2772
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:56:05 +0000

    8f349b2375 fix(compiler): source span for microsyntax text att should be key span (#38766)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+21.sha-03447ba/, version);
+      banner = banner.replace(/11.0.0-next.1+20.sha-8f349b2/, version);
     }
   }
 }
