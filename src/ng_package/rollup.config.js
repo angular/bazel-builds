d0c44334545f2d6a4042b01f5fdc9abdbb1fbb1d
Andrew Kushnir <akushnir@google.com>
2020-09-10 22:57:05 +0000

    6acea54f62 fix(common): mark locale data arrays as readonly (#30397)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+22.sha-2d52c80/, version);
+      banner = banner.replace(/11.0.0-next.1+23.sha-6acea54/, version);
     }
   }
 }
