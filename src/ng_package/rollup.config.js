e391802e31e6393d8559ce9d1f360be4b6c21850
Andrew Kushnir <akushnir@google.com>
2020-09-11 20:43:44 +0000

    b05d79d14a build(router): update symbols for routing app (#38817)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.1+15.sha-8096c63/, version);
+      banner = banner.replace(/10.1.1+18.sha-b05d79d/, version);
     }
   }
 }
