00ead2e5d528f170083319ce8843e1ba71785e8f
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:22:01 +0000

    6768fe9927 fix(dev-infra): correct build setup for dev-infra (#38815)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+29.sha-57c442f/, version);
+      banner = banner.replace(/11.0.0-next.1+30.sha-6768fe9/, version);
     }
   }
 }
