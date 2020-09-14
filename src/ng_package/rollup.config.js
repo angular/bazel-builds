5123c42ef1acb989e8aaab5836786ca741b455dc
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:51:36 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+34.sha-297c060/, version);
+      banner = banner.replace(/11.0.0-next.1+35.sha-3934f0a/, version);
     }
   }
 }
