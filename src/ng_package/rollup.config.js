96ba1645e9174b2ca9cf43b058c7b7c34ef8dd2c
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:55:05 +0000

    26deef2d3e feat(dev-infra): allow local ng-dev configuration to error on invalid commit messages (#38784)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.1+22.sha-ebede67/, version);
+      banner = banner.replace(/10.1.1+24.sha-26deef2/, version);
     }
   }
 }
