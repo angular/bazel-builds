96eb09b49baf01617d0891a03db5c37bc0b16274
Andrew Kushnir <akushnir@google.com>
2020-09-14 19:16:15 +0000

    297c060ae7 perf(compiler-cli): optimize computation of type-check scope information (#38539)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+32.sha-e162da0/, version);
+      banner = banner.replace(/11.0.0-next.1+34.sha-297c060/, version);
     }
   }
 }
