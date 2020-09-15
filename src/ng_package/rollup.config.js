825ee509bb3208d23ee6c2f0f80c9a6b224f5fd1
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:28:27 +0000

    d192c87f6a refactor(dev-infra): refactor commit-message files (#38845)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+45.sha-3817e5f/, version);
+      banner = banner.replace(/11.0.0-next.1+46.sha-d192c87/, version);
     }
   }
 }
