fd07406545605eb115273169cb050fce8404feff
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:53:27 +0000

    171a0d0696 docs: add ngc to glossary (#36781)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+43.sha-a1c1c45/, version);
+      banner = banner.replace(/11.0.0-next.1+44.sha-171a0d0/, version);
     }
   }
 }
