1b64cf5f817d4ac2d600374c8ce58e736031cd1f
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:44:22 +0000

    a1c1c450dc test(ngcc): load standard files only once (#38840)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+40.sha-b9ca6d2/, version);
+      banner = banner.replace(/11.0.0-next.1+43.sha-a1c1c45/, version);
     }
   }
 }
