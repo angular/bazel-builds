4df5fdf5a4f7618660b4dd0eafdf35b368706d80
Andrew Kushnir <akushnir@google.com>
2020-09-15 00:14:25 +0000

    dc4f85888e build: create temporary script for symbol extractor tests (#38819)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.1+24.sha-26deef2/, version);
+      banner = banner.replace(/10.1.1+26.sha-dc4f858/, version);
     }
   }
 }
