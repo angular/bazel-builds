c9d2ac89ef1341dfcde13835cda5aa72a18e3c5b
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:09:48 +0000

    e00535a2a4 fix(dev-infra): remove ANSI escape codes from log file outputs (#38792)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.1+38.sha-593bd59/, version);
+      banner = banner.replace(/11.0.0-next.1+39.sha-e00535a/, version);
     }
   }
 }
