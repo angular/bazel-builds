ae8d02a6fc216c1a0c36bf87a914b8b0841eb346
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:53:25 +0000

    129107191c refactor(compiler): always return a mutable clone from `Scope#resolve` (#38857)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+13.sha-dd8d8c8/, version);
+      banner = banner.replace(/11.0.0-next.2+12.sha-1291071/, version);
     }
   }
 }
