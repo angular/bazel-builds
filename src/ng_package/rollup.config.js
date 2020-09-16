66d7f16c5ed7a2c9a06382e1dbd924b4cb4dedaa
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:47:53 +0000

    722699fb0c fix(dev-infra): handle no caretaker config being defined (#38862)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+8.sha-49ee90b/, version);
+      banner = banner.replace(/11.0.0-next.2+11.sha-722699f/, version);
     }
   }
 }
