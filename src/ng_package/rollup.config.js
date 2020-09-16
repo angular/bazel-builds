cdffa871f8f9fb79b9587dd82df5d1eb48736487
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:43:13 +0000

    5614258cc7 docs(zone.js): Add breaking changes info for zone.js v0.11.1 (#38821)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+8.sha-49ee90b/, version);
+      banner = banner.replace(/11.0.0-next.2+10.sha-5614258/, version);
     }
   }
 }
