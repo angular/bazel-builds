d316c6b10b89e78c0c82ce622e7d5ea080097262
Misko Hevery <misko@hevery.com>
2020-09-17 16:59:03 +0000

    c4b8964424 docs: drop `newEvent()` compatibility function (#37251)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+15.sha-a33d630/, version);
+      banner = banner.replace(/11.0.0-next.2+14.sha-c4b8964/, version);
     }
   }
 }
