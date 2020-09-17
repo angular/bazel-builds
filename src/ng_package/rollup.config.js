57f11820380a21a1100d53a38d40f98839a38b88
Misko Hevery <misko@hevery.com>
2020-09-17 16:54:18 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+12.sha-1291071/, version);
+      banner = banner.replace(/11.0.0-next.2+15.sha-a33d630/, version);
     }
   }
 }
