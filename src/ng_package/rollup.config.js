b996df9a4f5a1de6c35743031568db8a7857e5f4
Misko Hevery <misko@hevery.com>
2020-09-18 18:45:40 +0000

    3902ec0dbe fix(http): Fix error message when we call jsonp without importing HttpClientJsonpModule (#38756)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.2+16.sha-6b05dc4/, version);
+      banner = banner.replace(/10.1.2+21.sha-3902ec0/, version);
     }
   }
 }
