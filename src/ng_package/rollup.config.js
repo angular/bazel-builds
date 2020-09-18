fb7984d021ca11720c154f099d4293de1708e025
Misko Hevery <misko@hevery.com>
2020-09-18 15:27:51 +0000

    7fb388f929 docs: updating the text of user input page to reflect real location of the forms page (#38802)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+21.sha-d3169c5/, version);
+      banner = banner.replace(/11.0.0-next.2+18.sha-7fb388f/, version);
     }
   }
 }
