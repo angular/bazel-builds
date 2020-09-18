a263c4f3984d39e2a440e15db2774c0bc53c2247
Misko Hevery <misko@hevery.com>
2020-09-18 18:41:37 +0000

    2d6105a784 feat(dev-infra): output the number of new and fixed cycles (#38805)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+22.sha-5548571/, version);
+      banner = banner.replace(/11.0.0-next.2+25.sha-2d6105a/, version);
     }
   }
 }
