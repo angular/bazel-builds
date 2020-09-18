2514cc0ec6fbcb118188e94b324df81e5357a695
Misko Hevery <misko@hevery.com>
2020-09-18 15:28:42 +0000

    d795a00137 refactor(compiler): replace Comment nodes with leadingComments property (#38811)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+20.sha-e442486/, version);
+      banner = banner.replace(/11.0.0-next.2+19.sha-d795a00/, version);
     }
   }
 }
