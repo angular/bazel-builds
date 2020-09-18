343c311bf27b41f10b882aee8c3e07ee2638551b
Misko Hevery <misko@hevery.com>
2020-09-18 18:41:42 +0000

    55485713a3 fix(dev-infra): skip husky git commit hooks while merging pull requests (#38888)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+18.sha-7fb388f/, version);
+      banner = banner.replace(/11.0.0-next.2+22.sha-5548571/, version);
     }
   }
 }
