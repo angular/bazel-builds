a4061d5629652becdd6b2a365a26cfdd435f0bbd
Misko Hevery <misko@hevery.com>
2020-09-18 18:48:07 +0000

    d92a0dd72f fix(zone.js): should invoke xhr send task when no response error occurs (#38836)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+26.sha-88d7bb8/, version);
+      banner = banner.replace(/11.0.0-next.2+23.sha-d92a0dd/, version);
     }
   }
 }
