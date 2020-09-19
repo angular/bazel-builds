444c5d9c3d0508f82944dac381d47897bc521bfc
Misko Hevery <misko@hevery.com>
2020-09-19 00:17:01 +0000

    7d5b5153cf docs: Grammar fixes (#38900)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.2+21.sha-3902ec0/, version);
+      banner = banner.replace(/10.1.2+24.sha-7d5b515/, version);
     }
   }
 }
