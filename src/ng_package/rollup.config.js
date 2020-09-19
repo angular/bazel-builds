055be5d3b9440dbc9f215718e8deb906391ea845
Misko Hevery <misko@hevery.com>
2020-09-19 00:08:25 +0000

    b68fab547a ci: do not require g3 checks for the changes in `packages/zone.js/dist` folder (#38901)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+23.sha-d92a0dd/, version);
+      banner = banner.replace(/11.0.0-next.2+28.sha-b68fab5/, version);
     }
   }
 }
