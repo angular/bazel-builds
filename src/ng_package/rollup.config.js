9aa0ba33500c34c136c01e1aeb39067851021941
Misko Hevery <misko@hevery.com>
2020-09-21 19:56:16 +0000

    297b123151 refactor(compiler-cli): make the output AST translator generic (#38775)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+40.sha-6ae3b68/, version);
+      banner = banner.replace(/11.0.0-next.2+47.sha-297b123/, version);
     }
   }
 }
