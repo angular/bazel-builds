8c353f3e6fa73f573d16a474125e7bb29e9e47ab
Misko Hevery <misko@hevery.com>
2020-09-21 19:51:21 +0000

    0c0c54d615 refactor(compiler): simplify visitor logic for attributes (#38899)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+33.sha-dfbfabc/, version);
+      banner = banner.replace(/11.0.0-next.2+34.sha-0c0c54d/, version);
     }
   }
 }
