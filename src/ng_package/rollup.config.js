2f83bdf1295d3a644a5d38793bba38edd49c010c
Misko Hevery <misko@hevery.com>
2020-09-21 19:53:00 +0000

    49f27e31ed test(compiler-cli): re-enable dynamic value diagnostic tests on Windows CI (#37782)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+34.sha-0c0c54d/, version);
+      banner = banner.replace(/11.0.0-next.2+39.sha-49f27e3/, version);
     }
   }
 }
