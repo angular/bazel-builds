124eaab1d3dfb677f1be18286729e5655aaaf1b1
Misko Hevery <misko@hevery.com>
2020-09-22 22:35:02 +0000

    b1682526dd refactor(dev-infra): simplify runBenchmark (#38941)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+52.sha-ba3f4c2/, version);
+      banner = banner.replace(/11.0.0-next.2+54.sha-b168252/, version);
     }
   }
 }
