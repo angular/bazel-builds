3931cbfb1d5a6b12bc4069c3edb54d2d1e031979
Misko Hevery <misko@hevery.com>
2020-09-22 22:36:32 +0000

    75610505c6 refactor(zone.js): remove usages of blacklist related to UNPATCHED_EVENTS (#38930)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+54.sha-b168252/, version);
+      banner = banner.replace(/11.0.0-next.2+53.sha-7561050/, version);
     }
   }
 }
