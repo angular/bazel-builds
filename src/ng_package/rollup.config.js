e56457a36e36e174560e8d6e938e4f533602fbf1
Misko Hevery <misko@hevery.com>
2020-09-22 16:55:00 +0000

    a2068523fd feat(service-worker): add the option to prefer network for navigation requests (#38565)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.2+49.sha-145ab3d/, version);
+      banner = banner.replace(/11.0.0-next.2+50.sha-a206852/, version);
     }
   }
 }
