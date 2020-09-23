5065ae4ffb4d67731114855795f11d7385830f42
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:09:23 +0000

    f979914d4e test(platform-browser): remove usage of blacklist in test naming (#38928)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3/, version);
+      banner = banner.replace(/11.0.0-next.3+4.sha-f979914/, version);
     }
   }
 }
