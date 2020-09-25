9db3128c1ba72d71aa80db8b97325247726dda3c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:59:46 +0000

    e790c8547e test(compiler-cli): load test files into memory only once (#38909)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+17.sha-4ca1c73/, version);
+      banner = banner.replace(/11.0.0-next.3+14.sha-e790c85/, version);
     }
   }
 }
