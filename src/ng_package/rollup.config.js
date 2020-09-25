aef6d7577af18afc57c922ca09b75d67e710e1d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 19:00:08 +0000

    239968d2f1 feat(compiler): Add keySpan to Variable Node (#38965)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+14.sha-e790c85/, version);
+      banner = banner.replace(/11.0.0-next.3+20.sha-239968d/, version);
     }
   }
 }
