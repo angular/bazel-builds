847449e7093770d65cd570d6444a24727c969e5d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:58:13 +0000

    323be39297 fix(language-service): hybrid visitor returns parent node of BoundAttribute (#38995)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+20.sha-239968d/, version);
+      banner = banner.replace(/11.0.0-next.3+22.sha-323be39/, version);
     }
   }
 }
