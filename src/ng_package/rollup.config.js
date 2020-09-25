808288018abe3eecaad642c7cf47b1655de66525
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:54:18 +0000

    2f6777df18 docs(docs-infra): add upcoming conference (#38956)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+12.sha-7f6f8bb/, version);
+      banner = banner.replace(/11.0.0-next.3+18.sha-2f6777d/, version);
     }
   }
 }
