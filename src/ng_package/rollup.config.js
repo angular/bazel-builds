7b9a43bd8134353b876a5d3671d14e04e8cfeb62
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 21:00:33 +0000

    875fcfe5a9 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.3+13.sha-5bf5513/, version);
+      banner = banner.replace(/10.1.3+49.sha-875fcfe/, version);
     }
   }
 }
