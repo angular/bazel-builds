cc1ba5415d41ed989d2e079d981d883fec0d985c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:41:01 +0000

    f96dcc5ce0 feat(dev-infra): tool for staging and publishing releases (#38656)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+29.sha-4744c22/, version);
+      banner = banner.replace(/11.0.0-next.3+46.sha-f96dcc5/, version);
     }
   }
 }
