2ab9333dc56586e43c19fb4b366b599a43f7badb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:56:28 +0000

    06525cfed3 test(compiler-cli): fix tests to have at least one component (#39011)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+77.sha-3ba97ab/, version);
+      banner = banner.replace(/11.0.0-next.3+72.sha-06525cf/, version);
     }
   }
 }
