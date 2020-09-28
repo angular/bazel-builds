16e75bde5fdac5e0d7a21abdddac5d2ed525c29f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:50:24 +0000

    4a9f4520ce docs: remove usage of whitelist in cli analytics docs (#38963)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+62.sha-914d709/, version);
+      banner = banner.replace(/11.0.0-next.3+63.sha-4a9f452/, version);
     }
   }
 }
