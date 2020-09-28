1837772e60917da1bae1de114c7aeebeea993b3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:46:22 +0000

    914d7099ee build: remove usage of blacklist in benchmark tooling (#38926)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+64.sha-cb7164a/, version);
+      banner = banner.replace(/11.0.0-next.3+62.sha-914d709/, version);
     }
   }
 }
