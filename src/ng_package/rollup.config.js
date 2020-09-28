bc6b43d1c9435de488949e91f8af02726ec42ba2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 16:45:47 +0000

    4744c229db fix(common): correct typing and implementation of `SlicePipe` (#37447)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+22.sha-323be39/, version);
+      banner = banner.replace(/11.0.0-next.3+29.sha-4744c22/, version);
     }
   }
 }
