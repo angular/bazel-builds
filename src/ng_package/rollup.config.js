ed793bf5fdfc44f233d10450d8a543222185dcff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:59:35 +0000

    31e42f0947 refactor(language-service): create getTypeDefinitionAtPosition stubs (#39050)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+95.sha-ddc9e8e/, version);
+      banner = banner.replace(/11.0.0-next.3+96.sha-31e42f0/, version);
     }
   }
 }
