becff799a89f8c86940ceed7501e3aabbf929338
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:50:27 +0000

    8f11b516f8 refactor(compiler-cli): API for getting components from a template file (#39002)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+90.sha-393ce55/, version);
+      banner = banner.replace(/11.0.0-next.3+87.sha-8f11b51/, version);
     }
   }
 }
