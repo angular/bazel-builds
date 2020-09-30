1bf8089f10ba2776fbe4405404d1dba16b039064
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:52:04 +0000

    393ce5574b ci: do not require g3 checks for the changes in `ngtsc/sourcemaps` folder (#39035)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+88.sha-8f66540/, version);
+      banner = banner.replace(/11.0.0-next.3+90.sha-393ce55/, version);
     }
   }
 }
