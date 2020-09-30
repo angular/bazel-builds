146c081acbb3efaddbdcfcc0f9d5d84698ef0fd8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 16:07:35 +0000

    c8d757624b refactor(dev-infra): use common git client for git environment reset (#39051)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+96.sha-31e42f0/, version);
+      banner = banner.replace(/11.0.0-next.3+97.sha-c8d7576/, version);
     }
   }
 }
