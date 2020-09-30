05fa27e0b3de468bcd2db7125461f5fd52962575
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:47:31 +0000

    8f66540152 test(language-service): Update AppComponent test project to include more fields (#39033)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+82.sha-ded9aeb/, version);
+      banner = banner.replace(/11.0.0-next.3+88.sha-8f66540/, version);
     }
   }
 }
