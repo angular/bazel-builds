6a1720470909e30c250980064dc01f442c748eb8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:00:51 +0000

    ddc9e8e47a refactor(compiler): refactor template symbol builder (#39047)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+92.sha-827245f/, version);
+      banner = banner.replace(/11.0.0-next.3+95.sha-ddc9e8e/, version);
     }
   }
 }
