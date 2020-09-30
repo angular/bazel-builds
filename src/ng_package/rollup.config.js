cf7283f9bd7a49a2a6ead4c180ceb16b1374ade9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:14:37 +0000

    a1bf0de711 release: cut the zone.js-0.11.2 release (#39032)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.3+60.sha-69302ad/, version);
+      banner = banner.replace(/10.1.3+72.sha-a1bf0de/, version);
     }
   }
 }
