7a7da2b48dafa29bf222e06cb59b39c9a7bc3322
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:41:01 +0000

    1b70dc931d docs: edit template-statements doc (#38742)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.3+80.sha-a6971ba/, version);
+      banner = banner.replace(/11.0.0-next.3+78.sha-1b70dc9/, version);
     }
   }
 }
