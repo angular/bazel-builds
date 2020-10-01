19c3f07bd8c6aecf0770a4b5bce13df0da747f2f
Joey Perrott <josephperrott@gmail.com>
2020-10-01 15:55:52 +0000

    fb12c617a4 ci: change required labels for issue triage (#38932)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+11.sha-ea19683/, version);
+      banner = banner.replace(/11.0.0-next.4+12.sha-fb12c61/, version);
     }
   }
 }
