77d32874b57037c9095c151579074c2bb4d6ea66
Joey Perrott <josephperrott@gmail.com>
2020-10-01 19:53:14 +0000

    4bce21358d feat(dev-infra): add a command to verify NgBot YAML config syntax (#39071)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+25.sha-6205ed0/, version);
+      banner = banner.replace(/11.0.0-next.4+26.sha-4bce213/, version);
     }
   }
 }
