158609c725b89d8a621e12beef11af43ece790f4
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:58:17 +0000

    ac54019315 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+23.sha-8894706/, version);
+      banner = banner.replace(/11.0.0-next.4+22.sha-ac54019/, version);
     }
   }
 }
