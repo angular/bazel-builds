02e7c39bac089d2f74785ebcf80ad25843160905
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:57:13 +0000

    889470663d fix(core): use single quotes for relative link resolution migration to align with style guide (#39070)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+16.sha-7dd0db6/, version);
+      banner = banner.replace(/11.0.0-next.4+23.sha-8894706/, version);
     }
   }
 }
