7a37c23b9cfe390922e0aad1c06eff8e0b151cd1
Joey Perrott <josephperrott@gmail.com>
2020-10-02 21:53:20 +0000

    5dbf357224 feat(compiler-cli): support getting resource dependencies for a source file (#38048)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+34.sha-3bbbf39/, version);
+      banner = banner.replace(/11.0.0-next.4+35.sha-5dbf357/, version);
     }
   }
 }
