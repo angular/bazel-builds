010e22546cd414197f4967ced92a5d85ae5dbe62
Joey Perrott <josephperrott@gmail.com>
2020-10-02 19:08:10 +0000

    3bbbf39b58 feat(compiler): Recover on malformed keyed reads and keyed writes (#39004)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+32.sha-4fe673d/, version);
+      banner = banner.replace(/11.0.0-next.4+34.sha-3bbbf39/, version);
     }
   }
 }
