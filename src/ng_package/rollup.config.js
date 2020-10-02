22441358a2d455b08c131ae174725c94cd4eab5a
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:23:32 +0000

    904adb72d2 feat(language-service): add quick info for inline templates in ivy (#39060)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+31.sha-d3bd00b/, version);
+      banner = banner.replace(/11.0.0-next.4+33.sha-904adb7/, version);
     }
   }
 }
