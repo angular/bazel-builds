8f9e324f130e62bba8ea635b525d1e6504c4ad1d
Joey Perrott <josephperrott@gmail.com>
2020-10-05 23:35:57 +0000

    faa81dcdd1 refactor(language-service): Extract common methods from VE and Ivy to new package (#39098)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+46.sha-e10b3e2/, version);
+      banner = banner.replace(/11.0.0-next.4+47.sha-faa81dc/, version);
     }
   }
 }
