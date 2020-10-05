e93409c335d32e56baec665c01c7ca878c192382
Joey Perrott <josephperrott@gmail.com>
2020-10-05 22:14:12 +0000

    e10b3e22ac refactor(compiler): Add ngModule to directive symbol (#39099)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+45.sha-57f8dd2/, version);
+      banner = banner.replace(/11.0.0-next.4+46.sha-e10b3e2/, version);
     }
   }
 }
