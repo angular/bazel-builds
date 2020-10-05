a47854417de7c95e5e16e794aee4cd3421f9565c
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:05:11 +0000

    26afcdd86c docs: Made a minor change in the documentation (#38917) PR Close #38917

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+41.sha-75e90fa/, version);
+      banner = banner.replace(/11.0.0-next.4+40.sha-26afcdd/, version);
     }
   }
 }
