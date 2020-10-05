cd7dbfc9c033d1f82fad0fc7431042dedb3c1786
Joey Perrott <josephperrott@gmail.com>
2020-10-05 17:44:46 +0000

    878e2f0deb feat(dev-infra): show CI status of all active release trains (#39067)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.4+23.sha-4c30f51/, version);
+      banner = banner.replace(/10.1.4+24.sha-878e2f0/, version);
     }
   }
 }
