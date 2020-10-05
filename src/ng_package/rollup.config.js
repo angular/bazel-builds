edb8b3d1d6238de19588a7adc2a98015ca7226d6
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:52:07 +0000

    bc69182bdd docs: Migrate section, view encapsulation, from Component Styles topic into its own topic. (#38986)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+43.sha-82d54fe/, version);
+      banner = banner.replace(/11.0.0-next.4+44.sha-bc69182/, version);
     }
   }
 }
