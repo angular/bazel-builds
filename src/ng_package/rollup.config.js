21cd139e3155f885f3fa0927ab40efbfcfccb596
Joey Perrott <josephperrott@gmail.com>
2020-10-05 23:59:53 +0000

    d8c05347c3 fix(router): properly assign ExtraOptions to Router in RouterTestingModule (#39096)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+47.sha-faa81dc/, version);
+      banner = banner.replace(/11.0.0-next.4+48.sha-d8c0534/, version);
     }
   }
 }
