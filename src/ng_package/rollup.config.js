c0beeab68e7451177f06d444b90954f252b00d3a
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:05:03 +0000

    75e90fa456 docs(common): update docs for HttpClient methods (#38878)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+39.sha-f315022/, version);
+      banner = banner.replace(/11.0.0-next.4+41.sha-75e90fa/, version);
     }
   }
 }
