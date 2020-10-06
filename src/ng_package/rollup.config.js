f246aa4be3d588fbfe7ccb9f3b3581bf748d3e28
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:31:28 +0000

    3c474ecf56 fix(common): add boolean to valid json for testing (#37893)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+49.sha-ee03408/, version);
+      banner = banner.replace(/11.0.0-next.4+50.sha-3c474ec/, version);
     }
   }
 }
