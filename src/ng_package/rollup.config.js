385ba57d460438c2a5842a75f81e91c6b06595c6
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:33:42 +0000

    21dfb33b27 build: update bazelversion (#39123)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+50.sha-3c474ec/, version);
+      banner = banner.replace(/11.0.0-next.4+51.sha-21dfb33/, version);
     }
   }
 }
