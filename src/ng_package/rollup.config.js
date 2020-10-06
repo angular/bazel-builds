da1d02656987b73e4b0e1b566883644b130063b3
Joey Perrott <josephperrott@gmail.com>
2020-10-06 15:59:40 +0000

    07a66fc4c2 docs: remove explicit boolean type in examples as TS infers it automatically (#39081)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+52.sha-6e0a8eb/, version);
+      banner = banner.replace(/11.0.0-next.4+53.sha-07a66fc/, version);
     }
   }
 }
