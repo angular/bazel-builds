0c0290a436767293bbb67d417e1720e3b09486ff
Joey Perrott <josephperrott@gmail.com>
2020-10-06 20:47:11 +0000

    3975dd90a6 feat(language-service): Add getDefinitionAndBoundSpan (go to definition) (#39101)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+56.sha-05672ab/, version);
+      banner = banner.replace(/11.0.0-next.4+57.sha-3975dd9/, version);
     }
   }
 }
