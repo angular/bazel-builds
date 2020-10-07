0a9ef33b4e13e9e78641b11bc5be60625c5efafa
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:56:53 +0000

    cc13b37446 build(docs-infra): render optional decorator options with a ? (#39167)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.4+33.sha-9f132d0/, version);
+      banner = banner.replace(/10.1.4+36.sha-cc13b37/, version);
     }
   }
 }
