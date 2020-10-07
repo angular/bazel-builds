0921e1c5498ce510f8399fa95272d5b049b655ed
atscott <atscott01@gmail.com>
2020-10-07 22:17:34 +0000

    4ede190571 refactor(compiler-cli): wrap RHS of property write in parens (#39143)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.4+63.sha-2932706/, version);
+      banner = banner.replace(/11.0.0-next.5+1.sha-4ede190/, version);
     }
   }
 }
