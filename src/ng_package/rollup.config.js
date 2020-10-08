567ec77f7ca0129dc15f7d54a80de0a722aa2f12
atscott <atscott01@gmail.com>
2020-10-08 19:21:49 +0000

    4a1c12c773 feat(core): remove ViewEncapsulation.Native (#38882)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+8.sha-42a164f/, version);
+      banner = banner.replace(/11.0.0-next.5+13.sha-4a1c12c/, version);
     }
   }
 }
