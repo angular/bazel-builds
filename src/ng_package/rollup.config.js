61c4160f0f5f4d75eb130154e0b7a5706f830b9e
atscott <atscott01@gmail.com>
2020-10-08 16:08:45 +0000

    8cf16b4815 feat(docs-infra): add short url for strict mode guide (#39129)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.5+2.sha-eb34037/, version);
+      banner = banner.replace(/10.1.5+4.sha-8cf16b4/, version);
     }
   }
 }
