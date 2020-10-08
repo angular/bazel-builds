b368fa7cd2345fb67e56356a316e5ff8246ffc01
atscott <atscott01@gmail.com>
2020-10-08 17:00:33 +0000

    dace4aacb6 docs: fix for HttpRequest expansion panel (#39166)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+5.sha-63624a2/, version);
+      banner = banner.replace(/11.0.0-next.5+6.sha-dace4aa/, version);
     }
   }
 }
