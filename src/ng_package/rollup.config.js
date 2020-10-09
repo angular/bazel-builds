4b74ded6983e58ccb35dc312613555c85971dc99
atscott <atscott01@gmail.com>
2020-10-09 15:59:58 +0000

    7f689a291a fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+14.sha-ca4ef61/, version);
+      banner = banner.replace(/11.0.0-next.5+15.sha-7f689a2/, version);
     }
   }
 }
