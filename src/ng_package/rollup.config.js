99c2a3addb6be3541e49200c8fa0df77b5a0564f
atscott <atscott01@gmail.com>
2020-10-09 15:21:21 +0000

    8913aee527 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.5+6.sha-7628c36/, version);
+      banner = banner.replace(/10.1.5+7.sha-8913aee/, version);
     }
   }
 }
