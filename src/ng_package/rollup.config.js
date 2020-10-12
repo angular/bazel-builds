96a49c3dd5aef58c419a905e1c5d8c791c856a3a
atscott <atscott01@gmail.com>
2020-10-12 20:02:48 +0000

    3487d5a13a docs: move Inputs and Outputs topic to Components section (#38984)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.5+17.sha-1efc75c/, version);
+      banner = banner.replace(/10.1.5+18.sha-3487d5a/, version);
     }
   }
 }
