8c8caa10b6213d6daf7b828efba78a900dd5763e
atscott <atscott01@gmail.com>
2020-10-12 20:18:49 +0000

    b0b4953fd6 fix(router): Allow undefined inputs on routerLink (#39151)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+46.sha-72f4497/, version);
+      banner = banner.replace(/11.0.0-next.5+47.sha-b0b4953/, version);
     }
   }
 }
