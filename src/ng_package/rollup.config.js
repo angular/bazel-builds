8dd27875b39b2ac20ee42133d56776bcdf9b3067
atscott <atscott01@gmail.com>
2020-10-12 15:33:38 +0000

    a84976fdfc feat(language-service): Add getTypeDefinitionAtPosition (go to type definition) (#39145)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+17.sha-9f3388e/, version);
+      banner = banner.replace(/11.0.0-next.5+19.sha-a84976f/, version);
     }
   }
 }
