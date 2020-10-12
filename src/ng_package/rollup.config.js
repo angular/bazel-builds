b19d7f146f2da1ab254dff604d216f7add642e39
atscott <atscott01@gmail.com>
2020-10-12 16:15:25 +0000

    952710b43b refactor(compiler-cli): ensure `isNamed....()` helpers check name is identity (#38959)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+22.sha-e930b0c/, version);
+      banner = banner.replace(/11.0.0-next.5+40.sha-952710b/, version);
     }
   }
 }
