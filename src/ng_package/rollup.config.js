5dca910805b6d04f690a2e468ee61b506e40336a
atscott <atscott01@gmail.com>
2020-10-12 18:08:53 +0000

    5471789664 refactor(animations): do not assign to innerHTML (#37397)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+40.sha-952710b/, version);
+      banner = banner.replace(/11.0.0-next.5+41.sha-5471789/, version);
     }
   }
 }
