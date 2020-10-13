9e335f5ddc32d2d3e265f616e97aa755e185d2ce
atscott <atscott01@gmail.com>
2020-10-13 22:18:58 +0000

    b642f0bf45 docs(zone.js): update changelog to add jest useFakeTimers feature (#39126)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+51.sha-0875fd2/, version);
+      banner = banner.replace(/11.0.0-next.5+53.sha-b642f0b/, version);
     }
   }
 }
