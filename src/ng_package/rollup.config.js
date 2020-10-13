3eb04c1eee16282a896668cfca35e86f12f36f46
atscott <atscott01@gmail.com>
2020-10-13 22:21:07 +0000

    23fc2b43ac updated sajee's profile (#39019)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.5+18.sha-3487d5a/, version);
+      banner = banner.replace(/10.1.5+20.sha-23fc2b4/, version);
     }
   }
 }
