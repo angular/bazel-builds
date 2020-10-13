e7e2f12fe8c92693fd5382141f97b62fb5f9493a
atscott <atscott01@gmail.com>
2020-10-13 19:21:08 +0000

    0875fd2360 feat(core): create internal Trusted Types module (#39207)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+49.sha-5ce71e0/, version);
+      banner = banner.replace(/11.0.0-next.5+51.sha-0875fd2/, version);
     }
   }
 }
