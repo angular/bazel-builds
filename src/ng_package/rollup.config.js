b54999354e91d379ded43289f07562c55969cd80
atscott <atscott01@gmail.com>
2020-10-14 17:01:15 +0000

    f6d5cdfbd7 fix(core): use Trusted Types policy in named_array_type (#39209)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+68.sha-898be92/, version);
+      banner = banner.replace(/11.0.0-next.5+67.sha-f6d5cdf/, version);
     }
   }
 }
