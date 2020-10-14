e1e6ef417f89e6f96414e313813724afbf71d7f7
atscott <atscott01@gmail.com>
2020-10-14 16:57:29 +0000

    5221df8e92 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.5+20.sha-23fc2b4/, version);
+      banner = banner.replace(/10.1.5+22.sha-5221df8/, version);
     }
   }
 }
