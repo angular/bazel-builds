306342fa800269e427686bcd9da1beac6306c5dc
atscott <atscott01@gmail.com>
2020-10-14 18:10:10 +0000

    c6350116b5 docs: add Discord as an official communication channel (#39149)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+67.sha-f6d5cdf/, version);
+      banner = banner.replace(/11.0.0-next.5+69.sha-c635011/, version);
     }
   }
 }
