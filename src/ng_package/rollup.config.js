814e0a5cf1b68340e4efe4457be906d08018927d
atscott <atscott01@gmail.com>
2020-10-14 20:13:20 +0000

    0ec7043490 feat(core): add initialNavigation schematic (#36926)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+69.sha-c635011/, version);
+      banner = banner.replace(/11.0.0-next.5+74.sha-0ec7043/, version);
     }
   }
 }
