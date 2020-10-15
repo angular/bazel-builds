b47c23394e57c9030df4595cc82c3e2f3bc5ce3c
atscott <atscott01@gmail.com>
2020-10-15 16:33:16 +0000

    497af77d7a build(zone.js): zone.js should output esm format for fesm2015 bundles (#39203)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+13.sha-6e18d2d/, version);
+      banner = banner.replace(/11.0.0-next.6+11.sha-497af77/, version);
     }
   }
 }
