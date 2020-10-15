0aaf450ea8354358836d288d271f738ef903acee
Andrew Kushnir <akushnir@google.com>
2020-10-15 18:42:33 +0000

    14f15ecd38 ci: add jessicajaniuk to require-minimum-review reviewers (#39268)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+14.sha-56ab6d5/, version);
+      banner = banner.replace(/11.0.0-next.6+15.sha-14f15ec/, version);
     }
   }
 }
