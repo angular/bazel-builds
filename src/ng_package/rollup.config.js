8c8904ade46dbe7431daeadac718cb8684f04c76
Andrew Kushnir <akushnir@google.com>
2020-10-15 21:40:47 +0000

    7669827436 docs: fix typo at console.log (#39252)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+23.sha-f9f3c54/, version);
+      banner = banner.replace(/11.0.0-next.6+22.sha-7669827/, version);
     }
   }
 }
