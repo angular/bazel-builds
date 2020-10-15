93461e69e79ababc78b3884fad5461473ef91ddb
Andrew Kushnir <akushnir@google.com>
2020-10-15 21:39:17 +0000

    6acddb9518 refactor(language-service): Return directive defs when input name is part of selector (#39243)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+15.sha-14f15ec/, version);
+      banner = banner.replace(/11.0.0-next.6+22.sha-6acddb9/, version);
     }
   }
 }
