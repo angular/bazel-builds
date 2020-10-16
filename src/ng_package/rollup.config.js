45165bf22a633c7c8a367e8bf6e92cbb32d4bf6f
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:31:43 +0000

    492e236ae6 docs: Typos fixes in built-in directives guide (#38520)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+36.sha-4dc8d83/, version);
+      banner = banner.replace(/11.0.0-next.6+35.sha-492e236/, version);
     }
   }
 }
