dd598f6f71d0d44a1eb6a01ab450060d7aaf2b2e
Andrew Kushnir <akushnir@google.com>
2020-10-16 23:31:18 +0000

    8a744d5246 refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+33.sha-6c9c70a/, version);
+      banner = banner.replace(/11.0.0-next.6+34.sha-8a744d5/, version);
     }
   }
 }
