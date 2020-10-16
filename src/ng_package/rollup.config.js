a19cfa75dc01ebc46349fe768f26c219f8c8efb6
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:44:29 +0000

    4a5f17cd8e build(docs-infra): add a tool to create new examples (#39283)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.6+16.sha-94d7ef3/, version);
+      banner = banner.replace(/10.1.6+18.sha-4a5f17c/, version);
     }
   }
 }
