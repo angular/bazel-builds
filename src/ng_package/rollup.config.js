fd7abff3aebe27af9dca4118dfd4863564905e31
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:21:41 +0000

    94d7ef3909 refactor(core): renames checkNoChangesMode to be clearer (#39277)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.6+14.sha-af17038/, version);
+      banner = banner.replace(/10.1.6+16.sha-94d7ef3/, version);
     }
   }
 }
