ad61d06d61a6670542d07faa452367a0f599ffa4
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:24:47 +0000

    96f59f64a0 docs: Very minor spelling mistake (#39299)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+24.sha-5bda62c/, version);
+      banner = banner.replace(/11.0.0-next.6+25.sha-96f59f6/, version);
     }
   }
 }
