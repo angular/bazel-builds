a038c36a43e071a43163a41cb69d0333b4e1812f
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:42:37 +0000

    a0fde29f80 release: cut the zone.js-0.11.2 release to fix several new issues (#39298)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+26.sha-fdbc611/, version);
+      banner = banner.replace(/11.0.0-next.6+30.sha-a0fde29/, version);
     }
   }
 }
