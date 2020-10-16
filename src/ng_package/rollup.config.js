5f9357fe189f5fa2fca6c9ca3db6bb878e56af1d
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:12:33 +0000

    6c9c70a0e4 ci: separate the windows CI tests into build and test (#39289)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+32.sha-d2ea9b0/, version);
+      banner = banner.replace(/11.0.0-next.6+33.sha-6c9c70a/, version);
     }
   }
 }
