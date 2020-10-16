72cdfc265b8a60e44640bd8dedc56616b713dade
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:59:00 +0000

    563fb6cdbe feat(language-service): Implement go to definition for style and template urls (#39202)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+41.sha-1b21350/, version);
+      banner = banner.replace(/11.0.0-next.6+39.sha-563fb6c/, version);
     }
   }
 }
