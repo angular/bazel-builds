fe5c743b9498b00719ce5319ed822d0361688ac2
Andrew Kushnir <akushnir@google.com>
2020-10-19 23:47:56 +0000

    cd8ec3d902 docs: add Jessica Janiuk to contributors.json (#39253)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.6+27.sha-02b8750/, version);
+      banner = banner.replace(/10.1.6+28.sha-cd8ec3d/, version);
     }
   }
 }
