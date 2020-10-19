698c4503f2f047e85db958b19eca533aafa7ecd4
Andrew Kushnir <akushnir@google.com>
2020-10-19 18:46:40 +0000

    7827d3ee73 docs: change definition of NgModules in Angular concepts guide (#36179)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.1.6+24.sha-38db976/, version);
+      banner = banner.replace(/10.1.6+25.sha-7827d3e/, version);
     }
   }
 }
