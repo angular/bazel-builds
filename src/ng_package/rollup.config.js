502dc946e0923b06cd52b666ee50854442bfea61
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:53:09 +0000

    d3087be237 feat(dev-infra): prompt caretaker to confirm the merge branches on merge (#39333)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+38.sha-aba1cb4/, version);
+      banner = banner.replace(/11.0.0-next.6+39.sha-d3087be/, version);
     }
   }
 }
