d956948754b1831bc522ff583f2ac1c77cfae023
Andrew Kushnir <akushnir@google.com>
2020-10-19 15:33:29 +0000

    a93b7fd674 build(docs-infra): upgrade cli command docs sources to 04ae7cbf5 (#39318)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+43.sha-79d67dc/, version);
+      banner = banner.replace(/11.0.0-next.6+47.sha-a93b7fd/, version);
     }
   }
 }
