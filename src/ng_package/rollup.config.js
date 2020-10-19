df496fdb1d66bcb65b7f94aca5f240b421b61319
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:53:34 +0000

    f5a3e44e9d refactor(compiler): remove support for TypeScript 3.9 (#39313)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+51.sha-767fdcc/, version);
+      banner = banner.replace(/11.0.0-next.6+53.sha-f5a3e44/, version);
     }
   }
 }
