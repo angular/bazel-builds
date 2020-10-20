2b237b13d4a240d74392b8c3f3ef3250f4c46d6a
Andrew Kushnir <akushnir@google.com>
2020-10-20 20:57:55 +0000

    9fb7bdea89 fix(router): incorrect signature for createUrlTree (#39347)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+59.sha-a7964f4/, version);
+      banner = banner.replace(/11.0.0-next.6+60.sha-9fb7bde/, version);
     }
   }
 }
