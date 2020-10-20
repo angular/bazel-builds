8a48c2127e72c30d5a5ef7f93f7506353ed57db2
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:16:13 +0000

    0f1a18e26f build: upgrade karma to version 4.4.0

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.5+2.sha-a8c0972/, version);
+      banner = banner.replace(/11.0.0-next.6+52.sha-0f1a18e/, version);
     }
   }
 }
