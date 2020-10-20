ac95d4642df0968b18cd6215c81defc9d851b317
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:45:38 +0000

    a67895cee3 docs: Add Component Overview topic to angular.io (#39186)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+54.sha-7040f17/, version);
+      banner = banner.replace(/11.0.0-next.6+57.sha-a67895c/, version);
     }
   }
 }
