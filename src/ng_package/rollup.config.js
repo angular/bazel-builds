d2d6a661531d3db31a0a7551d7fabac6998bf401
Andrew Kushnir <akushnir@google.com>
2020-10-21 19:28:03 +0000

    eb4c05d97a build: update bazelversion (#39351)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+67.sha-3525b2f/, version);
+      banner = banner.replace(/11.0.0-next.6+69.sha-eb4c05d/, version);
     }
   }
 }
