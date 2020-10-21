f3fab61e3bfd3016fcd3b571083ab1b3d24fa984
Andrew Kushnir <akushnir@google.com>
2020-10-21 15:51:03 +0000

    ef2d07fdd1 docs: remove `style` commit type from CONTRIBUTING.md (#39357)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+61.sha-cfac8e0/, version);
+      banner = banner.replace(/11.0.0-next.6+64.sha-ef2d07f/, version);
     }
   }
 }
