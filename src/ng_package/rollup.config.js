f4a3de9afa502004fb706f0df83760bbeaf19840
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:01:02 +0000

    b4e8399144 fix(platform-server): Resolve absolute URL from baseUrl (#39334)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+54.sha-d8c9f42/, version);
+      banner = banner.replace(/11.0.0-next.6+55.sha-b4e8399/, version);
     }
   }
 }
