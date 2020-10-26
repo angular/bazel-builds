09afa7f5e997059cf1ca6be525363447b4f9daf0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:12:41 +0000

    cf60e0e889 docs: use SimpleChanges in component interaction guide (#39342)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+99.sha-90c0772/, version);
+      banner = banner.replace(/11.0.0-next.6+100.sha-cf60e0e/, version);
     }
   }
 }
