8cb903f90a575a49021d8b71d44cfb11bd80e8a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:12:10 +0000

    35fdc6d92f build(docs-infra): ignore `doc.basePath` when generating keywords for each document (#39409)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+100.sha-cf60e0e/, version);
+      banner = banner.replace(/11.0.0-next.6+103.sha-35fdc6d/, version);
     }
   }
 }
