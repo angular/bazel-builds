477174ffe56fc744d25ea2de0cd856d61be5a485
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-27 22:20:04 +0000

    e649f1dda6 ci: bust cache of stored node_modules on CircleCI (#39461)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+102.sha-2b09f5b/, version);
+      banner = banner.replace(/11.0.0-next.6+130.sha-e649f1d/, version);
     }
   }
 }
