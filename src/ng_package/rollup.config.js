ec8cb9899295dd7f92c98634a12845321ce866ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:14:58 +0000

    b9358c2ad5 docs: add carlos caballero to GDE resources (#37976)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+132.sha-e6ca3d3/, version);
+      banner = banner.replace(/11.0.0-next.6+133.sha-b9358c2/, version);
     }
   }
 }
