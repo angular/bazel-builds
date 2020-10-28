0865e7c5b115288864366b7a52c50ccbfdfcd4dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 17:27:15 +0000

    e6ca3d3841 refactor(core): add top 10 runtime error codes (#39188)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+131.sha-0723331/, version);
+      banner = banner.replace(/11.0.0-next.6+132.sha-e6ca3d3/, version);
     }
   }
 }
