f51447ac1be1157b72276e1c392c7e77a35230e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:24:09 +0000

    fdb6b98d80 docs: add `enableI18nLegacyMessageIdFormat` option to Angular compiler options guide (#39453)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+136.sha-371fb9a/, version);
+      banner = banner.replace(/11.0.0-next.6+142.sha-fdb6b98/, version);
     }
   }
 }
