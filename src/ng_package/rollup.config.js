ff4e2d7683babeb7b19f277ac1234e2947904a40
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:19:29 +0000

    7c45cbc523 docs: fix links to Material examples (#39093)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+133.sha-b9358c2/, version);
+      banner = banner.replace(/11.0.0-next.6+134.sha-7c45cbc/, version);
     }
   }
 }
