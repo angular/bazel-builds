b8a4651983dabe4eb6cca2ef04836b145a3cbdfb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:24:36 +0000

    20c26cd49b build: add PullApprove: disable as a caretaker note label (#39430)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+137.sha-1508fd2/, version);
+      banner = banner.replace(/11.0.0-next.6+140.sha-20c26cd/, version);
     }
   }
 }
