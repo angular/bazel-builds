2611577ea647b3e5bd7a0cb3a49754739ee0f054
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:37:59 +0000

    6d51ae2571 docs(core): add jessicajaniuk to pullapprove list (#39473)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+144.sha-62a58e2/, version);
+      banner = banner.replace(/11.0.0-next.6+145.sha-6d51ae2/, version);
     }
   }
 }
