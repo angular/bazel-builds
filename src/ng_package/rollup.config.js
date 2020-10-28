10184b10c80bd1fbf0227cd2b335b96c7a9fc04e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:38:52 +0000

    bf05c979a7 release: cut the v11.0.0-rc.1 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.0+70.sha-ddc070e/, version);
+      banner = banner.replace(/11.0.0-rc.1/, version);
     }
   }
 }
