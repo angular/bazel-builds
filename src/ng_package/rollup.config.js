853ab3cf1faf0e1ff62e7835c8a6e148f8053445
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:09:00 +0000

    dd0ba3f4ab docs: rename `ng xi18n` to `ng extract-i18n` (#39337)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+149.sha-e3d2f46/, version);
+      banner = banner.replace(/11.0.0-next.6+150.sha-dd0ba3f/, version);
     }
   }
 }
