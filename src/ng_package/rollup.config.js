56008371621fbbcdd01fcf0953eb61a60bfd356b
Joey Perrott <josephperrott@gmail.com>
2020-10-29 19:45:43 +0000

    8d324ec314 ci: update NgBot config to include more labels that indicate that ticket is triaged (#39494)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+157.sha-f54662e/, version);
+      banner = banner.replace(/11.0.0-next.6+158.sha-8d324ec/, version);
     }
   }
 }
