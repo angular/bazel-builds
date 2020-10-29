0f41676c070a75602ac8b8e44285b0269d87dc8d
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:39:37 +0000

    4a8d5ae970 fix(core): markDirty() should only mark flags when really scheduling tick. (#39316)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+162.sha-170af07/, version);
+      banner = banner.replace(/11.0.0-next.6+163.sha-4a8d5ae/, version);
     }
   }
 }
