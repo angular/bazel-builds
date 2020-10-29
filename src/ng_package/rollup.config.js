4cfeced26affeafe7f96e5ec049b71e8768fa3fa
Joey Perrott <josephperrott@gmail.com>
2020-10-29 18:42:38 +0000

    8af32fcb16 docs(forms): update ngModel documentation (#39481)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+155.sha-d6a7659/, version);
+      banner = banner.replace(/11.0.0-next.6+156.sha-8af32fc/, version);
     }
   }
 }
