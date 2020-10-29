504151f35fe4027d725897ed6d4d895f6ced61c0
Joey Perrott <josephperrott@gmail.com>
2020-10-29 17:16:29 +0000

    cff61b2abe fix(dev-infra): update generated ng-dev compiled output (#39474)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.2.1+4.sha-9205fce/, version);
+      banner = banner.replace(/10.2.1+8.sha-cff61b2/, version);
     }
   }
 }
