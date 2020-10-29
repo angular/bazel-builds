b3477a316c287c2315ee99640c64e60ba8c49856
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:50:00 +0000

    c83b2ad87f docs: tView.preOrderHooks and tView.preOrderCheckHooks docs update (#39497)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+163.sha-4a8d5ae/, version);
+      banner = banner.replace(/11.0.0-next.6+165.sha-c83b2ad/, version);
     }
   }
 }
