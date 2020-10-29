c4d5aa04098a26cbee2bbfbb4e0bdfddc33de738
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:03:21 +0000

    0e60dc5597 release: cut the zone.js-0.11.3 release (#39440)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.1+12.sha-e45da28/, version);
+      banner = banner.replace(/11.0.0-rc.1+13.sha-0e60dc5/, version);
     }
   }
 }
