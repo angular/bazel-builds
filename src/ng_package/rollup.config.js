cfdcd7bdd8db7ce6f597d99800287017c8fa9b80
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:05:33 +0000

    3241d922fc refactor(compiler): parse bindings "by hand" rather than via regex (#39375)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+165.sha-c83b2ad/, version);
+      banner = banner.replace(/11.0.0-next.6+166.sha-3241d92/, version);
     }
   }
 }
