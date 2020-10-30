792ac312b42459cec23c57e947e08c088f3e4e56
Joey Perrott <josephperrott@gmail.com>
2020-10-30 23:48:23 +0000

    b2f3952946 fix(router): Ensure all outlets are used when commands have a prefix (#39456)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.1+19.sha-eb625b9/, version);
+      banner = banner.replace(/11.0.0-rc.1+21.sha-b2f3952/, version);
     }
   }
 }
