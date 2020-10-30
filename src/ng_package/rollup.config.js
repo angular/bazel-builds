bb93cd4f82c33eaa3bb9224a28e2596ba7c0dc91
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:48:30 +0000

    feda78e1c8 fix(common): change the week-numbering year format from `r` -> `Y` (#39495)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.1+16.sha-3091534/, version);
+      banner = banner.replace(/11.0.0-rc.1+17.sha-feda78e/, version);
     }
   }
 }
