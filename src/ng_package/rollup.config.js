e025042eda7eaead70b8126054f1cea9644c66f0
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:24:07 +0000

    3b0b7d2210 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+172.sha-3af585c/, version);
+      banner = banner.replace(/11.0.0-next.6+173.sha-3b0b7d2/, version);
     }
   }
 }
