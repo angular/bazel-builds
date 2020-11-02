56d91d9db0d91d5c93b93709c7e4f3c09e5b049e
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:59:02 +0000

    a0b6548a94 build(docs-infra): ensure that deployment works on CI (#39535)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.1+34.sha-40f38ce/, version);
+      banner = banner.replace(/11.0.0-rc.1+35.sha-a0b6548/, version);
     }
   }
 }
