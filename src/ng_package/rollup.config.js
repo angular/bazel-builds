691c6bad920878cb5ed4ff9a5bfcb6ae59ed33ed
Joey Perrott <josephperrott@gmail.com>
2020-11-02 16:28:19 +0000

    cf48d508af build(docs-infra): upgrade `firebase-tools` to v8.14.1 (#39470)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+173.sha-3b0b7d2/, version);
+      banner = banner.replace(/11.0.0-next.6+184.sha-cf48d50/, version);
     }
   }
 }
