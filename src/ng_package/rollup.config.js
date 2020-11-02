dc7a95141790eb3c2ccd2ba2e39cff2f1cfb3088
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:24:29 +0000

    40f38ce7df docs: update forwardRef example (#39519)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.1+33.sha-d386fb3/, version);
+      banner = banner.replace(/11.0.0-rc.1+34.sha-40f38ce/, version);
     }
   }
 }
