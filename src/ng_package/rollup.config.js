49d9c8cf63cec77aec6c35e0deafd25fc40e9af2
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:04:15 +0000

    db1c48ff36 ci: only check testing directories within packages/ for fw-testing (#39536)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+191.sha-be64f98/, version);
+      banner = banner.replace(/11.0.0-next.6+190.sha-db1c48f/, version);
     }
   }
 }
