c5343bc6d14faacb7f13ba38a9cb7bf6ca060271
Joey Perrott <josephperrott@gmail.com>
2020-11-03 22:11:03 +0000

    aa3bd27cbf build: update dev dependency on @angular/cli to 10.2.0 (#39553)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.2.1+15.sha-71d0063/, version);
+      banner = banner.replace(/10.2.1+29.sha-aa3bd27/, version);
     }
   }
 }
