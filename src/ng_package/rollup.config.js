f91928f7d3e9166a78d71eb11ed806c9b4e3b1d4
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:13:22 +0000

    08e077c0e0 docs: fix typo in the "Property Binding" guide (#39569) PR Close #39569

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.2.1+33.sha-8929355/, version);
+      banner = banner.replace(/10.2.2+1.sha-08e077c/, version);
     }
   }
 }
