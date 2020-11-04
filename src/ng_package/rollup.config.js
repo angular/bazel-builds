b3bd513ddb1807a9a42e499c5b53f1ba52cf8da0
Joey Perrott <josephperrott@gmail.com>
2020-11-04 20:22:17 +0000

    27ae0604c6 test(compiler-cli): resolve test failure (#39568)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+195.sha-2d79780/, version);
+      banner = banner.replace(/11.0.0-next.6+205.sha-27ae060/, version);
     }
   }
 }
