ae907bb88dce61fba7aa0b5ce2ddbe7ffe87676c
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:05:52 +0000

    3701376894 docs: release notes for the v10.2.2 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+205.sha-27ae060/, version);
+      banner = banner.replace(/11.0.0-next.6+206.sha-3701376/, version);
     }
   }
 }
