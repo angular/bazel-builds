105100d17338d65760fcb59c4e33c3ffe72b1e31
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:41:57 +0000

    1e91f84697 docs: fix Android 10 codename (#39399)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+193.sha-d0dd0e8/, version);
+      banner = banner.replace(/11.0.0-next.6+194.sha-1e91f84/, version);
     }
   }
 }
