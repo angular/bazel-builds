70de3a47fddc2ac194a493598c185ecc77b716c0
Joey Perrott <josephperrott@gmail.com>
2020-11-04 19:20:12 +0000

    43b4940c9d fix(forms): remove validators while cleaning up a control (#39234)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.1+42.sha-450830c/, version);
+      banner = banner.replace(/11.0.0-rc.1+44.sha-43b4940/, version);
     }
   }
 }
