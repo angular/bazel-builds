16a7d386b30f829df263305fd6ddecdee3216296
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:56:55 +0000

    7ed0f0f7d0 fix(dev-infra): run caretaker checks asyncronously (#39086)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.2.1+30.sha-250858a/, version);
+      banner = banner.replace(/10.2.1+32.sha-7ed0f0f/, version);
     }
   }
 }
