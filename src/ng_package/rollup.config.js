3890f5336dfea87d9c95dab57a8c117d4941607b
Misko Hevery <misko@hevery.com>
2020-11-05 19:47:51 +0000

    27358eb60f feat(zone.js): monkey patches queueMicrotask() (#38904)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+209.sha-6138bc2/, version);
+      banner = banner.replace(/11.0.0-next.6+210.sha-27358eb/, version);
     }
   }
 }
