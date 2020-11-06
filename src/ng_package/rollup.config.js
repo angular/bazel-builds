b65edb125430be66694322c9234e0fa3aa2af992
Misko Hevery <misko@hevery.com>
2020-11-06 17:57:31 +0000

    dd28855455 fix(elements): update the view of an `OnPush` component when inputs change (#39452)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-rc.2+5.sha-b8e9b3d/, version);
+      banner = banner.replace(/11.0.0-rc.2+14.sha-dd28855/, version);
     }
   }
 }
