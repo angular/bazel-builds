de139fe079826901827042dc28bd2b847d036923
Misko Hevery <misko@hevery.com>
2020-11-06 17:57:57 +0000

    290ea57a93 fix(core): Access injected parent values using SelfSkip (#39464)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+217.sha-b9b9178/, version);
+      banner = banner.replace(/11.0.0-next.6+218.sha-290ea57/, version);
     }
   }
 }
