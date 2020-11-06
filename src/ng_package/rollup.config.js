4cc2d51d91160db02bf14865f4296cfe3c3bad18
Misko Hevery <misko@hevery.com>
2020-11-06 17:37:34 +0000

    8e384d9f0d docs: typo fix for 'Intall' (#39585)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+214.sha-1f95618/, version);
+      banner = banner.replace(/11.0.0-next.6+215.sha-8e384d9/, version);
     }
   }
 }
