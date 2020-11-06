a5fd4e0db209c9e9861ecf14a5096cd96729ad3b
Misko Hevery <misko@hevery.com>
2020-11-06 23:51:12 +0000

    861e4fa627 fix(compiler-cli): avoid duplicate diagnostics about unknown pipes (#39517)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.2.2+7.sha-1c6cf8a/, version);
+      banner = banner.replace(/10.2.2+8.sha-861e4fa/, version);
     }
   }
 }
