750211dc7c36c796c3f9b78a5dbdb41bba596823
Misko Hevery <misko@hevery.com>
2020-11-06 17:27:52 +0000

    1f956184c4 fix(compiler): skipping leading whitespace should not break placeholder source-spans (#39486)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+210.sha-27358eb/, version);
+      banner = banner.replace(/11.0.0-next.6+214.sha-1f95618/, version);
     }
   }
 }
