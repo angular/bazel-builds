51111b1e4f81b805385ddccab96f00acae9596fb
Misko Hevery <misko@hevery.com>
2020-11-09 16:10:06 +0000

    48d15fc760 docs: update cli deprecations (#39547)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+229.sha-0edf6fc/, version);
+      banner = banner.replace(/11.0.0-next.6+227.sha-48d15fc/, version);
     }
   }
 }
