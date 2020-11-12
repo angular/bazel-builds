85ba7df7dd0dd8514455350f8a3c57a953c7af13
atscott <atscott01@gmail.com>
2020-11-12 22:48:21 +0000

    21651d362d refactor(compiler-cli): add keySpan to text attributes (#39613)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+247.sha-ade6da9/, version);
+      banner = banner.replace(/11.0.0-next.6+248.sha-21651d3/, version);
     }
   }
 }
