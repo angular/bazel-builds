a6876955c6c21f2fa569d563e034e56c3594c422
Misko Hevery <misko@hevery.com>
2020-11-12 00:04:16 +0000

    8a03e3ee48 release: cut the v11.0.0 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+232.sha-5205db5/, version);
+      banner = banner.replace(/11.0.0-next.6+233.sha-8a03e3e/, version);
     }
   }
 }
