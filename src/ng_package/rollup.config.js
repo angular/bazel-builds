4e94cd0e0a703caac33ac3af82b6d439f53284d8
atscott <atscott01@gmail.com>
2020-11-12 22:16:48 +0000

    7ce825011b docs: archive di-navtree doc (#39543)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+244.sha-c8ed497/, version);
+      banner = banner.replace(/11.0.0-next.6+243.sha-7ce8250/, version);
     }
   }
 }
