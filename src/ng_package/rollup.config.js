3398f92e3441c7cb1752873b4b5b96b1c4722f0a
atscott <atscott01@gmail.com>
2020-11-12 23:40:31 +0000

    8a1c98c5e8 refactor(compiler-cli): add keySpan to reference nodes (#39616)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+249.sha-c33326c/, version);
+      banner = banner.replace(/11.0.0-next.6+250.sha-8a1c98c/, version);
     }
   }
 }
