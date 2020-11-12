689422abe87530b9c4fc88514c3485fd07b41439
atscott <atscott01@gmail.com>
2020-11-12 20:01:55 +0000

    20db90a41d fix(core): not inserting ViewContainerRef nodes when inside root of a component (#39599)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0+6.sha-050cea9/, version);
+      banner = banner.replace(/11.0.0+7.sha-20db90a/, version);
     }
   }
 }
