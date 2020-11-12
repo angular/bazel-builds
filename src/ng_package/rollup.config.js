66fed21f7b7db73c54d77e5298288022df4c1af0
atscott <atscott01@gmail.com>
2020-11-12 22:13:16 +0000

    a87236292e docs: edit two-way binding doc (#38952)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+240.sha-b015d3e/, version);
+      banner = banner.replace(/11.0.0-next.6+241.sha-a872362/, version);
     }
   }
 }
