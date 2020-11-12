0ec6dbf837da2f690c215c4c5fc5516b7e401e9b
atscott <atscott01@gmail.com>
2020-11-12 17:28:40 +0000

    3be344cdc2 docs: correct appearance of numbering (#39638)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+234.sha-6d13473/, version);
+      banner = banner.replace(/11.0.0-next.6+235.sha-3be344c/, version);
     }
   }
 }
