6813bbe4c383f687af074ebc65e08bf2a462d866
Misko Hevery <misko@hevery.com>
2020-11-12 00:07:48 +0000

    6d1347363e docs: update the roadmap with our priorities at Q4 (#39635)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+233.sha-8a03e3e/, version);
+      banner = banner.replace(/11.0.0-next.6+234.sha-6d13473/, version);
     }
   }
 }
