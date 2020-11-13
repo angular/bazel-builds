c20d905517195b3dd8be297e28053d712b7382dd
atscott <atscott01@gmail.com>
2020-11-13 22:00:37 +0000

    7f3314a1e8 ci: update angular-robot config for L2 triage of PRs (#39655)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0+19.sha-83a6ab1/, version);
+      banner = banner.replace(/11.0.0+20.sha-7f3314a/, version);
     }
   }
 }
