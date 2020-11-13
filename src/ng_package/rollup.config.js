033c5c0b4e8910627a22c9b90b3abab648e6f3e1
atscott <atscott01@gmail.com>
2020-11-13 20:21:26 +0000

    24e13e0ed6 refactor(core): document setComponentScope to provide context (#39662)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+259.sha-25d6fca/, version);
+      banner = banner.replace(/11.0.0-next.6+261.sha-24e13e0/, version);
     }
   }
 }
