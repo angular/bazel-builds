4879e2c243935bdd4b9bf9b1c53eace363b7c613
atscott <atscott01@gmail.com>
2020-11-13 01:02:11 +0000

    06de803963 ci: fix payload size (#39666)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0+7.sha-20db90a/, version);
+      banner = banner.replace(/11.0.0+14.sha-06de803/, version);
     }
   }
 }
