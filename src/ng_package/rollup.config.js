26b5cdbca0fe6e61e10ecfa14abf8f0b18949609
atscott <atscott01@gmail.com>
2020-11-16 20:06:57 +0000

    9f20942f89 ci: fix integration payload size (#39713)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+265.sha-b0c4ecf/, version);
+      banner = banner.replace(/11.0.0-next.6+280.sha-9f20942/, version);
     }
   }
 }
