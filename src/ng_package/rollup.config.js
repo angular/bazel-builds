9d999ed16f6f0ef310fe1618edcfd584d11a48b1
atscott <atscott01@gmail.com>
2020-11-16 17:40:01 +0000

    b0c4ecf563 docs: improve commit message format documentation (#39705)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+263.sha-5e92d64/, version);
+      banner = banner.replace(/11.0.0-next.6+265.sha-b0c4ecf/, version);
     }
   }
 }
