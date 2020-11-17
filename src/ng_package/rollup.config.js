1529578ef9e30a912a92340e817434dc9f3de857
atscott <atscott01@gmail.com>
2020-11-17 20:16:47 +0000

    209a18c624 docs: remove bazel from deprecations doc (#39540)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+282.sha-d281ea8/, version);
+      banner = banner.replace(/11.0.0-next.6+283.sha-209a18c/, version);
     }
   }
 }
