82303f6c86d7462e0f70041979d45f55eee3f78b
atscott <atscott01@gmail.com>
2020-11-17 23:08:21 +0000

    d39c4bbe37 refactor(language-service): language_service_adapter -> adapters (#39619)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+289.sha-3926665/, version);
+      banner = banner.replace(/11.0.0-next.6+292.sha-d39c4bb/, version);
     }
   }
 }
