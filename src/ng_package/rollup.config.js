b1bbfb24f3b1641ade7f7475151a8db26b946200
atscott <atscott01@gmail.com>
2020-11-17 20:21:36 +0000

    806d7aa22c docs: add documentation for ngNonBindable (#36560)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+283.sha-209a18c/, version);
+      banner = banner.replace(/11.0.0-next.6+284.sha-806d7aa/, version);
     }
   }
 }
