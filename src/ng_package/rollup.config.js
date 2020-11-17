79e0c8f7c67ae6fda4d865abad08a2918867dc1b
atscott <atscott01@gmail.com>
2020-11-17 20:22:51 +0000

    b6893d23c5 test(language-service): introduce new virtual testing environment (#39594)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+284.sha-806d7aa/, version);
+      banner = banner.replace(/11.0.0-next.6+288.sha-b6893d2/, version);
     }
   }
 }
