17d8ed768e395c0454f1548249461d2e32735383
atscott <atscott01@gmail.com>
2020-11-18 17:45:27 +0000

    3b2e5be6cb refactor(core): clean up circular dependencies (#39722)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+296.sha-7d68f92/, version);
+      banner = banner.replace(/11.0.0-next.6+299.sha-3b2e5be/, version);
     }
   }
 }
