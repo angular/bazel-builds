6a898d41abc9fd8c088eef19041b90b45546cdc7
atscott <atscott01@gmail.com>
2020-11-18 19:30:40 +0000

    ea2312ea85 release: cut the v11.1.0-next.0 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+307.sha-79bd1bd/, version);
+      banner = banner.replace(/11.1.0-next.0/, version);
     }
   }
 }
