e7e0d870da59d9b22f2edf86a412afedc4536226
atscott <atscott01@gmail.com>
2020-11-18 17:36:11 +0000

    e4028ae5c4 build(docs-infra): compute previous version list (#39689)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+295.sha-b33b89d/, version);
+      banner = banner.replace(/11.0.0-next.6+297.sha-e4028ae/, version);
     }
   }
 }
