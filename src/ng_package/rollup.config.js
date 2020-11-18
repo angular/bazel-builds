d852986eeb9c6d8507349fb7740536807ca95dcc
atscott <atscott01@gmail.com>
2020-11-18 20:31:53 +0000

    3273856d33 docs: fixes on paragraph (#39664)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+3.sha-cc509d6/, version);
+      banner = banner.replace(/11.1.0-next.0+4.sha-3273856/, version);
     }
   }
 }
