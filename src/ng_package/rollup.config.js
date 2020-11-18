00e78187f1096aa0ec66b214fb8d15a1382c41ae
atscott <atscott01@gmail.com>
2020-11-18 18:48:12 +0000

    7261e393f1 refactor(dev-infra): extract the parsing of a range of commits into a util function (#39726)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.0-next.6+306.sha-d20cab1/, version);
+      banner = banner.replace(/11.0.0-next.6+300.sha-7261e39/, version);
     }
   }
 }
