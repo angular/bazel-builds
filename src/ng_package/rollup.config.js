ebe093308fa50ccb05dc804b00a1901bb9921d05
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:37:42 +0000

    935cf433ed fix(docs-infra): support recovering from unrecoverable SW states (#39651)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+22.sha-066126a/, version);
+      banner = banner.replace(/11.1.0-next.0+21.sha-935cf43/, version);
     }
   }
 }
