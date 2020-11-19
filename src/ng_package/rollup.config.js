6e7c6689e3783f295faa0ad3835b1e34694c7fe7
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:45:27 +0000

    be998e830b refactor(core): move `injectAttributeImpl` to avoid cycles (#37085)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+31.sha-a965589/, version);
+      banner = banner.replace(/11.1.0-next.0+33.sha-be998e8/, version);
     }
   }
 }
