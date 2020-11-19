f1987612141eed5abbfa3735ce2c1bbbb8fbb69a
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:06:18 +0000

    33639db911 release: cut the v11.0.2 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.1+17.sha-2fe2104/, version);
+      banner = banner.replace(/11.0.2/, version);
     }
   }
 }
