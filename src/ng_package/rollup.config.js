ce118adaf88361dda5dd038e594cfb6baaf61a86
Andrew Kushnir <akushnir@google.com>
2020-11-19 17:23:43 +0000

    61506404a9 docs: move di-in-action doc to conceptual ref section (#39544)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+15.sha-1951342/, version);
+      banner = banner.replace(/11.1.0-next.0+16.sha-6150640/, version);
     }
   }
 }
