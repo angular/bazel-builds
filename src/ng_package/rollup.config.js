dbe553d2d274c8b8d9162b92e4cb8317272e5560
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:35:51 +0000

    5b45fab19d build: update to latest "yargs" package (#39749)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+13.sha-d001ecc/, version);
+      banner = banner.replace(/11.1.0-next.0+14.sha-5b45fab/, version);
     }
   }
 }
