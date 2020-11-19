31afa714fe8bf5033997877e8fb718b3211d792c
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:41:12 +0000

    a965589eb8 feat(core): adds get method to QueryList (#36907)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+24.sha-1eb4066/, version);
+      banner = banner.replace(/11.1.0-next.0+31.sha-a965589/, version);
     }
   }
 }
