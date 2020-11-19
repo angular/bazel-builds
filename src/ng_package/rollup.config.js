d9d46b7956327c8f79b6871ef0675b1fb338a4a8
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:34:28 +0000

    066126ae2f fix(core): remove duplicated noop function (#39761)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+18.sha-0e85938/, version);
+      banner = banner.replace(/11.1.0-next.0+22.sha-066126a/, version);
     }
   }
 }
