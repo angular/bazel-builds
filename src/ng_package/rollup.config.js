2b7ab30ebe1aa5314aafa14b7822f1dcbcb563c0
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:13:19 +0000

    b521b5eb39 build(docs-infra): update @angular/material to 11.0.0 (#39600)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+5.sha-dc697d3/, version);
+      banner = banner.replace(/11.1.0-next.0+11.sha-b521b5e/, version);
     }
   }
 }
