3ce37cd6419aa5aae333b30e6aed0a0627eb4ac7
Pete Bacon Darwin <pete@bacondarwin.com>
2020-11-19 00:38:01 +0000

    1806d6e0c5 build: update to latest "yargs" package

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/10.2.3/, version);
+      banner = banner.replace(/10.2.3+1.sha-1806d6e/, version);
     }
   }
 }
