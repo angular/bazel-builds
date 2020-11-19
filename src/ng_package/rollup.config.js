6154cd882308ceeab3f859a2c731decd2cdd4860
Andrew Kushnir <akushnir@google.com>
2020-11-19 21:08:52 +0000

    17d5266cdb docs: add ngx-skeleton-loader module into resources page (#39352)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+33.sha-be998e8/, version);
+      banner = banner.replace(/11.1.0-next.0+35.sha-17d5266/, version);
     }
   }
 }
