8eef17fe000683e5d0cde2f94e75c09dfb5850e7
Andrew Kushnir <akushnir@google.com>
2020-11-20 16:55:42 +0000

    66d863febe fix(animations): getAnimationStyle causes exceptions in older browsers (#29709)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+36.sha-fe1ada8/, version);
+      banner = banner.replace(/11.1.0-next.0+38.sha-66d863f/, version);
     }
   }
 }
