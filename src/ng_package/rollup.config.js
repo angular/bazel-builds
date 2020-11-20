701c50aaadc2cc67c4d0b5af63299c55d0badcdf
Andrew Kushnir <akushnir@google.com>
2020-11-20 16:54:20 +0000

    fe1ada86e8 docs: add missing return to example storage.service.ts (#31434)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+35.sha-17d5266/, version);
+      banner = banner.replace(/11.1.0-next.0+36.sha-fe1ada8/, version);
     }
   }
 }
