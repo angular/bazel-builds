a9811bbd9a722f1cc6a678f20cbb69b797daa6b4
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:04:42 +0000

    1cba56e11f refactor(core): remove unused fakeAsyncFallback and asyncFallback (#37879)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+38.sha-66d863f/, version);
+      banner = banner.replace(/11.1.0-next.0+39.sha-1cba56e/, version);
     }
   }
 }
