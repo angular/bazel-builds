e0dbb89701429eb019849271b8b9539f561ee8e9
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:46:12 +0000

    156f9f30ae docs: edit di providers doc (#39403)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+43.sha-c33a823/, version);
+      banner = banner.replace(/11.1.0-next.0+45.sha-156f9f3/, version);
     }
   }
 }
