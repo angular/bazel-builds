9a09d0d8a0fe23d0476fb6a5576912f291c74250
Andrew Kushnir <akushnir@google.com>
2020-11-20 21:35:53 +0000

    3e1e5a15ba docs: update links to use HTTPS as protocol (#39718)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+46.sha-ff0a90e/, version);
+      banner = banner.replace(/11.1.0-next.0+51.sha-3e1e5a1/, version);
     }
   }
 }
