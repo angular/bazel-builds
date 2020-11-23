b26cb40982efadf41e8ca61c87b2eedac6d52d3a
Andrew Kushnir <akushnir@google.com>
2020-11-23 23:22:24 +0000

    7bf73d7442 fix(service-worker): correctly handle failed cache-busted request (#39786)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.2+42.sha-7e52be3/, version);
+      banner = banner.replace(/11.0.2+43.sha-7bf73d7/, version);
     }
   }
 }
