a22ac3f6e9f67bcd8f6835355184a962da041a8a
Andrew Kushnir <akushnir@google.com>
2020-11-23 20:30:01 +0000

    5a49465ce0 fix(dev-infra): verify the version of generated build artifacts (#39789)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+60.sha-938abc0/, version);
+      banner = banner.replace(/11.1.0-next.0+63.sha-5a49465/, version);
     }
   }
 }
