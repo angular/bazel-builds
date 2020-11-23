6752465c6a0bb1a424a244bc539e6f0477bc3f09
Andrew Kushnir <akushnir@google.com>
2020-11-23 20:32:39 +0000

    726d7c123c ci: migrate rebase-pr CI script to the circleci directory (#39592)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+63.sha-5a49465/, version);
+      banner = banner.replace(/11.1.0-next.0+61.sha-726d7c1/, version);
     }
   }
 }
