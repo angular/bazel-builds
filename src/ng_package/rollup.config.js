7232acfffe22a6a8674e01723f3e420a4da1e5ae
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:03:56 +0000

    88fbc06677 docs: add an exception for accepting contributions (#39611)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+67.sha-2c275f6/, version);
+      banner = banner.replace(/11.1.0-next.0+66.sha-88fbc06/, version);
     }
   }
 }
