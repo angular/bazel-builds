8e1d417c1019aee3c2fb6ccc06bdfd3b2ea0ad4b
Andrew Kushnir <akushnir@google.com>
2020-11-24 19:13:08 +0000

    39a47c21f9 docs(core): update links to schema.json of @schematics/angular (#39827)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+71.sha-a1b6ad0/, version);
+      banner = banner.replace(/11.1.0-next.0+72.sha-39a47c2/, version);
     }
   }
 }
