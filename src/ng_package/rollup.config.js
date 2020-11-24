fc473565ccc4978f4d49a7907064b4938d3213bc
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:15:06 +0000

    453b32f4b9 fix(compiler-cli): report error when a reference target is missing instead of crashing (#39805)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+69.sha-b5c0f9d/, version);
+      banner = banner.replace(/11.1.0-next.0+70.sha-453b32f/, version);
     }
   }
 }
