587ee161db5b4b3ca58ed3c1f61629f2b7ba07ef
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:31:50 +0000

    e75244ec00 feat(compiler-cli): support for partial compilation of components (#39707)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+72.sha-39a47c2/, version);
+      banner = banner.replace(/11.1.0-next.0+82.sha-e75244e/, version);
     }
   }
 }
