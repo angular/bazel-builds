5c5d5475f53a0fb130a1b4c005eaa2b894f0e605
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:08:49 +0000

    b5c0f9d7d1 feat(platform-browser): add doubletap HammerJS support (#26362)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+66.sha-88fbc06/, version);
+      banner = banner.replace(/11.1.0-next.0+69.sha-b5c0f9d/, version);
     }
   }
 }
