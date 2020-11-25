869442750020b5b68cd29725142acb74f7b8d9c2
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:20:10 +0000

    5684ac5e34 feat(localize): support Application Resource Bundle (ARB) translation file format (#36795)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+89.sha-23c36a2/, version);
+      banner = banner.replace(/11.1.0-next.0+91.sha-5684ac5/, version);
     }
   }
 }
