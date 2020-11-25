952e1887c4588d162524e70d0f331dd0b63f112a
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 23:10:33 +0000

    68d4a74411 fix(core): Ensure OnPush ancestors are marked dirty when events occur (#39833)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+103.sha-e148382/, version);
+      banner = banner.replace(/11.1.0-next.0+104.sha-68d4a74/, version);
     }
   }
 }
