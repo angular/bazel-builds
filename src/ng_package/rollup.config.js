0c9f9f48e63e0e0f9d87b769a42b839454e47b6b
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:39:11 +0000

    e2040af412 docs: fix typo in two-way-binding guide (#39859)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+107.sha-df97fbc/, version);
+      banner = banner.replace(/11.1.0-next.0+108.sha-e2040af/, version);
     }
   }
 }
