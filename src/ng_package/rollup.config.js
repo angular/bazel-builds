18186dc283cc3b64236f404fba7c0c949dd403f1
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:28:41 +0000

    1539c64fb0 docs(common): change HTTPResponse to HttpResponse (#39860) PR Close #39860

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+110.sha-c622b17/, version);
+      banner = banner.replace(/11.1.0-next.0+112.sha-1539c64/, version);
     }
   }
 }
