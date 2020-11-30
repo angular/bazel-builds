18789f1f566935b3c19dbbbc581c88e9ddaa4e97
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:36:36 +0000

    df97fbc077 docs: added a missing ` in inputs-outputs.md (#39856)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+105.sha-93d689f/, version);
+      banner = banner.replace(/11.1.0-next.0+107.sha-df97fbc/, version);
     }
   }
 }
