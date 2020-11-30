554134cb4b45da4d7068894b0755b5a98fba5a0f
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:54:04 +0000

    9a2f83b826 docs: edit Getting Started start/index.md copy and headers (#39583)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+111.sha-c5619ca/, version);
+      banner = banner.replace(/11.1.0-next.0+106.sha-9a2f83b/, version);
     }
   }
 }
