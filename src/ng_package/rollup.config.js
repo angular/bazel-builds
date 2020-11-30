70e5f6dc4d787061baa2d2b6455e66f1e49166da
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:46:34 +0000

    c5619ca56e test(compiler-cli): convert styling compliance tests (#39881)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+108.sha-e2040af/, version);
+      banner = banner.replace(/11.1.0-next.0+111.sha-c5619ca/, version);
     }
   }
 }
