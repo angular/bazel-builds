7fbdb7a6750a3f2a6e56f7def72b9f308c5d11f0
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 00:44:06 +0000

    11cd37fae3 fix(core): not invoking object's toString when rendering to the DOM (#39843)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+115.sha-1de59d2/, version);
+      banner = banner.replace(/11.1.0-next.0+116.sha-11cd37f/, version);
     }
   }
 }
