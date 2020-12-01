e9a166aad99e618fae41506c3a2278bd4eb40b9e
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 18:42:01 +0000

    0b5b24abd7 docs: edit interpolation doc (#38687)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+116.sha-11cd37f/, version);
+      banner = banner.replace(/11.1.0-next.0+117.sha-0b5b24a/, version);
     }
   }
 }
