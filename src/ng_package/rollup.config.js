22ada480f86f7406a35dee408e5cae7d9254be8a
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:00:11 +0000

    518fbeac9b ci: enable the Selenium Promise Manager in AIO e2e tests to avoid flakiness on CI (#39905)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.2+74.sha-425e329/, version);
+      banner = banner.replace(/11.0.2+76.sha-518fbea/, version);
     }
   }
 }
