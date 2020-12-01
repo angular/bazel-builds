609b1ec9908007f3edc6ffefecaf3d099f3eb4d2
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 23:40:38 +0000

    eac85b4e23 test(compiler-cli): migrate view compliance tests (#39914)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.0+125.sha-7695671/, version);
+      banner = banner.replace(/11.1.0-next.0+126.sha-eac85b4/, version);
     }
   }
 }
