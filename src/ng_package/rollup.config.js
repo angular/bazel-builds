5f86e74c5125bd76c3925619d17e4b02b1842c01
Misko Hevery <misko@hevery.com>
2020-12-02 23:24:29 +0000

    790ca09e04 docs: remove superfluous "NOTE:" (#39933)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+25.sha-8d613c1/, version);
+      banner = banner.replace(/11.1.0-next.1+21.sha-790ca09/, version);
     }
   }
 }
