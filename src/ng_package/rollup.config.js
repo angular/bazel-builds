df4b608a649fd45021b468346b22ec3a7eb79ad4
Misko Hevery <misko@hevery.com>
2020-12-02 23:28:46 +0000

    efd0d33391 test(compiler-cli): convert bindings compliance tests (#39862)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+21.sha-790ca09/, version);
+      banner = banner.replace(/11.1.0-next.1+24.sha-efd0d33/, version);
     }
   }
 }
