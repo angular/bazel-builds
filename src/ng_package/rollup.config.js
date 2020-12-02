c9739bbc32bf684da7a3f9edaee152ca9185c310
Misko Hevery <misko@hevery.com>
2020-12-02 21:25:37 +0000

    a55e01b89c test(core): verify `onDestroy` callbacks are invoked when ComponentRef is destroyed (#39876)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+12.sha-eba185e/, version);
+      banner = banner.replace(/11.1.0-next.1+20.sha-a55e01b/, version);
     }
   }
 }
