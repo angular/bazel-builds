735c8510d0bee117b93d17be4de2e36a23f65837
Misko Hevery <misko@hevery.com>
2020-12-02 23:21:27 +0000

    8d613c1d42 feat(compiler): allow trailing comma in array literal (#22277)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+22.sha-744f46c/, version);
+      banner = banner.replace(/11.1.0-next.1+25.sha-8d613c1/, version);
     }
   }
 }
