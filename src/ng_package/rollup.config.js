a036cc92e7cc852c17ac05bd60354cb4b250b10d
Misko Hevery <misko@hevery.com>
2020-12-02 21:19:00 +0000

    75fc89384d refactor(compiler-cli): expose TTC method to determine if file is tracked shim (#39768)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+6.sha-c923aaf/, version);
+      banner = banner.replace(/11.1.0-next.1+18.sha-75fc893/, version);
     }
   }
 }
