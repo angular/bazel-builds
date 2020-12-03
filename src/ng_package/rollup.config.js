2a35b5a393259a6c8103529c4c46b3bb57aa8219
Misko Hevery <misko@hevery.com>
2020-12-03 19:07:30 +0000

    e84ea8d6a7 docs: add alvarocamillont to GDE resources (#39138)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+26.sha-2b84882/, version);
+      banner = banner.replace(/11.1.0-next.1+27.sha-e84ea8d/, version);
     }
   }
 }
