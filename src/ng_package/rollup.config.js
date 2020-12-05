498c5bc87da90b4ac892e6017556305b77c570a7
Misko Hevery <misko@hevery.com>
2020-12-05 00:31:08 +0000

    7954c8dfa3 perf(forms): use `ngDevMode` to tree-shake `_ngModelWarning` (#39964)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+45.sha-d2042a0/, version);
+      banner = banner.replace(/11.1.0-next.1+49.sha-7954c8d/, version);
     }
   }
 }
