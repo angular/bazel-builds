8fb7ef8c3e8534029e32bee6b1a3e6d0c8960899
Misko Hevery <misko@hevery.com>
2020-12-07 21:44:11 +0000

    6bb6dfd616 test(compiler-cli): Add test for checkTypeOfDomReferences = false (#39539)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.3+39.sha-6d62971/, version);
+      banner = banner.replace(/11.0.3+41.sha-6bb6dfd/, version);
     }
   }
 }
