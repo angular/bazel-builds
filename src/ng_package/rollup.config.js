09c395194f8771353d4c3bf0105d8812c1c9a223
Misko Hevery <misko@hevery.com>
2020-12-07 21:45:56 +0000

    7901ebafa0 build(docs-infra): upgrade cli command docs sources to 2eb97bf5b (#39943)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+56.sha-e92d8a8/, version);
+      banner = banner.replace(/11.1.0-next.1+55.sha-7901eba/, version);
     }
   }
 }
