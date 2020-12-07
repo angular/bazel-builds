18fd2ff08a140f9149a79f7f610845eac9c4ef96
Misko Hevery <misko@hevery.com>
2020-12-07 18:15:14 +0000

    6d19c16d32 build: remove check-env script (#39980)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+51.sha-d9356f2/, version);
+      banner = banner.replace(/11.1.0-next.1+52.sha-6d19c16/, version);
     }
   }
 }
