ce7ea79b2795469bf0c91f0d648b897b04d5ebbd
Misko Hevery <misko@hevery.com>
2020-12-07 18:14:20 +0000

    d9356f2842 feat(dev-infra): close pull requests on merge which do not target master branch (#39979)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+50.sha-a8aeb74/, version);
+      banner = banner.replace(/11.1.0-next.1+51.sha-d9356f2/, version);
     }
   }
 }
