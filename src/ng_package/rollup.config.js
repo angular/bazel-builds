d464172e6706febf9a66f51bf1fbc81d227983aa
Misko Hevery <misko@hevery.com>
2020-12-07 18:15:19 +0000

    5fc45082ca fix(core): Support extending differs from root `NgModule` (#39981)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+53.sha-775394c/, version);
+      banner = banner.replace(/11.1.0-next.1+54.sha-5fc4508/, version);
     }
   }
 }
