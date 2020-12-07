5364fd5665f1205459799b69c4f47ddbb9478817
Misko Hevery <misko@hevery.com>
2020-12-07 18:14:18 +0000

    775394c809 docs: use double colon for pseudo-elements (#40004)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+52.sha-6d19c16/, version);
+      banner = banner.replace(/11.1.0-next.1+53.sha-775394c/, version);
     }
   }
 }
