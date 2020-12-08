8a7bb8d399f7e578dc6eb72020371e4750b327bb
Misko Hevery <misko@hevery.com>
2020-12-08 19:33:31 +0000

    bc6e6691e9 Revert "build: update bazel rules_nodejs to version 2.3.1 (#39636)"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+73.sha-a694838/, version);
+      banner = banner.replace(/11.1.0-next.1+78.sha-bc6e669/, version);
     }
   }
 }
