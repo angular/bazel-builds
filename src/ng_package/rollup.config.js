a9266fa756ecf21416029e809f44f4c3d7544da2
Misko Hevery <misko@hevery.com>
2020-12-08 17:14:49 +0000

    f408af30bb docs: removes the outdated content (#39242)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+64.sha-6e7a57f/, version);
+      banner = banner.replace(/11.1.0-next.1+66.sha-f408af3/, version);
     }
   }
 }
