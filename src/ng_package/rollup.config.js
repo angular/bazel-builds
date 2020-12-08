2f0983c35a5120375bfd541805582aef6fd63fca
Misko Hevery <misko@hevery.com>
2020-12-08 20:35:56 +0000

    65e93ea7b3 ci: add atscott to language-service approvers (#40027)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.3+56.sha-19309b5/, version);
+      banner = banner.replace(/11.0.3+63.sha-65e93ea/, version);
     }
   }
 }
