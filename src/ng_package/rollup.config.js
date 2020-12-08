4acb48de56bab477ad80aa86e26ccb911f0c619a
Misko Hevery <misko@hevery.com>
2020-12-08 17:48:00 +0000

    8ab1231908 docs(animations): a small typo (#37164)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.3+42.sha-5631daa/, version);
+      banner = banner.replace(/11.0.3+54.sha-8ab1231/, version);
     }
   }
 }
