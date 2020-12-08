8ed3cbeca30e3298f6c70affc3cec09b3b2f804f
Misko Hevery <misko@hevery.com>
2020-12-08 17:09:25 +0000

    6e7a57f97a docs: add note that sting based lazy loading is opt-in (#35956)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+63.sha-3ec2807/, version);
+      banner = banner.replace(/11.1.0-next.1+64.sha-6e7a57f/, version);
     }
   }
 }
