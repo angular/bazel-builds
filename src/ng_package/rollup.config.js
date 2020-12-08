41bbcbb90e19ceedf9b290afbf40b2a06fb95937
Misko Hevery <misko@hevery.com>
2020-12-08 19:13:06 +0000

    a694838c41 refactor(compiler-cli): TemplateTypeChecker with checkTypeOfAttributes=false should still work (#39537)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+76.sha-5a2a907/, version);
+      banner = banner.replace(/11.1.0-next.1+73.sha-a694838/, version);
     }
   }
 }
