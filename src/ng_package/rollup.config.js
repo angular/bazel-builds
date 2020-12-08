9bd59ed7924eda22ce9cc334149789f4b217d586
Misko Hevery <misko@hevery.com>
2020-12-08 00:49:40 +0000

    b8714c384f test(compiler-cli): add source-map compliance tests for inline-templates (#39939)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+55.sha-7901eba/, version);
+      banner = banner.replace(/11.1.0-next.1+62.sha-b8714c3/, version);
     }
   }
 }
