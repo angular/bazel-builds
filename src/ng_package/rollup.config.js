512349b1bac6198a941d9c30bd5db5cb3ad50e5c
Misko Hevery <misko@hevery.com>
2020-12-09 17:33:45 +0000

    42d478b16a docs(common): use table layout for Pre-defined format options (#40036)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+101.sha-13126d2/, version);
+      banner = banner.replace(/11.1.0-next.1+102.sha-42d478b/, version);
     }
   }
 }
