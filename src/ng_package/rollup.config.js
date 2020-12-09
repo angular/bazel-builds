b9d70c29649c8708dc6ec8ad7f4e1961d540273f
Misko Hevery <misko@hevery.com>
2020-12-09 17:23:02 +0000

    bb3f207f44 docs: add reason to use Angular in README (#36927)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+95.sha-cfb094d/, version);
+      banner = banner.replace(/11.1.0-next.1+96.sha-bb3f207/, version);
     }
   }
 }
