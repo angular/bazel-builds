ee029b2f1f7f0b70ee347eb39bda764e1958449a
Misko Hevery <misko@hevery.com>
2020-12-09 00:33:38 +0000

    61b9adb1b1 docs: add Pato Vargas to GDE resources (#40029)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+89.sha-55c21fa/, version);
+      banner = banner.replace(/11.1.0-next.1+90.sha-61b9adb/, version);
     }
   }
 }
