ed393387142eaf2d1ffa992e2e1dbb9dce743d25
Misko Hevery <misko@hevery.com>
2020-12-09 20:29:52 +0000

    d39d64c15d Revert "ci: update pullapprove config to add comp labels" (#40053)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.1+103.sha-85760cb/, version);
+      banner = banner.replace(/11.1.0-next.2+1.sha-d39d64c/, version);
     }
   }
 }
