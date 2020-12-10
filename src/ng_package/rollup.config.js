0cff7e48a1947aa8940fea592ce220c23d0e0f8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:46:20 +0000

    112324a614 feat(router): add `relativeTo` as an input to `routerLink` (#39720)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+6.sha-dc6d40e/, version);
+      banner = banner.replace(/11.1.0-next.2+7.sha-112324a/, version);
     }
   }
 }
