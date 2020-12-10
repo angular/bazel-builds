fa8ee3155eeacb4fbeff94a44cf9ef0ff4abefc3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 21:44:23 +0000

    85b07adc67 docs: add Teri Glover to contributor file (#39830)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+7.sha-112324a/, version);
+      banner = banner.replace(/11.1.0-next.2+8.sha-85b07ad/, version);
     }
   }
 }
