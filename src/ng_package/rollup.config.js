09aa4d0b051d87c37b679edfdd73160baa3c3ba3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:53:22 +0000

    b08fd0c18c docs: remove unused CSS rules (#40069)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+23.sha-91fb830/, version);
+      banner = banner.replace(/11.1.0-next.2+24.sha-b08fd0c/, version);
     }
   }
 }
