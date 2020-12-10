d5631583ddd7ebdb2c1742ee4f99ebf8521c4951
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:40:56 +0000

    79a42d951e docs: update the link to the TypeScript documentation for strict mode (#40063)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+20.sha-19b9ec8/, version);
+      banner = banner.replace(/11.1.0-next.2+21.sha-79a42d9/, version);
     }
   }
 }
