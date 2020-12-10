ad63127da17207101f75f9d442aa22fc477bb504
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:14:56 +0000

    3e68b79106 Revert "Revert "ci: update pullapprove config to add comp labels"" (#40059)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+18.sha-2a38641/, version);
+      banner = banner.replace(/11.1.0-next.2+19.sha-3e68b79/, version);
     }
   }
 }
