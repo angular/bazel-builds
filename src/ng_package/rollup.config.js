42de1e62ef063457e099479e12dc3f046bd9e399
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:41:08 +0000

    fea95059d5 build(docs-infra): upgrade cli command docs sources to f6f0cde41 (#40064)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+21.sha-79a42d9/, version);
+      banner = banner.replace(/11.1.0-next.2+22.sha-fea9505/, version);
     }
   }
 }
