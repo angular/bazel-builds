ac99bcf13c141b7c42d1ce35a5a0bbde5daf3824
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:11:36 +0000

    0817758b6a feat(docs-infra): add no upcoming events message (#40055)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+16.sha-973f797/, version);
+      banner = banner.replace(/11.1.0-next.2+17.sha-0817758/, version);
     }
   }
 }
