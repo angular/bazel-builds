dbeac27d63ad93459d056bf86579eec9b45bd6c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:53:03 +0000

    91fb830c86 docs: remove prefixed border-radius related properties (#40066)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+22.sha-fea9505/, version);
+      banner = banner.replace(/11.1.0-next.2+23.sha-91fb830/, version);
     }
   }
 }
