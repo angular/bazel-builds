100f0e38f7a07dc21947fd775474a6dbb7251b28
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:06:44 +0000

    b4b21bdff4 fix(upgrade): fix HMR for hybrid applications (#40045)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+10.sha-76e3de2/, version);
+      banner = banner.replace(/11.1.0-next.2+13.sha-b4b21bd/, version);
     }
   }
 }
