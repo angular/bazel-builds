c310f9fe1d2d859b72fbda4d7eaeddc4f9c02ed5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:41:11 +0000

    dc6d40e5bc fix(compiler): handle strings inside bindings that contain binding characters (#39826)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+5.sha-93a8326/, version);
+      banner = banner.replace(/11.1.0-next.2+6.sha-dc6d40e/, version);
     }
   }
 }
