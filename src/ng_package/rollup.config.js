dd6e6aacc135b0d77bc5ea264f4443dc6ec25178
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:14:00 +0000

    2a386415ce build(language-service): install @angular/language-service in root node_modules (#40058)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+17.sha-0817758/, version);
+      banner = banner.replace(/11.1.0-next.2+18.sha-2a38641/, version);
     }
   }
 }
