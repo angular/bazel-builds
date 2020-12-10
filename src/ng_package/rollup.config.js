41ae31ba308fed3b7a71391da90a7bbc4ea90bd4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:31:17 +0000

    269a775287 refactor(compiler-cli): produce binding access when checkTypeOfOutputEvents is false (#39515)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+2.sha-29298f4/, version);
+      banner = banner.replace(/11.1.0-next.2+4.sha-269a775/, version);
     }
   }
 }
