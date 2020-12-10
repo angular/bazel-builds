91e89fe7617c6906852f4017ae7060a35d35bb20
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:33:37 +0000

    19b9ec8e4b docs(router): Correct equivalent for 'enabled' initial navigation (#40061)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+19.sha-3e68b79/, version);
+      banner = banner.replace(/11.1.0-next.2+20.sha-19b9ec8/, version);
     }
   }
 }
