a092b8710cc85f6c87eef19596193bf72ada080f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:02:56 +0000

    76e3de253c docs: remove -webkit-box-sizing and -moz-box-sizing (#40039)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+9.sha-1f73af7/, version);
+      banner = banner.replace(/11.1.0-next.2+10.sha-76e3de2/, version);
     }
   }
 }
