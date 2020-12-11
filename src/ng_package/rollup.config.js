e14c0476db9dc4f8f6082fc9bfe36c62ac797d61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-11 22:21:51 +0000

    6e4e68cb30 refactor(compiler-cli): Add flag to TCB for non-diagnostic requests (#40071)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+24.sha-b08fd0c/, version);
+      banner = banner.replace(/11.1.0-next.2+26.sha-6e4e68c/, version);
     }
   }
 }
