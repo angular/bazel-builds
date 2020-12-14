c08a1a41572168cbb013afe91e5ee2eee6956284
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:12:11 +0000

    c18a9d5c79 ci: reduce flakiness of docs-infra `deploy-to-firebase.js` tests (#40088)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+33.sha-c977e1b/, version);
+      banner = banner.replace(/11.1.0-next.2+36.sha-c18a9d5/, version);
     }
   }
 }
