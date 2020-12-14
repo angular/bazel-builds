bcecc1a6e0676c85dd5b05b4b0ed4f14b3c3ad02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:03:17 +0000

    c977e1b629 test(compiler): use `yarn bazel` instead of `bazel` in error message (#40078)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+32.sha-044cd52/, version);
+      banner = banner.replace(/11.1.0-next.2+33.sha-c977e1b/, version);
     }
   }
 }
