a910013a1041cbf135d83c35f93ab3ed610fad91
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 19:44:17 +0000

    028e4f7f6d fix(language-service): force compileNonExportedClasses: false in LS (#40092)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+27.sha-2b74a05/, version);
+      banner = banner.replace(/11.1.0-next.2+28.sha-028e4f7/, version);
     }
   }
 }
