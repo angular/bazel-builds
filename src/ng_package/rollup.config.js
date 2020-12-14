4dd870ba275a69aaa1566ed4edc50b9e86a50e0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 19:59:06 +0000

    3b02e4e26a docs: Rename Main concepts to Understanding Angular, Built-in Features to Developer Guides (#39930)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+28.sha-028e4f7/, version);
+      banner = banner.replace(/11.1.0-next.2+31.sha-3b02e4e/, version);
     }
   }
 }
