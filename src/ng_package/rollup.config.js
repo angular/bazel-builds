81c38b903a31e69f0596becd50c6925bf7cc01e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 19:59:41 +0000

    4f4f318d28 docs: remove prefixed animation related CSS properties (#40084)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+30.sha-35a1975/, version);
+      banner = banner.replace(/11.1.0-next.2+35.sha-4f4f318/, version);
     }
   }
 }
