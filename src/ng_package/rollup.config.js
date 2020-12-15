204917b607a5c058021844361f829417cd26219f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 00:38:45 +0000

    973bb403a5 fix(compiler-cli): remove classes in .d.ts files from provider checks (#40118)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+46.sha-2a74431/, version);
+      banner = banner.replace(/11.1.0-next.2+47.sha-973bb40/, version);
     }
   }
 }
