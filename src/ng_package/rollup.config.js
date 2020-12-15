927a9ef3a9e0b1736e11f48f0422afcd3a4d6aea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 21:58:04 +0000

    a1c82e772d build: update config flags for snapshot builds (#40095)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.4+22.sha-7d9ac37/, version);
+      banner = banner.replace(/11.0.4+24.sha-a1c82e7/, version);
     }
   }
 }
