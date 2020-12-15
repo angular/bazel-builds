b2b3ea266c6ffd5c6801e3f31618f405202c1217
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 22:07:31 +0000

    caa4666335 fix(compiler): avoid duplicate i18n blocks for i18n attrs on elements with structural directives (#40077)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.2+52.sha-245dccc/, version);
+      banner = banner.replace(/11.1.0-next.2+53.sha-caa4666/, version);
     }
   }
 }
