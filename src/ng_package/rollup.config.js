8212ded45ca4179fd6fa3a8417a8732273f518d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 21:41:20 +0000

    922f492a3b fix(core): set `ngDevMode` to `false` when calling `enableProdMode()` (#40124)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.4+26.sha-0aa220b/, version);
+      banner = banner.replace(/11.0.4+27.sha-922f492/, version);
     }
   }
 }
