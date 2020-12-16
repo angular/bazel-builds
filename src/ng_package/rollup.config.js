973374515675e49e27018999b869c9968fb20ab3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 22:55:36 +0000

    9ae216ced5 build: upgrade typescript (dev dep) in zone.js to 3.8.3 (#40159)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -138,7 +138,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/9.1.12+10.sha-5b38050/, version);
+      banner = banner.replace(/9.1.12+13.sha-9ae216c/, version);
     }
   }
 }
