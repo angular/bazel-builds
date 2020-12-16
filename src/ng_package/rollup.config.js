83e69aab3a3b2b2f6c9ffae6b0797b1b7ac3a4c5
Joey Perrott <josephperrott@gmail.com>
2020-12-16 23:41:11 +0000

    e0e2157a28 release: cut the v11.0.5 release (#40164)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.4+27.sha-922f492/, version);
+      banner = banner.replace(/11.0.4+28.sha-e0e2157/, version);
     }
   }
 }
