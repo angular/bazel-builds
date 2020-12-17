779d3cb51db84441583f0e9f28404b35329e7124
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:15:13 +0000

    475468cc8f refactor(core): remove custom globalThis (#40123)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+4.sha-effc079/, version);
+      banner = banner.replace(/11.1.0-next.3+5.sha-475468c/, version);
     }
   }
 }
