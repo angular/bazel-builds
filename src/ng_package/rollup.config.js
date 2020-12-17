1caec4ac275f0fe968d5efbb01b8427a821d1ce9
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:28:26 +0000

    2075cf79a3 docs: use an explicit product id in the getting started example (#34934)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.5+2.sha-ab34211/, version);
+      banner = banner.replace(/11.0.5+5.sha-2075cf7/, version);
     }
   }
 }
