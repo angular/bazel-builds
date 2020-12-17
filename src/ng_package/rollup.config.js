c828d2f525627969f7b520f9b200ac69dc3b481d
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:23:41 +0000

    ab34211eed refactor(docs-infra): enable tslint rules for the angular.io app (#39307)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.4+28.sha-e0e2157/, version);
+      banner = banner.replace(/11.0.5+2.sha-ab34211/, version);
     }
   }
 }
