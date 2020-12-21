9a04ab3d3a010b079bf0707ab5a081fb8cc7bfad
Joey Perrott <josephperrott@gmail.com>
2020-12-21 19:04:41 +0000

    7413cb4386 build(docs-infra): update Lighthouse to v7.0.0 (#40198)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+11.sha-df7ed41/, version);
+      banner = banner.replace(/11.1.0-next.3+12.sha-7413cb4/, version);
     }
   }
 }
