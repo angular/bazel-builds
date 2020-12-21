f1fa30dfa118701b134a9b5412d0fc80d385c67e
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:45:02 +0000

    eae07e10aa docs: fix styles in binding guides (#40182)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+9.sha-f7642ce/, version);
+      banner = banner.replace(/11.1.0-next.3+10.sha-eae07e1/, version);
     }
   }
 }
