204f327380f582351ab29bfa9aae9c514e93a8d3
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:47:02 +0000

    df7ed4160f docs: remove -webkit-transition-timing-function CSS property (#40153)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+10.sha-eae07e1/, version);
+      banner = banner.replace(/11.1.0-next.3+11.sha-df7ed41/, version);
     }
   }
 }
