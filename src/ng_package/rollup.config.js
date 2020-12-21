f3ffaf3aff6714b9f6ccde422cbbf7255636bc8d
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:36:37 +0000

    10a468aab9 build(docs-infra): upgrade cli command docs sources to 3e58bafd2 (#40213)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+8.sha-5831d2b/, version);
+      banner = banner.replace(/11.1.0-next.3+7.sha-10a468a/, version);
     }
   }
 }
