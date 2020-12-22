c9669dd8ad96516e1f8b2b1434cd0b3e535cbaed
Joey Perrott <josephperrott@gmail.com>
2020-12-22 20:08:44 +0000

    382f906948 refactor(compiler-cli): include linker entry-points in NPM package (#40180)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+12.sha-7413cb4/, version);
+      banner = banner.replace(/11.1.0-next.3+24.sha-382f906/, version);
     }
   }
 }
