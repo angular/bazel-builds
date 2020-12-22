b1944349d475cd1ffb4fd37a539ed3afc9b5e98f
Joey Perrott <josephperrott@gmail.com>
2020-12-22 23:19:09 +0000

    ef13e834fe fix(core): error if detectChanges is called at the wrong time under specific circumstances (#40206)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.5+11.sha-3f4a1d6/, version);
+      banner = banner.replace(/11.0.5+23.sha-ef13e83/, version);
     }
   }
 }
