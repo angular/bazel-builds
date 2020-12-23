46fbcae6d3a738f3046c111ca5e05a8ff56945c8
Joey Perrott <josephperrott@gmail.com>
2020-12-23 16:57:36 +0000

    53b64fb659 docs: use kebab-case for CLI commands (#40240)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+27.sha-e4fbab9/, version);
+      banner = banner.replace(/11.1.0-next.3+29.sha-53b64fb/, version);
     }
   }
 }
