b4909c5af30bc2b9dd7dfc18821c0b466d4ca9ff
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:15:11 +0000

    e54261b8d8 docs: update boolean and enumerated CLI section (#40224)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+29.sha-53b64fb/, version);
+      banner = banner.replace(/11.1.0-next.3+30.sha-e54261b/, version);
     }
   }
 }
