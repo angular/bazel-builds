a368806ad05fde419631df65fd04db2c589eec78
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:34:16 +0000

    805b4f936b feat(core): Add schematic to fix invalid `Route` configs (#40067)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+38.sha-d466db8/, version);
+      banner = banner.replace(/11.1.0-next.3+40.sha-805b4f9/, version);
     }
   }
 }
