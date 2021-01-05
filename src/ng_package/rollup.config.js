d77f936dfeb3c1086b31d347f18e49124031689f
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:23:20 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+52.sha-c1d7805/, version);
+      banner = banner.replace(/11.1.0-next.3+53.sha-8cdfd77/, version);
     }
   }
 }
