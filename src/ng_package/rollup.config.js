e684534ab4c452de15cac02e473e822de53e12d7
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:30:26 +0000

    d466db8285 fix(language-service): Do not include $event parameter in reference results (#40158)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+37.sha-4eac7e6/, version);
+      banner = banner.replace(/11.1.0-next.3+38.sha-d466db8/, version);
     }
   }
 }
