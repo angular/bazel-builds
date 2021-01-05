f29c5324dbee1bf471b0b12cfa25702a80054891
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:22:12 +0000

    6a9d7e5969 refactor(core): express trusted constants with tagged template literals (#40082)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+53.sha-8cdfd77/, version);
+      banner = banner.replace(/11.1.0-next.3+54.sha-6a9d7e5/, version);
     }
   }
 }
