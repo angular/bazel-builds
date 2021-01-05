c4308709da01983b320d31c985e8c5173e760a1e
Joey Perrott <josephperrott@gmail.com>
2021-01-05 21:08:49 +0000

    e43f7e26fe fix(router): apply redirects should match named outlets with empty path parents (#40029)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+44.sha-b473bc2/, version);
+      banner = banner.replace(/11.1.0-next.3+50.sha-e43f7e2/, version);
     }
   }
 }
