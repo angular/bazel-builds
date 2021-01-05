93f04b3a62dad70ba6729267754d3dca1f669c7e
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:38:58 +0000

    3735633bb0 fix(core): take @Host into account while processing `useFactory` arguments (#40122)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+40.sha-805b4f9/, version);
+      banner = banner.replace(/11.1.0-next.3+42.sha-3735633/, version);
     }
   }
 }
