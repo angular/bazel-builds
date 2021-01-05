7235cbdaa99de1789ac55f53f58784784ef93e6a
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:27:34 +0000

    6abc13330b fix(compiler): don't report parse error for interpolation inside string in property binding (#40267)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+51.sha-6057753/, version);
+      banner = banner.replace(/11.1.0-next.3+55.sha-6abc133/, version);
     }
   }
 }
