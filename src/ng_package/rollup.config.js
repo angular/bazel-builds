ddb45d2b4fa6d44451a2e5ebb787e5d40834adad
Joey Perrott <josephperrott@gmail.com>
2021-01-06 17:09:37 +0000

    fdec650ef2 docs(forms): typo word "property" was missing (#40323)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+64.sha-d4327d5/, version);
+      banner = banner.replace(/11.1.0-next.3+65.sha-fdec650/, version);
     }
   }
 }
