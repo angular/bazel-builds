b25f02da5b652d0aa00868195c70ad5e2b5358d7
Joey Perrott <josephperrott@gmail.com>
2021-01-06 19:01:50 +0000

    8ebac24b48 fix(core): ensure sanitizer works if DOMParser return null body (#40107)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+66.sha-8dbd220/, version);
+      banner = banner.replace(/11.1.0-next.3+67.sha-8ebac24/, version);
     }
   }
 }
