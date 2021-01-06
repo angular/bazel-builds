d775c948c259a9438ee256c60e9a902be145de63
Joey Perrott <josephperrott@gmail.com>
2021-01-06 19:56:33 +0000

    183fb7e7b9 fix(language-service): return all typecheck files via getExternalFiles (#40162)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+70.sha-0264f76/, version);
+      banner = banner.replace(/11.1.0-next.3+71.sha-183fb7e/, version);
     }
   }
 }
