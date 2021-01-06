a5e7fd219c4260220075c8831823d61de563fb1b
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:28:35 +0000

    0a317f3bb4 docs: add Aristeidis Bampakos to contributor list (#40165)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+56.sha-9e6b3cf/, version);
+      banner = banner.replace(/11.1.0-next.3+58.sha-0a317f3/, version);
     }
   }
 }
