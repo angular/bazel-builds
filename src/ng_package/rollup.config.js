8f1ca7e5e4d795fcdf3f1f9496b3eae9e7ce1dfb
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:27:22 +0000

    9e6b3cffea docs: update homepage headline (#40056)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+55.sha-6abc133/, version);
+      banner = banner.replace(/11.1.0-next.3+56.sha-9e6b3cf/, version);
     }
   }
 }
