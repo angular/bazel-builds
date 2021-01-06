3c9be4cecb8b15497da319569c9a911738b4cec0
atscott <atscott01@gmail.com>
2021-01-06 22:14:14 +0000

    13020f904f fix(router): correctly deactivate children with componentless parent (#40196)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+73.sha-bff48a0/, version);
+      banner = banner.replace(/11.1.0-next.4+1.sha-13020f9/, version);
     }
   }
 }
