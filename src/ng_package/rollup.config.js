582f69bced16e9d58e4cbf306cd14eb36d9c6fe6
atscott <atscott01@gmail.com>
2021-01-06 22:15:21 +0000

    989b4a94d4 refactor(compiler-cli): Return symbols for all matching outputs (#40144)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+1.sha-13020f9/, version);
+      banner = banner.replace(/11.1.0-next.4+3.sha-989b4a9/, version);
     }
   }
 }
