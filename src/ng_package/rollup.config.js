759ece86e364c607a47f8e390bd94f16bf11eccf
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:33:25 +0000

    d5a7a268cf ci: add jessicajaniuk to pullapprove groups (#40176)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.3+58.sha-0a317f3/, version);
+      banner = banner.replace(/11.1.0-next.3+57.sha-d5a7a26/, version);
     }
   }
 }
