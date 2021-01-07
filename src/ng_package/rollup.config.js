af8acba293f2b6965f93db57895425846a325076
atscott <atscott01@gmail.com>
2021-01-07 21:34:07 +0000

    335c1ab37f docs: add Craig Spence to contributor list (#40335)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+10.sha-1437f4c/, version);
+      banner = banner.replace(/11.1.0-next.4+11.sha-335c1ab/, version);
     }
   }
 }
