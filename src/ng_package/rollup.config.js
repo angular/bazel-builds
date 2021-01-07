845c1b9e26e47a186d847344730bffa7cf79c1a8
atscott <atscott01@gmail.com>
2021-01-07 21:33:25 +0000

    40a03f0f28 docs: lifecycle-hooks example - extract things (components, directives) into their own files (#40212)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.6+7.sha-64d94e8/, version);
+      banner = banner.replace(/11.0.7+4.sha-40a03f0/, version);
     }
   }
 }
