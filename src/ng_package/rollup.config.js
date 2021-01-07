43d5f20ad968b0245387f378ec30e8bd641674e1
atscott <atscott01@gmail.com>
2021-01-07 21:42:10 +0000

    266cc9b162 refactor(compiler-cli): support external template source-mapping when linking (#40237)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+11.sha-335c1ab/, version);
+      banner = banner.replace(/11.1.0-next.4+21.sha-266cc9b/, version);
     }
   }
 }
