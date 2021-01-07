e55c9f7cb8ae7cfe79af6031df920f7a6fdbb1a4
atscott <atscott01@gmail.com>
2021-01-07 21:29:26 +0000

    9be9e466b1 docs: remove duplicated 'the' (#40333)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+13.sha-aea265b/, version);
+      banner = banner.replace(/11.1.0-next.4+12.sha-9be9e46/, version);
     }
   }
 }
