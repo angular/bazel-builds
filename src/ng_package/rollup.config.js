5c50b0c6b81ed0b8bd7b6fb0ccc69e0af575d48a
atscott <atscott01@gmail.com>
2021-01-08 22:08:44 +0000

    6a9e328432 feat(language-service): log Angular compiler options (#40364)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+44.sha-b36bece/, version);
+      banner = banner.replace(/11.1.0-next.4+46.sha-6a9e328/, version);
     }
   }
 }
