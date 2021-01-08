d5d7d33d19b3fd84272093cfb0778d9fda3d8de0
Andrew Scott <atscott@google.com>
2021-01-08 18:07:21 +0000

    7a819caef6 fix(dev-infra): delay checking if a closed pr has been merged by 30 seconds (#40181)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+35.sha-e27b920/, version);
+      banner = banner.replace(/11.1.0-next.4+37.sha-7a819ca/, version);
     }
   }
 }
