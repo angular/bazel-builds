d023eabc8a9cce58c9a33acb41497c6ec0e82882
Andrew Scott <atscott@google.com>
2021-01-08 18:13:41 +0000

    14389755e6 docs: import in-memory-web-api readme (#40203)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+38.sha-fdbd3ca/, version);
+      banner = banner.replace(/11.1.0-next.4+41.sha-1438975/, version);
     }
   }
 }
