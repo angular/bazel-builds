99f72289e9c2f3c29f2b9f7c30278d8264c0a2ce
Andrew Scott <atscott@google.com>
2021-01-08 18:18:44 +0000

    e05ac395fc docs: add @christianliebel to list of GDEs (#39788)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+39.sha-4f73820/, version);
+      banner = banner.replace(/11.1.0-next.4+43.sha-e05ac39/, version);
     }
   }
 }
