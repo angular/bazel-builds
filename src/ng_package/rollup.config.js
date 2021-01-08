9ac0df8bbfed66efad9490771df8a757a932d92d
Andrew Scott <atscott@google.com>
2021-01-08 18:15:57 +0000

    4f73820ad6 fix(core): memory leak if view container host view is destroyed while view ref is not (#40219)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+41.sha-1438975/, version);
+      banner = banner.replace(/11.1.0-next.4+39.sha-4f73820/, version);
     }
   }
 }
