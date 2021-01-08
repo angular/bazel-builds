d586a8525d396f8587d7fafb7ef3ff9bd368b7d0
Andrew Scott <atscott@google.com>
2021-01-08 18:00:18 +0000

    e27b920ac3 refactor(compiler-cli): split up `NodeJSFileSystem` class (#40281)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+22.sha-1045465/, version);
+      banner = banner.replace(/11.1.0-next.4+35.sha-e27b920/, version);
     }
   }
 }
