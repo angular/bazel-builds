514e544c1fa84ab4dd7753f485d0b7961e0240a2
Andrew Scott <atscott@google.com>
2021-01-10 01:01:07 +0000

    c44dd84a71 fix(router): Remove usage of `Object.values` to avoid the need for a polyfill (#40370)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.7+11.sha-6ccdb2d/, version);
+      banner = banner.replace(/11.0.7+13.sha-c44dd84/, version);
     }
   }
 }
