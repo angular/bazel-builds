a9cf31403e8fcf24c23545d31a5df1d21812fbff
atscott <atscott01@gmail.com>
2021-01-11 22:37:40 +0000

    f8495ca1c4 docs(router): clarify the description of legacy relativeLinkResolution (#40384)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.8+19.sha-adf42da/, version);
+      banner = banner.replace(/11.0.8+20.sha-f8495ca/, version);
     }
   }
 }
