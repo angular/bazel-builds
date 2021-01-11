f5ac4cbf57da31629a796b0de8363f3bae01fb2d
atscott <atscott01@gmail.com>
2021-01-11 19:19:23 +0000

    f8aad11866 refactor(service-worker): remove work-around for Chrome code highlighting bug (#40234)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+57.sha-27d0e54/, version);
+      banner = banner.replace(/11.1.0-next.4+64.sha-f8aad11/, version);
     }
   }
 }
