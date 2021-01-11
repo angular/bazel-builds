71da12162b75b1d1e77c1a3070dc9dd8b0f5db7b
atscott <atscott01@gmail.com>
2021-01-11 18:21:39 +0000

    6e57443acd docs(router): fix minor grammatical issue (#40380)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+51.sha-34d2988/, version);
+      banner = banner.replace(/11.1.0-next.4+54.sha-6e57443/, version);
     }
   }
 }
