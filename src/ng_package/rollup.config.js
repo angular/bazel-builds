b6d2a91fac9de6b7a3c29baf7097e4de031e0544
atscott <atscott01@gmail.com>
2021-01-11 18:11:02 +0000

    94ec027865 build(docs-infra): upgrade cli command docs sources to e1bcfba66 (#40373)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+48.sha-9a024d8/, version);
+      banner = banner.replace(/11.1.0-next.4+49.sha-94ec027/, version);
     }
   }
 }
