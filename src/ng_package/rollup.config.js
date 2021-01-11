a1b41a77489e46977b419ef421a8438ba884cf06
atscott <atscott01@gmail.com>
2021-01-11 18:09:12 +0000

    9a024d82ec docs: fix incorrect link "Debugging tests" (#40379)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+47.sha-74175f8/, version);
+      banner = banner.replace(/11.1.0-next.4+48.sha-9a024d8/, version);
     }
   }
 }
