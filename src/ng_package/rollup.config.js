374fdbed31a4a43ca4586a50327ce5486e142000
atscott <atscott01@gmail.com>
2021-01-11 23:55:30 +0000

    9105005192 refactor(router): refactor and simplify router RxJS chains (#40290)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+68.sha-4a7a649/, version);
+      banner = banner.replace(/11.1.0-next.4+69.sha-9105005/, version);
     }
   }
 }
