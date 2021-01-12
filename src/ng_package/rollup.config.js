37c354650ba4310f242e1030d8dd2ce774889011
atscott <atscott01@gmail.com>
2021-01-12 00:03:50 +0000

    bd17610c29 feat(dev-infra): make ts-api-guardian compatible with rules_nodejs version 3 (#40353)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+78.sha-66c27ff/, version);
+      banner = banner.replace(/11.1.0-next.4+79.sha-bd17610/, version);
     }
   }
 }
