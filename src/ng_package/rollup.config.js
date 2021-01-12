b850cbe180597dc0a54e6d6f79be3b6d40ed8048
atscott <atscott01@gmail.com>
2021-01-12 00:10:12 +0000

    b48eabddb8 refactor(compiler-cli): include `template` source directly inside declaration object (#40383)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+79.sha-bd17610/, version);
+      banner = banner.replace(/11.1.0-next.4+80.sha-b48eabd/, version);
     }
   }
 }
