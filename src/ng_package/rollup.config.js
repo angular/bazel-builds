a6472f771dcc3ea6f75c52eb64b24247394a9640
atscott <atscott01@gmail.com>
2021-01-13 19:19:07 +0000

    4db89f4576 fix(compiler-cli): report non-template diagnostics (#40331)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+81.sha-625d2c2/, version);
+      banner = banner.replace(/11.1.0-next.4+82.sha-4db89f4/, version);
     }
   }
 }
