43965d051145b013fbb570c9432f18e7a79d6ddd
atscott <atscott01@gmail.com>
2021-01-13 23:10:48 +0000

    49dcb06dca build: remove pre-check script from publish scripts as it no longer performs its check (#40422)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+92.sha-6cff877/, version);
+      banner = banner.replace(/11.1.0-next.4+93.sha-49dcb06/, version);
     }
   }
 }
