e881b6da2d554c12ae87bed7f3559dd4df06da62
atscott <atscott01@gmail.com>
2021-01-13 22:34:09 +0000

    3f6279d394 ci: add mgechev to docs-marketing approvers (#40396)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.8+31.sha-c6ca7b4/, version);
+      banner = banner.replace(/11.0.9+7.sha-3f6279d/, version);
     }
   }
 }
