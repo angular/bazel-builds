03e7f2b3d78f789662ab7ba703c42e6f31f426a8
atscott <atscott01@gmail.com>
2021-01-13 22:35:09 +0000

    0664d750b6 perf(core): make DI decorators tree-shakable when used for `useFactory` deps config (#40145)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.5+1.sha-ebd94ef/, version);
+      banner = banner.replace(/11.1.0-rc.0+7.sha-0664d75/, version);
     }
   }
 }
