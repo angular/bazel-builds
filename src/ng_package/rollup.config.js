49abe1f06a48d0808b5b3b3d87eb65a6e5f0d1fb
atscott <atscott01@gmail.com>
2021-01-13 22:31:17 +0000

    25892c1bcf docs: add links to TypeScript (#40410)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+85.sha-e6ac542/, version);
+      banner = banner.replace(/11.1.0-next.4+86.sha-25892c1/, version);
     }
   }
 }
