3d5b5187ae02d236274ac1d15f08cd558b065668
atscott <atscott01@gmail.com>
2021-01-13 23:16:06 +0000

    5013a7e12d docs: update identity-revealed.directive.ts to use `AbstractControl` instead of `FormGroup` (#40132)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+93.sha-49dcb06/, version);
+      banner = banner.replace(/11.1.0-next.4+94.sha-5013a7e/, version);
     }
   }
 }
