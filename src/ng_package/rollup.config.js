8ced07b2861f1d06456a7af1fa435dad5e56e6e0
atscott <atscott01@gmail.com>
2021-01-14 18:48:00 +0000

    8379b571f8 docs(core): add Ivy note to entryComponents page (#35479)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+95.sha-c33d2a2/, version);
+      banner = banner.replace(/11.1.0-next.4+96.sha-8379b57/, version);
     }
   }
 }
