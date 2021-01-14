168e08d698cc716390408fe2235f52e7fc07038c
Andrew Scott <atscott@google.com>
2021-01-14 17:38:14 +0000

    f4ed12211e docs(common): updated differential loading section (#37913)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-rc.0+9.sha-c6c7e9a/, version);
+      banner = banner.replace(/11.1.0-rc.0+10.sha-f4ed122/, version);
     }
   }
 }
