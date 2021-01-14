05e69e3aac1a8cb14a1e83509d76b33b055f20ef
Andrew Kushnir <akushnir@google.com>
2021-01-14 22:29:46 +0000

    d516113803 refactor(core): Remove the need for explicit static query instruction (#40091)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+104.sha-cf02cf1/, version);
+      banner = banner.replace(/11.1.0-next.4+106.sha-d516113/, version);
     }
   }
 }
