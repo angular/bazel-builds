492c4d59ed6b69bb7181d20df8ab6bfc01714a6c
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:15:10 +0000

    61792cc6f5 refactor(compiler): remove unused files in css_parser/ and corresponding spec tests (#37463)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+110.sha-96690ed/, version);
+      banner = banner.replace(/11.1.0-next.4+113.sha-61792cc/, version);
     }
   }
 }
