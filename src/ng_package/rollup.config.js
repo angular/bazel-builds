b7bb527bdfa8ab0554eb0da6d035ead2a6d579f1
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:21:21 +0000

    40e0bfdc0d fix(compiler): correct the `KeySpan` for animation events and properties (#40347)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+113.sha-61792cc/, version);
+      banner = banner.replace(/11.1.0-next.4+114.sha-40e0bfd/, version);
     }
   }
 }
