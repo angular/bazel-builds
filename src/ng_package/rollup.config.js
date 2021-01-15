fe1f96a1043873f75328a123a2e40cc0c3ab6124
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:14:31 +0000

    54579362ae docs: add UI Bakery to IDEs section in resources (#40444)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+107.sha-9a5ac47/, version);
+      banner = banner.replace(/11.1.0-next.4+108.sha-5457936/, version);
     }
   }
 }
