32a6d2638f8f55d72e5a7e709aa3bf38964f5c62
Andrew Kushnir <akushnir@google.com>
2021-01-15 00:46:40 +0000

    9a5ac47331 feat(language-service): initial implementation for `findRenameLocations` (#40140)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+106.sha-d516113/, version);
+      banner = banner.replace(/11.1.0-next.4+107.sha-9a5ac47/, version);
     }
   }
 }
