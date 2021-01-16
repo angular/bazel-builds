80d2e82a49e19a54cfa78ee775dd9b8781d9be69
Andrew Kushnir <akushnir@google.com>
2021-01-16 01:08:25 +0000

    4e8198d60f feat(language-service): Implement `getRenameInfo` (#40439)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+114.sha-40e0bfd/, version);
+      banner = banner.replace(/11.1.0-next.4+115.sha-4e8198d/, version);
     }
   }
 }
