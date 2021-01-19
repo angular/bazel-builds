c1f0524faede0c9cc8d070f3ddb5174c7be39df3
Andrew Kushnir <akushnir@google.com>
2021-01-19 18:43:02 +0000

    afabb83696 refactor(core): add links to top compiler errors (#40326)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+121.sha-0568c73/, version);
+      banner = banner.replace(/11.1.0-next.4+123.sha-afabb83/, version);
     }
   }
 }
