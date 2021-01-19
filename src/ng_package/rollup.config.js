bdd063226d4d947c8289bab1a7ccf6b3021f7732
Andrew Kushnir <akushnir@google.com>
2021-01-19 21:24:50 +0000

    3e97a1ea43 fix(language-service): fix go to definition for template variables and references (#40455)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+124.sha-1d1304c/, version);
+      banner = banner.replace(/11.1.0-next.4+125.sha-3e97a1e/, version);
     }
   }
 }
