e40a63398064dcdcd141e3be8fe963ef4fd015ed
Andrew Kushnir <akushnir@google.com>
2021-01-19 22:43:42 +0000

    96e6f20ce2 docs: Return SVG topic to documentation; light edits for clarity (#40487)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.9+25.sha-724e44a/, version);
+      banner = banner.replace(/11.0.9+26.sha-96e6f20/, version);
     }
   }
 }
