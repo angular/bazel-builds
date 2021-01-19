7885b948f9bce565e89c5783d9fa032bae907811
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:39:07 +0000

    f49447df20 fix(core): fix fakeAsync() error messages (#40442)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+115.sha-4e8198d/, version);
+      banner = banner.replace(/11.1.0-next.4+116.sha-f49447d/, version);
     }
   }
 }
