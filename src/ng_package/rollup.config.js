b6634aa3148c547610eb2ba325912a35b04e46ec
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:45:02 +0000

    b636406043 docs: fix broken link to XSRF common protection mechanism (#40458)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+116.sha-f49447d/, version);
+      banner = banner.replace(/11.1.0-next.4+119.sha-b636406/, version);
     }
   }
 }
