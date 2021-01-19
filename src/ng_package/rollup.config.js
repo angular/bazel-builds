cf015013b44f0fb490da0342ea61e73de0cbc61e
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:47:13 +0000

    2047d632d8 docs: add Angular Builders to community resources (#40475)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-rc.0+28.sha-524415e/, version);
+      banner = banner.replace(/11.1.0-rc.0+32.sha-2047d63/, version);
     }
   }
 }
