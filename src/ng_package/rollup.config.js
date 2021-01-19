550551a719f8de67c38fe1f5cd3489ce1be24a0e
Andrew Kushnir <akushnir@google.com>
2021-01-19 20:07:15 +0000

    1d1304c70c fix(forms): error if control is removed as a result of another one being reset (#40462)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+123.sha-afabb83/, version);
+      banner = banner.replace(/11.1.0-next.4+124.sha-1d1304c/, version);
     }
   }
 }
