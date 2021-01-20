30d22cbd4d8f1e6c3125b3dfef37fd2a96fd3870
Andrew Kushnir <akushnir@google.com>
2021-01-20 17:24:18 +0000

    d684e5a8d3 docs: add embedded videos to error guides (#40453)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.0.9+26.sha-96e6f20/, version);
+      banner = banner.replace(/11.0.9+27.sha-d684e5a/, version);
     }
   }
 }
