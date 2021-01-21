529e1b27aea4faa4969c358a2567af825b2d8fbf
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:27:23 +0000

    2b2a847ad7 refactor(migrations): remove rxjs usage within static queries migration (#38657)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+153.sha-dc06873/, version);
+      banner = banner.replace(/11.1.0-next.4+151.sha-2b2a847/, version);
     }
   }
 }
