c71bfd18626d13e03980d1fd0722f628ff7c648b
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 20:46:07 +0000

    0e95460cf8 build: bump ts-api-guardian to 0.5.1 (#40511)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+145.sha-f0733d1/, version);
+      banner = banner.replace(/11.1.0-next.4+144.sha-0e95460/, version);
     }
   }
 }
