29c253cd44305dd756715a0d4a8fe3d18586886a
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 20:49:14 +0000

    69fd94220e fix(router): Fix occasional error when creating url tree in IE 11 and Edge (#40488)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0+11.sha-e995d85/, version);
+      banner = banner.replace(/11.1.0+14.sha-69fd942/, version);
     }
   }
 }
