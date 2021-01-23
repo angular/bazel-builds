49e7ef7633d317f5dbfbd428498b0e6b255ef98c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-23 01:07:42 +0000

    fc64fa8e1a Revert "fix(compiler): support multiple `:host-context()` selectors (#40494)" (#40531)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+159.sha-c64a56f/, version);
+      banner = banner.replace(/11.1.0-next.4+160.sha-fc64fa8/, version);
     }
   }
 }
