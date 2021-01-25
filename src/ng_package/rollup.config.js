b0bb0cc8f3063251c5914e3856b58ce34e560a90
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 20:01:36 +0000

    2fab148cb5 fix(forms): allow `patchValue()` method of `FormGroup` and `FormArray` classes to skip `null` values (#40534)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0+36.sha-5661298/, version);
+      banner = banner.replace(/11.1.0+39.sha-2fab148/, version);
     }
   }
 }
