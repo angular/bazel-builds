6b71ba489c7c34967723b04bd8aa977bfbfff990
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:39:40 +0000

    21bc16d4d8 fix(service-worker): handle error with console.error (#40236)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+160.sha-fc64fa8/, version);
+      banner = banner.replace(/11.1.0-next.4+164.sha-21bc16d/, version);
     }
   }
 }
