702603be1e31e4a50b79f2c9fc2c5b6b6fb93d0c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:59:33 +0000

    6bf99e0eda fix(core): fix possible XSS attack in development through SSR (#40525)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+178.sha-365ac5e/, version);
+      banner = banner.replace(/11.1.0-next.4+179.sha-6bf99e0/, version);
     }
   }
 }
