90db11bdd1301142bdbae5870d4f54ce027c0384
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 00:49:46 +0000

    956f75f068 docs: fix the docs because the interface does not exist (#40308)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+176.sha-b630b09/, version);
+      banner = banner.replace(/11.1.0-next.4+177.sha-956f75f/, version);
     }
   }
 }
