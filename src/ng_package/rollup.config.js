c56ea4558a0c78c31dd1c79e4fa9212ec464942a
GitHub <noreply@github.com>
2021-01-27 20:18:13 +0000

    e75a4f261e docs: release notes for the v11.1.1 release (#40599)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+194.sha-be979c9/, version);
+      banner = banner.replace(/11.1.0-next.4+195.sha-e75a4f2/, version);
     }
   }
 }
