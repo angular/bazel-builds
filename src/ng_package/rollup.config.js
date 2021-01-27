9f90713c887c72c29b735764323daf988bc44eab
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 18:45:44 +0000

    f1cf875872 docs(elements): add notice about choosing a tag name in Elements guide (#40510)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+184.sha-a826926/, version);
+      banner = banner.replace(/11.1.0-next.4+186.sha-f1cf875/, version);
     }
   }
 }
