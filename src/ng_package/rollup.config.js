21547013c62967ccaabf7c1aab4b2115816b165d
Joey Perrott <josephperrott@gmail.com>
2021-01-27 23:20:57 +0000

    b819bbb540 docs: fix typo to remove space

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+196.sha-fd03afe/, version);
+      banner = banner.replace(/11.1.0-next.4+196.sha-b819bbb/, version);
     }
   }
 }
