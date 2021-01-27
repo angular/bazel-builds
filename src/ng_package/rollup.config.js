0c16ae99e566cece1a35cc29246af0b005b5251d
GitHub <noreply@github.com>
2021-01-27 21:21:42 +0000

    8772fbd4bb release: bump the version to 12.0.0-next.0 (#40604)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+195.sha-e75a4f2/, version);
+      banner = banner.replace(/11.1.0-next.4+196.sha-8772fbd/, version);
     }
   }
 }
