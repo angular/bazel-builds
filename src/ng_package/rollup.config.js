16570ac1f0ceb936df1db9c373774a46f7ee72f4
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:18:14 +0000

    be979c907b perf(compiler-cli): introduce fast path for resource-only updates (#40561)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+189.sha-e43cba5/, version);
+      banner = banner.replace(/11.1.0-next.4+194.sha-be979c9/, version);
     }
   }
 }
