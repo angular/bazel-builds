6ffa22e6612393cf7fea1fca81c919c31395b90f
Misko Hevery <misko@hevery.com>
2021-01-28 22:59:41 +0000

    e706f46a16 Revert "fix(core): remove duplicated EMPTY_ARRAY constant (#40587)"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.1+1.sha-de12485/, version);
+      banner = banner.replace(/11.1.1+10.sha-e706f46/, version);
     }
   }
 }
