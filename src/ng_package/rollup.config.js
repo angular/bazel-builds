68afe92e3d4dc21b0aba0733eb757f2f83b5faec
Misko Hevery <misko@hevery.com>
2021-01-29 00:15:37 +0000

    2ba62e80fd ci: add pete to the fw-http group (#40619)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+209.sha-22f9e45/, version);
+      banner = banner.replace(/11.1.0-next.4+211.sha-2ba62e8/, version);
     }
   }
 }
