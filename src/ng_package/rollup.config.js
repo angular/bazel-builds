06f49ba52e4c2b0afa4fde41b91f0a41b2b13d6f
Misko Hevery <misko@hevery.com>
2021-02-02 20:23:29 +0000

    cc8af03eec docs: fix typo in lazy-loading guide (#40668)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+216.sha-bd0d191/, version);
+      banner = banner.replace(/11.1.0-next.4+218.sha-cc8af03/, version);
     }
   }
 }
