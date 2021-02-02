7799b8c4c2036ec8b7cc3456732cffdcc2e96979
Misko Hevery <misko@hevery.com>
2021-02-02 20:23:56 +0000

    11ca2f04f9 refactor(docs-infra): remove unused styles for `.heading` CSS class (#40427)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+218.sha-cc8af03/, version);
+      banner = banner.replace(/11.1.0-next.4+229.sha-11ca2f0/, version);
     }
   }
 }
