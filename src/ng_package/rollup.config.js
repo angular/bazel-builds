09cae3d7ad3bfba7fc5e149aa02ab5b47392655d
Misko Hevery <misko@hevery.com>
2021-02-03 18:16:46 +0000

    7c38a4a037 docs: clarify that fileReplacements only supports compilation source files (#40687)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0-next.0+32.sha-4cc15fe/, version);
+      banner = banner.replace(/11.2.0-next.0+37.sha-7c38a4a/, version);
     }
   }
 }
