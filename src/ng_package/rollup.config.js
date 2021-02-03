e548a089c0c226fa6ed1bdc8a406bb7aa2b27a5d
Misko Hevery <misko@hevery.com>
2021-02-03 00:46:44 +0000

    a3b0864428 refactor(compiler-cli): remove the overrideComponentTemplate API (#40585)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+229.sha-11ca2f0/, version);
+      banner = banner.replace(/11.1.0-next.4+231.sha-a3b0864/, version);
     }
   }
 }
