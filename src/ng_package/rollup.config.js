800e6f64d7eeb1023077266385ecf1fe64661cdb
Misko Hevery <misko@hevery.com>
2021-02-03 19:36:51 +0000

    0ee9ea8148 refactor(language-service): pull out interfaces on package toplevel (#40621) (#40695)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.1+29.sha-20654c9/, version);
+      banner = banner.replace(/11.1.1+37.sha-0ee9ea8/, version);
     }
   }
 }
