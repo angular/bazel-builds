1e0db356da89df1e1d491f24901d139992c6db76
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:18:58 +0000

    5324a62ebe docs: add guide for branching process (#39924)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+240.sha-836b176/, version);
+      banner = banner.replace(/11.1.0-next.4+241.sha-5324a62/, version);
     }
   }
 }
