4fc82b92cbddc9d00a08f901879d58119aec30d5
Misko Hevery <misko@hevery.com>
2021-02-04 00:20:44 +0000

    836b1764eb docs: release notes for the v11.2.0-rc.0 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+239.sha-a9322b7/, version);
+      banner = banner.replace(/11.1.0-next.4+240.sha-836b176/, version);
     }
   }
 }
