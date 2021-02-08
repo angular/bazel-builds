0e9857d517357efd389e0eb4f2801285ff607e92
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-08 18:00:23 +0000

    8fb83ea1b5 feat(forms): introduce min and max validators (#39063)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+244.sha-c9fa59a/, version);
+      banner = banner.replace(/11.1.0-next.4+246.sha-8fb83ea/, version);
     }
   }
 }
