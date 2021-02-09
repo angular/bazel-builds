0a3dd945eb40fe80f6c5c006ab192026b8f18ca6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:05:57 +0000

    2a851c0924 docs: edit example to fetch hero before sending message (#40419)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+310.sha-820d95b/, version);
+      banner = banner.replace(/11.1.0-next.4+311.sha-2a851c0/, version);
     }
   }
 }
