706acd728b71c402b52c3568f5b2aac00fac212c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:16:23 +0000

    9aa3e9f0fc fix(docs-infra): correctly handle entry-points with no public exports (#40737)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0-rc.0+4.sha-7c14536/, version);
+      banner = banner.replace(/11.2.0-rc.0+62.sha-9aa3e9f/, version);
     }
   }
 }
