5ba045fd030788ccd441e91bf5a2a255c1271679
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:03:13 +0000

    2282069388 docs: clarify vscode integration maintenance (#40647)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+248.sha-d9e4d75/, version);
+      banner = banner.replace(/11.1.0-next.4+250.sha-2282069/, version);
     }
   }
 }
