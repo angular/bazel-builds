2ec41571589f1e86a32ee612bdfe0ef84058c8e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:09:39 +0000

    415ad8df13 fix(localize): support downleveled inlined helper localize calls (#40754)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0-rc.0+62.sha-9aa3e9f/, version);
+      banner = banner.replace(/11.2.0-rc.0+63.sha-415ad8d/, version);
     }
   }
 }
