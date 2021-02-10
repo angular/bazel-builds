95c0b2269b7fd58d89539dbd9e07711366cd1260
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:37:29 +0000

    03f0b157c1 fix(common): date is not correctly formatted when year is between 0 and 99 (#40448)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+313.sha-378da71/, version);
+      banner = banner.replace(/11.1.0-next.4+316.sha-03f0b15/, version);
     }
   }
 }
