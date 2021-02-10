7a2dd8bea496e43f1261d4bff7947d6b3eacd0e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:42:47 +0000

    7a8821c9aa docs: add link to component-less routes description. (#40741)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+324.sha-fface51/, version);
+      banner = banner.replace(/11.1.0-next.4+321.sha-7a8821c/, version);
     }
   }
 }
