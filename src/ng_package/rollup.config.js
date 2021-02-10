fb442a4af0f4bcdd5ce4f61c7dd9560102d2e6d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:57:44 +0000

    cfbb1faa6f docs: add Angular + NgRx book by the asian BPB Publications (#39680)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0-rc.0+67.sha-3b0c51e/, version);
+      banner = banner.replace(/11.2.0-rc.0+79.sha-cfbb1fa/, version);
     }
   }
 }
