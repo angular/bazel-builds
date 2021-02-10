eca6379ec2576bd8998f46ca2f04ec21e8d878dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:37:49 +0000

    bbf61fc2be fix(compiler): include parenthesis in expression source spans (#40740)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+316.sha-03f0b15/, version);
+      banner = banner.replace(/11.1.0-next.4+320.sha-bbf61fc/, version);
     }
   }
 }
