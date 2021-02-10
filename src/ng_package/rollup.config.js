2a1d2941e34dcda352473195da40944599a08bba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:44:27 +0000

    ddff6b63d7 docs: fix absent documentation for digitsInfo parameter in the API docs (#40746)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+321.sha-7a8821c/, version);
+      banner = banner.replace(/11.1.0-next.4+322.sha-ddff6b6/, version);
     }
   }
 }
