fe6d8e5e709d90d471316432e23c54c3c591449d
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:19:37 +0000

    7ca244a38b docs(core): add missing dot at the end of the sentence (#40800)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+4.sha-68da05d/, version);
+      banner = banner.replace(/12.0.0-next.0+6.sha-7ca244a/, version);
     }
   }
 }
