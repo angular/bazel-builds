446be6ab8e814d48cb6e5b0d1dd8314ffb44120c
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:34:57 +0000

    dfc9f36dd8 build: update browserstacktunnel-wrapper (#40810)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+13.sha-e27787a/, version);
+      banner = banner.replace(/12.0.0-next.0+16.sha-dfc9f36/, version);
     }
   }
 }
