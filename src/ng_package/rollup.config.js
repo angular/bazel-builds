595af8ec5fdf6b751fe155d4988e57564aece3f6
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:28:35 +0000

    c56ecab515 feat(common): support ICU standard "stand alone day of week" with `DatePipe` (#40766)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+2.sha-6f017a4/, version);
+      banner = banner.replace(/12.0.0-next.0+3.sha-c56ecab/, version);
     }
   }
 }
