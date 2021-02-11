e3aa7468d4d6bd822a81ed22ca8a460c2d9f1da5
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:23:59 +0000

    6f017a4d60 docs: fix typo in workspace config docs (#40784)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+1.sha-06671e8/, version);
+      banner = banner.replace(/12.0.0-next.0+2.sha-6f017a4/, version);
     }
   }
 }
