6f57379264d5e5595bf21f92d9bf5a0a640a221b
Joey Perrott <josephperrott@gmail.com>
2021-02-11 19:50:47 +0000

    f72626d3cc build: Update dependencies (#40726)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+9.sha-e9a19a6/, version);
+      banner = banner.replace(/12.0.0-next.0+10.sha-f72626d/, version);
     }
   }
 }
