2ad0ea9569fc851e8daaf5489a2a94dce0f3ffe1
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:25:50 +0000

    06671e85f9 ci: Remove language-service from testing group (#40787)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+326.sha-9b4263b/, version);
+      banner = banner.replace(/12.0.0-next.0+1.sha-06671e8/, version);
     }
   }
 }
