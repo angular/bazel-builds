2004ebb5fff59f38b8692c29a409de596da3050f
Joey Perrott <josephperrott@gmail.com>
2021-02-12 00:19:00 +0000

    80f4ff3338 fix(compiler-cli): set TS original node on imported namespace identifiers (#40711)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+21.sha-ca43938/, version);
+      banner = banner.replace(/12.0.0-next.0+20.sha-80f4ff3/, version);
     }
   }
 }
