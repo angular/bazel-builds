2786cb9032b7b33745378f910d21c6c24e02cd16
Joey Perrott <josephperrott@gmail.com>
2021-02-12 00:18:31 +0000

    9482a4df4c docs: copy paste error (#40812)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0+16.sha-4c6d557/, version);
+      banner = banner.replace(/11.2.0+18.sha-9482a4d/, version);
     }
   }
 }
