5c5998ce07ee806dd39cdf89cb0122d1421ab9fb
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:49:38 +0000

    ebbcf0cc04 docs: Update start-deployment.md (#40817)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0+20.sha-8225bb5/, version);
+      banner = banner.replace(/11.2.0+21.sha-ebbcf0c/, version);
     }
   }
 }
