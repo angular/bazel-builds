48196a4833c0dd32e9d495a3663f48832199b687
Joey Perrott <josephperrott@gmail.com>
2021-02-12 20:19:40 +0000

    1516b5d391 docs: capitalize Angular word in zone.md file (#40813) PR Close #40813

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0+21.sha-ebbcf0c/, version);
+      banner = banner.replace(/11.2.0+22.sha-1516b5d/, version);
     }
   }
 }
