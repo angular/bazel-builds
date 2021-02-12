aa5f9d0875f564250e0743512a7027c2df33e0c2
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:19:31 +0000

    6425a6d543 docs: typo fix (#40804) PR Close #40804

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+25.sha-9cdf654/, version);
+      banner = banner.replace(/12.0.0-next.0+26.sha-6425a6d/, version);
     }
   }
 }
