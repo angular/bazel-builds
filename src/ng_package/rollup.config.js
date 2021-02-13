a0e8a1020a84fdfc3f8a1b81ab72219eabfdd7f0
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:29:03 +0000

    980f6a4958 fix(core): address Trusted Types bug in Chrome 83 (#40815)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+33.sha-5554a3f/, version);
+      banner = banner.replace(/12.0.0-next.0+35.sha-980f6a4/, version);
     }
   }
 }
