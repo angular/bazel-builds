84df31525e1905f50d5a7374901e3022973b6f87
Joey Perrott <josephperrott@gmail.com>
2021-02-13 00:31:48 +0000

    bbbfa3e6bd docs: clarify that support schedule is subject to change (#40770)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+31.sha-ccd10ea/, version);
+      banner = banner.replace(/12.0.0-next.0+32.sha-bbbfa3e/, version);
     }
   }
 }
