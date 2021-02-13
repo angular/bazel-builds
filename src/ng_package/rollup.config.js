38b58bd575f5a546bdbc7d252a831da77e9513c2
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:08:29 +0000

    5554a3f174 docs: add NgRx book to resources (#39841)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+34.sha-9661873/, version);
+      banner = banner.replace(/12.0.0-next.0+33.sha-5554a3f/, version);
     }
   }
 }
