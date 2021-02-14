71b995bc0c0168e157c1873593e757fd165a2fe4
JoostK <joost.koehoorn@gmail.com>
2021-02-14 15:14:30 +0000

    1646f8da87 perf(compiler-cli): detect semantic changes and their effect on an incremental rebuild

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.1.0-next.4+308.sha-0d8e6b4/, version);
+      banner = banner.replace(/12.0.0-next.0+37.sha-1646f8d/, version);
     }
   }
 }
