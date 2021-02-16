f369f322c6584bd00c96d6a57df9c8be1f40be2d
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:18:38 +0000

    19114dc11e docs: remove unnecessary `<hr />` tags in Getting Started sections (#40693)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+41.sha-72db3e8/, version);
+      banner = banner.replace(/12.0.0-next.0+42.sha-19114dc/, version);
     }
   }
 }
