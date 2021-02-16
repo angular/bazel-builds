1c65977bd09f5bd08d06b1616ddadeaa1ca35859
Joey Perrott <josephperrott@gmail.com>
2021-02-16 15:59:18 +0000

    5f0c219883 docs: fix schematics' package name for `ng generate` sub-commands (#40846)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+36.sha-72a00dc/, version);
+      banner = banner.replace(/12.0.0-next.0+38.sha-5f0c219/, version);
     }
   }
 }
