e2bce1f2d671c90701b63206ba0d3666131cef40
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:22:52 +0000

    12f7101d92 docs(forms): fix link for browser compatibility of using selects in IE (#40855)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+67.sha-75db0c7/, version);
+      banner = banner.replace(/12.0.0-next.0+69.sha-12f7101/, version);
     }
   }
 }
