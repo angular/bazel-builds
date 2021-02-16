c8feae40deb9dc39f3bc551d78097bf599d8d5a4
Joey Perrott <josephperrott@gmail.com>
2021-02-16 19:58:38 +0000

    95ad452e54 fix(router): fix load interaction of navigation and preload strategies (#40389)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+44.sha-7fa8819/, version);
+      banner = banner.replace(/12.0.0-next.0+66.sha-95ad452/, version);
     }
   }
 }
