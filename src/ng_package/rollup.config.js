5bda30765ab987cb840f3e58852526d1cb120f1b
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:00:55 +0000

    f2ee9d5679 refactor(docs-infra): use CDK clipboard service (#40840)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+37.sha-67d85b3/, version);
+      banner = banner.replace(/12.0.0-next.0+39.sha-f2ee9d5/, version);
     }
   }
 }
