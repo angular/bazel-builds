82042e12e5dfe461287c059c66fc9fcb3910226c
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:21:43 +0000

    7f09945771 docs: Add documentation about the CLI failing (#35385)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0+29.sha-91c68f4/, version);
+      banner = banner.replace(/11.2.0+37.sha-7f09945/, version);
     }
   }
 }
