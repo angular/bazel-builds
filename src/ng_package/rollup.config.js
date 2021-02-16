536c71a880dc3b7d24820a7521364d41af45cfe1
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:06:09 +0000

    8945d7c7c1 build(docs-infra): add a link to `usage notes` from `description` (#40835)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+39.sha-f2ee9d5/, version);
+      banner = banner.replace(/12.0.0-next.0+40.sha-8945d7c/, version);
     }
   }
 }
