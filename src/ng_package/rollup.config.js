da47c7892e13a3c4fa667260256c9ad71576676f
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:00:26 +0000

    67d85b3680 docs: capitalize Angular word in upgrade-setup.md file (#40862) PR Close #40862

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+38.sha-5f0c219/, version);
+      banner = banner.replace(/12.0.0-next.0+37.sha-67d85b3/, version);
     }
   }
 }
