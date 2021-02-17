302b2abe7acd2c07c0a35b1ef8f7398c1792c2a4
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:18:33 +0000

    13d82ce248 docs(core): add documentation for HostListener (#40720)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+72.sha-a4c00c2/, version);
+      banner = banner.replace(/12.0.0-next.0+74.sha-13d82ce/, version);
     }
   }
 }
