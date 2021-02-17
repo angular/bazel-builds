535b2d02ea23c867586d03c4f21a74e30635da1f
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:15:28 +0000

    a4c00c2148 build: upgrade `webdriver-manager` to v12.1.8 (#40756)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.0+69.sha-12f7101/, version);
+      banner = banner.replace(/12.0.0-next.0+72.sha-a4c00c2/, version);
     }
   }
 }
