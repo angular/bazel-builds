9821b43f06e61aefcfc0915e9b5a0b1889216e8d
atscott <atscott01@gmail.com>
2021-02-17 20:24:12 +0000

    c8a2e3ae1c perf(core): use `ngDevMode` to tree-shake warning (#40876)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.0+69.sha-572e3a3/, version);
+      banner = banner.replace(/11.2.1+2.sha-c8a2e3a/, version);
     }
   }
 }
