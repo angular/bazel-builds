6382fe020128f97bf57c14bd9224d5ffd2067b94
Zach Arend <zachzach@google.com>
2021-02-18 01:25:53 +0000

    65baac6055 docs: update attribute-binding.md to correctly describe [attr.binding] rules (#40878)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+8.sha-2375b85/, version);
+      banner = banner.replace(/12.0.0-next.1+9.sha-65baac6/, version);
     }
   }
 }
