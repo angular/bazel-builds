bff85da2524b240d552cfa5340708559dc440480
Zach Arend <zachzach@google.com>
2021-02-18 01:23:41 +0000

    2375b85fe0 docs: remove mention of unused `Intl` polyfill (#40883)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+6.sha-f57cd19/, version);
+      banner = banner.replace(/12.0.0-next.1+8.sha-2375b85/, version);
     }
   }
 }
