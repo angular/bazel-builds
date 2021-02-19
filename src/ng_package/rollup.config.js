6a7ab06ce3bb59d413063b1fe406db4d14ff3904
atscott <atscott01@gmail.com>
2021-02-19 17:35:57 +0000

    dcbf600bbc refactor(docs-infra): convert Sass mixin from camelCase to kebab-case to follow Sass conventions (#40881)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.1+10.sha-4e84b2f/, version);
+      banner = banner.replace(/11.2.1+30.sha-dcbf600/, version);
     }
   }
 }
