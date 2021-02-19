487943f517317fc3e31e1390c4c61412b103bf33
Andrew Scott <atscott@google.com>
2021-02-19 16:33:14 +0000

    30d5e25a28 docs: edit attribute-directives.md, move best practice to styleguide (#39849)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+10.sha-542ba1f/, version);
+      banner = banner.replace(/12.0.0-next.1+11.sha-30d5e25/, version);
     }
   }
 }
