d213a1f8b3d9d2880eda82881527c1ef5f026b75
atscott <atscott01@gmail.com>
2021-02-19 17:29:39 +0000

    b4301c3d88 docs: add missing '@usageNotes' tag to documentation (#40909)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+13.sha-228b5f7/, version);
+      banner = banner.replace(/12.0.0-next.1+14.sha-b4301c3/, version);
     }
   }
 }
