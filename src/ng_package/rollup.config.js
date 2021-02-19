213510fb32d851f06a628cec99a29c8052c4288a
Andrew Scott <atscott@google.com>
2021-02-19 20:28:45 +0000

    3b884ed1fc fix(http): ignore question mark when params are parsed (#40610)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.1+30.sha-dcbf600/, version);
+      banner = banner.replace(/11.2.1+32.sha-3b884ed/, version);
     }
   }
 }
