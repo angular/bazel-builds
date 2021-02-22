c74197b4dc14573ed4afed3aa9a4e58aa2cb6d43
atscott <atscott01@gmail.com>
2021-02-22 23:35:48 +0000

    ddf7970b78 refactor(compiler-cli): separate out constant target of g3 patch (#40950)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+44.sha-a58f066/, version);
+      banner = banner.replace(/12.0.0-next.1+45.sha-ddf7970/, version);
     }
   }
 }
