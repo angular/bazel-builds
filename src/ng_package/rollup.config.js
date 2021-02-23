f219f2060c9c8e8fb563dc0886de105cbe5fb962
atscott <atscott01@gmail.com>
2021-02-23 19:10:10 +0000

    06d3267824 docs: improve accessibility for toh-pt1 example (#40560)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+54.sha-1f7921b/, version);
+      banner = banner.replace(/12.0.0-next.1+56.sha-06d3267/, version);
     }
   }
 }
