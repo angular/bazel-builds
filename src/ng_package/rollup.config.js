ccdac9ebdb9026524a8d70594e867abfb4c6ca03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-23 00:26:50 +0000

    206ec9b67b wip: explicit reference tracking

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+41.sha-e6bf7c2/, version);
+      banner = banner.replace(/12.0.0-next.1+51.sha-206ec9b/, version);
     }
   }
 }
