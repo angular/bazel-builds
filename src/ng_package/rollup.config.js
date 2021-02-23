3009438b8657503f4c09baff89e7e724b831bb4f
atscott <atscott01@gmail.com>
2021-02-23 19:06:09 +0000

    49e02ca7d6 docs: move JsonParser class above CustomJsonInterceptor (#40964)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+49.sha-ad38cbb/, version);
+      banner = banner.replace(/12.0.0-next.1+48.sha-49e02ca/, version);
     }
   }
 }
