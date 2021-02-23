aa783bb088b64c5d8cba494fb15acc3a49ad00cb
atscott <atscott01@gmail.com>
2021-02-23 19:06:48 +0000

    ad38cbbe09 perf(language-service): Skip Angular analysis when quick info requested outside a template (#40956)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+53.sha-bd6f377/, version);
+      banner = banner.replace(/12.0.0-next.1+49.sha-ad38cbb/, version);
     }
   }
 }
