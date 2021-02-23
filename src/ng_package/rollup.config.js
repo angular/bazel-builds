660b3e395b65669a24b578bfd3a453ecdeae30a6
atscott <atscott01@gmail.com>
2021-02-23 19:09:56 +0000

    1f7921be33 docs: improve accessibility of tohpt3 (#40617)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+55.sha-1f91672/, version);
+      banner = banner.replace(/12.0.0-next.1+54.sha-1f7921b/, version);
     }
   }
 }
