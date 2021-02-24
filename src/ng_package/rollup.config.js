45d18be13ab1daec81210259ddde46158755f2e5
Andrew Scott <atscott@google.com>
2021-02-24 16:22:23 +0000

    f93eae7708 build: update to rules_nodejs@3.2.1 (#40972)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+63.sha-aaf9b31/, version);
+      banner = banner.replace(/12.0.0-next.1+64.sha-f93eae7/, version);
     }
   }
 }
