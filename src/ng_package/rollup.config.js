a5dc40b2847b52742b80389d69228fc59557f803
Andrew Scott <atscott@google.com>
2021-02-24 16:19:27 +0000

    aaf9b31fb4 feat(core): drop support for zone.js 0.10.x (#40823)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+60.sha-03de2f2/, version);
+      banner = banner.replace(/12.0.0-next.1+63.sha-aaf9b31/, version);
     }
   }
 }
