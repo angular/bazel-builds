f7b95a3025d1c17064675254d2cd3748dbe0eae4
Andrew Scott <atscott@google.com>
2021-02-24 16:15:51 +0000

    03de2f2b78 docs: fix ch 4 so it compiles in strict mode (#40970)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.1+59.sha-011a527/, version);
+      banner = banner.replace(/12.0.0-next.1+60.sha-03de2f2/, version);
     }
   }
 }
