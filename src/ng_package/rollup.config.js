e9a712a0007f3da61758318b8cb7b290b3cb1854
Zach Arend <zachzach@google.com>
2021-02-24 23:26:22 +0000

    e986a9787b fix(language-service): use single entry point for Ivy and View Engine (#40967)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+3.sha-51a7977/, version);
+      banner = banner.replace(/12.0.0-next.2+4.sha-e986a97/, version);
     }
   }
 }
