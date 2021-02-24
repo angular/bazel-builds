9e7b224f45536d7e79929ba527e8ebeab45955ef
Zach Arend <zachzach@google.com>
2021-02-24 23:28:54 +0000

    3c24136b98 fix(compiler): ensure JIT compilation of ɵɵngDeclarePipe() works (#40929)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+4.sha-e986a97/, version);
+      banner = banner.replace(/12.0.0-next.2+5.sha-3c24136/, version);
     }
   }
 }
