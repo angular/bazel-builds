c79b0f067bf96bfb783b8564c45faad9c91b435d
Zach Arend <zachzach@google.com>
2021-02-24 23:50:28 +0000

    6c05c80f19 feat(router): Add more find-tuned control in `routerLinkActiveOptions` (#40303)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+6.sha-29d8a0a/, version);
+      banner = banner.replace(/12.0.0-next.2+7.sha-6c05c80/, version);
     }
   }
 }
