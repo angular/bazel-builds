61b6a3cf9130fd0de99065f323d554c6b2d0d0d3
Zach Arend <zachzach@google.com>
2021-02-26 16:44:11 +0000

    953e98e211 docs: Change grammatically in security guide (#40995)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+7.sha-6c05c80/, version);
+      banner = banner.replace(/12.0.0-next.2+9.sha-953e98e/, version);
     }
   }
 }
