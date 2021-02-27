f125a9f07eead538ebbf4650941bfa91583d38db
Zach Arend <zachzach@google.com>
2021-02-27 00:12:07 +0000

    be8893fd1d docs: add youtube to social icons (#40987)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+33.sha-bebdeba/, version);
+      banner = banner.replace(/12.0.0-next.2+34.sha-be8893f/, version);
     }
   }
 }
