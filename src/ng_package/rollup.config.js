60f25beabb66fe0ef424c738515427626fda5b98
Zach Arend <zachzach@google.com>
2021-03-01 23:44:05 +0000

    8110cf0ed2 fix(language-service): can't provide the Input and Output custom binding property name (#41005)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+35.sha-e12d9de/, version);
+      banner = banner.replace(/12.0.0-next.2+37.sha-8110cf0/, version);
     }
   }
 }
