a8a5a9654c811c22341d07968329b00ebb4b78ac
Zach Arend <zachzach@google.com>
2021-03-01 23:51:15 +0000

    6c783c7fcb docs: rewrite structural-directives.md (#40015)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+40.sha-356d9a0/, version);
+      banner = banner.replace(/12.0.0-next.2+41.sha-6c783c7/, version);
     }
   }
 }
