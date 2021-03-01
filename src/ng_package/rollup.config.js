c898f8bba07654f9cd21a88d14fc4cc2a8fb5106
Zach Arend <zachzach@google.com>
2021-03-01 23:51:38 +0000

    356d9a0537 docs: improve contrast of colors and body padding in boilerplate CSS (#40890)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+39.sha-54b0889/, version);
+      banner = banner.replace(/12.0.0-next.2+40.sha-356d9a0/, version);
     }
   }
 }
