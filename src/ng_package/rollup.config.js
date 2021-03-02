337eefe5b6f30ca0eb82899dfef17c44eace18ee
Zach Arend <zachzach@google.com>
2021-03-02 00:04:39 +0000

    78bf697311 refactor(platform-browser): cast response of JSON.parse (#41020)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.3+28.sha-2e0dea6/, version);
+      banner = banner.replace(/11.2.3+38.sha-78bf697/, version);
     }
   }
 }
