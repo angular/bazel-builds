13ec038b796f11088e4d5f608bf5a4e38f5bbe1a
Zach Arend <zachzach@google.com>
2021-03-03 18:22:24 +0000

    97b88f3631 fix(compiler): allow binding to autocomplete property on select and textarea elements (#40928)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+52.sha-736b1f9/, version);
+      banner = banner.replace(/12.0.0-next.2+53.sha-97b88f3/, version);
     }
   }
 }
