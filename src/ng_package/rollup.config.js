36bb05efb0264257e58638d0f1e10cb737e8192d
Zach Arend <zachzach@google.com>
2021-03-03 18:13:24 +0000

    ffbacbbc98 build(docs-infra): upgrade cli command docs sources to d62203a9f (#41046)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+50.sha-e9e7c33/, version);
+      banner = banner.replace(/12.0.0-next.2+51.sha-ffbacbb/, version);
     }
   }
 }
