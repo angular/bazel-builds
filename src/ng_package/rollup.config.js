fcc034fe474f2962a940de953543f4f5e74173ca
Zach Arend <zachzach@google.com>
2021-03-03 23:12:13 +0000

    2028a77dc8 release: cut the v11.2.4 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.3+49.sha-9322e6a/, version);
+      banner = banner.replace(/11.2.4/, version);
     }
   }
 }
