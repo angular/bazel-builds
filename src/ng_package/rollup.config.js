6e773b0e9ae82bc011699b5e91dd7c6659ee2420
Zach Arend <zachzach@google.com>
2021-03-03 18:06:18 +0000

    21f0deeaa6 build(docs-infra): update Angular framework, Material and CLI to latest methods (#40994)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+47.sha-10aa564/, version);
+      banner = banner.replace(/12.0.0-next.2+49.sha-21f0dee/, version);
     }
   }
 }
