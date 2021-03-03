0ceb6607144acc137f97f9438680dd48e0bdc2e1
Zach Arend <zachzach@google.com>
2021-03-03 23:18:36 +0000

    4606a6097b docs: release notes for the v11.2.4 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+58.sha-bb6e5e2/, version);
+      banner = banner.replace(/12.0.0-next.2+59.sha-4606a60/, version);
     }
   }
 }
