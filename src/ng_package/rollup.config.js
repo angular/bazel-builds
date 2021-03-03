601128d044c0013f5b057f2ef1a359f7d28a7325
Zach Arend <zachzach@google.com>
2021-03-03 21:31:39 +0000

    0847a0353b fix(language-service): Always attempt HTML AST to template AST conversion for LS (#41068)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+55.sha-7765b64/, version);
+      banner = banner.replace(/12.0.0-next.2+57.sha-0847a03/, version);
     }
   }
 }
