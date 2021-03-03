a9355fadfd402931a9d6510c9c8bc3c633c253ab
Zach Arend <zachzach@google.com>
2021-03-03 17:55:13 +0000

    5eab6e14f1 docs: fix typo in router.md file (#41064) PR Close #41064

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.2+44.sha-96f15f0/, version);
+      banner = banner.replace(/12.0.0-next.2+45.sha-5eab6e1/, version);
     }
   }
 }
