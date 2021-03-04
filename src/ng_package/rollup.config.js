11984df3b90a7e35f9745739284cc836012948f3
Andrew Kushnir <akushnir@google.com>
2021-03-04 23:10:11 +0000

    45216ccc0d fix(language-service): Only provide dom completions for inline templates (#41078)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+13.sha-38524c4/, version);
+      banner = banner.replace(/12.0.0-next.3+14.sha-45216cc/, version);
     }
   }
 }
