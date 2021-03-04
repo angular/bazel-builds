e44b9a56b3d51fe077fdb62ab4b4bc9290bfe355
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:28:31 +0000

    bf158e7ff0 fix(core): remove duplicated EMPTY_OBJ constant (#41066)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+10.sha-d44c7c2/, version);
+      banner = banner.replace(/12.0.0-next.3+11.sha-bf158e7/, version);
     }
   }
 }
