960b846e6c2eff080d43a19eb206fc717bfa672a
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:20:20 +0000

    d9acaa8547 docs: remove Codelyzer recommendation from style guide (#40992)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+3.sha-ad40fca/, version);
+      banner = banner.replace(/12.0.0-next.3+4.sha-d9acaa8/, version);
     }
   }
 }
