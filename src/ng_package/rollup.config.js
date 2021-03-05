569cc7b9b8a53705071547ff23156d879204e4b6
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:05:47 +0000

    e7453f15d5 docs: fix issue #40941 (#40942)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+16.sha-4637df5/, version);
+      banner = banner.replace(/12.0.0-next.3+17.sha-e7453f1/, version);
     }
   }
 }
