13c65fc9ef2244925d62da4f2c733ebf0a775305
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:03:15 +0000

    4637df5551 test(forms): add integration test app to keep track of payload size (#41045)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+15.sha-fbc9df1/, version);
+      banner = banner.replace(/12.0.0-next.3+16.sha-4637df5/, version);
     }
   }
 }
