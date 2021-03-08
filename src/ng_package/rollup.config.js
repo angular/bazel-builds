a882489ce46afa013242adc5ca8c5a1ea6e65d07
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:18:09 +0000

    c1a93fcf32 docs: remove augury resource (#41107)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+29.sha-fed6a7c/, version);
+      banner = banner.replace(/12.0.0-next.3+31.sha-c1a93fc/, version);
     }
   }
 }
