efbb82fc97112f7585a3e2a4befae2fb79ab5e5c
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:09:16 +0000

    13681d94d6 docs: fix typo in `CHANGELOG.md` (find-tuned --> fine-tuned) (#41113)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+21.sha-44bb78d/, version);
+      banner = banner.replace(/12.0.0-next.3+19.sha-13681d9/, version);
     }
   }
 }
