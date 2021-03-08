f54bdcf26d644789d65ca3e0b1639cb4ec8270e1
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:50:39 +0000

    ed342f4e53 test(dev-infra): fix test order failure in tests for the release command (#41023)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.4+26.sha-925746b/, version);
+      banner = banner.replace(/11.2.4+27.sha-ed342f4/, version);
     }
   }
 }
