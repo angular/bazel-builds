c6b0a81842b7af83713a880581bded33f4d9244d
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:09:34 +0000

    44bb78dbbd docs: update repo link to auto scroll to readme section (#41105)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+18.sha-c676ec1/, version);
+      banner = banner.replace(/12.0.0-next.3+21.sha-44bb78d/, version);
     }
   }
 }
