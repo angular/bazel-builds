5e824b8c7322c70132c1469cae2eb7a6282659f7
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:14:18 +0000

    2d69f0c9ca docs: extend :host selector documentation (#41055)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+23.sha-7854c4d/, version);
+      banner = banner.replace(/12.0.0-next.3+26.sha-2d69f0c/, version);
     }
   }
 }
