8a796bc5799277bb11a0198e1b75aec79bed7e8a
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:12:59 +0000

    7854c4ddbe test(docs-infra): run a11y tests against `/tutorial` and update a11y scores (#41103)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -141,7 +141,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+25.sha-681c341/, version);
+      banner = banner.replace(/12.0.0-next.3+23.sha-7854c4d/, version);
     }
   }
 }
