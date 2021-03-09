de2014bd12cc9d42a08b3718ce049bdef5b59dc4
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:13:47 +0000

    7a27cd262f refactor(docs-infra): simplify code snippet indentation in the "Cheat sheet" guide (#41051)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+43.sha-fa3689f/, version);
+      banner = banner.replace(/12.0.0-next.3+57.sha-7a27cd2/, version);
     }
   }
 }
