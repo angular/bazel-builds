14bae1d687ab2bca3a1da6ce4184aff8bfdbf654
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:18:27 +0000

    f281310d39 fix(docs-infra): print info to help debugging SW cache issue (#41106)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+57.sha-7a27cd2/, version);
+      banner = banner.replace(/12.0.0-next.3+58.sha-f281310/, version);
     }
   }
 }
