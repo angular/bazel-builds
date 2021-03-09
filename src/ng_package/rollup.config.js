44d357f71c40da3bda74dd9728d850d16b06fab3
Andrew Kushnir <akushnir@google.com>
2021-03-09 18:59:41 +0000

    b50d88073e perf(compiler-cli): ensure module resolution cache is reused for type-check program (#39693)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+59.sha-a730315/, version);
+      banner = banner.replace(/12.0.0-next.3+61.sha-b50d880/, version);
     }
   }
 }
