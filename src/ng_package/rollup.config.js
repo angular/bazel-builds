3ff2ecb203740224252069e9dcc38953e84b4fbb
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:21:49 +0000

    e29a29ce9f docs(common): change misspelled constant (#41136)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.3+58.sha-f281310/, version);
+      banner = banner.replace(/12.0.0-next.3+60.sha-e29a29c/, version);
     }
   }
 }
