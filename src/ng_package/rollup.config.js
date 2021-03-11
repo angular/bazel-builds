6fe6e97a34f95f137bbb6fea7bc2a84d5e2932f3
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:43:55 +0000

    79e21b966e fix(docs-infra): fix angular.io on IE 11 (#41162)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+4.sha-3c66b10/, version);
+      banner = banner.replace(/12.0.0-next.4+6.sha-79e21b9/, version);
     }
   }
 }
