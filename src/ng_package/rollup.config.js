34d0d3837d4b0eba2f6b930a76b84fc1de8ed40d
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-11 23:20:33 +0000

    012a2b55e1 build(language-service): use 'export =' syntax for default export (#41165)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+10.sha-69afeb3/, version);
+      banner = banner.replace(/12.0.0-next.4+11.sha-012a2b5/, version);
     }
   }
 }
