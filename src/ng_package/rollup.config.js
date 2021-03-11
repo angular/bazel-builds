128b55957537616100e3506d8d6523459e2dd34a
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:48:32 +0000

    ed6c09ae45 fix(localize): trim extracted `equiv-text` values (#41180)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.5+6.sha-78e6713/, version);
+      banner = banner.replace(/11.2.5+7.sha-ed6c09a/, version);
     }
   }
 }
