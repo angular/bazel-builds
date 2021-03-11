0fbcffffd41e9b3c67cf423e47ceac662ab79333
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:52:04 +0000

    78e67130f7 docs: add stepan suvorov to GDE resources (#41178)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.5+4.sha-2fa231a/, version);
+      banner = banner.replace(/11.2.5+6.sha-78e6713/, version);
     }
   }
 }
