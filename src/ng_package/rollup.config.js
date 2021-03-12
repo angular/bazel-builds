a9e9b2e7b2a70e7c2b340cbcd00958334e907987
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 22:40:17 +0000

    60ac964333 fix(forms): properly handle the change to the FormGroup shape (#40829)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.5+16.sha-b37296a/, version);
+      banner = banner.replace(/11.2.5+17.sha-60ac964/, version);
     }
   }
 }
