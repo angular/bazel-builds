22bd41c5164fc5286e742cddd1068ec1c7dfcb28
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 22:09:26 +0000

    e3ea8630bc ci: remove `CHANGELOG.md` from the `public-api` group (#41135)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+19.sha-cfaf425/, version);
+      banner = banner.replace(/12.0.0-next.4+20.sha-e3ea863/, version);
     }
   }
 }
