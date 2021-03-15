b5457f66999444aec7e9d5b746c9163ead85faaf
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:37:55 +0000

    3bd1992218 perf(forms): avoid direct references to the `Validators` class (#41189)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+24.sha-1644d64/, version);
+      banner = banner.replace(/12.0.0-next.4+25.sha-3bd1992/, version);
     }
   }
 }
