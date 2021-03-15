109b49f9605f5e1df6f81f751f79353b67c8aaca
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:51:24 +0000

    9ad57e6cb3 docs: document `help wanted` and `good first issue` labels (#41195)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+22.sha-937e90c/, version);
+      banner = banner.replace(/12.0.0-next.4+23.sha-9ad57e6/, version);
     }
   }
 }
