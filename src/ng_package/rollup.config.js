82ba3e5a64a23a107752078636da790148daa25b
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:55:39 +0000

    6414590f8d perf(forms): make `RadioControlRegistry` class tree-shakable (#41126)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.5+22.sha-bec919a/, version);
+      banner = banner.replace(/11.2.5+25.sha-6414590/, version);
     }
   }
 }
