6c5d08e2bfab151a4470e19427818048ed71d8ab
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:52:27 +0000

    7d6db009e9 docs: update docs to reflect production mode by default (#41173)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+34.sha-1a9f526/, version);
+      banner = banner.replace(/12.0.0-next.4+35.sha-7d6db00/, version);
     }
   }
 }
