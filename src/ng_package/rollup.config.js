1fc6a83e87060cfee285be16141183b6398d828f
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 22:07:13 +0000

    7d5b60e409 docs: remove unused attribute (#41211)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+42.sha-66e9970/, version);
+      banner = banner.replace(/12.0.0-next.4+43.sha-7d5b60e/, version);
     }
   }
 }
