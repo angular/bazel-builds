dcd023e639961d62891fe2ff32c63cde434a691d
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 18:27:47 +0000

    66e9970691 feat(ngcc): support `__read` helper as used by TypeScript 4.2 (#41201)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+40.sha-6bdad5e/, version);
+      banner = banner.replace(/12.0.0-next.4+42.sha-66e9970/, version);
     }
   }
 }
