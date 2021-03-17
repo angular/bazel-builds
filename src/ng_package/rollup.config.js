a64dcb2535f6c2e319939dfdfe71127fa5c99e0e
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:32:17 +0000

    eb74a96935 refactor(compiler-cli): separate used components from used directives in partial declaration (#41104)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+47.sha-98de4cb/, version);
+      banner = banner.replace(/12.0.0-next.4+48.sha-eb74a96/, version);
     }
   }
 }
