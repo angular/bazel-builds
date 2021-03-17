c4eeb51d27d868c2c0e108864697f148ae8acfdc
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:37:01 +0000

    fa048948be feat(core): drop support for TypeScript 4.0 and 4.1 (#41158)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+48.sha-eb74a96/, version);
+      banner = banner.replace(/12.0.0-next.4+50.sha-fa04894/, version);
     }
   }
 }
