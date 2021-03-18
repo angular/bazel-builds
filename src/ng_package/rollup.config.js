b67a7292f4a85fba0b2d3b2e7f69f3cd4f3e189a
Misko Hevery <misko@hevery.com>
2021-03-18 17:34:32 +0000

    ac9c4289e0 docs: replace deprecated karma-coverage-istanbul-reporter (#41246)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+3.sha-81d2aab/, version);
+      banner = banner.replace(/12.0.0-next.5+6.sha-ac9c428/, version);
     }
   }
 }
