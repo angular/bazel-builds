e97b352737ce9543389f6fe2cf78f953e6445396
Misko Hevery <misko@hevery.com>
2021-03-18 17:32:54 +0000

    81d2aabcbb docs: add preston lamb to GDE resources (#41204)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.4+52.sha-02e8901/, version);
+      banner = banner.replace(/12.0.0-next.5+3.sha-81d2aab/, version);
     }
   }
 }
