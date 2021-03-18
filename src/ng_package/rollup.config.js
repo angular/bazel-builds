d99332b76a1f583594743289b9b441a9ac4cc7a0
Misko Hevery <misko@hevery.com>
2021-03-18 17:36:14 +0000

    ac9a250fc4 docs: update link to schematics schema (#41258)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+6.sha-ac9c428/, version);
+      banner = banner.replace(/12.0.0-next.5+7.sha-ac9a250/, version);
     }
   }
 }
