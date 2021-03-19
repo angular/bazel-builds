a62436e66c159a50865710a8b81245818ebe173c
Misko Hevery <misko@hevery.com>
2021-03-19 19:15:56 +0000

    4690b6dbca docs: update link to schematics schema (#41258)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.5+31.sha-cbba715/, version);
+      banner = banner.replace(/11.2.6+5.sha-4690b6d/, version);
     }
   }
 }
