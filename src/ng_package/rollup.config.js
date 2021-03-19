19026ca441988e9cd135437295d19b1cc24ddae6
Misko Hevery <misko@hevery.com>
2021-03-19 20:14:26 +0000

    3a55698402 fix(core): remove obsolete check for [class] and [className] presence (#41254)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+10.sha-2ccb579/, version);
+      banner = banner.replace(/12.0.0-next.5+11.sha-3a55698/, version);
     }
   }
 }
