954182b719f1d9303da1c2294835e1bb1f5f2b84
Misko Hevery <misko@hevery.com>
2021-03-19 20:13:44 +0000

    72c9565c34 docs: specify date format for reviewed tag (#41269)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.6+5.sha-4690b6d/, version);
+      banner = banner.replace(/11.2.6+12.sha-72c9565/, version);
     }
   }
 }
