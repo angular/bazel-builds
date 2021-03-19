1f0eacde63964a2883cd9ce4370777464dd077b1
Misko Hevery <misko@hevery.com>
2021-03-19 20:22:28 +0000

    5eb7f3491f fix(dev-infra): remove commit message wizard and builder (#41280)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+12.sha-aa039a1/, version);
+      banner = banner.replace(/12.0.0-next.5+15.sha-5eb7f34/, version);
     }
   }
 }
