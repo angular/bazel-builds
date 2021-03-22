ba88684b4ae981c2b8c312ad5691fec437336cef
Misko Hevery <misko@hevery.com>
2021-03-22 16:16:14 +0000

    ed5bfe8173 docs: simplify deleteHero method in tutorial (#41303)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+18.sha-5bc0a47/, version);
+      banner = banner.replace(/12.0.0-next.5+16.sha-ed5bfe8/, version);
     }
   }
 }
