632ee0c7f249cd1a971cef011ebe5d17d801a396
Misko Hevery <misko@hevery.com>
2021-03-22 16:18:47 +0000

    e7b1d434c8 refactor(compiler): use `ɵɵInjectorDeclaration` rather than `ɵɵInjectorDef` in compiled output (#41119)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+19.sha-efe02d8/, version);
+      banner = banner.replace(/12.0.0-next.5+23.sha-e7b1d43/, version);
     }
   }
 }
