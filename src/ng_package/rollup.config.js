8f812b825ebf50a5ed32404eb5470f7002806fd9
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:43:03 +0000

    65f7d5380d docs: add link to fork repo how-to (#41266)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+31.sha-190fa07/, version);
+      banner = banner.replace(/12.0.0-next.5+33.sha-65f7d53/, version);
     }
   }
 }
