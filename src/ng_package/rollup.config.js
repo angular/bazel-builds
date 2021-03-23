dec53497b7b4b8cd3a669ad217f1bcb1734928fd
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:10:25 +0000

    1eba57eb00 fix(language-service): show suggestion when type inference is suboptimal (#41072)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+29.sha-80f11a9/, version);
+      banner = banner.replace(/12.0.0-next.5+30.sha-1eba57e/, version);
     }
   }
 }
