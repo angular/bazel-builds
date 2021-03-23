c21d156f2c877343e4052a584b3b0b51f908fab0
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:36:54 +0000

    190fa07b9a feat(router): add migration for ActivatedRouteSnapshot.fragment (#41092)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.5+32.sha-6a6d332/, version);
+      banner = banner.replace(/12.0.0-next.5+31.sha-190fa07/, version);
     }
   }
 }
