b28565b8211aa0e40e6dfeb9d4a70029d17b35e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 17:16:01 +0000

    723bfc717b Revert "feat(bazel): allow setting `compilationMode` in `ng_module` rule (#41366)"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.7+9.sha-06e5132/, version);
+      banner = banner.replace(/11.2.7+13.sha-723bfc7/, version);
     }
   }
 }
