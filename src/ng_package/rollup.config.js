4262d62ab4aa53b69d6d78286929baa16a60ddcc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 10:30:28 +0000

    06e5132818 ci: re-activate "devversion" in pullapprove after OOO (#41352)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.7+5.sha-6dcea34/, version);
+      banner = banner.replace(/11.2.7+9.sha-06e5132/, version);
     }
   }
 }
