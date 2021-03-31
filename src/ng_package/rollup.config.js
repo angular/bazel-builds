764f363c0ad3485387738421333f3ffa33b66a47
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:24:16 +0000

    a371646a37 build: update yargs dependency to 16.2.0. (#41351)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+24.sha-5167030/, version);
+      banner = banner.replace(/12.0.0-next.6+25.sha-a371646/, version);
     }
   }
 }
