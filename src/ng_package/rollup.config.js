cf29aaaf0ad25cd69dff140a7c469caf0cb93dfe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:05:35 +0000

    d35751d442 docs(docs-infra): update docs on configuring project for Travis CI (#37473)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+9.sha-da22d49/, version);
+      banner = banner.replace(/12.0.0-next.6+10.sha-d35751d/, version);
     }
   }
 }
