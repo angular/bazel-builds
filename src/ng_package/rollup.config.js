118b43ef8d8d5b66dcf5d85a3f2342963a988b9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:03:55 +0000

    da22d4904c docs: review and update components overview guide (#41263)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+8.sha-5e46901/, version);
+      banner = banner.replace(/12.0.0-next.6+9.sha-da22d49/, version);
     }
   }
 }
