4281e7c475587f0a80635db0d597a335f75866d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:52:36 +0000

    db55f8e671 ci: update commit-message verification range parameters (#41341)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+36.sha-18bc9ff/, version);
+      banner = banner.replace(/12.0.0-next.6+38.sha-db55f8e/, version);
     }
   }
 }
