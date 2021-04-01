4f3ada272f0672404ca19c84a51b653db7f44c5d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:38:09 +0000

    6391a4a49d docs: fix `.docs-card` tooltip and footer text in `docs` guide (#41250)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+25.sha-a371646/, version);
+      banner = banner.replace(/12.0.0-next.6+34.sha-6391a4a/, version);
     }
   }
 }
