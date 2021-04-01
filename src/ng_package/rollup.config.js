a0d967db04e6cbc186ba7f0216140ad028bf5cbd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:44:11 +0000

    18bc9ffb51 ci: do not try to publish directories that are not packages (#41393)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+34.sha-6391a4a/, version);
+      banner = banner.replace(/12.0.0-next.6+36.sha-18bc9ff/, version);
     }
   }
 }
