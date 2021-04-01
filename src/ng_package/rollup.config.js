de06b4a6b729051de80b4db076f4f03409df6add
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:38:43 +0000

    ed77bfea26 test(core): fix JIT factory declaration test for IE11 (#41416)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+49.sha-18abd3c/, version);
+      banner = banner.replace(/12.0.0-next.6+50.sha-ed77bfe/, version);
     }
   }
 }
