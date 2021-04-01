fab5179afc32f2cc105629ab15605ea6f53de77a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:56:26 +0000

    0226a11c18 fix(language-service): Only provide Angular property completions in templates (#41278)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+40.sha-3470ea1/, version);
+      banner = banner.replace(/12.0.0-next.6+41.sha-0226a11/, version);
     }
   }
 }
