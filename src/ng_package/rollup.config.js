3f7fd0e001216df5fbe7a0fca86f04511f8f7d79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:21:00 +0000

    a5a3752859 docs: fix misspelled word in VSCode Remote Development guide (#41376)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.6+43.sha-c6d9a19/, version);
+      banner = banner.replace(/12.0.0-next.6+44.sha-a5a3752/, version);
     }
   }
 }
