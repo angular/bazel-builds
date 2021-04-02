6e2ab84fa226acef351f93248aae7cfb7d15915d
atscott <atscott01@gmail.com>
2021-04-02 17:48:45 +0000

    dc8a0818e7 fix(dev-infra): require npm login for all publishes via wombat proxy (#41422)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.7+33.sha-154129b/, version);
+      banner = banner.replace(/11.2.8+2.sha-dc8a081/, version);
     }
   }
 }
