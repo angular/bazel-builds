c6e6b256168154d3c758dc05ba664df5c23ad839
atscott <atscott01@gmail.com>
2021-04-05 16:16:01 +0000

    180c1b555f fix(docs-infra): avoid auto-linking generic word `state` (#41438)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+10.sha-fcd190a/, version);
+      banner = banner.replace(/12.0.0-next.7+12.sha-180c1b5/, version);
     }
   }
 }
