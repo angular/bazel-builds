6303fabb0dc38ad3931babe9fb658ac0c6bceb94
atscott <atscott01@gmail.com>
2021-04-05 16:20:08 +0000

    2cef385e43 feat(dev-infra): prevent `git push` from being called in dryRun mode (#41387)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+11.sha-ab47f41/, version);
+      banner = banner.replace(/12.0.0-next.7+14.sha-2cef385/, version);
     }
   }
 }
