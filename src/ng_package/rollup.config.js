61e627f1ec00560f71bbdfefdd6bcc60c3b4df6c
atscott <atscott01@gmail.com>
2021-04-06 00:00:15 +0000

    6eb3dc770d build: update the recommended `Dockerfile` for VSCode remote development (#41396)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.8+10.sha-754247f/, version);
+      banner = banner.replace(/11.2.8+11.sha-6eb3dc7/, version);
     }
   }
 }
