6fb51a4129f13e0ab025115a4a66e050b1b1f69b
atscott <atscott01@gmail.com>
2021-04-06 20:04:58 +0000

    ff9253b4a6 ci: add cache for windows run (#41467)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+23.sha-ba3344d/, version);
+      banner = banner.replace(/12.0.0-next.7+32.sha-ff9253b/, version);
     }
   }
 }
