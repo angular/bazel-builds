24099043087a6f91b13268f70075d0e2ef42d670
atscott <atscott01@gmail.com>
2021-04-06 20:06:00 +0000

    4a68e13d1b build: update package.json engines range for nodes in preparation for upgrade (#41472)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+32.sha-ff9253b/, version);
+      banner = banner.replace(/12.0.0-next.7+33.sha-4a68e13/, version);
     }
   }
 }
