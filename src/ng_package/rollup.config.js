d45a842c9318692b80d5d1e6357a0ecad89073cc
atscott <atscott01@gmail.com>
2021-04-06 18:54:14 +0000

    bc74735644 build: correctly publish `angular-in-memory-web-api` as CI build artifact (#41429) (#41471)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.8+17.sha-0eecbed/, version);
+      banner = banner.replace(/11.2.8+23.sha-bc74735/, version);
     }
   }
 }
