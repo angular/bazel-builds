05c6ca1da814b4cc12925930e92780816ea45950
atscott <atscott01@gmail.com>
2021-04-06 17:29:11 +0000

    0eecbed69f docs(router): make getCurrentNavigation() jsdoc more explicit (#41417)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.8+11.sha-6eb3dc7/, version);
+      banner = banner.replace(/11.2.8+17.sha-0eecbed/, version);
     }
   }
 }
