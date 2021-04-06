7bfd1e9a14b463f43b60a0c417886f1c4c9dc2e4
atscott <atscott01@gmail.com>
2021-04-06 08:37:00 +0000

    3a823abcc5 refactor(compiler): remove dependency on fs-extra (#41445)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+16.sha-2a76d84/, version);
+      banner = banner.replace(/12.0.0-next.7+18.sha-3a823ab/, version);
     }
   }
 }
