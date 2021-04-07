650c052796e614670f7f43ddf0d12c27c680695b
atscott <atscott01@gmail.com>
2021-04-07 16:57:32 +0000

    dd66da1864 release: bump angular-in-web-memory-api package to 0.12.0 (#41487)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+33.sha-4a68e13/, version);
+      banner = banner.replace(/12.0.0-next.7+34.sha-dd66da1/, version);
     }
   }
 }
