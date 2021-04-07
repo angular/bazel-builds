c3fce93990db0b8cae7cc0d447c6a7788fb4ebce
atscott <atscott01@gmail.com>
2021-04-07 17:03:13 +0000

    f98f379dd8 fix(dev-infra): make scopes for `test` and `refactor` commit types optional (#41486)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+34.sha-dd66da1/, version);
+      banner = banner.replace(/12.0.0-next.7+37.sha-f98f379/, version);
     }
   }
 }
