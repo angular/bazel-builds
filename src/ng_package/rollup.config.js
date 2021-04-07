5c04007af032a31e206b9e32d56fc6152bc0d66b
atscott <atscott01@gmail.com>
2021-04-07 19:26:00 +0000

    41636e6f5c build: rely on engines to prevent using npm for dependency install (#41477)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.8+32.sha-9408af6/, version);
+      banner = banner.replace(/11.2.9+8.sha-41636e6/, version);
     }
   }
 }
