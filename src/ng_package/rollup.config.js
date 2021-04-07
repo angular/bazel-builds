6a18139e5c9631557531ba1e1747657f38830994
atscott <atscott01@gmail.com>
2021-04-07 19:11:05 +0000

    5a81490554 docs: use the right tag in the HeroesComponent's template snippet (#41473)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.7+40.sha-4b8bbe2/, version);
+      banner = banner.replace(/12.0.0-next.8+1.sha-5a81490/, version);
     }
   }
 }
