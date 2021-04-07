c1a8519c22704b45c63b5bb478a6cfa534ee8607
Zach Arend <zachzach@google.com>
2021-04-07 22:55:39 +0000

    10a7c87692 refactor(compiler): implement `ngDeclareInjectable()` (#41316)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+13.sha-d28a391/, version);
+      banner = banner.replace(/12.0.0-next.8+16.sha-10a7c87/, version);
     }
   }
 }
