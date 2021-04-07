24a96b512614e968e7c327bf5a067dc3b5c5beb4
atscott <atscott01@gmail.com>
2021-04-07 18:12:27 +0000

    9408af6059 docs: fix link URL in NG0100 error page (#41484)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.8+30.sha-f8986e1/, version);
+      banner = banner.replace(/11.2.8+32.sha-9408af6/, version);
     }
   }
 }
