bc927d1f229b085c7bdb23e12ee56a01641fb772
Zach Arend <zachzach@google.com>
2021-04-08 22:41:05 +0000

    74d1769a48 fix(dev-infra): use base ref name rather than sha for the githubTargetBranch (#41523)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.9+12.sha-646f4a1/, version);
+      banner = banner.replace(/11.2.9+13.sha-74d1769/, version);
     }
   }
 }
