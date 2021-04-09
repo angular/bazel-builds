3f0cdd59c1b5f57b9bae32350438c7a40640c81d
Zach Arend <zachzach@google.com>
2021-04-09 15:09:54 +0000

    7946f6385b feat(dev-infra): create ReleaseNotes class for generating release notes during publishing (#41476)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.9+14.sha-e34299a/, version);
+      banner = banner.replace(/11.2.9+17.sha-7946f63/, version);
     }
   }
 }
