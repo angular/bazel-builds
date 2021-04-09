e4903f7fdd17f4170b747590b6231f06b7adc6b3
Zach Arend <zachzach@google.com>
2021-04-09 14:52:33 +0000

    e34299af25 fix(router): handle new navigations from a NavigationEnd event (#41262) (#41511)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.9+13.sha-74d1769/, version);
+      banner = banner.replace(/11.2.9+14.sha-e34299a/, version);
     }
   }
 }
