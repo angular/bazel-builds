9ad9c522b056545277034dbf81c0eacca48290d9
Zach Arend <zachzach@google.com>
2021-04-12 17:33:20 +0000

    4810f5c819 ci: remove old compliance tests (#41556)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+27.sha-05cc3c8/, version);
+      banner = banner.replace(/12.0.0-next.8+29.sha-4810f5c/, version);
     }
   }
 }
