31137fa0a5c3d4b007808288d3672e7bfd735d5b
Zach Arend <zachzach@google.com>
2021-04-12 17:40:11 +0000

    ebc80b3b5c build: add breakingChangesLabel to merge configuration (#41546)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+29.sha-4810f5c/, version);
+      banner = banner.replace(/12.0.0-next.8+31.sha-ebc80b3/, version);
     }
   }
 }
