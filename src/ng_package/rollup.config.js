2fd13f6bb41df2078e02ff3ddf2b0a6ae672c773
Zach Arend <zachzach@google.com>
2021-04-12 17:54:25 +0000

    42e3a5241d test(compiler-cli): add integration test for relative rootDir (#41359)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+32.sha-aa0e54f/, version);
+      banner = banner.replace(/12.0.0-next.8+35.sha-42e3a52/, version);
     }
   }
 }
