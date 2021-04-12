94237125a06b4c2e75ca5b8bc9dde8c91f76461d
Zach Arend <zachzach@google.com>
2021-04-12 18:06:56 +0000

    c20db69f9f refactor(compiler-cli): introduce declaration function to declare class metadata (#41200)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+33.sha-6ba67c6/, version);
+      banner = banner.replace(/12.0.0-next.8+36.sha-c20db69/, version);
     }
   }
 }
