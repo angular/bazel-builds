507e0527fc534e66bce7acd92422213fba90d5da
Zach Arend <zachzach@google.com>
2021-04-13 04:30:39 +0000

    d27c8b4d9f docs: add Nishu Goel to GDE list (#41347)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+50.sha-0f54d6c/, version);
+      banner = banner.replace(/12.0.0-next.8+53.sha-d27c8b4/, version);
     }
   }
 }
