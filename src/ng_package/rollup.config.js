8772a468c202eb91bdc1448514be6acce51e5dd5
Zach Arend <zachzach@google.com>
2021-04-13 21:17:52 +0000

    1d12c50f63 fix(compiler-cli): autocomplete literal types in templates. (#41456)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+71.sha-ba84fa6/, version);
+      banner = banner.replace(/12.0.0-next.8+72.sha-1d12c50/, version);
     }
   }
 }
