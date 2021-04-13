20cb7152608ea63712041679bde8b648043367db
Zach Arend <zachzach@google.com>
2021-04-13 00:01:15 +0000

    9bf8e5164d refactor(dev-infra): use a singleton for GitClient (#41515)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+36.sha-c20db69/, version);
+      banner = banner.replace(/12.0.0-next.8+37.sha-9bf8e51/, version);
     }
   }
 }
