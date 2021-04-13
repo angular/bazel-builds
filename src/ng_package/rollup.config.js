0b5ee830a2667b3e7b1bb78e16821331efd36aed
Zach Arend <zachzach@google.com>
2021-04-13 19:11:21 +0000

    4847c146dc fix(dev-infra): only create authenticated github instance once in yargs (#41603)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.9+35.sha-cfd67c1/, version);
+      banner = banner.replace(/11.2.9+36.sha-4847c14/, version);
     }
   }
 }
