7872bcb0219df8e51ccaad5e40ece705d516b3aa
Zach Arend <zachzach@google.com>
2021-04-13 04:28:09 +0000

    bfbdb8f84d refactor(compiler-cli): cleanup redundant storage of reuse `ts.Program` (#41289)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+49.sha-319da89/, version);
+      banner = banner.replace(/12.0.0-next.8+52.sha-bfbdb8f/, version);
     }
   }
 }
