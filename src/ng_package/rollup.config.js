2bffb912a408f9d217ef3d6e5cbf7e60bfa5c7a7
Zach Arend <zachzach@google.com>
2021-04-14 23:21:48 +0000

    b616f731b2 docs: add rx-angular to resources.json (#39291)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.10/, version);
+      banner = banner.replace(/11.2.10+4.sha-b616f73/, version);
     }
   }
 }
