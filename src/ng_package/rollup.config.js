c0a38b93de103fc68cd66f88cd037d83567dcf81
Zach Arend <zachzach@google.com>
2021-04-14 00:57:09 +0000

    fd9a7ca8c9 build: update to latest version of `rules_nodejs` v3.3.0 (#41599)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+74.sha-f7e391a/, version);
+      banner = banner.replace(/12.0.0-next.8+75.sha-fd9a7ca/, version);
     }
   }
 }
