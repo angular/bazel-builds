8866f89db7165409bb7dd81d8b1fc8cfe6e0531a
Zach Arend <zachzach@google.com>
2021-04-14 18:32:37 +0000

    3923201f95 refactor(compiler-cli): change how partial-linkers are matched to declaration versions (#41578)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+96.sha-5a8bc1b/, version);
+      banner = banner.replace(/12.0.0-next.8+97.sha-3923201/, version);
     }
   }
 }
