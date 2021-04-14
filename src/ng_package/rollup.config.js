3b2fe80d4f3dbbd552283166ce5cd421136b993b
Zach Arend <zachzach@google.com>
2021-04-14 23:11:53 +0000

    1043aa42ad build: update typescript to version 4.2.4 (#41618)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+101.sha-2af894d/, version);
+      banner = banner.replace(/12.0.0-next.8+102.sha-1043aa4/, version);
     }
   }
 }
