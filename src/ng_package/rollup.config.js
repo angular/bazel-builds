44d16adaa085f64e8c0b7f086825387f62d06d6b
Zach Arend <zachzach@google.com>
2021-04-14 17:39:32 +0000

    b8c4da9ae8 Revert "fix(compiler-cli): autocomplete literal types in templates. (#41456)" (#41623)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.9+37.sha-04235fa/, version);
+      banner = banner.replace(/11.2.9+41.sha-b8c4da9/, version);
     }
   }
 }
