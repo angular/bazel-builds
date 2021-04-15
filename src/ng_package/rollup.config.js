59d36282c1e89e1c1f1cd3de1b927cdca38eb909
Andrew Kushnir <akushnir@google.com>
2021-04-15 21:55:46 +0000

    9d8f345c0c docs: update copy in schematics.md (#41521)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+117.sha-8d0511d/, version);
+      banner = banner.replace(/12.0.0-next.8+120.sha-9d8f345/, version);
     }
   }
 }
