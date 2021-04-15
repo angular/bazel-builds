e223686289331704e7ea659f3c7238e14f4898b0
Zach Arend <zachzach@google.com>
2021-04-15 02:14:51 +0000

    0d55d4557f test(router): add Router regression test for hash location strategy (#40409)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+110.sha-5332a4a/, version);
+      banner = banner.replace(/12.0.0-next.8+109.sha-0d55d45/, version);
     }
   }
 }
