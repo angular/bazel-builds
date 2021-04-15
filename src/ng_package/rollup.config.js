68217dfaa5fb654b391d5e9aad703e3c9a429f41
Zach Arend <zachzach@google.com>
2021-04-15 02:12:24 +0000

    9438d48b89 refactor: update `$schema` and `id` keywords (#41574)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.9+9.sha-e655846/, version);
+      banner = banner.replace(/12.0.0-next.9+11.sha-9438d48/, version);
     }
   }
 }
