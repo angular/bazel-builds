736c5a31d99bd7fc907634ec6496fd0995a03483
Andrew Kushnir <akushnir@google.com>
2021-04-15 23:59:20 +0000

    21d3084512 docs: edits to remove jargon and abbreviations (#41530)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.10+8.sha-6c43a94/, version);
+      banner = banner.replace(/11.2.10+9.sha-21d3084/, version);
     }
   }
 }
