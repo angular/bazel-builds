6b0db6bf006941bc674bf60e43e901733bb28dd6
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:10:54 +0000

    f4376fc0d4 fix(router): Only retrieve stored route when reuse strategy indicates it should reattach (#30263)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.10+5.sha-c4b0753/, version);
+      banner = banner.replace(/11.2.10+6.sha-f4376fc/, version);
     }
   }
 }
