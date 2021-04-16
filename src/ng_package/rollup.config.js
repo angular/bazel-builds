421427f4cec3fcb8037802268871b8afd6e7bde9
Andrew Kushnir <akushnir@google.com>
2021-04-16 21:31:13 +0000

    83d6a96163 feat(dev-infra): prevent pr merges of features or breaking changes on rc (#41660)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+132.sha-62e3f32/, version);
+      banner = banner.replace(/12.0.0-next.8+133.sha-83d6a96/, version);
     }
   }
 }
