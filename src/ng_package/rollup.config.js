981bdfe2edae5275faa9e44ac92c903699c0bfaf
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:46:22 +0000

    de25cdac54 docs: move lazy loading section to the best practices section (#41378)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+136.sha-96175fd/, version);
+      banner = banner.replace(/12.0.0-next.8+138.sha-de25cda/, version);
     }
   }
 }
