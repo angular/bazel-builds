c6200be4d7bdfd2d8aa5745980524b69590d6a8c
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:41:18 +0000

    70341bcfed build(docs-infra): update docs examples to Angular v11.2.10 (#41689)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+172.sha-6cdb8fb/, version);
+      banner = banner.replace(/12.0.0-next.8+175.sha-70341bc/, version);
     }
   }
 }
