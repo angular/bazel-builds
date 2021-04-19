7230c1e24c9a02cf0c986b37fb5729e469a3992a
Andrew Kushnir <akushnir@google.com>
2021-04-19 22:28:13 +0000

    853016dca2 build: update firebase-tools to version 9.10.0 (#41686)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+175.sha-70341bc/, version);
+      banner = banner.replace(/12.0.0-next.8+177.sha-853016d/, version);
     }
   }
 }
