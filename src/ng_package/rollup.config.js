150782b188fe9de0fb113bb272178057678e6b8b
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:23:32 +0000

    6a153a2a6f build: update diff to version 5.0.0 (#41684)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+147.sha-1391323/, version);
+      banner = banner.replace(/12.0.0-next.8+149.sha-6a153a2/, version);
     }
   }
 }
