a421263c57086b4f6906526451dc819d17d1ff4d
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:15:03 +0000

    6d1df308c4 docs: documentation of descendants property of @ContentChildren (#35927)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.9+77.sha-b333a1f/, version);
+      banner = banner.replace(/12.0.0-next.9+78.sha-6d1df30/, version);
     }
   }
 }
