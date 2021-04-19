a7f1e208a5e19baa82d7222316738b7518a85846
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:21:08 +0000

    13913231f1 test(docs-infra): change URL separator from `\t` to `-->` in `URLS_TO_REDIRECT.txt` (#41680)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+144.sha-75168a2/, version);
+      banner = banner.replace(/12.0.0-next.8+147.sha-1391323/, version);
     }
   }
 }
