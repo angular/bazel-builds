bc7f913cdd3adc76e3b27074fbd8e86a79a30cf5
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:26:10 +0000

    0c23c8b0ac build: update image-size to version 1.0.0 (#41691)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+148.sha-185090b/, version);
+      banner = banner.replace(/12.0.0-next.8+154.sha-0c23c8b/, version);
     }
   }
 }
