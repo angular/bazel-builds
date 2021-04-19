10b986ca07abc52352371d9f10baa64d22284c94
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:35:57 +0000

    ef4cf878b1 build: update google-closure-compiler to version 20210406.0.0 (#41687)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+169.sha-5e34317/, version);
+      banner = banner.replace(/12.0.0-next.8+170.sha-ef4cf87/, version);
     }
   }
 }
