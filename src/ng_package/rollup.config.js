c7c5911504672aeb6a71f7c9e1a333634eca2dc8
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:15:49 +0000

    cf6c5c1d2c build: update dependency-graph to version 0.11.0 (#41665)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+140.sha-d0b1f99/, version);
+      banner = banner.replace(/12.0.0-next.8+142.sha-cf6c5c1/, version);
     }
   }
 }
