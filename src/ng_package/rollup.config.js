e914f52bc8cc6272441e0c582bc927ba9d3fdc35
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:38:24 +0000

    c617f1f768 build: update jsdom to version 16.5.3 (#41695)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+170.sha-ef4cf87/, version);
+      banner = banner.replace(/12.0.0-next.8+171.sha-c617f1f/, version);
     }
   }
 }
