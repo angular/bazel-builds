4349ec6e884c3ce25ebd90fd7f62251a82aa0609
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:40:45 +0000

    6cdb8fb0aa build: remove entities dependency from root package.json (#41705)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+171.sha-c617f1f/, version);
+      banner = banner.replace(/12.0.0-next.8+172.sha-6cdb8fb/, version);
     }
   }
 }
