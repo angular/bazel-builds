91a2e797bb63d341a14042314b7a1c5c79784044
Andrew Kushnir <akushnir@google.com>
2021-04-19 20:57:22 +0000

    3631934f2d Revert "fix(zone.js): should continue to executue listeners when throw error (#41562)" (#41707)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.10+29.sha-8c831a2/, version);
+      banner = banner.replace(/11.2.10+31.sha-3631934/, version);
     }
   }
 }
