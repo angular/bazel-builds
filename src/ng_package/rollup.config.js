a7daf1391189928b15844c843c1d5e4f50676c0f
Andrew Kushnir <akushnir@google.com>
2021-04-20 19:35:05 +0000

    a2b671e61b docs: add margin to button boilerplate CSS for examples (#41413)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+186.sha-73824d5/, version);
+      banner = banner.replace(/12.0.0-next.8+187.sha-a2b671e/, version);
     }
   }
 }
