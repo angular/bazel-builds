2f6fdaba66e3d38cb78f839c58b1da9ba1fbcbd5
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:00:20 +0000

    e238f2c33a docs: rewrites animation query () method use (#41710)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+179.sha-03f3dcb/, version);
+      banner = banner.replace(/12.0.0-next.8+182.sha-e238f2c/, version);
     }
   }
 }
