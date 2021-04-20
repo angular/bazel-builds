08b7e4e073346d2c710ddac0fc8a7d4026d5ec24
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:01:46 +0000

    c2011c0ba1 build: update semver to version 7.3.5 (#41715)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+182.sha-e238f2c/, version);
+      banner = banner.replace(/12.0.0-next.8+184.sha-c2011c0/, version);
     }
   }
 }
