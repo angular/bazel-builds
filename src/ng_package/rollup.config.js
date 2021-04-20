b864efb3356a26ab7d29e3600a9bea3e01ac164b
Andrew Kushnir <akushnir@google.com>
2021-04-20 23:18:59 +0000

    7bd8741109 build: update io_bazel_rules_sass to version 1.32.11 (#41713)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.9+87.sha-87b5026/, version);
+      banner = banner.replace(/12.0.0-next.9+88.sha-7bd8741/, version);
     }
   }
 }
