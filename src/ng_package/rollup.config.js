7b0a9edafc4e7f69098661de2d6215b1dc928323
Andrew Kushnir <akushnir@google.com>
2021-04-20 16:57:38 +0000

    03f3dcb700 docs: add API CMS capability to Angular App resources (#40924)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+178.sha-251bec1/, version);
+      banner = banner.replace(/12.0.0-next.8+179.sha-03f3dcb/, version);
     }
   }
 }
