3ea389ae4149a3afb87d24d56f2d105d148fa1cb
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:07:22 +0000

    73824d5337 fix(compiler): not generating update instructions for ng-template inside alternate namespaces (#41669)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+185.sha-364ff96/, version);
+      banner = banner.replace(/12.0.0-next.8+186.sha-73824d5/, version);
     }
   }
 }
