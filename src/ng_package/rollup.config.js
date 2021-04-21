d9c785061cd5084dd401fea1aefb9322a47428ae
GitHub <noreply@github.com>
2021-04-21 17:25:55 +0000

    03d2fc2834 release: cut the v11.2.11 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.10+41.sha-8f3e5a5/, version);
+      banner = banner.replace(/11.2.10+42.sha-03d2fc2/, version);
     }
   }
 }
