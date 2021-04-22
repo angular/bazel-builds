46a354c22666a8bde59fc78b2b33946023792d25
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:09:49 +0000

    de4fafb818 refactor(core): add single type for injector token (#41580)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+204.sha-64567d3/, version);
+      banner = banner.replace(/12.0.0-next.8+206.sha-de4fafb/, version);
     }
   }
 }
