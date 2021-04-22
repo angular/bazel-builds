5db69f993d56b04e01d4aa2f2dbfc94643fb6b0b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:26:54 +0000

    1d1fa1a8f3 test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.10+42.sha-03d2fc2/, version);
+      banner = banner.replace(/11.2.11+2.sha-1d1fa1a/, version);
     }
   }
 }
