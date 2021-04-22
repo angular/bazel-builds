8194c963e6002a788167035c9faacb156a62c8ca
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:51:07 +0000

    d061be99a9 fix(dev-infra): verify python3 is available before release (#41753)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.11+2.sha-1d1fa1a/, version);
+      banner = banner.replace(/11.2.11+4.sha-d061be9/, version);
     }
   }
 }
