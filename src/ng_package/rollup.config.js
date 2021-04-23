9b66298d31ef298d7d06644e90107081a3d3bd61
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:58:53 +0000

    9f88da220c docs: Indicate `HttpClientModule` being imported (#41749)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+212.sha-5071331/, version);
+      banner = banner.replace(/12.0.0-next.8+213.sha-9f88da2/, version);
     }
   }
 }
