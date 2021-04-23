d5ac49141bfde3f267b464dc8510792af8f81012
GitHub <noreply@github.com>
2021-04-23 18:50:31 +0000

    45ffab59f1 ci: remove trailing comma from renovate config

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+139.sha-aa35a1a/, version);
+      banner = banner.replace(/12.0.0-next.8+213.sha-45ffab5/, version);
     }
   }
 }
