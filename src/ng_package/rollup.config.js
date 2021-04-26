bcdd53121ed2ea6e7eeda2b70cb900a7acfef7f1
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:44:12 +0000

    27ad7624d0 fix(docs-infra): fix external link icons positioning (#41794)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+217.sha-01e546f/, version);
+      banner = banner.replace(/12.0.0-next.8+218.sha-27ad762/, version);
     }
   }
 }
