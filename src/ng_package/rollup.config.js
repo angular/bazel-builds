784fcda40e68f15d7e2dc39ab1dc6ea0fd38aec3
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:38:59 +0000

    f9c1f08957 fix(core): update supported range of node versions to only include LTS versions (#41822)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.0+42.sha-8d4460d/, version);
+      banner = banner.replace(/12.0.0-rc.0+61.sha-f9c1f08/, version);
     }
   }
 }
