a4d637a74c0ec32a5d7ca47dde445bae2ac74e53
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:29:38 +0000

    326884736e docs: make inputs-outputs example more accessible (#41288)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+215.sha-70fb3e9/, version);
+      banner = banner.replace(/12.0.0-next.8+216.sha-3268847/, version);
     }
   }
 }
