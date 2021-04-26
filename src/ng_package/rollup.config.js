ac91b39c70fda8b98ddd2b6267591f0ea851aaf6
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:46:38 +0000

    bd986cb460 build: update bazel packages (#41795)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+218.sha-27ad762/, version);
+      banner = banner.replace(/12.0.0-next.8+220.sha-bd986cb/, version);
     }
   }
 }
