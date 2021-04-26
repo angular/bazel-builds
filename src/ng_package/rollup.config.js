3985f172110364d1cf14e346206e26258c5aefce
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:48:26 +0000

    6465b5056a build(docs-infra): fix e2e testing SystemJS-based docs examples (#41796)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+220.sha-bd986cb/, version);
+      banner = banner.replace(/12.0.0-next.8+219.sha-6465b50/, version);
     }
   }
 }
