58c90d804b202d25d36498525aa4f723a51c3e8d
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:48:28 +0000

    6d9e3400e1 test(service-worker): replace `global || window` with `globalThis` (#41739)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+219.sha-6465b50/, version);
+      banner = banner.replace(/12.0.0-next.8+223.sha-6d9e340/, version);
     }
   }
 }
