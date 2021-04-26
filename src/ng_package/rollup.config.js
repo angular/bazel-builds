ef5f78a71457ee0158b87afc0632398c0e0430cc
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 18:19:51 +0000

    99f2ffc740 build: update stemmer to version 2.0.0 (#41724)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+224.sha-18b33e7/, version);
+      banner = banner.replace(/12.0.0-next.8+227.sha-99f2ffc/, version);
     }
   }
 }
