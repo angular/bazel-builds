dfd49553b7e6cc464c23af9945ba7b1975fb3530
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:30:41 +0000

    70fb3e958b docs: improve accessibility of http example (#41057)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+214.sha-73105aa/, version);
+      banner = banner.replace(/12.0.0-next.8+215.sha-70fb3e9/, version);
     }
   }
 }
