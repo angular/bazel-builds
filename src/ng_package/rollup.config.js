18bd07ec894c78d3e70a75c91a7d37d64e1c6959
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:50:25 +0000

    18b33e79d3 perf(core): avoid storing LView in __ngContext__ (#41358)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+223.sha-6d9e340/, version);
+      banner = banner.replace(/12.0.0-next.8+224.sha-18b33e7/, version);
     }
   }
 }
