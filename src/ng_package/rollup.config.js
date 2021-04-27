67f042fb9faa2b1e3d1fe41ad1340ecbfac2c9c7
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:35:29 +0000

    f978976970 build: update @types/jasmine to version 3.6.10 (#41830)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+252.sha-da6ed15/, version);
+      banner = banner.replace(/12.0.0-next.8+253.sha-f978976/, version);
     }
   }
 }
