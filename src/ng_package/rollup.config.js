0769c789d9159db9b1ddd57b94e843129e1068e5
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:59:41 +0000

    30db14b5db ci: several updates to renovate configuration (#41834)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.0+68.sha-947b451/, version);
+      banner = banner.replace(/12.0.0-rc.0+69.sha-30db14b/, version);
     }
   }
 }
