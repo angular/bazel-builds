6fb359ae65d7f6dd125e35177cfc7a345c80b164
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 19:42:50 +0000

    0140d04ced feat(dev-infra): create the prettier formatter for ng-dev format tooling (#41824)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+257.sha-cd004d4/, version);
+      banner = banner.replace(/12.0.0-next.8+258.sha-0140d04/, version);
     }
   }
 }
