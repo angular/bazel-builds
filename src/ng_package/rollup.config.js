c4ec24b8707fc7e4dfc7fbc59de5a990f2852814
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 18:53:49 +0000

    cd004d4a23 ci: add Zach Arend to pullapprove groups (#41839)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+256.sha-b1ae31c/, version);
+      banner = banner.replace(/12.0.0-next.8+257.sha-cd004d4/, version);
     }
   }
 }
