a7233ffd99872a8e7f3ecc7c6947d57f82de4f63
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:13:46 +0000

    eedbb758d2 build(docs-infra): update dgeni-packages to fix version list (#41841)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.11+24.sha-e758658/, version);
+      banner = banner.replace(/11.2.11+25.sha-eedbb75/, version);
     }
   }
 }
