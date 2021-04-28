dee3f3ab45b8ea73b6df9e802302c6c9212c7f3b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 18:45:19 +0000

    f550742020 release: cut the v11.2.12 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.11+38.sha-336fe91/, version);
+      banner = banner.replace(/11.2.12/, version);
     }
   }
 }
