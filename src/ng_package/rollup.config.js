bbd1553c316787f067fb478bba60ab674c01c71b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 19:55:39 +0000

    42a64f8676 docs: remove mention of scheduleTick in markForCheck (#41845)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+272.sha-bbda73e/, version);
+      banner = banner.replace(/12.0.0-next.8+278.sha-42a64f8/, version);
     }
   }
 }
