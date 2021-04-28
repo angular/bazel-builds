690c523129dfe6f11cdd97e6b5700749c2acf0d3
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:51:57 +0000

    fca8d7ae92 docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.12+1.sha-f555a55/, version);
+      banner = banner.replace(/11.2.12+3.sha-fca8d7a/, version);
     }
   }
 }
