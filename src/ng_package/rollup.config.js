1005d3de5c4490241c603c308f123e15763899c6
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:37:03 +0000

    7219d1b562 ci: prevent CI cache from growing indefinitely (#41814) (#41855)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.11+27.sha-4aafcc0/, version);
+      banner = banner.replace(/11.2.11+36.sha-7219d1b/, version);
     }
   }
 }
