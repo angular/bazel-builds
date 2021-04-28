03a9cb83a9a722b7b154f0e706b77ea619d3af0f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:18:15 +0000

    a66421e643 ci: re-enable the `@angular/components` unit tests (#41816)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.0+79.sha-77a5157/, version);
+      banner = banner.replace(/12.0.0-rc.0+80.sha-a66421e/, version);
     }
   }
 }
