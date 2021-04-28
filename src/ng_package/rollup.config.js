938e3fef9e1481f7974fb8fa068e5ec72764b4fa
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:58:54 +0000

    336fe9124e Revert "ci: re-enable the `@angular/components` unit tests (#41816)" (#41863)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.11+36.sha-7219d1b/, version);
+      banner = banner.replace(/11.2.11+38.sha-336fe91/, version);
     }
   }
 }
