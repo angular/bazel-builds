8ef97a4a0493e7f2327619de3b1c9210cf61b230
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:41:33 +0000

    1aed2f3234 fix(dev-infra): replace `chalk` require with namespace import (#41847)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+265.sha-3b7d4eb/, version);
+      banner = banner.replace(/12.0.0-next.8+268.sha-1aed2f3/, version);
     }
   }
 }
