614c69d400cf6c7207e25cfa2f4240e977e53478
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:45:59 +0000

    044b380e5a build: update remark packages (#41721)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+268.sha-1aed2f3/, version);
+      banner = banner.replace(/12.0.0-next.8+269.sha-044b380/, version);
     }
   }
 }
