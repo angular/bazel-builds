9a677514aa66e63059753b365b56dcb48f9d9e6b
Misko Hevery <misko@hevery.com>
2021-04-29 21:10:39 +0000

    ea89617880 docs(docs-infra): fix handling of client-side errors in networking (#36608)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+290.sha-e53c80c/, version);
+      banner = banner.replace(/12.0.0-next.8+293.sha-ea89617/, version);
     }
   }
 }
