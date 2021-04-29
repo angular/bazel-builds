8a4fb3666aa213ff9bac2ec5d645418a4dc7528a
GitHub <noreply@github.com>
2021-04-29 08:41:07 +0000

    f97dfe99b0 ci: exclude packages which don't have a major from being batched

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+272.sha-083ceec/, version);
+      banner = banner.replace(/12.0.0-next.8+273.sha-f97dfe9/, version);
     }
   }
 }
