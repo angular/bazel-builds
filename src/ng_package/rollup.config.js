53a978107a5c57bf248004033ce612be1c539af1
Misko Hevery <misko@hevery.com>
2021-04-29 17:45:28 +0000

    05209fa397 ci: exclude packages which don't have a major from being batched (#41865)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+288.sha-f3331a8/, version);
+      banner = banner.replace(/12.0.0-next.8+286.sha-05209fa/, version);
     }
   }
 }
