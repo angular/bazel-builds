3c30765afc0e355aa1ce607f7517c248b918bc1e
Misko Hevery <misko@hevery.com>
2021-04-29 17:50:57 +0000

    77b77ff525 ci: only run `publish_snapshot` when not PR (#41877)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.1+10.sha-ede0cfe/, version);
+      banner = banner.replace(/12.0.0-rc.1+16.sha-77b77ff/, version);
     }
   }
 }
