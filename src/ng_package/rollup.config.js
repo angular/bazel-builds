122db4c0a7dca7062b1cdfe3e94708de1b5bbde9
Misko Hevery <misko@hevery.com>
2021-04-30 21:42:51 +0000

    35450c78f7 fix(compiler-cli): prefer non-aliased exports in reference emitters (#41866)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+300.sha-72384a3/, version);
+      banner = banner.replace(/12.0.0-next.8+298.sha-35450c7/, version);
     }
   }
 }
