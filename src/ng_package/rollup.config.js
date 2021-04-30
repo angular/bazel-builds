dc4e64fe5321626468bc8ed32ee6ff38338181dc
Misko Hevery <misko@hevery.com>
2021-04-30 21:37:17 +0000

    72384a3357 fix(docs-infra): switch over to img[loading=lazy] for front page images (#34900)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+296.sha-6581a1b/, version);
+      banner = banner.replace(/12.0.0-next.8+300.sha-72384a3/, version);
     }
   }
 }
