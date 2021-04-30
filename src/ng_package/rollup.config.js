de27e653646129898d101d8085b2410101d7fd2e
Misko Hevery <misko@hevery.com>
2021-04-30 20:48:12 +0000

    be9b19e820 Revert "build: update dependency karma to v6 (#41696)" (#41904)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.1+20.sha-0255a2c/, version);
+      banner = banner.replace(/12.0.0-rc.1+21.sha-be9b19e/, version);
     }
   }
 }
