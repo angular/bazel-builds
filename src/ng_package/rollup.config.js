eb015118980f0c3563a44ed20ee46b0380cf624c
Misko Hevery <misko@hevery.com>
2021-04-30 21:33:12 +0000

    2dd96e08ae Revert "perf(core): avoid storing LView in __ngContext__ (#41358)" (#41901)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+294.sha-0dfd940/, version);
+      banner = banner.replace(/12.0.0-next.8+295.sha-2dd96e0/, version);
     }
   }
 }
