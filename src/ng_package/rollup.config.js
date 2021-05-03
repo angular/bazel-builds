719e5b27fc788ea6d708b96d16d4fbc0f50787e4
Misko Hevery <misko@hevery.com>
2021-05-03 21:44:12 +0000

    253ed5d484 docs: improve accessibility of form-validation example (#41283)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+311.sha-6967f3c/, version);
+      banner = banner.replace(/12.0.0-next.8+312.sha-253ed5d/, version);
     }
   }
 }
