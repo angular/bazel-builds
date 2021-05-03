21233bfdc6289b45566e18aa050b5bc18df4d3d5
Misko Hevery <misko@hevery.com>
2021-05-03 21:50:38 +0000

    741c295129 docs: update file structure document (#41923)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+315.sha-f42796a/, version);
+      banner = banner.replace(/12.0.0-next.8+314.sha-741c295/, version);
     }
   }
 }
