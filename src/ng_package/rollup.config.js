ee4e710a92d90bbb067dcdd8516b5291bfce7b38
Misko Hevery <misko@hevery.com>
2021-05-03 21:54:51 +0000

    a75a3175ee Revert "fix(language-service): only provide template results on reference requests (#41041)" (#41930)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.12+12.sha-7e9ef4d/, version);
+      banner = banner.replace(/11.2.12+18.sha-a75a317/, version);
     }
   }
 }
