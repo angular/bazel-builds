3ba06da779261bcf95c9d820b3841214491c22c0
Misko Hevery <misko@hevery.com>
2021-05-03 17:30:15 +0000

    6967f3ca28 build(docs-infra): revert `watchr` to v3.0.1 to restore `docs-watch` performance (#41903)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+310.sha-38f47be/, version);
+      banner = banner.replace(/12.0.0-next.8+311.sha-6967f3c/, version);
     }
   }
 }
