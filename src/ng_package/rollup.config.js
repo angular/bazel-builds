70ce14b81446cd0491558142949cbbd868ad6c97
Misko Hevery <misko@hevery.com>
2021-05-04 15:37:42 +0000

    6d7d659919 docs: add new migration guide to v12 documentation that describes how to migrate older localization ids to new ones (#41828)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+318.sha-b7db98f/, version);
+      banner = banner.replace(/12.0.0-next.8+319.sha-6d7d659/, version);
     }
   }
 }
