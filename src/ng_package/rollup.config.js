9baf00a4a8823cbbdd740358be3d566840a37e7b
Misko Hevery <misko@hevery.com>
2021-05-04 15:39:20 +0000

    4dae97de43 docs: update Node.js supported version statement (#41820)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+319.sha-6d7d659/, version);
+      banner = banner.replace(/12.0.0-next.8+320.sha-4dae97d/, version);
     }
   }
 }
