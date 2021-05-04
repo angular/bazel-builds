eb66591686c0e9b0669d56923fdca86b41a6b7dd
Misko Hevery <misko@hevery.com>
2021-05-04 15:54:41 +0000

    dac78d1ba1 build: update dependency yargs to v17 (#41920)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+320.sha-4dae97d/, version);
+      banner = banner.replace(/12.0.0-next.8+321.sha-dac78d1/, version);
     }
   }
 }
