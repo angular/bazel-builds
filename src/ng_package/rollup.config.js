3f66d1fb4a54f30ffdf6e21bd32fe93cd381dc92
Misko Hevery <misko@hevery.com>
2021-05-05 04:24:32 +0000

    fdf2e02bff build: fix crash when validating commit messages (#41949)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+323.sha-e387d22/, version);
+      banner = banner.replace(/12.0.0-next.8+325.sha-fdf2e02/, version);
     }
   }
 }
