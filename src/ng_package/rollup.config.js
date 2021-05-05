17730bb9fa2cb8d6122f09522bea9b9651a0d735
Misko Hevery <misko@hevery.com>
2021-05-05 04:27:43 +0000

    43cc5a126b docs: add dylhunn to contributors (#41936)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+325.sha-fdf2e02/, version);
+      banner = banner.replace(/12.0.0-next.8+327.sha-43cc5a1/, version);
     }
   }
 }
