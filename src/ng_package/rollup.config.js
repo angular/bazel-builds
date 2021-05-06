fd0bebeeb2f2cb2ccb17e197af5b913959addfc6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 21:59:31 +0000

    83bb0effc2 docs: use relative path for the feature request process diagram (#41970)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.13+6.sha-e090a6d/, version);
+      banner = banner.replace(/11.2.13+8.sha-83bb0ef/, version);
     }
   }
 }
