f31ace0cf77718539843a7695a502a0ba3380a63
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 19:22:35 +0000

    a4e3ea0311 feat(docs-infra): add support for unformatted code-snippets (#41335)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+348.sha-90d4b22/, version);
+      banner = banner.replace(/12.0.0-next.8+351.sha-a4e3ea0/, version);
     }
   }
 }
