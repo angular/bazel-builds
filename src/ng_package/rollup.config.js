0e621d838098d93c4623070b0004998db7c46f9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:01:24 +0000

    5c9bd237e3 build: update release configuration to use new release notes system (#41905)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+345.sha-35fbd4f/, version);
+      banner = banner.replace(/12.0.0-next.8+350.sha-5c9bd23/, version);
     }
   }
 }
