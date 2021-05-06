0a8b0266bcd171294c9ce472e523ee980795d8bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:23:03 +0000

    e090a6dbc1 feat(dev-infra): include release notes in Github release entries when publishing (#41964)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.13+4.sha-c7bef55/, version);
+      banner = banner.replace(/11.2.13+6.sha-e090a6d/, version);
     }
   }
 }
