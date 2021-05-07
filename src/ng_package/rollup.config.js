159ae465da667369f40bf9083942f54dcba22568
Andrew Scott <atscott@google.com>
2021-05-07 01:30:13 +0000

    e29eb8abfb docs: reword a few phrases in the GITHUB_PROCESS.md doc (#41977)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+355.sha-4d7294d/, version);
+      banner = banner.replace(/12.0.0-next.8+364.sha-e29eb8a/, version);
     }
   }
 }
