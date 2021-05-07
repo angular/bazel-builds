e5ae7cd9a95bf4b9fd33ff82c020d51d0f91dbb3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 14:33:42 +0000

    cb418bd696 ci: introduce action implementing feature request prioritization process (#41975)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+365.sha-3927e25/, version);
+      banner = banner.replace(/12.0.0-next.8+367.sha-cb418bd/, version);
     }
   }
 }
