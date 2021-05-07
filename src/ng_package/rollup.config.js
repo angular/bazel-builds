90871c8edf795720fb61b487f022b0fe60fbb8b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 17:31:16 +0000

    89c02980ae build(docs-infra): ensure that terminal code snippets render correctly (#41986)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.2+21.sha-d6c217b/, version);
+      banner = banner.replace(/12.0.0-rc.2+22.sha-89c0298/, version);
     }
   }
 }
