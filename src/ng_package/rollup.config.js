94d9f045fabb9c4816e0145c9de619dde5f37969
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 15:05:30 +0000

    4bc3cf216d feat(common): add URLSearchParams to request body (#37852)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+367.sha-cb418bd/, version);
+      banner = banner.replace(/12.0.0-next.8+370.sha-4bc3cf2/, version);
     }
   }
 }
