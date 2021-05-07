428b64a971049025cc2b3ba454d3b3000953c9cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 20:08:20 +0000

    7a4d9805ea fix(compiler-cli): use '' for the source map URL of indirect templates (#41973)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.2+23.sha-c580fbb/, version);
+      banner = banner.replace(/12.0.0-rc.2+24.sha-7a4d980/, version);
     }
   }
 }
