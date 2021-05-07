07d377afc3f53825483b29434137fdecf952324b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 14:37:41 +0000

    ed20342404 docs: change the documentation of creating-libraries by providing material links (#41982)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.13+9.sha-9883dab/, version);
+      banner = banner.replace(/11.2.13+12.sha-ed20342/, version);
     }
   }
 }
