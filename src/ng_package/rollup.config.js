bdccd9869a509607ab37f32fe6805a190868ab03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 16:30:47 +0000

    388f4056e6 docs: fix a grammatical error in lazy-loading-ngmodules guide (#42008)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.2+38.sha-cffba6f/, version);
+      banner = banner.replace(/12.0.0-rc.2+39.sha-388f405/, version);
     }
   }
 }
