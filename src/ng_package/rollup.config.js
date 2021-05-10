444b9abfbf96febf5c403b0af94405ee6a9d2a83
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:58:19 +0000

    089daea98b refactor(compiler-cli): remove redundant `potentialSourceMapUrl` property (#42000)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+387.sha-ddf2a6c/, version);
+      banner = banner.replace(/12.0.0-next.8+385.sha-089daea/, version);
     }
   }
 }
