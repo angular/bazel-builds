580f3a5c4c2a474244ad8d00dd1627147bcb5661
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 17:03:46 +0000

    b075481059 build(docs-infra): revert optional chaining usage (#42021)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.13+12.sha-ed20342/, version);
+      banner = banner.replace(/11.2.13+24.sha-b075481/, version);
     }
   }
 }
