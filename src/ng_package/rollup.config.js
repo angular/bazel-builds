0fa32a908af81cd6847c8e5bb528c8531ceaf934
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:47:36 +0000

    23f6b76d1a fix(docs-infra): update glossary to include the term template-input-variable (#41632)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+380.sha-d59f2b0/, version);
+      banner = banner.replace(/12.0.0-next.8+376.sha-23f6b76/, version);
     }
   }
 }
