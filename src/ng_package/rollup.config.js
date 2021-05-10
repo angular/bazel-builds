080bf57310216a1d78d3dd28873e43363efeb66a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 21:06:39 +0000

    d5555558d0 fix(platform-browser): prevent memory leak of style nodes if shadow DOM encapsulation is used (#42005)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-rc.2+42.sha-d43e351/, version);
+      banner = banner.replace(/12.0.0-rc.2+45.sha-d555555/, version);
     }
   }
 }
