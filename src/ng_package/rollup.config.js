9de3a6bdc9c745f36487511ea087f06ee30bb513
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 17:05:53 +0000

    6ebb86f9a2 docs: clarify the IE11 deprecation timeline, and link to the RFC (#42023)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+389.sha-d2296bc/, version);
+      banner = banner.replace(/12.0.0-next.8+391.sha-6ebb86f/, version);
     }
   }
 }
