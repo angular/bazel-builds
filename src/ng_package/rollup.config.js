47dd11fadd017153956d2049768c098dc9578739
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:29:13 +0000

    dad205e502 docs: add release notes for v12.0.0 to CHANGELOG

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+396.sha-a74eb52/, version);
+      banner = banner.replace(/12.0.0-next.8+397.sha-dad205e/, version);
     }
   }
 }
