837920fee648705eed00d92108dca6af84d4ad50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 17:50:21 +0000

    45083bc302 build: lock file maintenance (#42015)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+401.sha-53cd7e5/, version);
+      banner = banner.replace(/12.0.0-next.8+403.sha-45083bc/, version);
     }
   }
 }
