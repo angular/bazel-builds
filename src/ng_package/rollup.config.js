fa61626587e1ac153a1c9fadb90c58e8eab87344
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 18:25:35 +0000

    04f41554dc build(docs-infra): show a dist-tag when installing the CLI on non-stable versions (#41991) (#42041)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/11.2.14/, version);
+      banner = banner.replace(/11.2.14+1.sha-04f4155/, version);
     }
   }
 }
