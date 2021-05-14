61a87d735f5b529740429026fddcc31c7e73760f
Zach Arend <zachzach@google.com>
2021-05-14 19:30:52 +0000

    678bce2419 ci: update to latest feature request triage bot commit (#42084)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0+12.sha-3a46ad9/, version);
+      banner = banner.replace(/12.0.0+13.sha-678bce2/, version);
     }
   }
 }
