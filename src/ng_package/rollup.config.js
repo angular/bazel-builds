39df9091d53f242a59efdb24f16f75aefd178209
Zach Arend <zachzach@google.com>
2021-05-14 21:30:21 +0000

    5960d4c513 docs(benchpress): reword a comment (#42096)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+415.sha-6d87895/, version);
+      banner = banner.replace(/12.0.0-next.8+416.sha-5960d4c/, version);
     }
   }
 }
