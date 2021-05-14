da651aab260dd99b0c3587b053273d3e1703b79d
Zach Arend <zachzach@google.com>
2021-05-14 21:25:22 +0000

    7fb8527e1e refactor(dev-infra): remove invoke bazel clean command from release tool (#42101)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+412.sha-3aec55b/, version);
+      banner = banner.replace(/12.0.0-next.8+413.sha-7fb8527/, version);
     }
   }
 }
