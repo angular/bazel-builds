fc6a371e8362eafb22bbd2f40288638e6aa71902
atscott <atscott01@gmail.com>
2021-05-17 18:18:03 +0000

    edfcbb43c5 docs: change `project` to `projected` for past tense (#42118)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0+13.sha-678bce2/, version);
+      banner = banner.replace(/12.0.0+22.sha-edfcbb4/, version);
     }
   }
 }
