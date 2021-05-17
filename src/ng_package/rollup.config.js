fd1ea2d20d6f2a8ab32d06798470716ca82e223a
atscott <atscott01@gmail.com>
2021-05-17 18:16:33 +0000

    fc1bc0e0e8 docs: update examples to use correct `Injector.create()` overload (#42006)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+417.sha-e86a1d3/, version);
+      banner = banner.replace(/12.0.0-next.8+418.sha-fc1bc0e/, version);
     }
   }
 }
