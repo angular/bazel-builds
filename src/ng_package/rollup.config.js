d04beefaf7824af77e7bf7f782a8b944b2baa1fb
atscott <atscott01@gmail.com>
2021-05-17 18:10:34 +0000

    e86a1d3441 docs: make all examples compatible with strict mode (#41999)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+416.sha-5960d4c/, version);
+      banner = banner.replace(/12.0.0-next.8+417.sha-e86a1d3/, version);
     }
   }
 }
