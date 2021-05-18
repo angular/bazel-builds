c8fe6f9806c1ed6426bccd347c80d47eae11ed6b
Andrew Scott <atscott@google.com>
2021-05-18 22:59:28 +0000

    ec78e68a36 fix(dev-infra): correct FormatConfig interface (#42154)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+435.sha-b7d1614/, version);
+      banner = banner.replace(/12.0.0-next.8+437.sha-ec78e68/, version);
     }
   }
 }
