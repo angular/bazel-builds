80e5d12dd06ee767e30148f23da9717980a11105
Andrew Scott <atscott@google.com>
2021-05-18 23:00:14 +0000

    2dd956421c build: remove publishConfig entry from package.json entries (#42104)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+437.sha-ec78e68/, version);
+      banner = banner.replace(/12.0.0-next.8+438.sha-2dd9564/, version);
     }
   }
 }
