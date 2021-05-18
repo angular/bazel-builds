3911602050d8edef2fe5e06de74f7c08178f6053
Andrew Scott <atscott@google.com>
2021-05-18 18:38:22 +0000

    c58e15a4b0 ci: add pullapprove entries for devtools (#42152)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0+26.sha-6861911/, version);
+      banner = banner.replace(/12.0.0+32.sha-c58e15a/, version);
     }
   }
 }
