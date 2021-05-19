fba34dcfb81e7769da43272f12cf890ce32687ab
atscott <atscott01@gmail.com>
2021-05-19 16:46:21 +0000

    8d20bb9855 ci: update to latest feature request triage bot commit (#42157)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.0-next.8+439.sha-d0958e8/, version);
+      banner = banner.replace(/12.0.0-next.8+442.sha-8d20bb9/, version);
     }
   }
 }
