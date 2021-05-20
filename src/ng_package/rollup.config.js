0f6227efbdb863c4b40f6f970551f4359528c795
Zach Arend <zachzach@google.com>
2021-05-20 23:23:05 +0000

    ce6d49fc97 docs: edits to remove jargon (#41282)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.1+4.sha-640578f/, version);
+      banner = banner.replace(/12.0.1+10.sha-ce6d49f/, version);
     }
   }
 }
