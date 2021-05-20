b7ae9c608ea4e3a854bc6af8b234aa384eb9c43e
Zach Arend <zachzach@google.com>
2021-05-20 17:45:24 +0000

    d6bc61ab5f build: update angular (#42090)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+6.sha-a01fe8d/, version);
+      banner = banner.replace(/12.1.0-next.2+8.sha-d6bc61a/, version);
     }
   }
 }
