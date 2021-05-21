8dbc8f7c0490125bf06ed8e3c15bb543777e813f
Zach Arend <zachzach@google.com>
2021-05-21 18:24:26 +0000

    62becc88f9 fix(docs-infra): fix example compilation issues (#42199)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+14.sha-8c158ba/, version);
+      banner = banner.replace(/12.1.0-next.2+15.sha-62becc8/, version);
     }
   }
 }
