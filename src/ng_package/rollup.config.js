a1ee241927c42592d085e98f4334db3317ba88b2
Joey Perrott <josephperrott@gmail.com>
2021-05-21 21:00:04 +0000

    30c461d862 ci: update to latest feature request triage bot commit (#42229)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+17.sha-03825cf/, version);
+      banner = banner.replace(/12.1.0-next.2+18.sha-30c461d/, version);
     }
   }
 }
