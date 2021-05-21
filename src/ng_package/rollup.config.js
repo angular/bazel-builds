8148ec5b517d03725f19ac9c0eaf7457e54899a0
Zach Arend <zachzach@google.com>
2021-05-21 18:23:43 +0000

    8c158babdc fix(dev-infra): formatting errors not reported with failure message (#42202)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+11.sha-d2a89db/, version);
+      banner = banner.replace(/12.1.0-next.2+14.sha-8c158ba/, version);
     }
   }
 }
