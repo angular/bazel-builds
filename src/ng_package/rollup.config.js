0f06acfdc49451ac114b32edd85cd0f47c5160c8
Zach Arend <zachzach@google.com>
2021-05-24 19:44:41 +0000

    c820066d98 docs: align code in `product-example.component.*.ts` files (#42155)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+25.sha-bf505ad/, version);
+      banner = banner.replace(/12.1.0-next.2+31.sha-c820066/, version);
     }
   }
 }
