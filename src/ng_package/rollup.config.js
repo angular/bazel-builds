ba4b47ff71cf62c6038e1fdae83d31244e7d6d2c
Zach Arend <zachzach@google.com>
2021-05-24 22:59:39 +0000

    1040697787 docs: remove sentence about how the not reloading the page. (#42290)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+33.sha-ad12b31/, version);
+      banner = banner.replace(/12.1.0-next.2+37.sha-1040697/, version);
     }
   }
 }
