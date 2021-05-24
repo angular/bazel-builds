720e125eb7b1bf07c624a48520eada26f7447ef6
Zach Arend <zachzach@google.com>
2021-05-24 22:43:18 +0000

    ad12b31c27 docs: unsubscribe from observables (#42284)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+36.sha-29ece98/, version);
+      banner = banner.replace(/12.1.0-next.2+33.sha-ad12b31/, version);
     }
   }
 }
