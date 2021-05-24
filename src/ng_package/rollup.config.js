89606e006766677608c992c10adc99ca4cba5cd0
Zach Arend <zachzach@google.com>
2021-05-24 17:26:57 +0000

    bf505ad707 docs(core): tighten up PipeTransform example types (#42240)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+24.sha-4ac55ca/, version);
+      banner = banner.replace(/12.1.0-next.2+25.sha-bf505ad/, version);
     }
   }
 }
