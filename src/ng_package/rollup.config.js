e0a0be086c4b7bbf7663de02d6d332b830def650
Zach Arend <zachzach@google.com>
2021-05-25 22:34:00 +0000

    b0581f3117 docs: add link to the style precedence guide in ivy compatibility (#42317)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+73.sha-9f50495/, version);
+      banner = banner.replace(/12.1.0-next.2+78.sha-b0581f3/, version);
     }
   }
 }
