6ecdfd99c509eeda2cda97b2411363ed1f3fc804
Zach Arend <zachzach@google.com>
2021-05-25 17:43:27 +0000

    12b1e87e6c build: update CLI packages to 12.0.1 (#42307)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.1+15.sha-99802dc/, version);
+      banner = banner.replace(/12.0.1+38.sha-12b1e87/, version);
     }
   }
 }
