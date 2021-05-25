f7a23e45f2f3c0fe88e5a222c7f7baa5e8a4c9a0
Zach Arend <zachzach@google.com>
2021-05-25 20:48:57 +0000

    3a6af8e629 fix(platform-browser): update started state on reset (#41608)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+68.sha-d7768c6/, version);
+      banner = banner.replace(/12.1.0-next.2+69.sha-3a6af8e/, version);
     }
   }
 }
