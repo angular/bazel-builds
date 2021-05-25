5fd3a964091eb553fa33cbdfd5651e97d53c90f0
Zach Arend <zachzach@google.com>
2021-05-25 03:02:14 +0000

    1c3fe69f92 docs: change Zeit -> Vercel (#42285)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+38.sha-a391698/, version);
+      banner = banner.replace(/12.1.0-next.2+40.sha-1c3fe69/, version);
     }
   }
 }
