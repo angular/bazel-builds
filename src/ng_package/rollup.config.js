9490c1844648062533617e22f704bc0e064ac17c
Zach Arend <zachzach@google.com>
2021-05-25 18:09:42 +0000

    9f09c3b9b1 build(core): update supported range of node versions to be less restrictive (#42205)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+40.sha-1c3fe69/, version);
+      banner = banner.replace(/12.1.0-next.2+56.sha-9f09c3b/, version);
     }
   }
 }
