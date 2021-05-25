d50b04a64fa57888fa253452754f4e0ac2c4bb34
Zach Arend <zachzach@google.com>
2021-05-25 18:14:27 +0000

    ef33806480 docs: add small clarity around navigationCancel (#42282)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+61.sha-2ff5bfe/, version);
+      banner = banner.replace(/12.1.0-next.2+62.sha-ef33806/, version);
     }
   }
 }
