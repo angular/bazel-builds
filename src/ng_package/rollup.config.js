b88dc1ea27e44cc5ffd9931173d8fb8b0dce2a46
Zach Arend <zachzach@google.com>
2021-05-25 22:33:17 +0000

    4a2cc3e451 docs: add explanation of how to use locally created build artifacts in a local project (#42319)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+71.sha-063d178/, version);
+      banner = banner.replace(/12.1.0-next.2+77.sha-4a2cc3e/, version);
     }
   }
 }
