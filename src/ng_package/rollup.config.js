edf66301eeb637a1f1714d0f22c8452fa469fbdc
Zach Arend <zachzach@google.com>
2021-05-25 22:30:26 +0000

    063d1789c2 docs: move "Browser Support" page in navigation tree (#42326)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.2+72.sha-bdd3cca/, version);
+      banner = banner.replace(/12.1.0-next.2+71.sha-063d178/, version);
     }
   }
 }
