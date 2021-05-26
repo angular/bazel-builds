348710c27b27cb8b82863968dfd4e1ca9bf91e9c
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:41:27 +0000

    15726594a9 ci: update the github feature request process action and run on cron (#42337)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+17.sha-bcfe4af/, version);
+      banner = banner.replace(/12.1.0-next.3+16.sha-1572659/, version);
     }
   }
 }
