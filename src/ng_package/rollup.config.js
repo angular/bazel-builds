b40c69b8ecf63c0449e206e2d27c89452cf84f3f
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:39:51 +0000

    4cb11cc4e7 docs: add note about why module import order is important (#42323)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+14.sha-4e9b61a/, version);
+      banner = banner.replace(/12.1.0-next.3+15.sha-4cb11cc/, version);
     }
   }
 }
