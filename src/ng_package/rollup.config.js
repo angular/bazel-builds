0c6a18727d6371cd98c033452e0badf228fe2b08
Zach Arend <zachzach@google.com>
2021-05-26 20:25:57 +0000

    40bbcaa12d docs: move and update browser compatibility docs to browser-support guide (#42334)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+2.sha-0166391/, version);
+      banner = banner.replace(/12.1.0-next.3+5.sha-40bbcaa/, version);
     }
   }
 }
