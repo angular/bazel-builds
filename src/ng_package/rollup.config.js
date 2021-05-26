2a3344fbe4cb8ac47535ffad7092d2588d7c9af0
Zach Arend <zachzach@google.com>
2021-05-26 15:23:31 +0000

    54b23aef42 docs: document shallow equality emit for ActivatedRoute (#42280)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.1+66.sha-c01da21/, version);
+      banner = banner.replace(/12.0.1+83.sha-54b23ae/, version);
     }
   }
 }
