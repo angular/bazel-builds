795c29bffca8255429991b1f73ca5088b3d96f7b
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:40:54 +0000

    bcfe4afda9 docs: clarify that `forRoot` can be called in other modules (#42361)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+15.sha-4cb11cc/, version);
+      banner = banner.replace(/12.1.0-next.3+17.sha-bcfe4af/, version);
     }
   }
 }
