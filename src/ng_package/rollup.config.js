414f82ff55ea4f4f02ee1a4c18bea644e1da7f5a
Andrew Kushnir <akushnir@google.com>
2021-05-27 00:34:00 +0000

    2513a533ed docs: add note to clarify that ngOnChanges is only called if inputs are bound (#42374)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.2+23.sha-75a33e5/, version);
+      banner = banner.replace(/12.0.2+25.sha-2513a53/, version);
     }
   }
 }
