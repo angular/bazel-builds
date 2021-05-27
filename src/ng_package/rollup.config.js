bf74dca6642b444c315fa1e458068f77abc525ad
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:54:10 +0000

    306f7ac543 docs: cherry pick release notes for 12.0.2 to main (#42390)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+29.sha-deb8930/, version);
+      banner = banner.replace(/12.1.0-next.3+30.sha-306f7ac/, version);
     }
   }
 }
