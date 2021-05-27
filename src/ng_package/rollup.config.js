200229742eba88a38f7f15f4b402de54fd267cc4
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:27:42 +0000

    b60c9c18f1 docs: clarify behavior of strict templates flags (#42392)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.2+39.sha-a86a6b9/, version);
+      banner = banner.replace(/12.0.2+42.sha-b60c9c1/, version);
     }
   }
 }
