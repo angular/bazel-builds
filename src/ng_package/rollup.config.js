d578cb5c7b794194c942a158a885b54f127fcd51
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:06:32 +0000

    4cd2cc439d docs: Edits to remove jargon in Reference section (#42033)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+45.sha-ee0e3fb/, version);
+      banner = banner.replace(/12.1.0-next.3+46.sha-4cd2cc4/, version);
     }
   }
 }
