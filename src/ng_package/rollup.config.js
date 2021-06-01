4774c0b456f301a091569f0a2580fb61cbb70bfa
Andrew Kushnir <akushnir@google.com>
2021-06-01 19:35:09 +0000

    e039075a28 fix(compiler-cli): better detect classes that are indirectly exported (#42207)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+60.sha-3e192bf/, version);
+      banner = banner.replace(/12.1.0-next.3+62.sha-e039075/, version);
     }
   }
 }
