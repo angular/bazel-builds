d78a98086b64a666c78f4f110372769a2801d602
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:36:54 +0000

    44b737ecb4 docs: clarify how declaration files can be installed (#42417)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+50.sha-1b8bbdd/, version);
+      banner = banner.replace(/12.1.0-next.3+61.sha-44b737e/, version);
     }
   }
 }
