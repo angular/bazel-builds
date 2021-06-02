f4bba7730b29579a62a7dfb54880c9cb0c7a3aa9
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:36:27 +0000

    1fe1dd1f19 docs: clarify the wording around live examples for all testing guides (#42406)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+5.sha-4c39869/, version);
+      banner = banner.replace(/12.1.0-next.4+6.sha-1fe1dd1/, version);
     }
   }
 }
