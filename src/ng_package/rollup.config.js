0cd5e4cc49a185dea1786fcd4f17cf7c9adb31c5
Andrew Kushnir <akushnir@google.com>
2021-06-02 21:07:34 +0000

    0a1d8e5102 refactor(docs-infra): clean up AIO Sass files (#42442)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.3/, version);
+      banner = banner.replace(/12.0.3+11.sha-0a1d8e5/, version);
     }
   }
 }
