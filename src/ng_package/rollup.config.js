8b0df27f30bb186f8073b71a6a943cef9eb6487b
Andrew Kushnir <akushnir@google.com>
2021-06-02 18:28:37 +0000

    7bd5ea4a7e release: cut the v12.1.0-next.4 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.3+66.sha-574f640/, version);
+      banner = banner.replace(/12.1.0-next.4/, version);
     }
   }
 }
