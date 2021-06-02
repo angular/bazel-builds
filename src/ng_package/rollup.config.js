3e4bb688e467ed90a751f983d4e0e621d8a29407
GitHub <noreply@github.com>
2021-06-02 20:32:36 +0000

    f8814d3558 fix(dev-infra): update outdated build-worker.js file (#42458)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4/, version);
+      banner = banner.replace(/12.1.0-next.4+2.sha-f8814d3/, version);
     }
   }
 }
