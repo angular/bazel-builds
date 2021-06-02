b14199bd5c3132299bd918ac7f4ab7039d4297bc
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:41:00 +0000

    c0b2eeb54c docs: remove unused imports from unit tests (#42441)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+6.sha-1fe1dd1/, version);
+      banner = banner.replace(/12.1.0-next.4+8.sha-c0b2eeb/, version);
     }
   }
 }
