b7b9fa54793001a9c3e3431e061f90b9163decd6
Andrew Kushnir <akushnir@google.com>
2021-06-02 17:08:02 +0000

    e937f77cd4 docs: specify correct interceptor class type (#42437)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.2+63.sha-8bdcca1/, version);
+      banner = banner.replace(/12.0.2+65.sha-e937f77/, version);
     }
   }
 }
