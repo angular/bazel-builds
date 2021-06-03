546f1b643f41f2410456845ccc567a7262b4f908
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 20:52:55 +0000

    ba084857ea feat(compiler): support safe keyed read expressions (#41911)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+33.sha-47270d9/, version);
+      banner = banner.replace(/12.1.0-next.4+34.sha-ba08485/, version);
     }
   }
 }
