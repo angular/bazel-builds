15a0dd5eab7eaf425e02333bb28e3f5a529de029
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 18:36:58 +0000

    71e14a71f5 test: update compiler-cli compliance goldens due to TS 4.3 emit format regression (#42022)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+41.sha-6929625/, version);
+      banner = banner.replace(/12.1.0-next.4+43.sha-71e14a7/, version);
     }
   }
 }
