9f7bd9e0479518f345cc7d3f61746e78a5b3a36f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:51:43 +0000

    6080db1839 build: suppress errors in git hooks (#42484)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.3+31.sha-beae425/, version);
+      banner = banner.replace(/12.0.3+33.sha-6080db1/, version);
     }
   }
 }
