e68a153c6e9f262d2509876e3d075d6bd302b0d1
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 17:21:43 +0000

    cc904b5226 docs(core): clarify deprecation of `entryComponents` (#42248)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+50.sha-b061e5b/, version);
+      banner = banner.replace(/12.1.0-next.4+51.sha-cc904b5/, version);
     }
   }
 }
