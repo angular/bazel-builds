552e77c7e2b2aaee39cca8c94230d2158a3b89ef
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:12:09 +0000

    65d59305d5 build: remove unused no-strict tsconfig file (#42506)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.3+38.sha-f5d81d3/, version);
+      banner = banner.replace(/12.0.3+41.sha-65d5930/, version);
     }
   }
 }
