4b76db7e46107c3acd55efd48a24830cd4306901
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:50:37 +0000

    cd22c7a1f5 docs: Updated the code snippet path (#42494) PR Close #42494

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+47.sha-3005917/, version);
+      banner = banner.replace(/12.1.0-next.4+48.sha-cd22c7a/, version);
     }
   }
 }
