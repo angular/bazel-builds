6f2215eda5e6b96bcc147799d08be94f781403ae
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:54:06 +0000

    5d70f09032 docs: Corrected spelling (#42493)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+48.sha-cd22c7a/, version);
+      banner = banner.replace(/12.1.0-next.4+49.sha-5d70f09/, version);
     }
   }
 }
