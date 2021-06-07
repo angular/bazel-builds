ce312cb2b6f5c27e89e14b6def4f5ddbc8a7ca67
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:43:47 +0000

    34dd3c360b fix(language-service): fix autocomplete info display for some cases (#42472)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.3+42.sha-8bb6fb7/, version);
+      banner = banner.replace(/12.0.3+43.sha-34dd3c3/, version);
     }
   }
 }
