e7131c846b060b60af0a0a6cf7cd97ac0dcf5d81
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:55:38 +0000

    b061e5b2d7 fix(docs-infra): fix search results font color (#42488)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+49.sha-5d70f09/, version);
+      banner = banner.replace(/12.1.0-next.4+50.sha-b061e5b/, version);
     }
   }
 }
