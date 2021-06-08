5cef092a4cc44b29d02d507b8b3f43a54d4f2710
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 19:34:01 +0000

    5746afe93d docs: improve accessibility of attribute-binding example (#41432)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.3+53.sha-93de5a0/, version);
+      banner = banner.replace(/12.0.3+55.sha-5746afe/, version);
     }
   }
 }
