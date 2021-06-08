7817eede2269b80bc5941846df4da199651c52bb
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:06:44 +0000

    43b4a8c07c docs: Edited to remove jargon in Reference Errors (#42186)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+86.sha-f3fde4b/, version);
+      banner = banner.replace(/12.1.0-next.4+85.sha-43b4a8c/, version);
     }
   }
 }
