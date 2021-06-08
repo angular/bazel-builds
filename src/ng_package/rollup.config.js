96832615d967cfc8571b45e4d30377c516345f7a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:13:20 +0000

    2a28a1ddb2 docs: add Angular Rocks podcast (#42078)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+89.sha-0777fac/, version);
+      banner = banner.replace(/12.1.0-next.4+88.sha-2a28a1d/, version);
     }
   }
 }
