23dd29ecd71f719ebcbbe7b27cb3cdff39cf4e05
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:01:54 +0000

    d10c38a8f8 docs: add Roman Sedov to contributors page (#42515)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+73.sha-645cad5/, version);
+      banner = banner.replace(/12.1.0-next.4+72.sha-d10c38a/, version);
     }
   }
 }
