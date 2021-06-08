808e77326fa0fd6bc47d7f69434d2632b81a0b9a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:09:08 +0000

    0777faccfb docs: Edited to remove jargon in Reference, Glossary, and Style sections (#42187)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+87.sha-4975a10/, version);
+      banner = banner.replace(/12.1.0-next.4+89.sha-0777fac/, version);
     }
   }
 }
