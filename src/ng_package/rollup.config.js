53776481b1613a3e44390fd3fab2c22fe813a16e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 19:27:43 +0000

    e36c5b4c86 docs: fix `ngProjectAs` selector in example (#42523)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+97.sha-28a4631/, version);
+      banner = banner.replace(/12.1.0-next.5+1.sha-e36c5b4/, version);
     }
   }
 }
