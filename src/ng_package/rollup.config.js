a38ae6be08b1295aa739f2a2b69025809d2145d9
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 01:14:12 +0000

    3de774e778 docs: add guidance on adding filtering and sorting logic to componetns instead of pipes (#42368)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.4+92.sha-e1a80d4/, version);
+      banner = banner.replace(/12.1.0-next.4+93.sha-3de774e/, version);
     }
   }
 }
