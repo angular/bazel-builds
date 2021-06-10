f8f80ba5e3efa4812bfdeaeb526ed0042ed77fa3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:23:36 +0000

    5e4a886259 docs: fix i18n guide typo (#42545)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+8.sha-ec90d8d/, version);
+      banner = banner.replace(/12.1.0-next.5+10.sha-5e4a886/, version);
     }
   }
 }
