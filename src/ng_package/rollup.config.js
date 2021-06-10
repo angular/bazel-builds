fb56f0d2c719a2a2c0dbae057199925d3c5be859
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:42:07 +0000

    e0158a1515 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.4+4.sha-a75874c/, version);
+      banner = banner.replace(/12.0.4+5.sha-e0158a1/, version);
     }
   }
 }
