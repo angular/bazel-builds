7ce7a81d7da0774a36167208b2503f0a19bd1cc5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:26:51 +0000

    efb440eb2f refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+11.sha-52e0987/, version);
+      banner = banner.replace(/12.1.0-next.5+12.sha-efb440e/, version);
     }
   }
 }
