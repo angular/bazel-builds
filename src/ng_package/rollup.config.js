665c427d987c8cd12b3f92897736c92c3d9d44c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:32:39 +0000

    4001e9d808 fix(language-service): 'go to defininition' for objects defined in template (#42559)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+30.sha-228beea/, version);
+      banner = banner.replace(/12.1.0-next.5+31.sha-4001e9d/, version);
     }
   }
 }
