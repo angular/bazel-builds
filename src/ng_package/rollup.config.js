127b432dee67174d5c884f64d928cd16a700a953
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:18:29 +0000

    e7c69b8551 docs: Grammatical error (#42571)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+20.sha-7180ec9/, version);
+      banner = banner.replace(/12.1.0-next.5+21.sha-e7c69b8/, version);
     }
   }
 }
