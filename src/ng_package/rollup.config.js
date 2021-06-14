cee4f49b1698e66d7564b26e9f3decf7b9e0fc6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:32:40 +0000

    a66dd8834c build: remove unnecessary polyfills from tests (#42567)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+31.sha-4001e9d/, version);
+      banner = banner.replace(/12.1.0-next.5+32.sha-a66dd88/, version);
     }
   }
 }
