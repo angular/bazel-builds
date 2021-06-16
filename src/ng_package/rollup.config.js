7fc1d1e31941273cb50347507a62abda3fd6a34c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:19:16 +0000

    5c25595647 docs: release notes for the v12.0.5 release (#42586)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+47.sha-eb04684/, version);
+      banner = banner.replace(/12.1.0-next.5+48.sha-5c25595/, version);
     }
   }
 }
