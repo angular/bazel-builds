8f219d6ba7d1baa458a56784d1af9d29317a6efd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:52:12 +0000

    0e33521705 Revert "release: bump the next branch to v12.2.0-next.0"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+49.sha-7961e2f/, version);
+      banner = banner.replace(/12.1.0-next.5+50.sha-0e33521/, version);
     }
   }
 }
