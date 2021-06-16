4a68e25f11bd95dc76ef76037b251408b73fc96a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:23:36 +0000

    7b85a4d457 fix(docs-infra): add horizontal margins to nav menu (#42561)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+43.sha-3b81528/, version);
+      banner = banner.replace(/12.1.0-next.5+46.sha-7b85a4d/, version);
     }
   }
 }
