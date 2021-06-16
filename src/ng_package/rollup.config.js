a4d63216c307571747a5c707f3a4c0e77cfccceb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:35 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+40.sha-2d1347b/, version);
+      banner = banner.replace(/12.1.0-next.5+41.sha-62aca30/, version);
     }
   }
 }
