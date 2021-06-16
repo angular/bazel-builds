826b923316d0a53cfdab29f6ca86429b17dda25d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:19:27 +0000

    f3a79878af docs(forms): correct sample code for FormArray.reset (#42477)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.5+41.sha-62aca30/, version);
+      banner = banner.replace(/12.1.0-next.5+42.sha-f3a7987/, version);
     }
   }
 }
