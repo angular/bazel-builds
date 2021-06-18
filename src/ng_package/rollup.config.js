7c804575a85a69d58b8b177e682729216def6582
Dylan Hunn <dylhunn@gmail.com>
2021-06-18 17:54:57 +0000

    41123231c5 docs(docs-infra): document how to configure redirects (#42452)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.4+33.sha-7db8e1f/, version);
+      banner = banner.replace(/12.0.5+20.sha-4112323/, version);
     }
   }
 }
