dc77ec2c4bb2005ad2ff0929f3ee049e349b180d
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 17:03:30 +0000

    d77f560403 build: update to typescript 4.3.4 (#42600)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.6+23.sha-4429188/, version);
+      banner = banner.replace(/12.1.0-next.6+25.sha-d77f560/, version);
     }
   }
 }
