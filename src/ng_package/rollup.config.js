2abd75e02e1f979dcb5f83d1138a7cfb0ad4c94c
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 17:55:28 +0000

    1cb85ba5ad ci: update pullapprove config (#42613)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.5+22.sha-c5d2681/, version);
+      banner = banner.replace(/12.0.5+23.sha-1cb85ba/, version);
     }
   }
 }
