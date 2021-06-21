bc21ee48448c0aad179999ccfadebcff1875c746
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 17:12:45 +0000

    e83d7cb2d3 refactor(compiler-cli): support xi18n in ngtsc (#42485)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.6+25.sha-d77f560/, version);
+      banner = banner.replace(/12.1.0-next.6+27.sha-e83d7cb/, version);
     }
   }
 }
