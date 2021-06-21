594625ee910640f3ce4069abfa48ff0a58211288
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 19:06:04 +0000

    1ce5c20656 docs: add new i18n formats to i18n guide (#42597)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.0.5+23.sha-1cb85ba/, version);
+      banner = banner.replace(/12.0.5+28.sha-1ce5c20/, version);
     }
   }
 }
