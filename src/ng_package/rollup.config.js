92b76e6fb4d2789a25d8c539bc26d28054c396fe
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:46:56 +0000

    81a19e4e65 feat(dev-infra): update pullapprove verification to ensure all groups have reviewers (#42614)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.6+36.sha-75bbcf7/, version);
+      banner = banner.replace(/12.1.0-next.6+37.sha-81a19e4/, version);
     }
   }
 }
