31530408e63d42f2262f6fa6f5732e2d79697a5e
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:52:30 +0000

    8a67770687 docs(core): improve applicationref.bootstrap docs (#42407)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.6+38.sha-9498da1/, version);
+      banner = banner.replace(/12.1.0-next.6+39.sha-8a67770/, version);
     }
   }
 }
