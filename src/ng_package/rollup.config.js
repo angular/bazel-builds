f53ee292ad1e69b23f0e08ec1c8756d5bf08db0f
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:45:17 +0000

    75bbcf7c2f docs: improve styles of the roadmap done section (#42616)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.6+35.sha-cc672f0/, version);
+      banner = banner.replace(/12.1.0-next.6+36.sha-75bbcf7/, version);
     }
   }
 }
