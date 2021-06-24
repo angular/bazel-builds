a6ceb3a1bb44adffb40401ef43c1ec63d28edce2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 17:29:05 +0000

    4e46aef17b build: update dependency madge to v5 (#42629)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.6+55.sha-cc30dc0/, version);
+      banner = banner.replace(/12.1.0-next.6+56.sha-4e46aef/, version);
     }
   }
 }
