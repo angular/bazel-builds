aec0ae61ef000c01a35b7d5714d1beefc4374f2b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 20:16:47 +0000

    2203217c40 build(docs-infra): disambiguate doc paths for global APIs (#42648)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0-next.6+58.sha-637ac00/, version);
+      banner = banner.replace(/12.1.0-next.6+59.sha-2203217/, version);
     }
   }
 }
