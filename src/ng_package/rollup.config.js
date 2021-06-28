7842f1ed874eec8042117a49f241817069364c30
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 19:17:11 +0000

    c93df7dc6d docs: correct literal string example (#42623)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.0+13.sha-b54e8ae/, version);
+      banner = banner.replace(/12.2.0-next.0+12.sha-c93df7d/, version);
     }
   }
 }
