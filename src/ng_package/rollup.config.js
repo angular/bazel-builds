e4853441ddb84bbc1821fb6029f43ca8b1aacac9
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 16:47:16 +0000

    2c490d5c6c docs: remove duplicate link to `guide/inputs-outputs.md` (#42654)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.0+14.sha-41823ff/, version);
+      banner = banner.replace(/12.2.0-next.0+15.sha-2c490d5/, version);
     }
   }
 }
