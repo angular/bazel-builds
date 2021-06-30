6cc8b976d2244f8ac958a5c9f3a6556a164f18c3
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:03:04 +0000

    aab41ca42f build: update API goldens to reflect new tool (#42688)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.0+18.sha-d19ddd1/, version);
+      banner = banner.replace(/12.1.0+22.sha-aab41ca/, version);
     }
   }
 }
