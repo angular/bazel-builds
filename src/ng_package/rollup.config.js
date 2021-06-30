b71fe79344e495fbd92f0ef060cd2f873c2cbb6f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 16:35:05 +0000

    f3965ff834 docs: setting context value inside an interceptor (#42699)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.0+20.sha-8528073/, version);
+      banner = banner.replace(/12.2.0-next.0+21.sha-f3965ff/, version);
     }
   }
 }
