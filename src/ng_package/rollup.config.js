92577ac8680cd2c44077c148c1f87933ec097aa5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:01:58 +0000

    0e20b05f7d docs: add last updated date to roadmap (#42615)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.0+23.sha-9f5cc7c/, version);
+      banner = banner.replace(/12.2.0-next.0+24.sha-0e20b05/, version);
     }
   }
 }
