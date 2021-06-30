385f95bf7abf6f2f505302f72edf718e3ffa95bf
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 21:53:10 +0000

    0f23f7343e fix(core): error in TestBed if module is reset mid-compilation in ViewEngine (#42669)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.0+29.sha-1445dba/, version);
+      banner = banner.replace(/12.2.0-next.1+1.sha-0f23f73/, version);
     }
   }
 }
