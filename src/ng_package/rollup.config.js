07868cd273416226cace4ae19204e34c9ba3a7ed
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:55:01 +0000

    bfa1b5d9eb fix(docs-infra): move dots inside Usage Notes anchor tag (#42725)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+2.sha-d997303/, version);
+      banner = banner.replace(/12.2.0-next.1+3.sha-bfa1b5d/, version);
     }
   }
 }
