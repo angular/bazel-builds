266ec2b746acd3225383fcbd0553608257511059
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:51:21 +0000

    d997303cf4 build: update dependency husky to v7 (#42730)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+1.sha-0f23f73/, version);
+      banner = banner.replace(/12.2.0-next.1+2.sha-d997303/, version);
     }
   }
 }
