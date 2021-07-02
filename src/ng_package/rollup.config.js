c33cdd67aedb4afd1a1a82f8dedbb9624aaabfa6
atscott <atscott01@gmail.com>
2021-07-02 17:09:05 +0000

    e81efe6667 docs: convert commit SHAs and PR numbers in changelog to links (#42732)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+7.sha-19bcdfb/, version);
+      banner = banner.replace(/12.2.0-next.1+9.sha-e81efe6/, version);
     }
   }
 }
