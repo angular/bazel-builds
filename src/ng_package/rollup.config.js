630c0fc8fc5a223dd9eb653797cccf5c0ed565b8
atscott <atscott01@gmail.com>
2021-07-02 19:50:43 +0000

    6da24e6451 build: update API goldens after api-extractor update (#42749)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+7.sha-aa261e7/, version);
+      banner = banner.replace(/12.1.1+12.sha-6da24e6/, version);
     }
   }
 }
