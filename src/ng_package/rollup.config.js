904796ce813efa567f32c41d9371084a2724b2aa
atscott <atscott01@gmail.com>
2021-07-02 22:32:47 +0000

    4c78984ad2 fix(compiler-cli): support reflecting namespace declarations (#42728)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+12.sha-e99af18/, version);
+      banner = banner.replace(/12.2.0-next.1+15.sha-4c78984/, version);
     }
   }
 }
