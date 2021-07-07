76297eaaa205ba19bde74af32e974904c772da05
Andrew Scott <atscott@google.com>
2021-07-07 20:57:55 +0000

    8b95816fc4 fix(docs-infra): log more SW debug info (#42776)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+19.sha-24ac378/, version);
+      banner = banner.replace(/12.2.0-next.1+24.sha-8b95816/, version);
     }
   }
 }
