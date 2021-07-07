1c30b8fd519de8138b35da063d17eb125208790f
Andrew Scott <atscott@google.com>
2021-07-07 20:59:16 +0000

    95ba5b4edb fix(docs-infra): ensure the sidenav backdrop covers the floating ToC (#42787)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+24.sha-8b95816/, version);
+      banner = banner.replace(/12.2.0-next.1+25.sha-95ba5b4/, version);
     }
   }
 }
