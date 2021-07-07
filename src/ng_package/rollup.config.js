532e3b11efce04795ddcf0f0c7e11fab790e2a68
Andrew Scott <atscott@google.com>
2021-07-07 17:34:24 +0000

    eca48016c4 docs: add missing import to `CartService` tutorial instructions (#42701)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+16.sha-9557e87/, version);
+      banner = banner.replace(/12.1.1+18.sha-eca4801/, version);
     }
   }
 }
