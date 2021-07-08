1d89a24055fc2aad3d05171defa1fc45111e354b
atscott <atscott01@gmail.com>
2021-07-08 21:26:59 +0000

    f6bce3158a ci: use issue forms for issue creation (#42099)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+39.sha-d879175/, version);
+      banner = banner.replace(/12.1.1+41.sha-f6bce31/, version);
     }
   }
 }
