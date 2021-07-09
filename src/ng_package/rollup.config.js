a06fdcdba173058b4462f6ebb2f327744fea0108
Andrew Kushnir <akushnir@google.com>
2021-07-09 17:37:40 +0000

    3791ae0c95 refactor(router): Adjust behavior for computed navigation restoration (#42751)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+49.sha-5356796/, version);
+      banner = banner.replace(/12.2.0-next.1+50.sha-3791ae0/, version);
     }
   }
 }
