3e389d666e0eb5475f8fc9b47641f10cfd214ee3
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:00:20 +0000

    4e73b8820a build: no longer ship ecmascript module files within NPM packages (#42809)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+59.sha-ef15b97/, version);
+      banner = banner.replace(/12.2.0-next.1+61.sha-4e73b88/, version);
     }
   }
 }
