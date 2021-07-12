805350cb2d2422788b9b7de56553eaff5f5f7e3c
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:09:02 +0000

    2ed5a2db79 docs(core): fix `ViewChildren` code examples to avoid TS error (#42816)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+46.sha-1c31828/, version);
+      banner = banner.replace(/12.1.1+55.sha-2ed5a2d/, version);
     }
   }
 }
