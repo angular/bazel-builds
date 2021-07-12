2e836c787b79ad86ff521bdbdc45177217ce49be
Andrew Kushnir <akushnir@google.com>
2021-07-12 21:59:33 +0000

    e6593ad94a fix(dev-infra): transitive targets cannot be resolved by API extractor (#42828)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+90.sha-a524af1/, version);
+      banner = banner.replace(/12.2.0-next.1+93.sha-e6593ad/, version);
     }
   }
 }
