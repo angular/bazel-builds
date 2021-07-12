3d2f3148a35752348fab10b807f3bb01a8624a44
Andrew Kushnir <akushnir@google.com>
2021-07-12 22:00:23 +0000

    9456eca7c5 feat(dev-infra): better caching for browser archive contents (#42814)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+93.sha-e6593ad/, version);
+      banner = banner.replace(/12.2.0-next.1+91.sha-9456eca/, version);
     }
   }
 }
