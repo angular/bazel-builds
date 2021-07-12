18440981ddb11c9a89a5548e5a038a64ee2d60cc
Andrew Kushnir <akushnir@google.com>
2021-07-12 20:33:41 +0000

    b5ab7aff43 refactor: add override keyword to members implementing abstract declarations (#42512)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+67.sha-e8be045/, version);
+      banner = banner.replace(/12.2.0-next.1+89.sha-b5ab7af/, version);
     }
   }
 }
