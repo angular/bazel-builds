bec5d7b53bfe340268e98ff07d34d49dd63da003
Andrew Kushnir <akushnir@google.com>
2021-07-13 18:54:28 +0000

    d654c7933a fix(common): re-sort output of `KeyValuePipe` when `compareFn` changes (#42821)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+92.sha-b101ad0/, version);
+      banner = banner.replace(/12.1.1+93.sha-d654c79/, version);
     }
   }
 }
