fab6239d6b6c4e40ac2e867a9f9121765ec2233f
Andrew Kushnir <akushnir@google.com>
2021-07-13 23:19:03 +0000

    63013546e1 fix(core): associate the NgModule scope for an overridden component (#42817)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+95.sha-9ebd41e/, version);
+      banner = banner.replace(/12.1.1+96.sha-6301354/, version);
     }
   }
 }
