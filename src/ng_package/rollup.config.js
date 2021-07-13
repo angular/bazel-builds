86158d05797e76fa0d04b2618edcc403cccade35
Andrew Kushnir <akushnir@google.com>
2021-07-13 21:29:11 +0000

    970d7f7c6f docs: update routing-overview topic with link to custom route matches tutorial (#42837)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+101.sha-e42aa6c/, version);
+      banner = banner.replace(/12.2.0-next.1+102.sha-970d7f7/, version);
     }
   }
 }
