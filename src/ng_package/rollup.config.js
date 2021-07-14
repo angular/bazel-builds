0e9892999585bf22f6e648b6d628e1d8795e497e
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:44:06 +0000

    437759ba47 revert: docs: remove references to Trusted Types due to incompatibility with Webpack 5 (#42796)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+2.sha-48c1c81/, version);
+      banner = banner.replace(/12.2.0-next.2+3.sha-437759b/, version);
     }
   }
 }
