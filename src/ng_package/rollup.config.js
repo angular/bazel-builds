02f4bf88b5b86dcbbd46b3d190d5967aaf711a7f
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:36:26 +0000

    48c1c81780 docs: remove End-to-end test files structure (#42844)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+1.sha-64bb7cd/, version);
+      banner = banner.replace(/12.2.0-next.2+2.sha-48c1c81/, version);
     }
   }
 }
