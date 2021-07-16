8f3cd7fb8363931011728f4ed0b93cd8958e1556
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-16 17:23:29 +0000

    3da71467e9 build(docs-infra): upgrade Lighthouse to version 8.1.0 (#42846)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+13.sha-7422821/, version);
+      banner = banner.replace(/12.2.0-next.2+16.sha-3da7146/, version);
     }
   }
 }
