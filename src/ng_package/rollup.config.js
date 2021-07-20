c6a798b27c2b6c7184ee112d4c40772f842a43a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 19:17:09 +0000

    e2aa7652e5 fix(docs-infra): fix keyword color in CLI pages code blocks (#42889)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+44.sha-70c3461/, version);
+      banner = banner.replace(/12.2.0-next.2+45.sha-e2aa765/, version);
     }
   }
 }
