86f362e6a6122214be4dc7a7cb96e332ef5e5323
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 01:04:06 +0000

    e6b7472d11 build(docs-infra): improve @usageNotes message (#42888)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.2+13.sha-97fa1cf/, version);
+      banner = banner.replace(/12.1.2+29.sha-e6b7472/, version);
     }
   }
 }
