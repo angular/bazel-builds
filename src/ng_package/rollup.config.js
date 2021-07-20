b8329b4ec1d1acf649c1683f311efe52d20988a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 18:50:01 +0000

    282e86ad71 fix(dev-infra): use the version from package.json rather than tags (#42872)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+40.sha-52dc65d/, version);
+      banner = banner.replace(/12.2.0-next.2+42.sha-282e86a/, version);
     }
   }
 }
