bbd8b6560927a62fe1767f7fd8ca895de82214bb
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:56:20 +0000

    eefe1682e8 fix(core): correctly handle `null` or `undefined` in `ErrorHandler#handleError()` (#42881)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.3+2.sha-992dc93.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.3+4.sha-eefe168.with-local-changes/, version);
     }
   }
 }
