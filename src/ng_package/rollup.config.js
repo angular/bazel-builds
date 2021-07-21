7bcb053928137afa9582bfccd417f1bc3951d5a1
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 23:00:31 +0000

    5fb23eccea perf(compiler-cli): skip analysis in incremental builds for files without Angular behavior (#42562)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.3+5.sha-a502279.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.3+6.sha-5fb23ec.with-local-changes/, version);
     }
   }
 }
