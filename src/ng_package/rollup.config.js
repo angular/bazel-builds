af2b5219f31eb00c6d6071f5b6854f9fcd37b411
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:56:52 +0000

    a502279592 feat(forms): allow minLength/maxLength validator to be bound to `null` (#42565)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.3+4.sha-eefe168.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.3+5.sha-a502279.with-local-changes/, version);
     }
   }
 }
