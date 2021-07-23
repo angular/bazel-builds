07f4bb6f1dbb4252003ab8c0909f5661910b1c9c
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 17:57:26 +0000

    648da4f8b5 build: update dependency globby to v12 (#42921)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.3+13.sha-e462743.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.1+70.sha-ccbb913.with-local-changes/, version);
     }
   }
 }
