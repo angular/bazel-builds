ad21c4654c5332747107e60732ff20f1b35e0f42
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 21:43:45 +0000

    ee1f824c40 test(router): move computed state restoration tests to own file (#42933)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+49.sha-8267d25.with-local-changes/, version);
+      banner = banner.replace(/12.1.1+51.sha-d8d7784.with-local-changes/, version);
     }
   }
 }
