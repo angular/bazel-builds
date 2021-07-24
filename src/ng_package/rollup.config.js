ec234f7a828c6d4cee6de32ac4bc6140c02e27f1
Dylan Hunn <dylhunn@gmail.com>
2021-07-24 00:31:17 +0000

    e8eb87ff58 docs: Edits to remove jargon (#42861)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+51.sha-d8d7784.with-local-changes/, version);
+      banner = banner.replace(/12.1.1+52.sha-7b5405b.with-local-changes/, version);
     }
   }
 }
