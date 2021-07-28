06daa3e6c9b8752764898aab3ba270942826463d
Joey Perrott <josephperrott@gmail.com>
2021-07-28 19:26:50 +0000

    8116895ece docs: release notes for the v12.2.0-rc.0 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+91.sha-9456eca.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.1+93.sha-e6593ad.with-local-changes/, version);
     }
   }
 }
