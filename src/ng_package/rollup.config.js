9d1f0a11896037ce02cb92b562cc92f127fff7d8
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 18:09:29 +0000

    047994b048 feat(dev-infra): introduce release action for directly branching-off into RC (#42973)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+86.sha-5b5868d.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.1+87.sha-1dffa51.with-local-changes/, version);
     }
   }
 }
