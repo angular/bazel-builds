3afe74e2821bd136317999dc52376f6cc08cc639
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 23:15:59 +0000

    8ce1ac603a docs: Add josmar-crwdstffng to contributors list. (#42972)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+94.sha-08444c6.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.1+95.sha-f0d857e.with-local-changes/, version);
     }
   }
 }
