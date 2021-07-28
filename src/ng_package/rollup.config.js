b02a40cf4054abb24f7e338fd46074672fcf5aca
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 20:20:51 +0000

    b075721a49 fix(dev-infra): ensure that building environment stamp fails silently (#42985)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+92.sha-867000e.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.1+93.sha-e6593ad.with-local-changes/, version);
     }
   }
 }
