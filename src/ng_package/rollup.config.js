b5954560b24f58722ef436c3904d1d863771efe9
Andrew Kushnir <akushnir@google.com>
2021-07-30 16:49:36 +0000

    dd6e80aac5 refactor(bazel): extract function to patch fileNameToModuleName on host (#42974)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+95.sha-f0d857e.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.1+98.sha-c1c1cda.with-local-changes/, version);
     }
   }
 }
