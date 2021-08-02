0bc00639c3b80e2c04312978ad042c54f36fe5b5
atscott <atscott01@gmail.com>
2021-08-02 17:17:09 +0000

    e4f62c12f5 test(compiler): add a test for parsing multiline expressions in attributes (#42062)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.1+98.sha-c1c1cda.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.2+6.sha-ca1fde9.with-local-changes/, version);
     }
   }
 }
