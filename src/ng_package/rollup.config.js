ad8ba574bf0f9979717c39d0102e0f98df12ef12
atscott <atscott01@gmail.com>
2021-08-02 22:58:17 +0000

    a036cb5d17 docs: amend links in content-projection guide (#43009)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.1.1+82.sha-04472d2.with-local-changes/, version);
+      banner = banner.replace(/12.1.1+83.sha-4f8a728.with-local-changes/, version);
     }
   }
 }
