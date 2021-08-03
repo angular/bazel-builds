ed15f26b3409827f8b926c37d221efdcdce25482
atscott <atscott01@gmail.com>
2021-08-03 00:29:34 +0000

    f0c5ba08f6 fix(language-service): global autocomplete doesn't work when the user tries to modify the symbol (#42923)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+13.sha-7422821.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.2+14.sha-bc1da1f.with-local-changes/, version);
     }
   }
 }
