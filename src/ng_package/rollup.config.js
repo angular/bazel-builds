77e7c54e1ab95a78ad206af74508d50e2396f959
atscott <atscott01@gmail.com>
2021-08-03 22:57:49 +0000

    f99ec27596 Revert "refactor(ngcc): remove unused import (#42062)" (#43033)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+14.sha-bc1da1f.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.2+28.sha-0ce8f6e.with-local-changes/, version);
     }
   }
 }
