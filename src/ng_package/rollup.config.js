df306de915452b35b869fec453a098d491650148
Andrew Kushnir <akushnir@google.com>
2021-08-04 23:48:07 +0000

    e49fc96ed3 feat(forms): Make Form Statuses use stricter types. (#42952)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+33.sha-4286b08.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.2+34.sha-30165f9.with-local-changes/, version);
     }
   }
 }
