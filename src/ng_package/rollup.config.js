c4c63aedd454a7288b17365dc46e4f9f573643ad
Andrew Scott <atscott@google.com>
2021-08-04 16:19:52 +0000

    8628826535 fix(core): incorrect error reported when trying to re-create view which had an error during creation (#43005)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+28.sha-0ce8f6e.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.2+29.sha-5327926.with-local-changes/, version);
     }
   }
 }
