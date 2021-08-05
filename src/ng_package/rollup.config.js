3737f6b3fe5707af28a3d404d828bb9722140d89
Andrew Kushnir <akushnir@google.com>
2021-08-05 22:53:51 +0000

    644f1b3924 docs: fix interval type in the example (#43042)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+34.sha-30165f9.with-local-changes/, version);
+      banner = banner.replace(/12.2.0-next.2+35.sha-7271ad1.with-local-changes/, version);
     }
   }
 }
