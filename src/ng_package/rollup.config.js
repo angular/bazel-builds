e210ddd1405feb65c408807c1bad6cd2075799a2
Andrew Kushnir <akushnir@google.com>
2021-08-06 16:14:19 +0000

    d8690d867c fix(dev-infra): search since HEAD on the local branch for version tags when creating stamps (#43065)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.0-next.2+36.sha-7eddd12.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.0+5.sha-d8690d8.with-local-changes/, version);
     }
   }
 }
