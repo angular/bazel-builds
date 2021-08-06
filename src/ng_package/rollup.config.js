b912fb7beb0c6280401da86eb00e604b4ad60d3d
Andrew Kushnir <akushnir@google.com>
2021-08-06 18:07:57 +0000

    784671597e fix(router): Allow question marks in query param values (#31187)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.0+5.sha-d8690d8.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.0+6.sha-7846715.with-local-changes/, version);
     }
   }
 }
