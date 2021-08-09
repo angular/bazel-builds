1025c37bf7b4b87f4d2386d2182525d5c43f621d
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:45:58 +0000

    bbad42310b fix(router): ensure check for match options is compatible with property renaming (#43086)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.0+8.sha-9359089.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.0+10.sha-bbad423.with-local-changes/, version);
     }
   }
 }
