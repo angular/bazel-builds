4d27e359e15ff3644617e7fbb3e1e625488b28d3
GitHub <noreply@github.com>
2021-08-10 19:05:03 +0000

    181adff213 Revert "build(dev-infra): exlude commit message filter from formatting (#43091)" (#43104)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.0+14.sha-7edb128.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.0+15.sha-181adff.with-local-changes/, version);
     }
   }
 }
