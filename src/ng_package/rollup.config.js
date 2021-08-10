4541c5205abbda93027870cad86a773097827ea1
Andrew Kushnir <akushnir@google.com>
2021-08-10 17:56:01 +0000

    cb988aebb0 refactor(core): replace references to goog.LOCALE with goog.getLocale() (#43089)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.0+12.sha-892bd92.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.0+13.sha-cb988ae.with-local-changes/, version);
     }
   }
 }
