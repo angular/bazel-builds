e813eee475230bd363d6c26947e0fcc9b5ea0c50
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:55:35 +0000

    c7d305b072 refactor(compiler): Remove compileUndecoratedClasses config file (#43043)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+3.sha-a2a9b91.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+4.sha-c7d305b.with-local-changes/, version);
     }
   }
 }
