acdf07a146f9d5a2655273efd11ece3e1bdff0ef
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 16:42:22 +0000

    d5f9890c92 feat(language-service): auto-apply optional chaining on nullable symbol (#42995)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+1.sha-9fb79d3.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+2.sha-d5f9890.with-local-changes/, version);
     }
   }
 }
