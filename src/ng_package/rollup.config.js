9e7e19ec884b32b25e294a5190572151384ae000
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:59:22 +0000

    470371f1b2 docs: fix description: "property decorator" (not parameter) (#43068) (#43128)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+5.sha-398be10.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+6.sha-470371f.with-local-changes/, version);
     }
   }
 }
