ce6e977d9edd525fe42bf025abc1b50d17a14fdd
Dylan Hunn <dylhunn@gmail.com>
2021-08-13 22:18:55 +0000

    b0b7ef2985 build: update dependency build_bazel_rules_nodejs to v3.8.0 (#43136)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+7.sha-2a1aa58.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+8.sha-b0b7ef2.with-local-changes/, version);
     }
   }
 }
