3d9e51fe94ade430b66a1c867bc560ec773b8215
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:09:46 +0000

    286b2807de fix(router): eagerly update internal state on browser-triggered navigations (#43102)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+11.sha-b3bb6ec.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+12.sha-286b280.with-local-changes/, version);
     }
   }
 }
