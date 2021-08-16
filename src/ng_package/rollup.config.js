91cfe3d80d572a5e98d22d8a4978242c2258c6d3
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 18:04:49 +0000

    83fa3ee581 docs: add tmdavis to contributors (#43135)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.1+9.sha-e0a8ca7.with-local-changes/, version);
+      banner = banner.replace(/12.2.1+12.sha-83fa3ee.with-local-changes/, version);
     }
   }
 }
