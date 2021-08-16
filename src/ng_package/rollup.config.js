942bdeae68701d6f482d4b4d469cbe89930a22c4
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:11:15 +0000

    30bf472597 docs: update Aristeidis bio in GDE list (#43164)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+12.sha-286b280.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+13.sha-30bf472.with-local-changes/, version);
     }
   }
 }
