cfd180174a291e8715d23845655381190f87d3b2
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 20:29:49 +0000

    66f1962fa6 test(compiler): add a test for parsing multiline expressions in attributes (#43129)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+22.sha-564ad06.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+29.sha-66f1962.with-local-changes/, version);
     }
   }
 }
