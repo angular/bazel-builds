150f2b5ae12d987e21f71df0a89ea2c4f24dc489
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 00:37:06 +0000

    b9fd76e257 docs(router): use the correct `live-example` name in `routing-overview.md` (#43180)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+29.sha-66f1962.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+30.sha-b9fd76e.with-local-changes/, version);
     }
   }
 }
