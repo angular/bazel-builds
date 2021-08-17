d95c62eae9d7ce14841481dd2d0458881621c993
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:42:22 +0000

    7e71370fe6 perf(platform-browser): avoid intermediate arrays in server transition (#43145)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+31.sha-cf33849.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+34.sha-7e71370.with-local-changes/, version);
     }
   }
 }
