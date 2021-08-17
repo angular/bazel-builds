7a2b7ea2a6e50369dc12f8a03dc3dded095b0ffa
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 00:39:47 +0000

    3cb1f18f97 fix(animations): add pure annotations to static property initializers (#43064)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+30.sha-b9fd76e.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+33.sha-3cb1f18.with-local-changes/, version);
     }
   }
 }
