15c213963074af0a936cc70e6247f07dc7a4b10c
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 16:43:40 +0000

    e849e57059 docs: tidy up description of NgComponentOutlet (#43159)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.1+30.sha-e31ed3d.with-local-changes/, version);
+      banner = banner.replace(/12.2.1+31.sha-e849e57.with-local-changes/, version);
     }
   }
 }
