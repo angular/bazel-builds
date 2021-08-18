16d6f10541991b3ba059cc4a61136404c6347f77
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 03:23:14 +0000

    d7de6d6725 build: update bazel to v4.0.0-rc.0 (#43138)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.1+37.sha-e9dd078.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.1+39.sha-d7de6d6.with-local-changes/, version);
     }
   }
 }
