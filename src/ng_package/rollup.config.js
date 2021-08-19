8de180df82444b7f1f5921cdf8183d06c655198a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-19 18:43:04 +0000

    e57691c9c5 feat(core): Add migration to update empty routerLinks in templates (#43176)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.2+1.sha-47cca52.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.2+2.sha-e57691c.with-local-changes/, version);
     }
   }
 }
