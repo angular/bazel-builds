fe823339144f2256d651d4b10592cbb7eb8415ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:03:26 +0000

    a39c68940a build(docs-infra): upgrade cli command docs sources to 2621e1d39 (#43238)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.2+19.sha-073b20b.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.2+20.sha-a39c689.with-local-changes/, version);
     }
   }
 }
