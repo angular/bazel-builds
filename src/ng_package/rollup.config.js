c75e555310911504a3c218df354ccf5b8d38151b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 22:31:58 +0000

    e7e78e6709 refactor(zone.js): Rename class ZoneDelegate to _ZoneDelegate to differentiate it from the ZoneDelgate interface (#43019)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.2+17.sha-b818361.with-local-changes/, version);
+      banner = banner.replace(/12.2.3+2.sha-e7e78e6.with-local-changes/, version);
     }
   }
 }
