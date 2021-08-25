d0d1ede1ce3d0257653e0512418ccf2faae7e8f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:04:16 +0000

    073b20b540 build: update all non-major dependencies (#43201)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.2+18.sha-00aad59.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.2+19.sha-073b20b.with-local-changes/, version);
     }
   }
 }
