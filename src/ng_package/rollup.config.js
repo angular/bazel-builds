1769e0c69132e14f5e59a0eb3319891f710c0e97
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:01:59 +0000

    00aad59047 test(language-service): add test for extended diagnostics (#43134)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.2+12.sha-27fdd52.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.2+18.sha-00aad59.with-local-changes/, version);
     }
   }
 }
