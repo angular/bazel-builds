8ff723575e926430f7aaf29195a7bbec3f7c6a1a
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:26:43 +0000

    fdcb420bd7 docs(router): Fix incorrect name for url match options (#43268)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.3+3.sha-fb64f71.with-local-changes/, version);
+      banner = banner.replace(/12.2.3+5.sha-fdcb420.with-local-changes/, version);
     }
   }
 }
