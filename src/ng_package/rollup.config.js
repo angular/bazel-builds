5334f2f46a0ae1f0bb9f97cdb2e5eb48871c88bd
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 23:55:33 +0000

    95cfd0b0e1 test(compiler-cli): add tests for `NullishCoalescingNotNullableCheck` (#43232)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.3+9.sha-c33ba8d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.3+16.sha-95cfd0b.with-local-changes/, version);
     }
   }
 }
