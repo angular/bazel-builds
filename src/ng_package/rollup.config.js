46e6e0d2ab21221a7d88290e79254c64f0d78870
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-30 20:57:16 +0000

    dcfabf08af docs: add step to declare `ProductAlertsComponent` in tutorial (#43229)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.3+18.sha-f3c8408.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.3+19.sha-dcfabf0.with-local-changes/, version);
     }
   }
 }
