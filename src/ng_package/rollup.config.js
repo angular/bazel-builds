11f45562b848dcb19e3dabc16809b53c883b0d95
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-01 15:42:28 +0000

    faf9f5a3bc feat(router): new output that would notify when link is activated (#43280)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.3+19.sha-dcfabf0.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.3+20.sha-faf9f5a.with-local-changes/, version);
     }
   }
 }
