453a152504fbed189a099da70c28cfe2ee2377ed
Andrew Scott <atscott01@gmail.com>
2021-09-01 21:39:54 +0000

    a0bd6e90f9 fix(router): add more context to `Unhandled Navigation Error` (#43291)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.3+17.sha-caecd18.with-local-changes/, version);
+      banner = banner.replace(/12.2.4+1.sha-a0bd6e9.with-local-changes/, version);
     }
   }
 }
