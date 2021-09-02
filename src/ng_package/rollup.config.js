73ece7e02882a86d97ada902a46ecd17db1a2d36
Andrew Scott <atscott01@gmail.com>
2021-09-02 21:38:32 +0000

    5472c28fa1 build: check .ng-dev tool configuration in CI (#43322)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.4+12.sha-c6a9300.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.4+15.sha-5472c28.with-local-changes/, version);
     }
   }
 }
