0d59ae0ba4fcdbf562cf88dfb0358b90c2157218
Andrew Scott <atscott01@gmail.com>
2021-09-07 19:03:42 +0000

    2be0505334 build: update dependency @types/inquirer to v8 (#43371)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.4+16.sha-652a6d3.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.4+17.sha-2be0505.with-local-changes/, version);
     }
   }
 }
