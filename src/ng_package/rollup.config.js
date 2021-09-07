d615ab3bf4b88fc77e5f111c2e5f4ef782e068cb
Andrew Scott <atscott01@gmail.com>
2021-09-07 19:05:40 +0000

    da0403e632 refactor: fix typo in postinstall-patches.js (#43362)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.4+17.sha-2be0505.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.4+18.sha-da0403e.with-local-changes/, version);
     }
   }
 }
