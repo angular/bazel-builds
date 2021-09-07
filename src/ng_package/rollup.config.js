ef76401f296ebc227cd823396faa8e3780630cb0
Andrew Scott <atscott01@gmail.com>
2021-09-07 23:36:41 +0000

    2efc18e675 fix(migrations): migration failed finding tsconfig file (#43343)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.4+25.sha-9c7d18e.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.4+26.sha-2efc18e.with-local-changes/, version);
     }
   }
 }
