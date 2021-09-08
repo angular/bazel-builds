c853333306b30ec50d960ef12bb5208a68776c9f
Andrew Scott <atscott01@gmail.com>
2021-09-08 17:14:01 +0000

    9e039ca68b fix(router): Only trigger router navigation on `popstate` events from `Location` subscription (#43328)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.4+29.sha-93c9932.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+1.sha-9e039ca.with-local-changes/, version);
     }
   }
 }
