fd04a5f489dbc6e8ef910dc30651d0167d9ce848
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:39:29 +0000

    2d93e9c0fd docs: add Michele Stieven to GDE resources (#43233)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+19.sha-99f63dc.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+20.sha-2d93e9c.with-local-changes/, version);
     }
   }
 }
