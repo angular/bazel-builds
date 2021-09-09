34ebc55fbfbc918296798cdba25228c1fb909747
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:47:10 +0000

    4761842440 docs: remove extraneous word in the Dependency injection in action guide (#43255)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+20.sha-2d93e9c.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+21.sha-4761842.with-local-changes/, version);
     }
   }
 }
