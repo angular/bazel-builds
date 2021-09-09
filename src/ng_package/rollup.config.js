6d1b394672b8edb60346f5ffeb3e75edf4228d8f
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:54:17 +0000

    b31adaf6cc docs: add Angular Projects book in resources (#43130)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+22.sha-a0fe899.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+23.sha-b31adaf.with-local-changes/, version);
     }
   }
 }
