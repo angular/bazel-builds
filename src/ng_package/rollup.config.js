e6743694e093aef45a07c458aed0904adc206f81
Andrew Kushnir <akushnir@google.com>
2021-09-09 22:14:04 +0000

    7c2434da7f docs: add Architect Training to resources (#43327)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+24.sha-012209f.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+26.sha-7c2434d.with-local-changes/, version);
     }
   }
 }
