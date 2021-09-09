dde148ec42569f5820e6ae4c5c0f6e1f17062302
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:37:40 +0000

    99f63dc13b fix(docs-infra): bump aio size limit (#42800)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+9.sha-f57f0ed.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+19.sha-99f63dc.with-local-changes/, version);
     }
   }
 }
