8fa8276f9aaff9f828326bb76da3b380ecd90c24
Andrew Kushnir <akushnir@google.com>
2021-09-10 16:41:38 +0000

    df177d9176 docs: amend code related to string-expressions for property binding (#43366)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+26.sha-7c2434d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+27.sha-df177d9.with-local-changes/, version);
     }
   }
 }
