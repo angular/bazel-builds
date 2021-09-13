fb98ee7b5b537c6fdb4a9c4618d3c622f0c29520
Andrew Kushnir <akushnir@google.com>
2021-09-13 16:44:27 +0000

    6d8a711f5c docs: remove unnecessary "https://angular.io/" from aio links (#43425)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+29.sha-75a3c77.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+30.sha-6d8a711.with-local-changes/, version);
     }
   }
 }
