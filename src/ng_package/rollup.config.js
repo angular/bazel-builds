18db15f59625bc79598b7624543a43126c4128a2
Andrew Kushnir <akushnir@google.com>
2021-09-13 22:29:44 +0000

    0e8548f667 fix(router): Allow renavigating to failed URLs (#43424)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+38.sha-5ccc9e8.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+39.sha-0e8548f.with-local-changes/, version);
     }
   }
 }
