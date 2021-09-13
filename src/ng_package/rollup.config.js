424d9001e93c1e0080ef09631b93033e611af7b7
Andrew Kushnir <akushnir@google.com>
2021-09-13 21:54:18 +0000

    59353c6305 fix(service-worker): clear service worker cache in safety worker (#43324)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+31.sha-de682eb.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+32.sha-59353c6.with-local-changes/, version);
     }
   }
 }
