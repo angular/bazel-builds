97bef19fbeab6b387513f9abdcae9ca9d16e86c9
Andrew Kushnir <akushnir@google.com>
2021-09-14 18:30:25 +0000

    24cf4eaa4d refactor(router): Remove TODO and slightly adjust eager browserUrlTree update (#43426)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.5+32.sha-2f94ac3.with-local-changes/, version);
+      banner = banner.replace(/12.2.5+33.sha-24cf4ea.with-local-changes/, version);
     }
   }
 }
