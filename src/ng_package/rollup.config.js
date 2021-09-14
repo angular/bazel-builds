90fb11146243f25369f07ba0b853e6139d3fd599
Andrew Kushnir <akushnir@google.com>
2021-09-14 15:54:40 +0000

    796da641f0 fix(router): Do not modify parts of URL excluded from with 'eager' updates (#43421)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.5+43.sha-e8e41ef.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.5+44.sha-796da64.with-local-changes/, version);
     }
   }
 }
