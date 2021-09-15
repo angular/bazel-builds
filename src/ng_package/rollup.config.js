8432c39c70e93311a0d9a033bfb63b38b7facb27
Andrew Kushnir <akushnir@google.com>
2021-09-15 03:55:50 +0000

    2050b0898b docs: update grammar (#43451)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.5+33.sha-24cf4ea.with-local-changes/, version);
+      banner = banner.replace(/12.2.5+40.sha-2050b08.with-local-changes/, version);
     }
   }
 }
