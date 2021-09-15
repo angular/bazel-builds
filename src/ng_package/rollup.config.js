3d266c3a6c85083f7cdf1fc835d2fc8aeccea565
Andrew Kushnir <akushnir@google.com>
2021-09-15 20:42:14 +0000

    def6315a76 docs: update reviewed tag (#43452)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.5+40.sha-2050b08.with-local-changes/, version);
+      banner = banner.replace(/12.2.6+15.sha-def6315.with-local-changes/, version);
     }
   }
 }
