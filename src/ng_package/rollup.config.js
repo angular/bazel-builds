4c721d2f92149430bec6da6fb503f205a859b4a9
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 16:18:31 +0000

    7efa47075c docs: remove http example dependency on 3rd party service (#43475)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+14.sha-e5d08c1.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+19.sha-7efa470.with-local-changes/, version);
     }
   }
 }
