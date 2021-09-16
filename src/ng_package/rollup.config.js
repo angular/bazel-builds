109631204fa783866c91abd317f0358b7f8444a9
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 20:40:34 +0000

    79700822fd refactor(router): remove unnecessary getTransition function (#43436)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+27.sha-e55d052.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+28.sha-7970082.with-local-changes/, version);
     }
   }
 }
