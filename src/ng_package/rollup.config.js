062c24ed1750332f4bd6e1aac16722055f43baea
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 17:14:37 +0000

    2292a9cbd9 refactor(compiler): improve regular expression for stripping comments (#43456)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+19.sha-7efa470.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+20.sha-2292a9c.with-local-changes/, version);
     }
   }
 }
