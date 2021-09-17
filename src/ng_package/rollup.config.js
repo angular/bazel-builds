dc3f2d789f8c9b420c3075bc1952081d8440b822
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 18:54:45 +0000

    3d090ba74d docs: Edits to remove jargon (#42986)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+37.sha-046b35f.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+36.sha-3d090ba.with-local-changes/, version);
     }
   }
 }
