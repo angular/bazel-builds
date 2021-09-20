0b4a4c81de265f530633069bb8403009f7bf283f
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 21:19:26 +0000

    c5d0bd4966 fix(router): Prevent URL flicker when new navigations cancel ongoing ones (#43496)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+43.sha-9470f56.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+44.sha-c5d0bd4.with-local-changes/, version);
     }
   }
 }
