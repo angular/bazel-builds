a6546d97027191daba406abcd613be5ee562ee17
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 18:01:47 +0000

    2690315b3e docs: reviewed tag added (#43493)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.6+38.sha-020f0d3.with-local-changes/, version);
+      banner = banner.replace(/12.2.6+41.sha-2690315.with-local-changes/, version);
     }
   }
 }
