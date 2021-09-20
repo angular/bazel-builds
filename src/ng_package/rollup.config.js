fba1d1e7fec07765ffc865237144316c9e1f5d99
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 16:21:35 +0000

    80f5a4e7fb fix(docs-infra): improve aio contributors page responsiveness (#43355)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+38.sha-91183eb.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+39.sha-80f5a4e.with-local-changes/, version);
     }
   }
 }
