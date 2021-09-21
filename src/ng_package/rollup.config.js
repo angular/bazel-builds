f1d0b670c39634a93d5899308cdc810f304b9cbd
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-21 18:08:19 +0000

    6ce6beb1d6 build: lock file maintenance (#43513)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+53.sha-d1baf7c.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+54.sha-6ce6beb.with-local-changes/, version);
     }
   }
 }
