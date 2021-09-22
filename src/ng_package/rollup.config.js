11891288d2151f8a2a2f7e2889a09ee4449e8cd8
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 16:49:39 +0000

    a2eae02b44 build(docs-infra): upgrade cli command docs sources to 5af45a096 (#43532)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+61.sha-6ff0eef.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+62.sha-a2eae02.with-local-changes/, version);
     }
   }
 }
