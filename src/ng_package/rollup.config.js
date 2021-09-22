306de64870d180366a2d2f1fbcae638f254e5dd9
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 16:21:44 +0000

    ce406e35e8 test(compiler-cli): rename test files to be more descriptive (#43419)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+57.sha-2028c39.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+59.sha-ce406e3.with-local-changes/, version);
     }
   }
 }
