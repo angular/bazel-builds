8e52df0facc78abb8e2969aeec14fdb02f6ddab7
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 18:08:33 +0000

    a1633c1c6e docs(service-worker): improve service worker guides (#43508)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.6+62.sha-a2eae02.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.6+63.sha-a1633c1.with-local-changes/, version);
     }
   }
 }
