bd841d9af2475f15e1be77d8b473254412d672ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 22:04:40 +0000

    2306a3250f docs(router): fix typo in router event API documentation (#43555)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.6+58.sha-dad213e.with-local-changes/, version);
+      banner = banner.replace(/12.2.7+9.sha-2306a32.with-local-changes/, version);
     }
   }
 }
