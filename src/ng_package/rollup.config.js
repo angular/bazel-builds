69fad2426ad697e050ef6d2604420569118f6256
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 22:11:03 +0000

    ea61ec2562 feat(core): support TypeScript 4.4 (#43281)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.7+10.sha-cc427d1.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.7+11.sha-ea61ec2.with-local-changes/, version);
     }
   }
 }
