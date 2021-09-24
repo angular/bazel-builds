4d7d1700d5e667756d1b0f341bb971dbdc9e5135
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:18:39 +0000

    d02c8da7bd fix(docs-infra): display deprecation notes for properties (#43566)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.7+40.sha-9d290bd.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.7+39.sha-d02c8da.with-local-changes/, version);
     }
   }
 }
