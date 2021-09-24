109ac35ce5f04ba6cba2ac688a640264d9b41df4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:11:17 +0000

    67ff93d33a docs: reviewed tag added (#43472)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.7+35.sha-28a3c0c.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.7+36.sha-67ff93d.with-local-changes/, version);
     }
   }
 }
