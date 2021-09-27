3b6a18817cf68683e9bcf96c1606d7925478cc1f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 18:05:31 +0000

    3e37e8979d fix(language-service): provide dom event completions (#43299)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+1.sha-353cad2.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+2.sha-3e37e89.with-local-changes/, version);
     }
   }
 }
