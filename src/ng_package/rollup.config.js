c59871abcab1f54688c2b248605ab7c6bad5deee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 18:11:57 +0000

    49e0b17e56 docs: links added for each builder schema (#43564)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+3.sha-81d09d1.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+5.sha-49e0b17.with-local-changes/, version);
     }
   }
 }
