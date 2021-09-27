10651fdc8162a457840d335487daf14a27a31590
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 21:41:05 +0000

    bdf5887077 docs(router): `params` or `queryParams` are not advised against (#43562)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+9.sha-66fb311.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+8.sha-bdf5887.with-local-changes/, version);
     }
   }
 }
