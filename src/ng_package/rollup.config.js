44ffd0da4b93eb95a3d096603aa319751613bd9f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:37:49 +0000

    8d34f86edb docs: deprecate `PlatformRef.bootstrapModuleFactory` and factory-based signature of `ApplicationRef.bootstrap` (#43560)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+14.sha-4f3beff.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+15.sha-8d34f86.with-local-changes/, version);
     }
   }
 }
