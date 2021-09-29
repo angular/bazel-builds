be7c10f5cf8ae3906ffcc9198624fe65537f3f96
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:35:15 +0000

    09d325a9e6 test(router): refactor tests to not use deprecated loadChildren (#43578)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+27.sha-6a3492b.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+29.sha-09d325a.with-local-changes/, version);
     }
   }
 }
