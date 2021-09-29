d5b4d61b53a1df1863bc7c6fb325181e026025bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:18:33 +0000

    e0a72857cc fix(bazel): construct a manifest file even when warnings are emitted (#43582)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+23.sha-1027780.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+25.sha-e0a7285.with-local-changes/, version);
     }
   }
 }
