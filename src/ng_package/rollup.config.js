4db5dc6a1b8c1a5d67f8cc0f4cd5c078f7bcb560
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:19:35 +0000

    6a3492b751 docs: fix typo in observables file (#43630) PR Close #43630

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+26.sha-d158960.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+27.sha-6a3492b.with-local-changes/, version);
     }
   }
 }
