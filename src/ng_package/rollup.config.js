c643c9c79c6d297cfd6f7d9fa0858b1991376a4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:57:59 +0000

    ab3de40ba3 Revert "refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.8+29.sha-09d325a.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.8+30.sha-ab3de40.with-local-changes/, version);
     }
   }
 }
