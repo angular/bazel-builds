1f0a5f6be6b1e07c131c67de84a43d1a16c864f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-30 00:22:04 +0000

    28b7f79a63 docs: Edits to remove jargon (#43000)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.7+32.sha-cadef4c.with-local-changes/, version);
+      banner = banner.replace(/12.2.7+33.sha-28b7f79.with-local-changes/, version);
     }
   }
 }
