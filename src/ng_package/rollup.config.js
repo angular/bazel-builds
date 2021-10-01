e5b719f60f18c70f09d529d077fdf6d0c8737955
Dylan Hunn <dylhunn@gmail.com>
2021-10-01 20:39:14 +0000

    c721135e37 release: cut the v13.0.0-next.10 release (#43672)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.9+84.sha-c15b8c7.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.9+85.sha-c721135.with-local-changes/, version);
     }
   }
 }
