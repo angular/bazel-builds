7d4e9e8eafe8e5b99a7ee62bdd035f54242f760f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-01 16:15:58 +0000

    7aea5256de docs: update commit message guidelines to reflect current requirements (#43650)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.9+3.sha-544cb43.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.9+4.sha-7aea525.with-local-changes/, version);
     }
   }
 }
