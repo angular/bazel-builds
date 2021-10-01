156b5d0dbe9cd0a1681e11ea566a9ef5fc95bcda
Dylan Hunn <dylhunn@gmail.com>
2021-10-01 16:51:22 +0000

    9eba2609f3 test(core): update matcher.jasmineToString() to work with jasmine@3.6.0 (#43411)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.9+6.sha-74ca3c5.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.9+10.sha-9eba260.with-local-changes/, version);
     }
   }
 }
