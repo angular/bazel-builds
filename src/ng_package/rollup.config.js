fbfb8527f2168aaacdde3098f42d0e7e65759894
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:13:26 +0000

    dbfb2d20b3 docs: remove web-animations from required polyfills (since IE11 is deprecated) (#43605)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.10+5.sha-2cc5cc9.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.10+7.sha-dbfb2d2.with-local-changes/, version);
     }
   }
 }
