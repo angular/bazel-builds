70945781336e45856fe8786bd70ac7101ecd0887
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 17:47:07 +0000

    f513b1773e refactor(router): add stub files for g3 patch of NgModuleFactoryLoader (#43660)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.10+2.sha-d04b550.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.10+3.sha-f513b17.with-local-changes/, version);
     }
   }
 }
