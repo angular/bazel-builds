28b253d99cf3f5619b9d026c8a68fa0ddd536edf
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 18:18:09 +0000

    0dc45446fe feat(service-worker): expose more version update events (#43668)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.11+6.sha-318cf91.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.11+9.sha-0dc4544.with-local-changes/, version);
     }
   }
 }
