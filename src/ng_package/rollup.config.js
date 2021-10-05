89b0099d79d0d3a8958f684ce75ff7e92764e578
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 17:06:41 +0000

    318cf9144e test(core): fix failing unit test (#43714)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.11+3.sha-bdc6ff4.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.11+6.sha-318cf91.with-local-changes/, version);
     }
   }
 }
