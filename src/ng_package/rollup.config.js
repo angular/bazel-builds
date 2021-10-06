0a1dd976d3b96b024d017e4c44a612b8d671ac00
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 17:23:17 +0000

    23ac3f7cef ci: test AIO/docs examples against the latest RxJS 7.x version (v7.3.1) (#43683)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.12+5.sha-52587e5.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.12+9.sha-23ac3f7.with-local-changes/, version);
     }
   }
 }
