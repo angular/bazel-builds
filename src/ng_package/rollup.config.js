87d4ec5f5b69ad25d8d3889631b1836a5b482504
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 23:15:54 +0000

    48aa0039ea refactor(compiler-cli): update peerDependencies to require rxjs ^7.4.0 (#43748)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.13+3.sha-04fec60.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.13+5.sha-48aa003.with-local-changes/, version);
     }
   }
 }
