752af07b086f16e8dbf6d5e03c0b245e409ace2a
Andrew Scott <atscott01@gmail.com>
2021-10-07 21:14:29 +0000

    5b1b2fd2f9 build: update dependency google-closure-compiler to v20211006 (#43754)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+2.sha-e0667ef.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+5.sha-5b1b2fd.with-local-changes/, version);
     }
   }
 }
