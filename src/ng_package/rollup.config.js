94f659285a848eb7f218216b9128c8bff7455007
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:59:03 +0000

    1869474386 refactor(docs-infra): remove linting from i18n example angular.json (#43747)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.15+8.sha-a64adab.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.15+12.sha-1869474.with-local-changes/, version);
     }
   }
 }
