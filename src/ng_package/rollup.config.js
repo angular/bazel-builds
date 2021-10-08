788612b2b60130fe730a25f9055cbf7dd60bc49a
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:34:04 +0000

    a64adabf65 fix(docs-infra): avoid prepending version info to error messages twice (#43767)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.15+2.sha-06a87c4.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.15+8.sha-a64adab.with-local-changes/, version);
     }
   }
 }
