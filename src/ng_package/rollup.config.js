719533c21527b43750bafcd3b5d248998df326e1
Andrew Scott <atscott01@gmail.com>
2021-10-11 17:02:52 +0000

    2a67227ab3 build: update dependency jsdom to v18 (#43786)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.15+12.sha-1869474.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.15+16.sha-2a67227.with-local-changes/, version);
     }
   }
 }
