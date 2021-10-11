2f2061a7020996c97e46a422cbd20404c75e335a
Andrew Scott <atscott01@gmail.com>
2021-10-11 17:36:17 +0000

    08caeadddb fix(core): avoid duplicating comments in TestBed teardown migration (#43776)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+21.sha-4daf631.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+23.sha-08caead.with-local-changes/, version);
     }
   }
 }
