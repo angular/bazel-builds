a55ad9cb54e7fb3921560271f5fa91f59bcb5c79
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:05:39 +0000

    8671055a16 docs(animations): fix wrong code links present in the animations guide (#43787)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+34.sha-fde329d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+35.sha-8671055.with-local-changes/, version);
     }
   }
 }
