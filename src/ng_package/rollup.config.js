98c1e9b3edd282a577b1ab745bf89503624c5bf7
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:14:26 +0000

    db01894791 docs: remove mention of router API that 'may be deprecated' (#43765)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.15+18.sha-7396021.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.15+33.sha-db01894.with-local-changes/, version);
     }
   }
 }
