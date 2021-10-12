d89aea59db32ed828a1ae69555586f4ec2fb6e5e
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:02:19 +0000

    62494c9f2a docs: update how to remove a configuration item (#43795)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+27.sha-baf8da0.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+32.sha-62494c9.with-local-changes/, version);
     }
   }
 }
