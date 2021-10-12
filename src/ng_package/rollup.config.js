2fffb9c8344bddd4b25254b38605f808bc81db3c
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:00:42 +0000

    17f5299967 ci: use Node.js browsers image (#43810)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+23.sha-08caead.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+30.sha-17f5299.with-local-changes/, version);
     }
   }
 }
