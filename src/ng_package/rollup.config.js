7475da465f3fefdf40f1a8c5a248c50cc16c6f31
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:07:27 +0000

    eec3839e83 fix(docs-infra): improve cancel icon in top-menu search (#43600)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+36.sha-16e5a31.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+38.sha-eec3839.with-local-changes/, version);
     }
   }
 }
