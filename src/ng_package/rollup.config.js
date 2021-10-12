bd340ed453992636a2260811b20e6c82437b1932
Andrew Scott <atscott01@gmail.com>
2021-10-12 21:55:52 +0000

    ad832900b7 docs: update internationalization topics (#43722)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.15+33.sha-db01894.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.15+34.sha-ad83290.with-local-changes/, version);
     }
   }
 }
