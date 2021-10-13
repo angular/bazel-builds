829e87af466d47b74a57a58142cad7308a913804
Andrew Scott <atscott01@gmail.com>
2021-10-13 17:09:58 +0000

    68d18a0422 refactor(docs-infra): remove unnecessary tsconfig.json file (#43818)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+44.sha-512f643.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+45.sha-68d18a0.with-local-changes/, version);
     }
   }
 }
