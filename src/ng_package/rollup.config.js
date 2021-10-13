e7dfb3679e3939b32cf81349f4bf6e84cbf76a3c
GitHub <noreply@github.com>
2021-10-13 18:43:05 +0000

    b5eccde447 release: cut the v13.0.0-rc.0 release (#43828)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.15+41.sha-7ba9591.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.15+42.sha-b5eccde.with-local-changes/, version);
     }
   }
 }
