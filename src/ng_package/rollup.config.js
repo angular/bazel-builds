5e21b7061114c8b2a5a1a6a89b3118f14e3b51f6
Andrew Kushnir <akushnir@google.com>
2021-10-14 23:11:57 +0000

    bd0d7c15c1 docs: internationalization topics (#43819)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+56.sha-892158f.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+57.sha-bd0d7c1.with-local-changes/, version);
     }
   }
 }
