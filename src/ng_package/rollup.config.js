8e081a8e5abbf26c4d5aea0fe24ed0cb2eb21b75
Andrew Kushnir <akushnir@google.com>
2021-10-14 17:23:13 +0000

    5cc51880db refactor(router): clean up internal hooks (#43804)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+53.sha-41cf9e9.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+54.sha-5cc5188.with-local-changes/, version);
     }
   }
 }
