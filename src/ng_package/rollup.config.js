d99df760159b7de6668389e7ce2b9d4d8d6ac525
Andrew Kushnir <akushnir@google.com>
2021-10-15 17:34:35 +0000

    426a3ecae7 fix(compiler-cli): updates `ngc` to pass the build when only warnings are emitted (#43673)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-rc.0+8.sha-91e73c4.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-rc.0+11.sha-426a3ec.with-local-changes/, version);
     }
   }
 }
