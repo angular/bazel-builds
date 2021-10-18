d0562cf3e6325881befac76b809964807608d6b9
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:56:43 +0000

    c85bcb0c63 feat(compiler): reference ICU message IDs from their placeholders (#43534)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+71.sha-25b9c8c.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+69.sha-c85bcb0.with-local-changes/, version);
     }
   }
 }
