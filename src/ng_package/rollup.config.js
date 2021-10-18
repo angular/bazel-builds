53079bfa12475e5180d26ad7441d1337ce60649e
Andrew Kushnir <akushnir@google.com>
2021-10-18 20:32:37 +0000

    60f3b33b4b Revert "fix(compiler): support i18n interpolated only attribute bindings (#43815)" (#43882)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+75.sha-c944fee.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+76.sha-60f3b33.with-local-changes/, version);
     }
   }
 }
