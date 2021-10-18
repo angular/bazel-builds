c8630817a054cfd6a78f05680e19d5188d3975d9
Andrew Kushnir <akushnir@google.com>
2021-10-18 22:40:23 +0000

    b489ae5ecd ci: remove browserstack configuration (#43881)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-rc.0+25.sha-fc6519c.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-rc.0+27.sha-b489ae5.with-local-changes/, version);
     }
   }
 }
