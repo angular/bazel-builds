97f3540322d0f88a0a646c26e4adc719beecc692
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:39:22 +0000

    67a7c48683 fix(docs-infra): move security anchors outside callout elements (#43865)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+64.sha-e79e3ca.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+66.sha-67a7c48.with-local-changes/, version);
     }
   }
 }
