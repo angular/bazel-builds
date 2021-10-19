ecfb27d21c7654f30ef0f2b4c4c56e80e57365bc
Andrew Kushnir <akushnir@google.com>
2021-10-19 18:02:08 +0000

    9d9346c57a docs: add docs about build cache (#43843)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-rc.0+39.sha-812a33d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-rc.0+40.sha-9d9346c.with-local-changes/, version);
     }
   }
 }
