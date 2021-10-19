6020c60bb1cd41adb4cc559687c4438c0a3ea530
Andrew Kushnir <akushnir@google.com>
2021-10-19 17:25:36 +0000

    812a33df8c ci: mark core/schematics/test:test and compiler-cli/test:perform_watch test as view engine only (#43862)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-rc.0+27.sha-b489ae5.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-rc.0+39.sha-812a33d.with-local-changes/, version);
     }
   }
 }
