6219476dc82ab8e4bda69e9ec60cde40d92419fd
Andrew Kushnir <akushnir@google.com>
2021-10-19 23:43:58 +0000

    007b96eb2e test(compiler-cli): run i18n extraction tests using Ivy compiler (#43893)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+91.sha-222c5a8.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+93.sha-007b96e.with-local-changes/, version);
     }
   }
 }
