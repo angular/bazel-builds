5afddd6bbc0ff0ac66af4b0c750f03d4e9ce7daa
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-20 20:31:15 +0000

    b8f1f82fe2 docs: clarify meaning of ellipsis in bazel command (#43890)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+96.sha-4c9f35b.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+97.sha-b8f1f82.with-local-changes/, version);
     }
   }
 }
