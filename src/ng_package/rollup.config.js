eaf56c5ebb90eb0a5271ee5ab1b1950ef5383efb
GitHub <noreply@github.com>
2021-10-20 20:08:20 +0000

    e3d72646e0 docs: release notes for the v13.0.0-rc.1 release (#43912)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+94.sha-25c536d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+95.sha-e3d7264.with-local-changes/, version);
     }
   }
 }
