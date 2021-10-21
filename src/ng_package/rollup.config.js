9e600b0ce3c6f52544640c8e2732e98998528578
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 15:01:12 +0000

    974767d428 docs: fix typos in the workspace config guide (#43918)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+99.sha-c5d0b76.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+100.sha-974767d.with-local-changes/, version);
     }
   }
 }
