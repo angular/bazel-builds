acedfbb20c4694b247ca4541c3a51e15c8b5a47b
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 19:15:18 +0000

    b1c028677f refactor(core): made comment structure consistent in index.ts (#43684)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+104.sha-4ed8dc8.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+107.sha-b1c0286.with-local-changes/, version);
     }
   }
 }
