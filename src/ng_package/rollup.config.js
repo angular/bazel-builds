1bc5c7305a89c8ced15b592dd47a30ab520b640e
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 16:45:35 +0000

    cef01b804a ci: update `angular/dev-infra/github-actions` sha (#43904)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.11+1.sha-e09556f.with-local-changes/, version);
+      banner = banner.replace(/12.2.11+3.sha-cef01b8.with-local-changes/, version);
     }
   }
 }
