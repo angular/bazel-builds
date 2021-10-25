228220c22aef9d128b38ee42513a370250f7925d
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 22:18:33 +0000

    0873ae2580 Revert "refactor(migrations): remove no longer accessible migrations" (#43943)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+115.sha-0b4350c.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+116.sha-0873ae2.with-local-changes/, version);
     }
   }
 }
