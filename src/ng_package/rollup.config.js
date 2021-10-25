05195d9bcc800c60131271453f5a21e7795bd5dc
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:16:12 +0000

    8277c38bd8 test(platform-browser): complete component bootstrap before switching to the next test (#43933)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-rc.1+11.sha-97fc9de.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-rc.1+17.sha-8277c38.with-local-changes/, version);
     }
   }
 }
