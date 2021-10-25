ab040ee135ff5041f60eee7e2592f4d5e9a75243
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:14:56 +0000

    6e37c938e2 fix(ngcc): support alternate UMD layout when adding new imports (#43931)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+112.sha-786f02b.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+113.sha-6e37c93.with-local-changes/, version);
     }
   }
 }
