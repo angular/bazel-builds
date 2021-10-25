4151e8ee75b71df53e1c0c6ee27a9a0e567ff370
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 22:24:06 +0000

    940eafa351 docs(localize): fix spelling of documentation link (#43941)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-rc.1+18.sha-087fae5.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-rc.1+20.sha-940eafa.with-local-changes/, version);
     }
   }
 }
