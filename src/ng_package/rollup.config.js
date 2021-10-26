61af065063f9259af5a29c6a0b9af5372f17c168
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 20:42:11 +0000

    5c22dfe861 refactor(forms): update minlength/maxlength to inherit abstractValidator (#43835)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+123.sha-5d66b4a.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+124.sha-5c22dfe.with-local-changes/, version);
     }
   }
 }
