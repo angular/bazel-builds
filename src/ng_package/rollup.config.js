0637772439cf88a0c15946e9b86490510f42e143
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:05:37 +0000

    408e4dafbd fix(docs-infra): search box input line-height (#43983)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+171.sha-a01e02d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+175.sha-408e4da.with-local-changes/, version);
     }
   }
 }
