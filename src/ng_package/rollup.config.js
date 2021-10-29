05e5a1af8eeebf75df6089574de1e6eb15ba11b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 21:41:37 +0000

    6b8cdc9339 docs: update `creating libraries` guide (#43982)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+144.sha-7b35a34.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+145.sha-6b8cdc9.with-local-changes/, version);
     }
   }
 }
