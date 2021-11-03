9f255d9a1d9a230b9bd78adb1211b35c53a86bdc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 23:54:11 +0000

    9741473f04 ci: update aio payloads size (#44046)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0+1.sha-b361242.with-local-changes/, version);
+      banner = banner.replace(/13.0.0+5.sha-9741473.with-local-changes/, version);
     }
   }
 }
