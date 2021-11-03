f7fad670270393d3d2ac67cb18324a521ecdb590
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 17:06:51 +0000

    8d308dc4ec fix(migrations): prevent migrations from updating external templates multiple times (#44013)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0-next.14+194.sha-3574e7d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0-next.14+197.sha-8d308dc.with-local-changes/, version);
     }
   }
 }
