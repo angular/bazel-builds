d886cff01b48927b4bf8f37ff038d062c18ff7c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-04 00:20:55 +0000

    07440752b3 docs: update CHANGELOG with deprecations (#44035)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0+5.sha-9741473.with-local-changes/, version);
+      banner = banner.replace(/13.0.0+6.sha-0744075.with-local-changes/, version);
     }
   }
 }
