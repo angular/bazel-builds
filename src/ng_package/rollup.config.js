07dd36e5af914fe60abaa129f788bfb5799126e6
Andrew Scott <atscott01@gmail.com>
2021-11-04 19:12:07 +0000

    36388b3ab2 build(docs-infra): configure region parser to allow `.mjs` (#44027)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.0+7.sha-b6798f3.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.0+17.sha-36388b3.with-local-changes/, version);
     }
   }
 }
