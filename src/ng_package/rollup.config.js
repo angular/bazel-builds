776a3709fab47ba40efd229ff45031ce30826e30
Andrew Scott <atscott01@gmail.com>
2021-11-04 19:13:51 +0000

    7441747087 docs: update roadmap update date (#44036)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.0+17.sha-36388b3.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.0+18.sha-7441747.with-local-changes/, version);
     }
   }
 }
