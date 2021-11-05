1aab249a8789eba500e51117ee7e0d9861087866
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:38:33 +0000

    bac03a95d7 ci: do not capture nested bazel integration test workspace within Bazel (#44061)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.0+22.sha-523e8fd.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.0+26.sha-bac03a9.with-local-changes/, version);
     }
   }
 }
