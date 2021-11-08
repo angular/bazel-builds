131818196c629b821cfca630cfeffb34da3eab71
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:41:11 +0000

    49964c89e6 docs: add Angular 13 support information (#44098)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.0+46.sha-baab456.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.0+47.sha-49964c8.with-local-changes/, version);
     }
   }
 }
