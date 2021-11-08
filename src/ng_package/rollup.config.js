80d5fa8ed833c8f911a5bf2745dc068c4410a7c5
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:31:03 +0000

    acddb7f1fa build: update dependency vlq to v2 (#43901)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.0+40.sha-9f37914.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.0+42.sha-acddb7f.with-local-changes/, version);
     }
   }
 }
