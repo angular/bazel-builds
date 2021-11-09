9ab52aec3373d94f460ffe393475a9fa99079b82
Andrew Scott <atscott01@gmail.com>
2021-11-09 18:22:51 +0000

    4f8eaac7ff fix(core): do not use Function constructors in development mode to avoid CSP violations (#43587)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.0+47.sha-49964c8.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.0+57.sha-4f8eaac.with-local-changes/, version);
     }
   }
 }
