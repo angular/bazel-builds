b670d416ee6101b4e2ecf0f819905397f6ca854c
Andrew Scott <atscott01@gmail.com>
2021-11-10 01:38:39 +0000

    30a27adf9a fix(platform-browser): use correct parent in animation removeChild callback (#44033)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0+54.sha-5eac434.with-local-changes/, version);
+      banner = banner.replace(/13.0.0+68.sha-30a27ad.with-local-changes/, version);
     }
   }
 }
