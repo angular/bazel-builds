e069ed031dbeca833cfbe5d8abf43bff60cafbdd
Andrew Scott <atscott01@gmail.com>
2021-11-10 17:54:38 +0000

    18cc7c8b04 test(compiler): remove IE11 match in JIT compiler tests (#44122)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.0+68.sha-236bdff.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.0+71.sha-18cc7c8.with-local-changes/, version);
     }
   }
 }
