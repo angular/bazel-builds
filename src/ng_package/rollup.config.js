6b39c3c3ee525db818e58f479422fe85dc0ac6be
Andrew Scott <atscott01@gmail.com>
2021-11-11 00:02:32 +0000

    ca0873fca9 build: update to latest @angular/dev-infra-private to address issues related to verifying environment (#44138)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.0+73.sha-ee2031d.with-local-changes/, version);
+      banner = banner.replace(/13.0.0+78.sha-ca0873f.with-local-changes/, version);
     }
   }
 }
