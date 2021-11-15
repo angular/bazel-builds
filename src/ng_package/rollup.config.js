b446ae30e25832a8499ac3aef61c92c7a860ce8a
Andrew Kushnir <akushnir@google.com>
2021-11-15 19:39:20 +0000

    b7fd9ebf1d ci: remove `cla: yes` from required labels (#44179)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.1+1.sha-4489043.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.1+3.sha-b7fd9eb.with-local-changes/, version);
     }
   }
 }
