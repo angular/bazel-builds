8f7e83970bcbde6c19d6731ad073e513c669c4d2
Andrew Kushnir <akushnir@google.com>
2021-11-16 18:33:55 +0000

    00ae55c3fb docs(animations): improve wording for animation example (#44168)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.1+3.sha-b7fd9eb.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.1+6.sha-00ae55c.with-local-changes/, version);
     }
   }
 }
