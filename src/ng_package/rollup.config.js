5532bfacba418d64ccd59f76de3c189172ace17c
Andrew Kushnir <akushnir@google.com>
2021-11-16 22:26:54 +0000

    158ad09945 docs: Adding type annotation to ngOnInit() to make it consistent with the class structure produced by the CLI (#43579)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.1+6.sha-aa98cf1.with-local-changes/, version);
+      banner = banner.replace(/13.0.1+12.sha-158ad09.with-local-changes/, version);
     }
   }
 }
