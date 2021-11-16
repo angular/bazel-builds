3eb26ba7cc1633e1542344366cdd1e349d0aa091
Andrew Kushnir <akushnir@google.com>
2021-11-16 20:24:10 +0000

    5a24f96792 refactor(elements): remove non-existent schematic collection breaking with `ng add` (#44187)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.1+6.sha-00ae55c.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.1+8.sha-5a24f96.with-local-changes/, version);
     }
   }
 }
