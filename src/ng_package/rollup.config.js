984ea5c9258961ccd87ad69a6c67f1dedb5b173d
Andrew Kushnir <akushnir@google.com>
2021-11-18 19:05:07 +0000

    16d026e499 ci: add dylhunn to review groups for forms. (#44204)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.2+5.sha-96fedd2.with-local-changes/, version);
+      banner = banner.replace(/13.0.2+10.sha-16d026e.with-local-changes/, version);
     }
   }
 }
