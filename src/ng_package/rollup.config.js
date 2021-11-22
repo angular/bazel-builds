25f63a7fc92be9a7e5c0f8b485391244e58f1183
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-22 18:11:38 +0000

    24b635395f fix(ngcc): ensure that ngcc does not write a lock-file into node_modules package directories (#44228)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+28.sha-6214636.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+31.sha-24b6353.with-local-changes/, version);
     }
   }
 }
