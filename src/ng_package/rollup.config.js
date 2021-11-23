2f5e8b5e340d02305b896c072b30adc4c63c8072
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 18:54:59 +0000

    f44cb57c12 fix(router): prevent componentless routes from being detached (#44240)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+34.sha-40eb2f5.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+36.sha-f44cb57.with-local-changes/, version);
     }
   }
 }
