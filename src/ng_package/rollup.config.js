a18b01a7b1ae34fc97fe9d5e0026e58d47d94b47
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:15:07 +0000

    83bbbd3784 docs: document `ng update` support guarantees (#44142)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+50.sha-b06e398.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+49.sha-83bbbd3.with-local-changes/, version);
     }
   }
 }
