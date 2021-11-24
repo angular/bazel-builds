13bbdeff4d0e6264328f45118d18387c5e62168f
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:58:36 +0000

    b31973c176 test: remove Ivy/ViewEngine switch helpers and obsolete tests (#44120)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+52.sha-d8e6577.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+53.sha-b31973c.with-local-changes/, version);
     }
   }
 }
