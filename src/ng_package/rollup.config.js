88493ffe4b25c6bf4b545cfaa5776efcd3fd31d2
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 17:20:30 +0000

    2bf131a8a0 fix(docs-infra): move aio footer inside mat-sidenav-container (#44236)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+40.sha-414d5fe.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+43.sha-2bf131a.with-local-changes/, version);
     }
   }
 }
