ca8408eda5650c6b6fff297bb95966b019b6449f
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:14:05 +0000

    6ae38584b0 docs(core): improve viewEncapsulation documentation (#44151)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+43.sha-2bf131a.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+44.sha-6ae3858.with-local-changes/, version);
     }
   }
 }
