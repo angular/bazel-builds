73df568a8a6fd3b9ec943d8d24dcd9b7052509ab
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:38:22 +0000

    708eb8c8d6 docs: fix instructions for debugging a karma test (#44244)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+56.sha-47f1c0f.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+66.sha-708eb8c.with-local-changes/, version);
     }
   }
 }
