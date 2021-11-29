ff19ea6d5fc8ef79ef6fbc28bc19e067eb240b04
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:38:13 +0000

    cabc1786de fix(language-service): Correctly parse inputs and selectors with dollar signs (#44268)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.2+48.sha-dcea04b.with-local-changes/, version);
+      banner = banner.replace(/13.0.2+62.sha-cabc178.with-local-changes/, version);
     }
   }
 }
