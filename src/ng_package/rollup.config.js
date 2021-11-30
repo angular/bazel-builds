207306838239a8f860277a8e225e29c9beaa6531
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:40:07 +0000

    53bdf11fe1 docs(forms): add an example for async validator token (#44301) to be consistent with the validator token, have the example code for the async validator token.

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.2+72.sha-c92095c.with-local-changes/, version);
+      banner = banner.replace(/13.0.2+76.sha-53bdf11.with-local-changes/, version);
     }
   }
 }
