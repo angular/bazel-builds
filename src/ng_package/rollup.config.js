5d8e0c30b6fed85ab0fb9764644ac57d7b987be2
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:31:06 +0000

    ca5b9b558c refactor(forms): eagerly initialize data members (#44292)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+79.sha-95ed446.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+81.sha-ca5b9b5.with-local-changes/, version);
     }
   }
 }
