8849e97acf1d9b0cc9889e19b64f096bd073df88
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:34:29 +0000

    f058109284 release: bump angular-in-web-memory-api package to 0.13.0 (#44313)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+81.sha-ca5b9b5.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+80.sha-f058109.with-local-changes/, version);
     }
   }
 }
