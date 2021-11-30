5f8b0cf6cf8c025100c26c221dd513a75100b5a6
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 22:02:15 +0000

    3f6d8f38de refactor(forms): minor refactoring of `SelectMultipleControlValueAccessor` (#44261)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+85.sha-5124c06.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+86.sha-3f6d8f3.with-local-changes/, version);
     }
   }
 }
