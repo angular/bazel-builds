f3a49a1b86a02496c5dc53967e75d63647f91017
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 17:12:54 +0000

    0fcf18d5f4 ci: increase jasmine default timeout for saucelabs legacy tests (#44281)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+67.sha-3aafa76.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+74.sha-0fcf18d.with-local-changes/, version);
     }
   }
 }
