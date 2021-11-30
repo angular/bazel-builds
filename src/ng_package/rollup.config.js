466c36a7a839e1540e9796e451f91d15c453ecbe
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:06:16 +0000

    95ed446009 refactor(docs-infra): clean up entryComponents usages (#44257)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+78.sha-5dff077.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+79.sha-95ed446.with-local-changes/, version);
     }
   }
 }
