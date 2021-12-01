3f26135b53f45c2235a7ed48bd2ab93b27f8166c
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 20:16:07 +0000

    f7ea5249de refactor(docs-infra): replace use of deprecated `Compiler` with `createNgModuleRef()` (#44293)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+90.sha-4192ab4.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+92.sha-f7ea524.with-local-changes/, version);
     }
   }
 }
