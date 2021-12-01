2bb37b4c2886de9c25d2d42721e40634a4ba9d35
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 18:55:06 +0000

    1922032786 refactor(compiler-cli): dismantle View Engine implementation of ngc (#44269)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+86.sha-3f6d8f3.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+89.sha-1922032.with-local-changes/, version);
     }
   }
 }
