660126dd7be1ae67c9471c6f98a05ac97eb65028
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 23:37:59 +0000

    f3898f8d84 fix(docs-infra): prevent automatic linking of 'json' (#44294)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+95.sha-6769df7.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.3+1.sha-f3898f8.with-local-changes/, version);
     }
   }
 }
