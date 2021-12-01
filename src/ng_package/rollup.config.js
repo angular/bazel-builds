4cc7593eaf584b688a539c20f824b4b5936af83d
GitHub <noreply@github.com>
2021-12-01 23:29:21 +0000

    4cf1a81265 release: cut the v12.2.14 release (#44341)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.13+9.sha-a1bd9d7.with-local-changes/, version);
+      banner = banner.replace(/12.2.13+10.sha-4cf1a81.with-local-changes/, version);
     }
   }
 }
