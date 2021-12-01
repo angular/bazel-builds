12171703a8e509ede095692b68aabc8b40857d50
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 21:31:21 +0000

    f1f6e7104f docs: release notes for the v13.0.3 release (#44333)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.2+93.sha-e65a245.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.2+94.sha-f1f6e71.with-local-changes/, version);
     }
   }
 }
