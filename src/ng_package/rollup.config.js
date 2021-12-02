f4924dbb8cf90bfec6090f6497fcda306fe6f17b
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:04:44 +0000

    360867b654 build: update dependency tsec to ^0.2.0 (#44274)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.3+1.sha-f3898f8.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.3+2.sha-360867b.with-local-changes/, version);
     }
   }
 }
