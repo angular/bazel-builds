a285d01097de2fc52a7d5034fe628718348c1f88
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:42:27 +0000

    7fb4309cca perf(common): code size reduction of `ngFor` directive (#44315)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.3+3.sha-5624dc6.with-local-changes/, version);
+      banner = banner.replace(/13.0.3+4.sha-7fb4309.with-local-changes/, version);
     }
   }
 }
