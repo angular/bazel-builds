53a60eb41fa7a530a23503efddfbeb2619088302
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:51:52 +0000

    b6554d75cd fix(ngcc): correctly resolve UMD dependencies (#44382)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.13+10.sha-4cf1a81.with-local-changes/, version);
+      banner = banner.replace(/12.2.14+1.sha-b6554d7.with-local-changes/, version);
     }
   }
 }
