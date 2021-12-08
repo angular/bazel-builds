318facfb56c022cfdcfa640a170cebc009032efe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-08 18:58:17 +0000

    2664bc2b3e test: switch integration tests from puppeteer/webdriver-manager to Bazel-managed chromium (#44238)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.3+16.sha-cdf50ff.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.3+23.sha-2664bc2.with-local-changes/, version);
     }
   }
 }
