67ce96214604b88cf199a23474863ecabaaebeec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-08 18:40:38 +0000

    86843f8916 refactor(animations): remove unnecessary code (#44378)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.0.3+15.sha-763212d.with-local-changes/, version);
+      banner = banner.replace(/13.0.3+17.sha-86843f8.with-local-changes/, version);
     }
   }
 }
