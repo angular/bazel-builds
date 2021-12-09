a8fbeaea9e9768963ede6f0e8033cea9dcf4fc4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 18:29:50 +0000

    79baf7a87a Revert "release: cut the v13.2.0-next.0 release (#44421)"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0-next.3+26.sha-e3f4360.with-local-changes/, version);
+      banner = banner.replace(/13.1.0-next.3+27.sha-79baf7a.with-local-changes/, version);
     }
   }
 }
