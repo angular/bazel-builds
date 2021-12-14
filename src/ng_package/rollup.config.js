f74b86648fb561e4d26ab1d139cfe4d6f4877059
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:25:55 +0000

    65717999c8 refactor(forms): Make the minimum changes to the forms unit tests in order to support the typed forms PR (#43834). (#44451)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+13.sha-ab2248e.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+16.sha-6571799.with-local-changes/, version);
     }
   }
 }
