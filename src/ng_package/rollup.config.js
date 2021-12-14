cca14dd14895b62b7f9b6ab1ecb8e646e53a9bc3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:23:10 +0000

    38ddae1c2e fix(animations): don't consume instructions for animateChild (#44357)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+11.sha-c68385a.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+9.sha-38ddae1.with-local-changes/, version);
     }
   }
 }
