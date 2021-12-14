a86000aadd6906db9f52927f415aa0284939acfd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:29:13 +0000

    d640fa7062 docs: Fixed typo 'test' => 'text' (#44464) PR Close #44464

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+18.sha-f56ac0f.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+20.sha-d640fa7.with-local-changes/, version);
     }
   }
 }
