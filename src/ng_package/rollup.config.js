1799541374f30dbb0482bc6e2ebacac4409343ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:22:09 +0000

    030747a688 docs: update Aristeidis Bampakos bio (#44410) PR Close #44410

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+8.sha-bab7ed3.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+10.sha-030747a.with-local-changes/, version);
     }
   }
 }
