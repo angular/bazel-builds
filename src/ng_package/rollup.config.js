4c4f2eeb2e33a71ef1b5fade34ae7f49a289ea45
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:23:44 +0000

    12f7c85fd0 docs: fix typo (#44427)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+9.sha-38ddae1.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+12.sha-12f7c85.with-local-changes/, version);
     }
   }
 }
