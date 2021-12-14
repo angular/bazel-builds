472f73f506ad11c3ca41133a684abda256610596
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:09 +0000

    e4839939b0 test(ngcc): don't use the workspace typescript version to generate integration test fixtures (#44448)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+12.sha-12f7c85.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+14.sha-e483993.with-local-changes/, version);
     }
   }
 }
