ccd276ef5c9b8d6336bd56a043effcf92c5f6f67
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:21 +0000

    ab2248e773 docs(docs-infra): change variable names in route guards examples (#44444)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+14.sha-e483993.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+13.sha-ab2248e.with-local-changes/, version);
     }
   }
 }
