330081ddc0f30e39b6087802220e65005beff61f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:29:51 +0000

    f394831ec4 refactor(animations): remove the unused enter and leave selectors (#44460)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+20.sha-d640fa7.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+19.sha-f394831.with-local-changes/, version);
     }
   }
 }
