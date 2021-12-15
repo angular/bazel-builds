0b0a577b340c03ea58cb342ad77cd2d4bae99def
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 17:42:50 +0000

    6a0dadf46f fix(forms): `_reduceValue` arrow function now has correct types. (#44483)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.0+21.sha-093fd39.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.0+24.sha-6a0dadf.with-local-changes/, version);
     }
   }
 }
