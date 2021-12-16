b007288e0d7bba1922e885e049da07a8d6575aa1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-16 19:46:46 +0000

    beb18286aa docs: remove recent departures from contributors page (#44514)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.0+27.sha-0115e2b.with-local-changes/, version);
+      banner = banner.replace(/13.1.1+1.sha-beb1828.with-local-changes/, version);
     }
   }
 }
