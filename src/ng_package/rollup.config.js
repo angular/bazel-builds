90f9e0e8dd95d5111c1d8ecccfe7f159b4160ad9
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:26:07 +0000

    756761d534 fix(language-service): revert the test files for Ivy (#44528)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+13.sha-7cb2999.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+12.sha-756761d.with-local-changes/, version);
     }
   }
 }
