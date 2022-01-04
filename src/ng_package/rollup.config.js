5e9a66e755e250066fa96b56e2228646c09638f7
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:32:33 +0000

    bfd94b39d3 docs: Replace OnInit with AfterViewInit  (#44485)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+16.sha-9d2401d.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+24.sha-bfd94b3.with-local-changes/, version);
     }
   }
 }
