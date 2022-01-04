2a05f87a5da225ed478635aa5b0af3d3e5748d2a
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:29:31 +0000

    67c81ccef0 refactor(docs-infra): improve aio-footer's layout (#44532)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+14.sha-f7aa937.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+15.sha-67c81cc.with-local-changes/, version);
     }
   }
 }
