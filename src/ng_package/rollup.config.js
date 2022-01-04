7d792bac25fb9765199714bfb34652ac49b13497
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:22:43 +0000

    62a5027482 docs: fixed meetup URL (#44585)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+8.sha-79d1afa.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+9.sha-62a5027.with-local-changes/, version);
     }
   }
 }
