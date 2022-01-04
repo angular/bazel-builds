dd2f51a1c1f7ecac7a7be7645dbfa773a9369e74
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:32:37 +0000

    f0b4c709c9 docs: fix code sample indentation (#44517)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+24.sha-bfd94b3.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+26.sha-f0b4c70.with-local-changes/, version);
     }
   }
 }
