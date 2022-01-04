fb176099478117520e8be261aff1759d143eb954
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:29:20 +0000

    f7aa937cac fix(forms): Make some minor fixups for forward-compatibility with typed forms. (#44540)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+12.sha-756761d.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+14.sha-f7aa937.with-local-changes/, version);
     }
   }
 }
