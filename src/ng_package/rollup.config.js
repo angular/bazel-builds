62bf0028ad5a735dd1b91c66a106e3d9a66c3c27
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:20:11 +0000

    a01c077a67 docs: remove Angular 10 from support table (#44611)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+4.sha-af512de.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+5.sha-a01c077.with-local-changes/, version);
     }
   }
 }
