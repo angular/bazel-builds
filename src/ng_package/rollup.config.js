76d5652515d29eab97b19fd7ade90bc846fb824d
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:37:44 +0000

    a864d1c926 fix(docs-infra): adjust code link styling height (#44376)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+27.sha-5ae1030.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+29.sha-a864d1c.with-local-changes/, version);
     }
   }
 }
