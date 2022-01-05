e3b1785e6d9b9f53481a88990c6a25bdf48f1702
Andrew Scott <atscott01@gmail.com>
2022-01-05 19:00:26 +0000

    0daa9bfd26 refactor(forms): make validators code compatible with property renaming (#44500)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.1+48.sha-426b6f9.with-local-changes/, version);
+      banner = banner.replace(/13.1.1+49.sha-0daa9bf.with-local-changes/, version);
     }
   }
 }
