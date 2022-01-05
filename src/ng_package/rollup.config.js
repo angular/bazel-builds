7b3fc0de2684b4b4cdedf31c8ce135eb239cc123
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:13:50 +0000

    0ac4c0b406 refactor(compiler): remove output AST functions (#44411)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.1+26.sha-25aef62.with-local-changes/, version);
+      banner = banner.replace(/13.1.1+46.sha-0ac4c0b.with-local-changes/, version);
     }
   }
 }
