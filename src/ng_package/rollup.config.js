01c522b68e41d991818bfadd7c17ef8c2aa848a7
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:36:41 +0000

    2d14724965 docs(common): url link format updated (#44638)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+59.sha-12e0424.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+61.sha-2d14724.with-local-changes/, version);
     }
   }
 }
