49f1e3547b994b1d2ab368cd4c218fcf5978f318
Andrew Scott <atscott01@gmail.com>
2022-01-05 18:02:49 +0000

    7278a9c584 docs(animations): fix filter-animations docregion (#44544)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+50.sha-d5ced77.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+51.sha-7278a9c.with-local-changes/, version);
     }
   }
 }
