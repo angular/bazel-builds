857ede2e25c5f8fa4f6657e0122c014944963bf8
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:09:05 +0000

    f2b858d24f refactor(animations): add unit test for leaving animated child (#44489)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+29.sha-a864d1c.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+31.sha-f2b858d.with-local-changes/, version);
     }
   }
 }
