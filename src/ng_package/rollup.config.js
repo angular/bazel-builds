437dc197980ebcd5a142ceb6eb6ccfef9c0fdf59
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:52:03 +0000

    7f51f1dcae docs(docs-infra): improve the content of attribute directives (#44628)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+62.sha-b69a514.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+65.sha-7f51f1d.with-local-changes/, version);
     }
   }
 }
