7386383d69ddd532fa39c94958bf5333c1beafdc
Andrew Scott <atscott01@gmail.com>
2022-01-06 00:39:56 +0000

    b69a5148aa docs(animations): fix links to the AnimationBuilder.build method (#44594)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+63.sha-ea91edb.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+62.sha-b69a514.with-local-changes/, version);
     }
   }
 }
