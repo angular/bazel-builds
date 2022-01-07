7bef899f39c35954ea31eb6244c6c2b8f3526ef5
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:28:24 +0000

    eeaabe7fbf fix(compiler-cli): incorrectly interpreting $any calls with a property read (#44657)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+80.sha-de93b6e.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+84.sha-eeaabe7.with-local-changes/, version);
     }
   }
 }
