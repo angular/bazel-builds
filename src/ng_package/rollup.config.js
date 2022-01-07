6df84cb63ac6c1bd024ced5dd24357f5a7339740
Andrew Scott <atscott01@gmail.com>
2022-01-07 22:32:25 +0000

    abc217b28e fix(animations): retain triggers values for moved tracked list items (#44578)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.1+79.sha-919f8ae.with-local-changes/, version);
+      banner = banner.replace(/13.1.1+80.sha-abc217b.with-local-changes/, version);
     }
   }
 }
