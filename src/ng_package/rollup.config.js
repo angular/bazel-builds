cb54bf1d5534edc51284a21b3834458cc6656602
Andrew Kushnir <akushnir@google.com>
2022-01-07 00:58:59 +0000

    0cf5501d5c build: use correct target name (#44651)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+69.sha-67df935.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+77.sha-0cf5501.with-local-changes/, version);
     }
   }
 }
