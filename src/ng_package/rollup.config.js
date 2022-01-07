fd93f14a0dcce44237c9117df27ad94cb79e9864
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:27:15 +0000

    b184f0aa24 refactor: fix various typos across different packages (#44523)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+79.sha-21f8d12.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+82.sha-b184f0a.with-local-changes/, version);
     }
   }
 }
