407046700ca9ffb97fc78769fa6a2c50c1e39c2f
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:00:36 +0000

    d252cffc61 refactor(forms): use shared RuntimeError class (#44398) (#44652)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.1+70.sha-57d5f95.with-local-changes/, version);
+      banner = banner.replace(/13.1.1+73.sha-d252cff.with-local-changes/, version);
     }
   }
 }
