ec2fdcfc8e88708e33d1a7a669bc1f4903bf51e4
Andrew Scott <atscott01@gmail.com>
2022-01-10 21:41:32 +0000

    af2a1317cb feat(language-service): support completions for animation (#44630)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+92.sha-1a91218.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+94.sha-af2a131.with-local-changes/, version);
     }
   }
 }
