232680c208c535257969af8845f02879eca577ea
Andrew Scott <atscott01@gmail.com>
2022-01-10 21:37:45 +0000

    1a9121826e fix(compiler-cli): enable narrowing of using type guard methods (#44447)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+91.sha-ebf8446.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.1+92.sha-1a91218.with-local-changes/, version);
     }
   }
 }
