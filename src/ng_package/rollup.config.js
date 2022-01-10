b59970bc8b335675788f770c01e08817bb5b8de6
Andrew Scott <atscott01@gmail.com>
2022-01-10 20:14:42 +0000

    c375e5d22f docs: Updated described version number  (#44365)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.1+80.sha-abc217b.with-local-changes/, version);
+      banner = banner.replace(/13.1.1+84.sha-c375e5d.with-local-changes/, version);
     }
   }
 }
