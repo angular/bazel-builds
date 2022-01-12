d7cdbed98dc53e8c05d00947af4cc2bc4644ea42
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:57:31 +0000

    c7b210d484 refactor(forms): update email validator to inherit abstractValidator (#44545)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+3.sha-57d0ca1.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+2.sha-c7b210d.with-local-changes/, version);
     }
   }
 }
