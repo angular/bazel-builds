1624e685eaa03305db8aba461371ed06968f1a39
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:56:29 +0000

    c888dfdea0 docs: add Decoded Frontend YT channel to resources list (#44278)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.1+109.sha-42ce3ac.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+1.sha-c888dfd.with-local-changes/, version);
     }
   }
 }
