f948542cd03bd0aaf82975f1738e87ee89ddc042
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:18:03 +0000

    c2da290659 feat(docs-infra): Add a new processor for `@overriddenImplementation`. (#44689)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+10.sha-f06b0d9.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+13.sha-c2da290.with-local-changes/, version);
     }
   }
 }
