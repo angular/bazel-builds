e49eb1693675add1f0f2c5bf12f76129a301f567
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:43:43 +0000

    3c0446cd2d build: update shelljs dependencies to "^0.8.5" (#44697)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+5.sha-432e518.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+7.sha-3c0446c.with-local-changes/, version);
     }
   }
 }
