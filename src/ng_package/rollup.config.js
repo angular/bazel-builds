b02b6eebd4364d5c3c4ba6b9b948882b052b5cb3
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:20:03 +0000

    af0a152a2c fix(animations): apply setStyles to only rootTimelines (#44515)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.2+10.sha-44ff4fb.with-local-changes/, version);
+      banner = banner.replace(/13.1.2+12.sha-af0a152.with-local-changes/, version);
     }
   }
 }
