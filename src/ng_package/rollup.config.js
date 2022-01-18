a48131fdd1b8530d2f9741974a4fa839bddbd346
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 23:08:33 +0000

    cd5200ea68 refactor(forms): Move FormControl to an overridden exported constructor. (#44316)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+18.sha-4307b82.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+22.sha-cd5200e.with-local-changes/, version);
     }
   }
 }
