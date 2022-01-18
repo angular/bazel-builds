a87892bb05c7bc6172588ff89fb527b4a6d95f51
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 18:47:34 +0000

    4307b82058 refactor(common): make the error messages tree shakable (#44663)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+16.sha-35345b6.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+18.sha-4307b82.with-local-changes/, version);
     }
   }
 }
