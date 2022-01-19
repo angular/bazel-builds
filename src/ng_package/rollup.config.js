b5e2462ff6249ef43ab200870af0a456d422c42b
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 18:00:31 +0000

    4e95a316ce docs: deprecate unused config options from the `CompilerOptions` interface (#44749)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+29.sha-2b01e17.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+32.sha-4e95a31.with-local-changes/, version);
     }
   }
 }
