6d7a41e314e12644f9e0cf6a3ce35e81953ae8e9
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 00:35:24 +0000

    41b8ecc9b8 refactor(forms): Make a couple small cleanups encountered while rebasing typed forms. (#44748)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+23.sha-ece530f.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+24.sha-41b8ecc.with-local-changes/, version);
     }
   }
 }
