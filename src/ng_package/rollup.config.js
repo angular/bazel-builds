b782ea932905f6a1d18ff8a45a3c832dc7ed1a46
Dylan Hunn <dylhunn@gmail.com>
2022-01-20 01:03:48 +0000

    ed21f5c753 feat(common): add injector input to ngTemplateOutlet (#44761)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+34.sha-ec10570.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+35.sha-ed21f5c.with-local-changes/, version);
     }
   }
 }
