3858629220e13370ec730785e17b4c7ca01e4109
Andrew Kushnir <akushnir@google.com>
2022-01-20 22:20:25 +0000

    100091ebf0 fix(compiler-cli): remove leftover `_extendedTemplateDiagnostics` requirements (#44777)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-rc.0+2.sha-b7ff6f0.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-rc.0+3.sha-100091e.with-local-changes/, version);
     }
   }
 }
