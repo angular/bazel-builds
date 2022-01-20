b0c2c6ae2ec797dd5c8b15771130780747bcc336
Dylan Hunn <dylhunn@gmail.com>
2022-01-20 01:04:04 +0000

    d509a0d75f fix(docs-infra): fix date parsing in a flaky test (#44763)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.2+22.sha-7cb3b78.with-local-changes/, version);
+      banner = banner.replace(/13.1.3+1.sha-d509a0d.with-local-changes/, version);
     }
   }
 }
