b1e8b120f3eb1f61419436094ea42e001f18e3b6
Andrew Kushnir <akushnir@google.com>
2022-01-20 19:32:50 +0000

    b7ff6f06de refactor(compiler): pass rootDir to tsickle (#44768)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-rc.0+1.sha-6b75263.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-rc.0+2.sha-b7ff6f0.with-local-changes/, version);
     }
   }
 }
