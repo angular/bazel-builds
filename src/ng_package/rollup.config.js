0a048efdfdf6b24c5b75a641754df4cc27111465
Andrew Kushnir <akushnir@google.com>
2022-01-21 21:27:38 +0000

    627e807eb8 fix(compiler): properly compile DI factories when coverage reporting is enabled (#44732)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+45.sha-e2920d9.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+46.sha-627e807.with-local-changes/, version);
     }
   }
 }
