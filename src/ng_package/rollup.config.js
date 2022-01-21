afb427de7261582a196eb6927b9e0b93361fc990
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:39:29 +0000

    e2920d98aa build: update all non-major dependencies (#44713)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+44.sha-d8bff1a.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+45.sha-e2920d9.with-local-changes/, version);
     }
   }
 }
