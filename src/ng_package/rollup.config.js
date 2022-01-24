c04b7185b0a9975872b1610c24b705c694b44966
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:09:34 +0000

    c9fcc4f617 test(animations): Add bundling symbol test for animations package (#44809)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+59.sha-531d1cf.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+63.sha-c9fcc4f.with-local-changes/, version);
     }
   }
 }
