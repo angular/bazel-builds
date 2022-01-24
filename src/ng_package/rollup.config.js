18d675b5cdbbf093121c7e80661a8d7f23b689c3
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:07:20 +0000

    531d1cf9a3 refactor(forms): update required validator and checkbox validator to inherit abstractValidator (#44162)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+62.sha-ec0a0e0.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+59.sha-531d1cf.with-local-changes/, version);
     }
   }
 }
