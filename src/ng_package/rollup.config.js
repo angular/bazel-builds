f09ce548598f486e02e406e04cedfe67f9f22e33
Andrew Kushnir <akushnir@google.com>
2022-01-24 20:39:16 +0000

    a4aa9b3c28 Revert "feat(core): allow for injector to be specified when creating an embedded view (#44666)" (#44807)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-rc.1+7.sha-94bfcdd.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-rc.1+9.sha-a4aa9b3.with-local-changes/, version);
     }
   }
 }
