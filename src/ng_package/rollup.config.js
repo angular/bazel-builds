b5b458912d72cccb2048a9560ed19a84cfca1f56
Andrew Kushnir <akushnir@google.com>
2022-01-24 19:03:52 +0000

    a52685bdaf fix(core): error if NgZone.isInAngularZone is called with a noop zone (#44800)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.3+9.sha-45503b4.with-local-changes/, version);
+      banner = banner.replace(/13.1.3+13.sha-a52685b.with-local-changes/, version);
     }
   }
 }
