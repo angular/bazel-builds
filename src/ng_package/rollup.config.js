33effd8ba3981a5b9f8bf26c221b26736ff9e2e1
Andrew Kushnir <akushnir@google.com>
2022-01-24 18:58:29 +0000

    ec2e6f6a3f fix(compiler): correct spans when parsing bindings with comments (#44785)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+49.sha-25a83eb.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+54.sha-ec2e6f6.with-local-changes/, version);
     }
   }
 }
