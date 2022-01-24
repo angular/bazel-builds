1157b228e9361ea8c09d71e700be2e2ec3d88f3c
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:07:18 +0000

    ec0a0e0669 feat(service-worker): add `cacheOpaqueResponses` option for data-groups (#44723)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+58.sha-a534a78.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-next.2+62.sha-ec0a0e0.with-local-changes/, version);
     }
   }
 }
