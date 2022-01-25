734fc91394872c9d6bad46469d39d1b904ab70ec
Andrew Kushnir <akushnir@google.com>
2022-01-25 17:40:21 +0000

    02d3d65203 build(docs-infra): update `dgeni-packages` to v0.29.3 (#44810)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.1.3+27.sha-dd7364f.with-local-changes/, version);
+      banner = banner.replace(/13.1.3+29.sha-02d3d65.with-local-changes/, version);
     }
   }
 }
