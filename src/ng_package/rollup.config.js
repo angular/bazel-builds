7f4896aefe99dd822385f1633848c4587ac4c7ab
Andrew Kushnir <akushnir@google.com>
2022-01-26 20:51:37 +0000

    e46ac81f54 docs: cleanup CHANGELOG.md file (#44841)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-next.2+81.sha-04eec90.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1.sha-e46ac81.with-local-changes/, version);
     }
   }
 }
