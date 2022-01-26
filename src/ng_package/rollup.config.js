fd1f4829ff688cc2b25d7e5759657da749bf1621
Andrew Kushnir <akushnir@google.com>
2022-01-26 21:09:31 +0000

    112a6cdec3 test(animations): Add bundling size regression test for animations package (#44826)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1.sha-e46ac81.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+2.sha-112a6cd.with-local-changes/, version);
     }
   }
 }
