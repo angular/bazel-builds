62a1cbc9b51c204f15c3a4114ba9f874ff3e3af6
Andrew Kushnir <akushnir@google.com>
2022-01-26 01:19:57 +0000

    a38ae0aefa refactor(forms): Temporarily disable the typed forms migration. (#44828)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0-rc.1+25.sha-fe3e4d6.with-local-changes/, version);
+      banner = banner.replace(/13.2.0-rc.1+26.sha-a38ae0a.with-local-changes/, version);
     }
   }
 }
