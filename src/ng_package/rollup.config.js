f433af1a96d70d5a57e5087f07dbd338553d348f
Joey Perrott <josephperrott@gmail.com>
2022-01-26 22:50:24 +0000

    c020f60878 ci: reenable CI checks for commit message validation and rebasing on target branch (#44845)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1015.sha-8fb3aed.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1016.sha-c020f60.with-local-changes/, version);
     }
   }
 }
