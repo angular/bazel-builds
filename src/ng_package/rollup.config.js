1a4d7bf1755534b753bc600a758945e5275175e3
Andrew Kushnir <akushnir@google.com>
2022-01-27 17:40:28 +0000

    df50af2bc5 ci: only build test targets for components repo unit test job (#44832)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1020.sha-e9124b4.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1022.sha-df50af2.with-local-changes/, version);
     }
   }
 }
