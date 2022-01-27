fc4336ac807c90f5b6483dfec51e0f7f0bf9fad9
Andrew Kushnir <akushnir@google.com>
2022-01-27 17:35:34 +0000

    eb11bcb5e4 build: update dependency globby to v13 (#44801)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1017.sha-e5e8161.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1019.sha-eb11bcb.with-local-changes/, version);
     }
   }
 }
