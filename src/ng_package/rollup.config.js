9ce4244766d699c0e0bd7b78c0ba9cc2552139cb
Andrew Kushnir <akushnir@google.com>
2022-01-27 01:53:42 +0000

    a03f80a409 build: update dependency rules_pkg to v0.6.0 (#44818)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1016.sha-c020f60.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1018.sha-a03f80a.with-local-changes/, version);
     }
   }
 }
