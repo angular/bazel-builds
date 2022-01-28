f62a10aae7e1108dd507c6dc40537e0f9efd23ee
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 16:12:39 +0000

    07f827eec3 docs(core): edit and add docs for ng-container (#44762)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1029.sha-910de8b.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1030.sha-07f827e.with-local-changes/, version);
     }
   }
 }
