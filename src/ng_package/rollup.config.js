6446b8879f20a3b741c2d2a17cee8efca4fb0b7d
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 18:19:11 +0000

    9229511b1d build: bump version for `@angular/benchpress` to v0.2.2 (#44879)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1036.sha-c2e09e0.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1037.sha-9229511.with-local-changes/, version);
     }
   }
 }
