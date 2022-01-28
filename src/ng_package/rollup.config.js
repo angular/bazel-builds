d9232697debbca45b67b49ca46bd52b02b63d623
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 16:24:44 +0000

    c9abf34d88 docs: fix GitHub broken link in update guide (#44875)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0+10.sha-f6c2934.with-local-changes/, version);
+      banner = banner.replace(/13.2.0+13.sha-c9abf34.with-local-changes/, version);
     }
   }
 }
