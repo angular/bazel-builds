a780526fdfa2f01ce4e7a9669f8b3b89ddcffc77
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 22:15:19 +0000

    6b7fffcbeb fix(forms): Update the typed forms migration schematic to find all files. (#44881)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0+18.sha-641f27a.with-local-changes/, version);
+      banner = banner.replace(/13.2.0+19.sha-6b7fffc.with-local-changes/, version);
     }
   }
 }
