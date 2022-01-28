43d15e625d7de993cd8571fa763c68c3616f7531
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 17:54:01 +0000

    a8fca09371 docs: fix typo in update guide (#44853)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1034.sha-f490c2d.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1035.sha-a8fca09.with-local-changes/, version);
     }
   }
 }
