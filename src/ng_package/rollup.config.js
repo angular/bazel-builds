aaf3cfef32a0c501b93cf5cfc5f26cd88c544ff7
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:01:54 +0000

    3f0a2d3120 fix(devtools): show date type property value in preview (#44864)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1044.sha-b1b4547.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1045.sha-3f0a2d3.with-local-changes/, version);
     }
   }
 }
