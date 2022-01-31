54f03e4d1df1c6ba585b31274bc8705db1ac9263
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:47:13 +0000

    7155e9e5e2 build: fix `update_all_goldens.js` to use ESM. (#44916)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1060.sha-4c833955.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1061.sha-7155e9e.with-local-changes/, version);
     }
   }
 }
