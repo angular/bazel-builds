93e5c874c2dcbd1854bee431548fbe1901f177ed
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:17:37 +0000

    a8686c64b4 build: update dependency @types/chrome to ^0.0.177 (#44889)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1046.sha-e62f010.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1047.sha-a8686c6.with-local-changes/, version);
     }
   }
 }
