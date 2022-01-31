4e7b03fcfa3b85c4f6b05900d0bcf35f2efb7065
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:55:37 +0000

    5686f68fde fix(core): Add back support for namespace URIs in createElement of dom renderer (#44914)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1066.sha-9eff890.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1067.sha-5686f68.with-local-changes/, version);
     }
   }
 }
