68b215134180c91bc2f8283b17d406e24388986d
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:23:29 +0000

    3d07d1f772 fix(docs-infra): make it possible to see whole notification text on smaller screens (#44900)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1049.sha-390a653.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1051.sha-3d07d1f.with-local-changes/, version);
     }
   }
 }
