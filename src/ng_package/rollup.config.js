47432caa7755cfce00d5be115a9dfc6db90b3723
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:48:25 +0000

    f0d572eebf docs(core): showcase host listener support for specific keys in events (#44833)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1054.sha-7a81481.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1055.sha-f0d572e.with-local-changes/, version);
     }
   }
 }
