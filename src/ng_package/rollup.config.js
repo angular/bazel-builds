4bcd5a8a973df81d00851475abbb1a3280941272
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:48:41 +0000

    8fb2e3b71f docs: Update deployment docs for Vercel (#44904) PR Close #44904

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0+28.sha-07185f4.with-local-changes/, version);
+      banner = banner.replace(/13.2.0+34.sha-8fb2e3b.with-local-changes/, version);
     }
   }
 }
