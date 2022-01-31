56daa26308120b2046a05fb1b0e0d59b4c037040
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:56:26 +0000

    cec158b23a refactor(compiler-cli): remove unused `canonical-path` dependency (#44918)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1067.sha-5686f68.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1068.sha-cec158b.with-local-changes/, version);
     }
   }
 }
