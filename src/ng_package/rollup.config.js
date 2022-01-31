250406cafbf80c1b459a686bddfc07aacbc45fa1
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:49:35 +0000

    366e424a73 fix(compiler-cli): enable nullish coalescing check only with `strictNullChecks` (#44862)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1055.sha-f0d572e.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1057.sha-366e424.with-local-changes/, version);
     }
   }
 }
