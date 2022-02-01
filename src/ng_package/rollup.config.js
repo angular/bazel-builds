da1c803b87d5f0fc132348343778283c0bc12138
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 18:33:12 +0000

    fdfcef5a0a build: enable useUnknownInCatchVariables (#44679)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1072.sha-bc61cc2.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1073.sha-fdfcef5.with-local-changes/, version);
     }
   }
 }
