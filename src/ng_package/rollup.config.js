8c7de3566779d863187e1311332651fa774ee623
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 19:32:56 +0000

    5d7ffd5c0c docs: correct the demo code of NgZone.run (#44733)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1075.sha-e0a340e.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1077.sha-5d7ffd5.with-local-changes/, version);
     }
   }
 }
