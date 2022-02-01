3aad8e3f6419eacbdb85bc200a5e4762c4dbc801
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 19:34:20 +0000

    1cc1588c29 docs: fix incorrect title and tooltip in navigation (#44710)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0+41.sha-2916ac9.with-local-changes/, version);
+      banner = banner.replace(/13.2.0+48.sha-1cc1588.with-local-changes/, version);
     }
   }
 }
