5805caff3f5cf8b77e9a30d563623f2e93f051a3
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 23:56:05 +0000

    5a75721835 build: fix test for useUnknownInCatch (#44935)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.0+49.sha-812c1ba.with-local-changes/, version);
+      banner = banner.replace(/13.2.0+52.sha-5a75721.with-local-changes/, version);
     }
   }
 }
