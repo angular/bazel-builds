c4986ea7287d18ab3c95d15ac98e1a27a42c1765
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 00:32:37 +0000

    ed1732c268 refactor(core): the `RuntimeError` class should support more compact syntax (#44783)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1070.sha-db05ae1.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1071.sha-ed1732c.with-local-changes/, version);
     }
   }
 }
