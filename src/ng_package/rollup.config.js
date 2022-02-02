611f35c6482b590e358bad2bd1838b89691ffc75
Dylan Hunn <dylhunn@gmail.com>
2022-02-02 23:49:26 +0000

    85ba38aecb refactor(compiler): add `@suppress {msgDescriptions}` if no description is present on an i18n message (#44787)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+6.sha-bf42877.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+7.sha-85ba38a.with-local-changes/, version);
     }
   }
 }
