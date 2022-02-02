4568c48bf5d58945c3142b37bd37683a317baf5d
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:27:14 +0000

    202a1a5631 fix(upgrade): Do not trigger duplicate navigation events from Angular Router (#43441)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+2.sha-a01bcb8.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+4.sha-202a1a5.with-local-changes/, version);
     }
   }
 }
