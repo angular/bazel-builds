6bd58d7c71552c686ed2a102e816af37191b32a0
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:21:01 +0000

    a01bcb8e7e fix(platform-browser): do not run change detection when loading Hammer (#44921)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+1.sha-39b90b4.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+2.sha-a01bcb8.with-local-changes/, version);
     }
   }
 }
