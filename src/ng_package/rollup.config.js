cd1082bda0b25916e93459f9525aa484d3716124
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 00:22:48 +0000

    d82c957a26 fix(compiler-cli): ensure casing of logical paths is preserved (#44798)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.0+1086.sha-6763967.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.0+1088.sha-d82c957.with-local-changes/, version);
     }
   }
 }
