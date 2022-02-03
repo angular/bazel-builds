bced61827d9818cd778eda35322933f66c18cef5
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 21:00:16 +0000

    0d19be1996 refactor(docs-infra): add types to all the aio examples buttons (#44557)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+19.sha-226fac6.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+22.sha-0d19be1.with-local-changes/, version);
     }
   }
 }
