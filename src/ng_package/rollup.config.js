1437ce76c8f70645fc137a21e51bc3a316b214a0
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 23:06:49 +0000

    f22bb74d39 build: setup OpenSSF Scorecard (#44960)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+25.sha-f44fa4b.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+24.sha-f22bb74.with-local-changes/, version);
     }
   }
 }
