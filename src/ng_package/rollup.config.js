d4b4d8e2f50305eeea3a60ef68f8169bb64f6817
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 23:06:17 +0000

    f44fa4be5d docs: update input-output-diagram.svg (#39900)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+23.sha-76f5528.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+25.sha-f44fa4b.with-local-changes/, version);
     }
   }
 }
