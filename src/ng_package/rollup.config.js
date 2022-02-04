f72734796f3c680e9f6f1dac66e0690df1a9a26d
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 07:32:06 +0000

    8dd3f82f94 fix(forms): Correct empty validator to handle objects with a property `length: 0`. (#33729)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+29.sha-b32647d.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+30.sha-8dd3f82.with-local-changes/, version);
     }
   }
 }
