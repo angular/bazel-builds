fd4a27735a54746712e5a9f7e341393c035f7609
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:58:17 +0000

    459cbed33b refactor(dev-infra): convert nullish coalescing operator to plain javascript (#44968)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+30.sha-8dd3f82.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+31.sha-459cbed.with-local-changes/, version);
     }
   }
 }
