4c08b41b99151176fdeb83244e48d8fe63e6123a
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 20:23:35 +0000

    70e29be328 refactor(forms): update `PatternValidator` to inherit `AbstractValidatorDirective` (#44887)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+41.sha-d5719c2.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+42.sha-70e29be.with-local-changes/, version);
     }
   }
 }
