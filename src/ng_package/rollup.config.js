d1c61ee1c9f92efaf6c9c226b6676c811444d597
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 23:48:16 +0000

    64acbc4242 refactor(core): drop all `.ngfactory` and `.ngsummary` imports (#44957)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+45.sha-d4da832.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+46.sha-64acbc4.with-local-changes/, version);
     }
   }
 }
