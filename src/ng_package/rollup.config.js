af418a6570459e8bf4b92e35ffeb9f5b4bf71d9b
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 21:36:02 +0000

    d4da832088 docs: add Cost-Effective Angular Testing (#44969)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+44.sha-ebf2fc5.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+45.sha-d4da832.with-local-changes/, version);
     }
   }
 }
