8928f5f1ee64c2d0d4469a1427fcf644e072dfda
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 18:01:04 +0000

    33df8405c9 docs(router): clarify execution order for resolvers (#44995)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+36.sha-2cda6cf.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.1+37.sha-33df840.with-local-changes/, version);
     }
   }
 }
