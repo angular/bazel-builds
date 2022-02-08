748d0d0572f6b270bd1e252d38c4f03b1fd7d683
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 18:13:52 +0000

    822439fddd fix(core): `ViewContainerRef.createComponent` should consult module injector when custom one is provided (#44966)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+7.sha-1b91e10.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+8.sha-822439f.with-local-changes/, version);
     }
   }
 }
