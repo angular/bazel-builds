3e9900045419f012812e348f1d78553f5b11eae0
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 17:56:13 +0000

    7a47add255 release: cut the v13.2.2 release (#45018)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.1+29.sha-7bc4dbd.with-local-changes/, version);
+      banner = banner.replace(/13.2.1+30.sha-7a47add.with-local-changes/, version);
     }
   }
 }
