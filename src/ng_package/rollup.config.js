a173361d19298eb74d31ebebff2ddc691067e3c6
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 01:50:43 +0000

    4d63b80163 refactor: CHANGELOG is poorly formatted for most recent version. (#45009)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.1+47.sha-a3de44c.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+1.sha-4d63b80.with-local-changes/, version);
     }
   }
 }
