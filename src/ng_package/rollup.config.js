324db47752b4e9a38b34e9fead372de006ff02ed
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 18:12:49 +0000

    635683ae97 docs(common): mark lifecycle methods as nodoc (#45017)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+4.sha-df6f616.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+5.sha-635683a.with-local-changes/, version);
     }
   }
 }
