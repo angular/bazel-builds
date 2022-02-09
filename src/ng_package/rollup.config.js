49a28a9da6818cd6ab5737e3666b5de2d69b430c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:38:58 +0000

    b8f8e8cf63 build: update scorecard action dependencies to 7f9dca2 (#45026)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+17.sha-7d9413e.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+15.sha-b8f8e8c.with-local-changes/, version);
     }
   }
 }
