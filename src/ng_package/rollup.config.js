5f32b1a487eca6128377366db074e2459636cb3f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:41:33 +0000

    7ea70c4292 docs: improve try-it-now card focus area (#45002)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+15.sha-b8f8e8c.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+18.sha-7ea70c4.with-local-changes/, version);
     }
   }
 }
