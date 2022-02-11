b9a2eac0797c7eb7f8be1462e4d03116b533e1fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:49:06 +0000

    b4157449c4 refactor(docs-infra): replace deprecated subscribe usages (#45046)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+22.sha-8793dbd.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+24.sha-b415744.with-local-changes/, version);
     }
   }
 }
