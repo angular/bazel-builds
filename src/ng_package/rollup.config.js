c9e042eac658a5b463c47489b194b27053b75f9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:50:46 +0000

    a782be62e9 refactor(docs-infra): replace deprecated toPromise usages (#45045)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.2+13.sha-d8a1871.with-local-changes/, version);
+      banner = banner.replace(/13.2.2+15.sha-a782be6.with-local-changes/, version);
     }
   }
 }
