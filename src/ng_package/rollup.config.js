7f4711c4488d69f665fdf7c89238010feff48871
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-14 21:09:17 +0000

    f17e26f7c1 fix(docs-infra): fix broken link in Glossary (#45068)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+26.sha-e872e9d.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+28.sha-f17e26f.with-local-changes/, version);
     }
   }
 }
