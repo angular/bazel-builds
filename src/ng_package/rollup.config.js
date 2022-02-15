29f96669f029c6b972229c5aac9d176c6c2fb35c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 00:00:39 +0000

    787eaa7a4b test(animations): Fix e2e intermittent failures (#45083)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+29.sha-bedb257.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+34.sha-787eaa7.with-local-changes/, version);
     }
   }
 }
