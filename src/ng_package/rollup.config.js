1458ddd4fe39ebe74998f28a3b3fe4df57caead1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:45:41 +0000

    13172421f9 docs: fix grammatical error (#45092)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+34.sha-787eaa7.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+35.sha-1317242.with-local-changes/, version);
     }
   }
 }
