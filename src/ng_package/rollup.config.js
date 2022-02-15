55fb9b59aa228c2ac3a3f11ca16ca5e3c63d2053
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:47:52 +0000

    aeeb1b55d3 docs(localize): fix angular.json syntax error about i18n (#45048)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.2+22.sha-0050b01.with-local-changes/, version);
+      banner = banner.replace(/13.2.2+24.sha-aeeb1b5.with-local-changes/, version);
     }
   }
 }
