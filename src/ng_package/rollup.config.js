ff8167780d8b466ea5409c3a276f5b2168671b65
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:01:47 +0000

    81bb0eb333 docs: clarify install of build schematics deps (#45104)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+40.sha-80585f4.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+41.sha-81bb0eb.with-local-changes/, version);
     }
   }
 }
