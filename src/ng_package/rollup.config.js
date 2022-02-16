ae7c07b247dead272e491b64a5a9a90e2bdbdb1a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:01:50 +0000

    1eae65ead7 build: update .pullapprove.yml (#45061)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+41.sha-81bb0eb.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+43.sha-1eae65e.with-local-changes/, version);
     }
   }
 }
