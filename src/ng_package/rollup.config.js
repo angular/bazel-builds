586400a2cf5b18537c7cfce91c432f35b19fb7a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:07:35 +0000

    ee381dadc2 test(zone.js): add integration test for zone.js Mocha patch (#45047)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+45.sha-6769f94.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+47.sha-ee381da.with-local-changes/, version);
     }
   }
 }
