9316c62694b4f61ae562e58cb65b16b898eaa7f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 21:53:46 +0000

    df3a075b02 docs: fix case types guide (#45095)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+36.sha-f1d10ba.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+39.sha-df3a075.with-local-changes/, version);
     }
   }
 }
