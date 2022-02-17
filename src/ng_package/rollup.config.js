6e3b228dae53295fb8f8acb09fd32a3fce584fa7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-17 00:06:41 +0000

    54d09a6bc0 release: cut the v13.2.3 release (#45116)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.2+31.sha-782f678.with-local-changes/, version);
+      banner = banner.replace(/13.2.2+32.sha-54d09a6.with-local-changes/, version);
     }
   }
 }
