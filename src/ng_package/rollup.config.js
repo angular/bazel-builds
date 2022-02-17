c6b8ecf10d2fc0252655f9836c25a13e5e917520
Joey Perrott <josephperrott.github@gmail.com>
2022-02-17 21:51:46 +0000

    235bffc5f9 release: cut the v14.0.0-next.3 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+48.sha-db4a658.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.2+50.sha-235bffc.with-local-changes/, version);
     }
   }
 }
