150912c5306e93bb7ce998950109cb42d922c033
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:52:05 +0000

    702e196037 fix(docs-infra): make hamburger menu non focusable when not visible (#45067)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.3+6.sha-be50401.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.3+7.sha-702e196.with-local-changes/, version);
     }
   }
 }
