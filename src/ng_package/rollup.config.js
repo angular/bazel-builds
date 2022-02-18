d7ff8826f1a4ecd1605c0a0e6daf900cc2229760
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:48:49 +0000

    071c8af8ba perf(core): avoid storing LView in __ngContext__ (#45051)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.2+49.sha-4333662.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.3+1.sha-071c8af.with-local-changes/, version);
     }
   }
 }
