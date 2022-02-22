4c3fb466cc5b011205eadb4d1e130c5abf7c2561
Andrew Scott <atscott01@gmail.com>
2022-02-22 23:51:35 +0000

    032276253f fix(docs-infra): removed duplicated style in aio examples (#45157)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.3+11.sha-d607d0b.with-local-changes/, version);
+      banner = banner.replace(/13.2.3+12.sha-0322762.with-local-changes/, version);
     }
   }
 }
