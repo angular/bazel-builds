7033ab86aedc879c1a4bce674d4a8eed523db6e3
Andrew Scott <atscott01@gmail.com>
2022-02-22 21:04:27 +0000

    bd168fc0c3 build: update ossf/scorecard-action commit hash to 3662744 (#45162)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.3+14.sha-c0778b4.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.3+16.sha-bd168fc.with-local-changes/, version);
     }
   }
 }
