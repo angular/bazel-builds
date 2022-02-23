0f33fc762ed7d55e41ef468c2e1d16e4d9bca897
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:14:22 +0000

    f8dc660605 fix(animations): allow animations with unsupported CSS properties (#44729)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.3+18.sha-4fa5307.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.3+20.sha-f8dc660.with-local-changes/, version);
     }
   }
 }
