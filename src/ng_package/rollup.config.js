b2c2e999aee4b43b6baf220020441e8aab971cd3
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:15:36 +0000

    6550c59df5 docs: fix test description to match code example (#45101)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.3+12.sha-0322762.with-local-changes/, version);
+      banner = banner.replace(/13.2.3+13.sha-6550c59.with-local-changes/, version);
     }
   }
 }
