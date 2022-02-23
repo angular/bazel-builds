f7554dac857e535241cd3fc50c9768d7c073ebbf
GitHub <noreply@github.com>
2022-02-23 21:06:38 +0000

    ded96ca6cb release: cut the v14.0.0-next.4 release (#45184)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.3+22.sha-f58ad88.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.3+24.sha-ded96ca.with-local-changes/, version);
     }
   }
 }
