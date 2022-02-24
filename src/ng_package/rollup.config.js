052f976db861b639917d022be1e6ae05ba64e3aa
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 19:00:59 +0000

    7fd416d060 fix(router): Fix type of Route.pathMatch to be more accurate (#45176)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+6.sha-909b21a.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.4+8.sha-7fd416d.with-local-changes/, version);
     }
   }
 }
