ec06c9e62ddbced5c4ce1d54ac42ece4c7dc3086
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 17:41:36 +0000

    3b86dda068 refactor(http): change <script>'s ownerDocument in jsonp teardown (#36807) handler

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.4+3.sha-4f18703.with-local-changes/, version);
+      banner = banner.replace(/13.2.4+6.sha-3b86dda.with-local-changes/, version);
     }
   }
 }
