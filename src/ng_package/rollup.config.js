ac072c8638ca9a881bd4aec94e1764728838ff1c
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 01:11:43 +0000

    4f1870362e docs: remove activatedRouteData backward accesses in aio guides (#45140)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.3+14.sha-e914da1.with-local-changes/, version);
+      banner = banner.replace(/13.2.4+3.sha-4f18703.with-local-changes/, version);
     }
   }
 }
