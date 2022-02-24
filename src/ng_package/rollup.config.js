cb053bfda89189a5826a397b545afe212fc6c4f9
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 21:59:04 +0000

    0debf84045 ci: allow Babel to be updated by Renovate (#44931)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+10.sha-8d634ac.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.4+12.sha-0debf84.with-local-changes/, version);
     }
   }
 }
