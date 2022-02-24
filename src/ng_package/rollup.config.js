d8eb4712937dc277e8ab4da9b5ebd8e6afde0065
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 21:55:05 +0000

    7379d75782 ci: add permissions to current github action workflows (#45177)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.4+8.sha-21a51fb.with-local-changes/, version);
+      banner = banner.replace(/13.2.4+9.sha-7379d75.with-local-changes/, version);
     }
   }
 }
