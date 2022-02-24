5e8dd62fb14f44ea5edf8487b83641f0467a58a6
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 17:29:35 +0000

    ca5603b09b fix(localize): avoid imports into `compiler-cli` package (#45180)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+3.sha-8eb8288.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.4+4.sha-ca5603b.with-local-changes/, version);
     }
   }
 }
