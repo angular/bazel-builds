8ebea9ce5dc4d18832c0721766824b0ac193a282
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 19:31:49 +0000

    b39db80f91 test(animations): add route animation delay to pages tests (#45144)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.4+6.sha-3b86dda.with-local-changes/, version);
+      banner = banner.replace(/13.2.4+7.sha-b39db80.with-local-changes/, version);
     }
   }
 }
