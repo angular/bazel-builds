ff3e8dfe0710f38c2f710c23af769cd2181e025e
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 21:32:56 +0000

    21a51fb6a4 test(animations): Fix flaky animations e2e test (#45196)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.4+7.sha-b39db80.with-local-changes/, version);
+      banner = banner.replace(/13.2.4+8.sha-21a51fb.with-local-changes/, version);
     }
   }
 }
