defdf21accd10ce62753c6d0d90b2f3e74dbf3df
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:19:06 +0000

    63e9cc7cba ci: update aio payload size golden to reflect Angular update (#45117)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+18.sha-aae07c3.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.4+21.sha-63e9cc7.with-local-changes/, version);
     }
   }
 }
