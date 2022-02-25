5c601b6d2dbebe48400486f31047242cb100ff85
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:30:03 +0000

    e56472d3c2 ci: fix stamping for builds performed in CI (#45147)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+22.sha-e01820c.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.4+23.sha-e56472d.with-local-changes/, version);
     }
   }
 }
