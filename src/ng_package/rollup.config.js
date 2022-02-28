fe2818e675123ceb7e5732ab6ea1034e540295bf
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 17:55:28 +0000

    f4fd488d4d build: update tooling to use new entry-point for dev-infra exports (#45206)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+26.sha-4c80ca3.with-local-changes/, version);
+      banner = banner.replace(/14.0.0-next.4+28.sha-f4fd488/, version);
     }
   }
 }
