3ca9996a5858ab25be27b8a2b6495b736af89bbb
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 19:48:14 +0000

    8907fbbfed docs: improve structure of http example (#44986)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.4+17.sha-84b4ad8.with-local-changes/, version);
+      banner = banner.replace(/13.2.4+18.sha-8907fbb.with-local-changes/, version);
     }
   }
 }
