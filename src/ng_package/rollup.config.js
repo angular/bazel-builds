95c2020f2677906e2b977681212d9f107447ea18
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 01:21:02 +0000

    4b936a1014 Revert "feat(core): allow for injector to be specified when creating an embedded view (#45156)" (#45220)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+34.sha-b817f3c/, version);
+      banner = banner.replace(/14.0.0-next.4+35.sha-4b936a1/, version);
     }
   }
 }
