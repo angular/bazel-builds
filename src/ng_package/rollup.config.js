7dd3b7bc0e6aae1b09332f75968366826c3b98e9
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 17:29:41 +0000

    c9679760b2 refactor(router): take only the first emitted value of every resolver to make it consistent with guards (#44573)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+36.sha-bd12f9e/, version);
+      banner = banner.replace(/14.0.0-next.4+37.sha-c967976/, version);
     }
   }
 }
