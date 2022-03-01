09fc721671273a1e539d1086bb3ccc203a77f70f
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 19:33:57 +0000

    c5df474c7c fix(core): remove individual commands for updating gold files (#45198)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.4+39.sha-1b85811/, version);
+      banner = banner.replace(/14.0.0-next.4+40.sha-c5df474/, version);
     }
   }
 }
