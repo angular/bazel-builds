a822bfae24f5718ee4d30dc7f16039471be67c86
Andrew Kushnir <akushnir@google.com>
2022-03-03 17:30:21 +0000

    5883e0536c refactor(router): Remove special logic for hybrid apps (#45240)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.5+1.sha-30c52ac/, version);
+      banner = banner.replace(/14.0.0-next.5+2.sha-5883e05/, version);
     }
   }
 }
