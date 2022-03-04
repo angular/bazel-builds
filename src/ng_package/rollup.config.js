ce9f3a07d2e033d93ce3e8d5bd850177366573af
Andrew Kushnir <akushnir@google.com>
2022-03-04 00:06:07 +0000

    9b1e9c0bd7 release: cut the zone.js-0.11.5 release (#45256)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.5+6.sha-bd04fbc/, version);
+      banner = banner.replace(/14.0.0-next.5+7.sha-9b1e9c0/, version);
     }
   }
 }
