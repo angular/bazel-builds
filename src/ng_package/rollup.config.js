8840a9db8b7e69b89d56ed9d39b39fb659fc283b
Andrew Kushnir <akushnir@google.com>
2022-03-04 23:39:25 +0000

    bc89598bb9 build: patch snapshot issue in dev-infra selecting the latest version tag (#45269)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.5+7.sha-9b1e9c0/, version);
+      banner = banner.replace(/14.0.0-next.5+8.sha-bc89598/, version);
     }
   }
 }
