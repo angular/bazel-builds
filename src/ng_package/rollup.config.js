2670bd78a42bfa310c18bb2caec459f0a652dc93
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:50:11 +0000

    aa7b857be8 fix(forms): Property renaming safe code (#45271)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.5+12.sha-8c171da/, version);
+      banner = banner.replace(/14.0.0-next.5+13.sha-aa7b857/, version);
     }
   }
 }
