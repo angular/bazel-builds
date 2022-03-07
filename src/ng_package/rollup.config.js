fdb51769ffcb1c42cd61a7ab9b2bf205112cceeb
Andrew Kushnir <akushnir@google.com>
2022-03-07 22:49:36 +0000

    43bbaf810d build: update dependency google-closure-compiler to v20220301 (#45260)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.5+13.sha-f261fa7/, version);
+      banner = banner.replace(/13.2.5+15.sha-43bbaf8/, version);
     }
   }
 }
