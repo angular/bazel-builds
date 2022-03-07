83a07035424572dcedc765d9d45b2a427d5bde15
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:58:57 +0000

    8390e12bdd docs: update `flush` description to include microtasks (#45237)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.5+15.sha-43bbaf8/, version);
+      banner = banner.replace(/13.2.5+16.sha-8390e12/, version);
     }
   }
 }
