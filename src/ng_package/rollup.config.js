38633972c2df67e70c130b565fe1148e97246384
Andrew Kushnir <akushnir@google.com>
2022-03-07 20:41:49 +0000

    d336ba96d9 fix(forms): Update the typed forms migration. (#45281)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.5+16.sha-d87f66c/, version);
+      banner = banner.replace(/14.0.0-next.5+17.sha-d336ba9/, version);
     }
   }
 }
