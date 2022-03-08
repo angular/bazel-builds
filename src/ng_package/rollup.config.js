0231145e7c85c1dc29b43af09a3aadc6af888435
Andrew Kushnir <akushnir@google.com>
2022-03-08 20:43:46 +0000

    6eaaefd22e feat(core): drop support for Node.js 12 (#45286)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.5+31.sha-6cec8aa/, version);
+      banner = banner.replace(/14.0.0-next.5+30.sha-6eaaefd/, version);
     }
   }
 }
