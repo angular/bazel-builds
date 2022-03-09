de6dc95ac7a5bbe93c2ba55317190edcfa128c1e
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:08:34 +0000

    89d71b545e docs: update CHANGELOG to fix broken markdown (#45305)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.5+37.sha-51b67eb/, version);
+      banner = banner.replace(/14.0.0-next.6+1.sha-89d71b5/, version);
     }
   }
 }
