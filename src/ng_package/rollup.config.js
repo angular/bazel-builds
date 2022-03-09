4632a33e7b26dbd2cca444a663042754da9b8cea
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:14:39 +0000

    c7cab69af1 docs(core): update triggerEventHandler samples (#45279)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.6+5.sha-a08ea3f/, version);
+      banner = banner.replace(/14.0.0-next.6+3.sha-c7cab69/, version);
     }
   }
 }
