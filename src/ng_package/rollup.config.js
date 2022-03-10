dcb9650e65ef58300e8ee07d9fc71826b54fa242
Andrew Scott <atscott01@gmail.com>
2022-03-10 21:07:41 +0000

    5f7c4f67d0 refactor(core): Switch over to new closure LOCALE vs getLocale() (#45302)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.6+7.sha-8848d2d/, version);
+      banner = banner.replace(/14.0.0-next.6+8.sha-5f7c4f6/, version);
     }
   }
 }
