6085a2c7eb344a31e7b40299ef601216a0c66938
Andrew Scott <atscott01@gmail.com>
2022-03-11 07:25:23 +0000

    7f043c98b7 build(docs-infra): upgrade cli command docs sources to a517160a2 (#45225)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.6+8.sha-5f7c4f6/, version);
+      banner = banner.replace(/14.0.0-next.6+11.sha-7f043c9/, version);
     }
   }
 }
