69eec16a5608a0d727f7be1447cba02b8876d176
Andrew Scott <atscott01@gmail.com>
2022-03-14 16:48:55 +0000

    b09686923b docs: Fix typo in issue template (#45327)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.6+3.sha-78781a9/, version);
+      banner = banner.replace(/13.2.6+4.sha-b096869/, version);
     }
   }
 }
