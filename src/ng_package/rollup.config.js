3214313dcdbed14de916a26b6b8bf4ec4960bad9
Andrew Scott <atscott01@gmail.com>
2022-03-14 16:41:47 +0000

    0dbd50bea5 build: update dependency eslint-plugin-jsdoc to v38 (#45332)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.6+11.sha-7f043c9/, version);
+      banner = banner.replace(/14.0.0-next.6+12.sha-0dbd50b/, version);
     }
   }
 }
