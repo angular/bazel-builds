fe407f349dbd3fad0f0112e28cc914cd4405c558
Andrew Scott <atscott01@gmail.com>
2022-03-15 20:43:40 +0000

    eac94f0945 fix(forms): improve error message for invalid value accessors (#45192)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.2.6+5.sha-16eda01/, version);
+      banner = banner.replace(/13.2.6+7.sha-eac94f0/, version);
     }
   }
 }
