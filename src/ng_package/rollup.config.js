4d1957aae153a42a1dc885d634d2867282ea437e
Andrew Scott <atscott01@gmail.com>
2022-03-16 22:23:09 +0000

    59a5b54c23 docs: add how to access the resolved data (#45345)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+1.sha-9d5ad67/, version);
+      banner = banner.replace(/14.0.0-next.7+2.sha-59a5b54/, version);
     }
   }
 }
