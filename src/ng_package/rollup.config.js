2d676f252fe7e656e7034c908827128b4a105118
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-17 21:21:03 +0000

    2a75754ee8 fix(animations): apply default params when resolved value is null or undefined (#45339)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+2.sha-59a5b54/, version);
+      banner = banner.replace(/14.0.0-next.7+3.sha-2a75754/, version);
     }
   }
 }
