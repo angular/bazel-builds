d3e579cb6c374df931d8cd84c4be2dec48ab8219
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 22:09:51 +0000

    3714305f84 fix(compiler): scope css rules within `@layer` blocks (#45396)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.0+3.sha-e5300d6/, version);
+      banner = banner.replace(/13.3.0+4.sha-3714305/, version);
     }
   }
 }
