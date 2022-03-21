62ad69d6168f54a3678d22e8d2d93c6b22bf5d09
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 21:58:48 +0000

    c26915c8cf refactor(core): Make the typed forms migration apply to all usages of the symbols. (#45311)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+5.sha-79d334b/, version);
+      banner = banner.replace(/14.0.0-next.7+6.sha-c26915c/, version);
     }
   }
 }
