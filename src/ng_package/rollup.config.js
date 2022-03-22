6f248ed47a0711319a849c85fff4a868ddf2f0b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:14:46 +0000

    ee20410a98 docs(docs-infra): fix TypeScript casing (#45307)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.0+4.sha-3714305/, version);
+      banner = banner.replace(/13.3.0+6.sha-ee20410/, version);
     }
   }
 }
