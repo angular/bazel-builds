72c63207cc16582b98c5c59ba3a8d54e3b7e84f2
Joey Perrott <josephperrott@gmail.com>
2022-03-23 19:37:52 +0000

    38ad849d98 fix(docs-infra): prevent framing of AIO with X-Frame-Options (#45419)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+18.sha-8155428/, version);
+      banner = banner.replace(/14.0.0-next.7+19.sha-38ad849/, version);
     }
   }
 }
