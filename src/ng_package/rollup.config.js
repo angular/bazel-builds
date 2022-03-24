79e6980716c5232f631e1cd11fe57bbdf44237f5
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:43:09 +0000

    638e86054c docs: small English improvement (#45375)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+32.sha-be161be/, version);
+      banner = banner.replace(/14.0.0-next.7+33.sha-638e860/, version);
     }
   }
 }
