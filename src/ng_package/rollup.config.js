fffec7ebd71b8c39cd028cb0ff217d60081f9d03
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:06:05 +0000

    fe0e42a996 fix(forms): Make UntypedFormBuilder assignable to FormBuilder, and vice versa. (#45421)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+19.sha-38ad849/, version);
+      banner = banner.replace(/14.0.0-next.7+20.sha-fe0e42a/, version);
     }
   }
 }
