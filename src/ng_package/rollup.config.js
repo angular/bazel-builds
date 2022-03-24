742382cda349421d6502e37d0938a26907f46524
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 21:48:49 +0000

    7a9c6f56dd build: update dependency send to ^0.18.0 (#45428)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+36.sha-dc0e39a/, version);
+      banner = banner.replace(/14.0.0-next.7+37.sha-7a9c6f5/, version);
     }
   }
 }
