15b3da470865db5bc37f4214e0751ac2b93e72d1
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:28:05 +0000

    be161bef79 fix(core): memory leak in event listeners inside embedded views (#43075)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+31.sha-71ee417/, version);
+      banner = banner.replace(/14.0.0-next.7+32.sha-be161be/, version);
     }
   }
 }
