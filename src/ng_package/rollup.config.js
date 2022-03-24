ef0b17e96f92e0a81a5c7700f4cdba723a9728e4
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:07:57 +0000

    85782e607d docs(router): Fix typo on segments (#45411)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.7+20.sha-fe0e42a/, version);
+      banner = banner.replace(/14.0.0-next.7+21.sha-85782e6/, version);
     }
   }
 }
