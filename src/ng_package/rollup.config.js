c89e02bfc22204fb2899486d93a0d54c642768f1
Dylan Hunn <dylhunn@gmail.com>
2022-03-26 00:06:05 +0000

    0c28997739 build: disable payload size uploading within bazel (#45446)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.0+28.sha-88dbeda/, version);
+      banner = banner.replace(/13.3.0+31.sha-0c28997/, version);
     }
   }
 }
