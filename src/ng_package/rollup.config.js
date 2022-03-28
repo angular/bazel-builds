d16ef013e0e63c4307b747b7bf511cce2ac8151a
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 16:42:18 +0000

    3d2a60e027 build: update dependency @bazel/ibazel to ^0.16.0 (#45259)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+7.sha-c996b47/, version);
+      banner = banner.replace(/14.0.0-next.9+8.sha-3d2a60e/, version);
     }
   }
 }
