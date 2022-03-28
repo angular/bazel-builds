9a6f3cf7ae39d558e2e793fd533a3b1acc3ae5a1
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 17:47:12 +0000

    bf98c646f7 docs: add image directive to public roadmap (#45448)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+10.sha-d43c0e9/, version);
+      banner = banner.replace(/14.0.0-next.9+11.sha-bf98c64/, version);
     }
   }
 }
