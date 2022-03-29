0c113bbc10a72eeba706e98a9b32129390798610
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:39:54 +0000

    6e4cb48bc5 docs: adjust contributing, developing and overview markdown files (#45464)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+13.sha-801d11d/, version);
+      banner = banner.replace(/14.0.0-next.9+14.sha-6e4cb48/, version);
     }
   }
 }
