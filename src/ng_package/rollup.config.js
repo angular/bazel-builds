6da9d73537626793d441d0650264195c9efb56d1
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:29:32 +0000

    801d11dd8a Revert "fix(compiler): scope css keyframes in emulated view encapsulation (#42608)" (#45466)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+12.sha-f03e313/, version);
+      banner = banner.replace(/14.0.0-next.9+13.sha-801d11d/, version);
     }
   }
 }
