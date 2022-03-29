b1baed2ad07c6cce9747401090818e988657f4c1
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:40:19 +0000

    16e635ca49 fix(docs-infra): update a11y min-scores (#45209)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+14.sha-6e4cb48/, version);
+      banner = banner.replace(/14.0.0-next.9+19.sha-16e635c/, version);
     }
   }
 }
