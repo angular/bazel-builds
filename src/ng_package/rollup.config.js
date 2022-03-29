86bf2a02bcaefcaad5cc0f7ab3b2747332500e98
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 22:41:58 +0000

    8e09445610 test: update aio payload size golden to reflect Angular update (#45376)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+32.sha-06050ac/, version);
+      banner = banner.replace(/14.0.0-next.9+34.sha-8e09445/, version);
     }
   }
 }
