07b0aef0ece2a7ee37560997a031c265bdfe281a
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 20:41:54 +0000

    e55e98dce5 build: update all non-major dependencies (#45374)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+28.sha-2142ffd/, version);
+      banner = banner.replace(/14.0.0-next.9+29.sha-e55e98d/, version);
     }
   }
 }
