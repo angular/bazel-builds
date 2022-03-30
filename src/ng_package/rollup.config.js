fa80d6784a43c3b7ba84edf85c02d6e63cb37f83
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 00:12:00 +0000

    7d9492353d fix(docs-infra): implement focus trap for aio search results (#44989)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+34.sha-8e09445/, version);
+      banner = banner.replace(/14.0.0-next.9+35.sha-7d94923/, version);
     }
   }
 }
