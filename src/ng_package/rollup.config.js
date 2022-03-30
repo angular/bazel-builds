ba6ce856a9896bbc1afa710d9dcc57c26bb5ed08
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 00:29:18 +0000

    e1e440d65a docs: add Angular university website to resources list (#45437)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.9+37.sha-82956dc/, version);
+      banner = banner.replace(/14.0.0-next.9+38.sha-e1e440d/, version);
     }
   }
 }
