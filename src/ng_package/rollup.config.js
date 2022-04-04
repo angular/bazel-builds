ff641e84d0d80d7becdd427df63913cface6d6b8
Andrew Kushnir <akushnir@google.com>
2022-04-04 22:12:49 +0000

    d56a537196 feat(migrations): Add migration to add explicit `Route`/`Routes` type (#45084)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.10+11.sha-035ba83/, version);
+      banner = banner.replace(/14.0.0-next.10+10.sha-d56a537/, version);
     }
   }
 }
