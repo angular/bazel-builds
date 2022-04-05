7e20d030e57a160dd12185e6eeb2309c67b6aed4
Andrew Kushnir <akushnir@google.com>
2022-04-05 17:48:44 +0000

    05121c0381 test(animations): update test to use an object instead of a Map (#45531)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.1+6.sha-fc86fe5/, version);
+      banner = banner.replace(/13.3.1+9.sha-05121c0/, version);
     }
   }
 }
