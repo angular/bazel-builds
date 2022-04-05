2c5caf6fb92631c6b845ebed5be74e99a1c843fe
Andrew Kushnir <akushnir@google.com>
2022-04-05 16:06:25 +0000

    1c11a57155 fix(router): merge interited resolved data and static data (#45276) in layers

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.10+13.sha-a3f344f/, version);
+      banner = banner.replace(/14.0.0-next.10+14.sha-1c11a57/, version);
     }
   }
 }
