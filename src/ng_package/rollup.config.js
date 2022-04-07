062f0007910ed896afaf13ed7e2da8bea1899e8b
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 22:20:01 +0000

    60b5a3de9d refactor(core): validate property (#45528)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.11+6.sha-2a81e44/, version);
+      banner = banner.replace(/14.0.0-next.11+7.sha-60b5a3d/, version);
     }
   }
 }
