524aea8518dd921612e856b8d53d88d77a592fab
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 21:18:55 +0000

    5adfe8ef24 docs(forms): remove the incorrect set value from previous commit (#45533)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.11+5.sha-f8f3ab3/, version);
+      banner = banner.replace(/14.0.0-next.11+4.sha-5adfe8e/, version);
     }
   }
 }
