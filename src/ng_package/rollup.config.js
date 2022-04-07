80f5d490501d2d1143802cd1aa0823973c4b5f16
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 21:20:04 +0000

    18f98e671b docs: remove out-of-order `<h3>` elements from footer (#45510)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.1+10.sha-fb5e16e/, version);
+      banner = banner.replace(/13.3.2+3.sha-18f98e6/, version);
     }
   }
 }
