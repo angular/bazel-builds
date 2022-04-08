d2e726b8f11cfb11c2396cf0963fd4abd366b76d
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-08 16:13:21 +0000

    1b2c67d9fd test(docs-infra): fix e2e test for `{@example}` dgeni tags (#45551) (#45563)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.2+4.sha-208d819/, version);
+      banner = banner.replace(/13.3.2+6.sha-1b2c67d/, version);
     }
   }
 }
