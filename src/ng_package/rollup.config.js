3b44fd8a4d7aa5c6c5654ec8bcba6b7e56d9ff2c
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 22:52:49 +0000

    61894570f1 docs: improve markdown (#45590)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.2+8.sha-09c5c18/, version);
+      banner = banner.replace(/13.3.2+9.sha-6189457/, version);
     }
   }
 }
