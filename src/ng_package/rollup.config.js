3b8152447fdfc22e51c3e784390220e36ff1ffff
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 17:08:51 +0000

    9572bb2ded refactor(animations): use full ngDevMode check in animatable prop validation (#45570)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.11+20.sha-b13a453/, version);
+      banner = banner.replace(/14.0.0-next.11+22.sha-9572bb2/, version);
     }
   }
 }
