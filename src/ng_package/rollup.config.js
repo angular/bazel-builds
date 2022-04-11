09439f3ae6c50ea243acaac2c2f18a93cf8a7c7b
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 21:18:53 +0000

    0facba5b70 build: update bazel integration test to RNJ v5.4.0 and Sass 1.50.0 (#45470)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.11+23.sha-f1630bb/, version);
+      banner = banner.replace(/14.0.0-next.11+30.sha-0facba5/, version);
     }
   }
 }
