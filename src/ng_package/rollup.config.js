e6bd7fdf383e6cea6f418031356fa260ae3e39ed
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 16:43:27 +0000

    09c5c18c7d build: update dependency google-closure-compiler to v20220405 (#45568)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.2+6.sha-1b2c67d/, version);
+      banner = banner.replace(/13.3.2+8.sha-09c5c18/, version);
     }
   }
 }
