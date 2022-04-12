a6658b8fd24a55e89cc13d2e78de7bb0befc4352
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:58:36 +0000

    222d8662a2 docs: fix `schematicCollections` option (#45602) PR Close #45602

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.11+35.sha-eb2f449/, version);
+      banner = banner.replace(/14.0.0-next.11+34.sha-222d866/, version);
     }
   }
 }
