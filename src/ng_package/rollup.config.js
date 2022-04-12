97dc960cbf28dba49b0068d6fc6a8ec03c55d3d6
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:19:12 +0000

    820077df76 build: update dependency glob to v8 (#45598)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.12+5.sha-83920af/, version);
+      banner = banner.replace(/14.0.0-next.12+6.sha-820077d/, version);
     }
   }
 }
