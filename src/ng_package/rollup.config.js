3caa6f6a7e0e62e73a3fb33a6c31d1c47d9fbea6
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:31:04 +0000

    79de026a20 docs: add powershell execution policies note (#45576)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.11+47.sha-6532328/, version);
+      banner = banner.replace(/14.0.0-next.12+1.sha-79de026/, version);
     }
   }
 }
