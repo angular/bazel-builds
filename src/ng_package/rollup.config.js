3b3dee480fe286b1a9fed4aa4af41049ff812c53
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:58:11 +0000

    5900f0e133 docs(docs-infra): add missing period in `aio/README.md` (#45584)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.2+9.sha-6189457/, version);
+      banner = banner.replace(/13.3.2+10.sha-5900f0e/, version);
     }
   }
 }
