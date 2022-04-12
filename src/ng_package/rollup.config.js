b312c091d2671219bd7e66c83c37a2990c6baada
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:47:01 +0000

    c49d23269c docs: new Pipes custom data doc (#45505)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.2+10.sha-5900f0e/, version);
+      banner = banner.replace(/13.3.2+15.sha-c49d232/, version);
     }
   }
 }
