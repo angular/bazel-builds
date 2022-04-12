ec6f877a050fd94f17a27196a422a44926b4b2e9
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:20:33 +0000

    89d299105a feat(forms): Implement strict types for the Angular Forms package. (#43834)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.11+32.sha-d11d1c0/, version);
+      banner = banner.replace(/14.0.0-next.11+33.sha-89d2991/, version);
     }
   }
 }
