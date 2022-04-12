c8dac4fb6a3b311f4f4d622c29d0cacacc48bf41
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:44:15 +0000

    3c1695e399 docs: make tutorial instructions consistent (#45372)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.12+6.sha-820077d/, version);
+      banner = banner.replace(/14.0.0-next.12+9.sha-3c1695e/, version);
     }
   }
 }
