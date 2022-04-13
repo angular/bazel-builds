f9107db6aca3bf07300430cea66024569ddb4053
GitHub <noreply@github.com>
2022-04-13 19:50:16 +0000

    fbbed3d723 release: cut the v14.0.0-next.13 release (#45616)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.12+20.sha-0f8e181/, version);
+      banner = banner.replace(/14.0.0-next.12+21.sha-fbbed3d/, version);
     }
   }
 }
