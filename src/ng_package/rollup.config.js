d27a3dda8c2750505620ac21d73489c81e4509e1
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 17:37:05 +0000

    47e7a2850b refactor(router): Add warning for `relativeLinkResolution: 'legacy'` (#45523)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.12+18.sha-598b759/, version);
+      banner = banner.replace(/14.0.0-next.12+19.sha-47e7a28/, version);
     }
   }
 }
