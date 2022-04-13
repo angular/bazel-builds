5c54d02a26b60b50cf2be37bf107ecdba19c2fa8
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 00:27:41 +0000

    a6ab3f7beb docs: fix grammar (#45455) PR Close #45455

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.12+11.sha-89ed8d8/, version);
+      banner = banner.replace(/14.0.0-next.12+16.sha-a6ab3f7/, version);
     }
   }
 }
