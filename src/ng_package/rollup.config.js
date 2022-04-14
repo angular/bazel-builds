add935dc2f29d16b746ee363f61b8b0a7f3cf62c
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 23:47:27 +0000

    acbd2e9339 docs: fix typo (#45637)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.3+3.sha-a1c7418/, version);
+      banner = banner.replace(/13.3.3+4.sha-acbd2e9/, version);
     }
   }
 }
