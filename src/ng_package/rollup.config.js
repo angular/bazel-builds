292cfd0ebe8deca5f66dcb963d4a3cf9af3a9e44
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:17:01 +0000

    94fba9b047 docs(core): fix typo in View Encapsulation code example (#45629)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+4.sha-e0a2248/, version);
+      banner = banner.replace(/14.0.0-next.13+5.sha-94fba9b/, version);
     }
   }
 }
