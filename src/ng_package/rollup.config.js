0eecbf2fa9a5a1ab9ddc8f1904634c24924d8703
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:18:46 +0000

    6444a02620 docs: move old changelog entries to a separate file (#45638)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+6.sha-2f08b80/, version);
+      banner = banner.replace(/14.0.0-next.13+7.sha-6444a02/, version);
     }
   }
 }
