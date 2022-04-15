2b7564447f900e92e2b53ead96f83e7a2c9a0b70
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:38:53 +0000

    0b6407241c fix(docs-infra): fix aio search-results issue with toolbar and notification (#45579)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+14.sha-c6e0e3f/, version);
+      banner = banner.replace(/14.0.0-next.13+18.sha-0b64072/, version);
     }
   }
 }
