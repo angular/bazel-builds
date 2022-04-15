7201f2179fe7b1b47614b1b363ac9394230ddc67
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:08:46 +0000

    2e973121ba test(core): add a test for multiple named interpolations with the same name (#45651)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+12.sha-96fd29c/, version);
+      banner = banner.replace(/14.0.0-next.13+13.sha-2e97312/, version);
     }
   }
 }
