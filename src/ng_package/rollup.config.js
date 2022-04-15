cb86b93d13a92e291b468a70144eb1a9f45b47e3
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:55:25 +0000

    68357104c9 build: lock file maintenance (#45457)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+18.sha-0b64072/, version);
+      banner = banner.replace(/14.0.0-next.13+19.sha-6835710/, version);
     }
   }
 }
