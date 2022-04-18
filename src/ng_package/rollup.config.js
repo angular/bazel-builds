317288c7fccefce3dd9bc1aa11e33a84bf6c0161
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 16:42:45 +0000

    8c83f12daa ci: add more reviewers to the `docs-packaging-and-releasing` group (#45652)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+20.sha-b29b95b/, version);
+      banner = banner.replace(/14.0.0-next.13+22.sha-8c83f12/, version);
     }
   }
 }
