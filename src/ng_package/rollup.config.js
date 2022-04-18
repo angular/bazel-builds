c72c2811ccf55abbadddb785456e567b63dea4b8
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 21:26:07 +0000

    aa966fd715 refactor(router): Use `EnvironmentInjector` for lazy loading APIs (#45671)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+29.sha-eb7661c/, version);
+      banner = banner.replace(/14.0.0-next.13+30.sha-aa966fd/, version);
     }
   }
 }
