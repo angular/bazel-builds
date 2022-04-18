c50ebe3c374ac407f054f79caf8f6cc645149d9e
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 17:09:55 +0000

    eb7661c072 refactor(router): unbundle the lazy loaded routes from the lazy loaded module injector (#45593)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+27.sha-3578e94/, version);
+      banner = banner.replace(/14.0.0-next.13+29.sha-eb7661c/, version);
     }
   }
 }
