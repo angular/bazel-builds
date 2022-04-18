731a7c9de3e6a4155b84bf8d13c48f3f3a86084c
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 16:43:48 +0000

    1fe255c76f refactor(compiler): add `original_code` to `goog.getMsg()` options (#45606)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+22.sha-8c83f12/, version);
+      banner = banner.replace(/14.0.0-next.13+23.sha-1fe255c/, version);
     }
   }
 }
