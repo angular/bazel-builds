5ba277a55213d3b1ad1a0a6f7bc1eb282dc703f2
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:37:13 +0000

    ceffdeda69 docs(common): fix links to locales in i18n guides (#45661)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+37.sha-c6feb0a/, version);
+      banner = banner.replace(/14.0.0-next.13+38.sha-ceffded/, version);
     }
   }
 }
