a1c0411d308f12e889607184b6f53c187c303c83
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 03:36:23 +0000

    0231a363fe build: preparation for primary branch rename in the Angular repos (#45681)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.13+39.sha-5d15595/, version);
+      banner = banner.replace(/14.0.0-next.13+41.sha-0231a36/, version);
     }
   }
 }
