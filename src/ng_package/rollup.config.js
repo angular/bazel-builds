7592fd6876be61981d2a6f8e8aa3edf93c94126d
Andrew Scott <atscott01@gmail.com>
2022-04-21 18:24:19 +0000

    81611dbc9d fix(docs-infra): fix placement of "Edit source" button on errors and diagnostics pages (#45634)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.3+27.sha-2ea541f/, version);
+      banner = banner.replace(/13.3.4+2.sha-81611db/, version);
     }
   }
 }
