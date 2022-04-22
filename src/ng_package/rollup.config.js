7663e8c5980ee214416b16f564622b97c954fa3e
Andrew Scott <atscott01@gmail.com>
2022-04-22 16:43:45 +0000

    4962a4a332 feat(router): Allow `loadChildren` to return a `Route` array (#45700)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.14+18.sha-3e46a42/, version);
+      banner = banner.replace(/14.0.0-next.14+19.sha-4962a4a/, version);
     }
   }
 }
