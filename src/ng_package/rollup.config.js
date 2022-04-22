b4282e7cda85a55b8d50b27076a3f44264d623d6
Andrew Scott <atscott01@gmail.com>
2022-04-22 20:04:27 +0000

    970a3b5c70 fix(bazel): add this_is_bazel marker (#45728)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.14+19.sha-4962a4a/, version);
+      banner = banner.replace(/14.0.0-next.14+20.sha-970a3b5/, version);
     }
   }
 }
