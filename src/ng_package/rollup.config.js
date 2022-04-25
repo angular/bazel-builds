d17ff85cdc0954334e079f68d3124e74b03228a2
Andrew Scott <atscott@google.com>
2022-04-25 22:54:06 +0000

    7992132be6 test(core): dynamic component creation and standalone injectors hierarchy (#45726)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.14+22.sha-31d7c3b/, version);
+      banner = banner.replace(/14.0.0-next.14+23.sha-7992132/, version);
     }
   }
 }
