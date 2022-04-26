339dfdb54dd9d7bde5295b5a66786d49dd733dc4
Andrew Scott <atscott01@gmail.com>
2022-04-26 17:25:57 +0000

    4e0784c2f0 refactor(compiler-cli): optimize InjectorDef imports generation (#45701)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.14+23.sha-7992132/, version);
+      banner = banner.replace(/14.0.0-next.14+27.sha-4e0784c/, version);
     }
   }
 }
