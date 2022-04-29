ac53568d34653797e94138bd3eba446cf3618361
Dylan Hunn <dylhunn@gmail.com>
2022-04-29 21:36:46 +0000

    dff5586d52 feat(platform-server): implement `renderApplication` function (#45785)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-e1454ae/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-dff5586/, version);
     }
   }
 }
