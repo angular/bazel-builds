6302e74a3ada0cf2a9a9279b510f465e82174a5e
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 16:41:15 +0000

    6662a97c61 feat(core): allow to throw on unknown elements in tests (#45479)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-8315974/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-6662a97/, version);
     }
   }
 }
