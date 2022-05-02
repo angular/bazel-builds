92de783285832e73c98b6e7559bdd5b4a9ffa464
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 16:39:46 +0000

    ab011b7f84 fix(docs-infra): fix wrong heading structure in aio resources page (#45822)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.5+1.sha-8393925/, version);
+      banner = banner.replace(/13.3.5+2.sha-ab011b7/, version);
     }
   }
 }
