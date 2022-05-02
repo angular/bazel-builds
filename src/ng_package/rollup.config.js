a10086bc65e8ba9c2c4260b51c4855631e00d28c
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:17:21 +0000

    38f2906422 docs(core): add public API docs for the ApplicationRef (#45836)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-22c71be/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-38f2906/, version);
     }
   }
 }
