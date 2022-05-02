e649bb3d0da3fbde91354c90bf52811b8eb20599
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 18:59:49 +0000

    9a04ded85b docs(docs-infra): improve clarity of toh-pt2 click code snippet (#45829)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-e702caf/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-9a04ded/, version);
     }
   }
 }
