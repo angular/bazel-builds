f9997473c57aa58b063bf5e869fd7e41d1827432
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 23:26:26 +0000

    fa755b2a54 fix(core): prevent `BrowserModule` providers from being loaded twice (#45826)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-7005da9/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-fa755b2/, version);
     }
   }
 }
