d707ef46175f2a1e6539e97e76a1548b8b81abc2
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 20:31:12 +0000

    410d81f0e2 refactor(core): disallow standalone components in importProvidersFrom calls (#45837)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-8ba4ddc/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-410d81f/, version);
     }
   }
 }
