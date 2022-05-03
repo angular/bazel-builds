1434d2a045ab78b89bafd7ee0e520a25e397d1ed
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 17:59:31 +0000

    9cf14ff03d feat(compiler-cli): exclude abstract classes from `strictInjectionParameters` requirement (#44615)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-401dec4/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-9cf14ff/, version);
     }
   }
 }
