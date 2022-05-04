86512fc4e8c7f3701c0815afcd93301c87ede790
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:39:52 +0000

    68a6a075f4 build: clean up references to old `master` branch (#45856)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-aef5c92/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-68a6a07/, version);
     }
   }
 }
