c091891345e3dc213f6a2e5a14c73ec74f06d52b
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:45:58 +0000

    949927edda build: remove outdated/obsolete github PR utility scripts (#45868)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-b5834de/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-949927e/, version);
     }
   }
 }
