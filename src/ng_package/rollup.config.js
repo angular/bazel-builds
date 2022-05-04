3782e2ddc30126352eb78987967bba6a8d334f17
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:01:06 +0000

    ea2b3a9dfa docs: update angular package format guide to reflect recent changes (#45725)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-10691c6/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-ea2b3a9/, version);
     }
   }
 }
