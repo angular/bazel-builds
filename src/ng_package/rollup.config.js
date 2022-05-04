7e8a4d7cb7464167f9fcf1abac00014afa0c930c
Joey Perrott <josephperrott.github@gmail.com>
2022-05-04 22:35:14 +0000

    ecae55d279 release: bump the next branch to v14.1.0-next.0

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.16+sha-e79260e/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-ecae55d/, version);
     }
   }
 }
