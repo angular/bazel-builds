3b61cfb0447f6b7bd501055c115701e2176888a0
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:54:03 +0000

    8786c609f8 refactor(docs-infra): Redirect old document title guide to new router feature (#45857)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.16+sha-967e934/, version);
+      banner = banner.replace(/14.0.0-next.16+sha-8786c60/, version);
     }
   }
 }
