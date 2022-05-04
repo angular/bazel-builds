04c3b7e726306f163ee2c5109b3fb62b9163f1af
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:02:24 +0000

    41e2a68e30 feat(router): add type properties to all router events (#44189)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.15+sha-32c625d/, version);
+      banner = banner.replace(/14.0.0-next.15+sha-41e2a68/, version);
     }
   }
 }
