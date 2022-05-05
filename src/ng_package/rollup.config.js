f800e173248bf7c9f30d2df047fdcdc723dd3a80
Andrew Kushnir <akushnir@google.com>
2022-05-05 17:33:47 +0000

    0483c7487c build: update dependency @types/chrome to ^0.0.184 (#45849)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-81f255b/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-0483c74/, version);
     }
   }
 }
