ccddeb991e399f27d839d09ee7f94fb20e16fe79
Andrew Kushnir <akushnir@google.com>
2022-05-05 21:14:45 +0000

    f6543c25aa build: use vendored yarn script for installing Sass repositories (#45892)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.16+sha-3f9faad/, version);
+      banner = banner.replace(/14.0.0-next.16+sha-f6543c2/, version);
     }
   }
 }
