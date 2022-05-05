c597d2ed97b6cd25d43642ff8b378093464ce902
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:06:52 +0000

    c9899637b0 build(docs-infra): remove unused Docker artifacts when updating the preview server (#45895)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.6+2.sha-719199c/, version);
+      banner = banner.replace(/13.3.6+4.sha-c989963/, version);
     }
   }
 }
