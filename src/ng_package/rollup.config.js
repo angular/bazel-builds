1f0dfcadb8610ed65055364d88c76337f9518145
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:15:22 +0000

    323ef1ebf7 docs(docs-infra): improve clarity of toh-pt2 li>button code (#45858)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.16+sha-86f0213/, version);
+      banner = banner.replace(/14.0.0-next.16+sha-323ef1e/, version);
     }
   }
 }
