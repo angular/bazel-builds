02bcf10a2397ef021dfb3d96a0c7110d2d0f9ab1
Andrew Kushnir <akushnir@google.com>
2022-05-06 17:10:02 +0000

    6644165e13 docs: remove redundant word (#45910) PR Close #45910

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.6+6.sha-f9f1f92/, version);
+      banner = banner.replace(/13.3.6+8.sha-6644165/, version);
     }
   }
 }
