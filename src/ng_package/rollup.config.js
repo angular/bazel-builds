0f039574089766f7feeb79ecc1b3a37e3babffde
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:51:59 +0000

    13eb6ca1bf ci: enable the `aio_preview` CircleCI job (#45935)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.16+sha-6ec6c87/, version);
+      banner = banner.replace(/14.0.0-next.16+sha-13eb6ca/, version);
     }
   }
 }
