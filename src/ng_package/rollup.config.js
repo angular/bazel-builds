e66b1f72b09db992b06f9abe1d1c07b1d3e2a887
Andrew Kushnir <akushnir@google.com>
2022-05-09 20:41:54 +0000

    73bbee2c0d perf(core): allow `checkNoChanges` mode to be tree-shaken in production (#45936)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.6+18.sha-02b99f6/, version);
+      banner = banner.replace(/13.3.6+19.sha-73bbee2/, version);
     }
   }
 }
