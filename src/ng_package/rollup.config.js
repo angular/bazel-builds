3b92f3e13147a382f49a33ff62b28c695b333284
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:25:23 +0000

    40b3cf4682 docs: remove unnecesarry backslash from pipes documentation (#45916)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.6+12.sha-5056cfe/, version);
+      banner = banner.replace(/13.3.6+17.sha-40b3cf4/, version);
     }
   }
 }
