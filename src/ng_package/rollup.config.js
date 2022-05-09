cf7a2a116c5135afa27f8ebeb6d2c0bd4decdff9
Andrew Kushnir <akushnir@google.com>
2022-05-09 21:14:54 +0000

    216a966757 docs: simplify the DevTools readme and add publishing instructions (#45905)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-91b833a/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-216a966/, version);
     }
   }
 }
