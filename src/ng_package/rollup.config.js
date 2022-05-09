d9357b0a0a399d7e614d2906c0cbfa1ef235cc84
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:14:03 +0000

    b5d78f4474 refactor(docs-infra): remove unnecessary types for stemmer dependency (#45831)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-c434097/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-b5d78f4/, version);
     }
   }
 }
