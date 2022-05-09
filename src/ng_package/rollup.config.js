78d9c85f63c177e7fe89b27d99308876640ddc4b
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:41:29 +0000

    5056cfe839 docs(docs-infra): clarify toh-2 error message (#45878)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.6+8.sha-6644165/, version);
+      banner = banner.replace(/13.3.6+12.sha-5056cfe/, version);
     }
   }
 }
