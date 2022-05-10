60dd464363d707266995e0f143e235e7dfaa8aca
Andrew Kushnir <akushnir@google.com>
2022-05-10 23:21:02 +0000

    f70b84f755 docs: add NGX-YOUI website to resources list (#45810)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-next.16+sha-1d3cf22/, version);
+      banner = banner.replace(/14.0.0-next.16+sha-f70b84f/, version);
     }
   }
 }
