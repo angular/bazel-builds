85194c957961dd532464e8908d9608aa80ccf2c5
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-05-11 17:35:43 +0000

    72c11f33b5 release: cut the v13.3.7 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.6+26.sha-3626464/, version);
+      banner = banner.replace(/13.3.6+27.sha-72c11f3/, version);
     }
   }
 }
