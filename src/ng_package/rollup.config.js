5460e140ba57454a671fe676769b444edb6dfd18
Andrew Kushnir <akushnir@google.com>
2022-05-11 18:33:12 +0000

    6238f7895f docs: Add a guide for typed forms. (#45939)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-b882d03/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-6238f78/, version);
     }
   }
 }
