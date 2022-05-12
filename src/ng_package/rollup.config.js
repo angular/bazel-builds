fba21e38f34f4697093422a7749cecc9d0365b43
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-12 16:20:11 +0000

    e223649dc5 fix(docs-infra): do not detect a running job as failed (#45948)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.6+27.sha-72c11f3/, version);
+      banner = banner.replace(/13.3.7+1.sha-e223649/, version);
     }
   }
 }
