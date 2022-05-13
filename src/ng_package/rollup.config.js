57cdb357961ac9aa791712f9cbf5778720273c7e
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 18:36:07 +0000

    45fedb50c4 docs: remove Angular 11 from support table (#45980) PR Close #45980

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.0+sha-25fde5d/, version);
+      banner = banner.replace(/14.0.0-rc.0+sha-45fedb5/, version);
     }
   }
 }
