28f37fa45f92d5d96a8e58569067c0210bbc899e
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 16:26:10 +0000

    dfc4852d6c build: manually unshallow the repo for builds publishing, use absolute paths (#45979)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.7+1.sha-e223649/, version);
+      banner = banner.replace(/13.3.8+1.sha-dfc4852/, version);
     }
   }
 }
