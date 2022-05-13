28684c425ac39087d90a2672a26f4c6b15ad427d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 22:22:12 +0000

    57adaded2a docs: add selin tunc to contributors (#45989)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.8+3.sha-c224ada/, version);
+      banner = banner.replace(/13.3.8+4.sha-57adade/, version);
     }
   }
 }
