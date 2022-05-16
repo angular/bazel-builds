6cfe6bedc8ca8808e5ddddf433fee3bca2750ddb
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 17:24:04 +0000

    db1b6fe657 fix(language-service): Fix detection of Angular for v14+ projects (#45998)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-ca04ece/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-db1b6fe/, version);
     }
   }
 }
