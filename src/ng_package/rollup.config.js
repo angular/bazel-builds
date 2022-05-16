5c645a0bdfe147014e5d14b89840d06dd02df781
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 18:23:50 +0000

    db15a0882e docs(devtools): add firefox extension references (#45985)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.8+6.sha-c85bff1/, version);
+      banner = banner.replace(/13.3.8+7.sha-db15a08/, version);
     }
   }
 }
