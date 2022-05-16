4cc949ea83dea96c212c0a650def78ad18af2c9a
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 21:29:39 +0000

    3c690bc1e9 build: use updated commit sha for github actions (#46010)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.0+sha-018550e/, version);
+      banner = banner.replace(/14.0.0-rc.0+sha-3c690bc/, version);
     }
   }
 }
