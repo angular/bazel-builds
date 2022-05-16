864b7ed8f5682ce14012feb2806081e0d2c51493
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 18:55:24 +0000

    018550ed50 fix(forms): Value and RawValue should be part of the public API. (#45978)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.0+sha-39a1364/, version);
+      banner = banner.replace(/14.0.0-rc.0+sha-018550e/, version);
     }
   }
 }
