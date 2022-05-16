1b3673e5aec1fef4cca2bc0532a4b6381bb27452
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 16:50:30 +0000

    bb07582d76 build: set up post approval changes commit action (#46006)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-3e873b5/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-bb07582/, version);
     }
   }
 }
