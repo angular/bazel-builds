f6eed844df49f66bfd9ff7961fa1d65eab0a832d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-17 18:05:03 +0000

    274c6fa643 docs: remove Narco from hero list in code, text, and images (#46008)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-35af7c1/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-274c6fa/, version);
     }
   }
 }
