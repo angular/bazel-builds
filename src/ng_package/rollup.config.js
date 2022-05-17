30fb5c9aa8f78f503f63a0dcc178b5d3cfe0ea26
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-17 16:16:15 +0000

    79025a69ba build: update angular (#46016)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.0+sha-add2bb8/, version);
+      banner = banner.replace(/14.0.0-rc.0+sha-79025a6/, version);
     }
   }
 }
