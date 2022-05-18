1f74017afce06108e0b62c98e3cf3156888f46e6
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-18 16:14:46 +0000

    e92799c97e test(router): Add tests for initialNavigation: enabledBlocking (#45733)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-1e07a11/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-e92799c/, version);
     }
   }
 }
