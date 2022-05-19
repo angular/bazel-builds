341d4e16d415d61bb1e5d37956866d127fdaef46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-19 20:49:33 +0000

    2bc65351b4 docs: fix inline JSDoc tags (@see --> @link) (#46040)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.1+sha-8869fce/, version);
+      banner = banner.replace(/14.0.0-rc.1+sha-2bc6535/, version);
     }
   }
 }
