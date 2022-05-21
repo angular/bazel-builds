0619331e72c9a40f24cee07507078510d43c9b49
Joey Perrott <josephperrott@gmail.com>
2022-05-21 20:39:35 +0000

    ac7fdbd93e build: update saucelabs key (#46089)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/12.2.16+1.sha-68901bf.with-local-changes/, version);
+      banner = banner.replace(/12.2.16+5.sha-ac7fdbd.with-local-changes/, version);
     }
   }
 }
