7e9ae592d05710cd1eb29fedb184f3790264f3a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 16:47:40 +0000

    b11a939e21 docs: add demo video for Angular CLI autocompletion doc (#45995)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.1+sha-183b5f3/, version);
+      banner = banner.replace(/14.0.0-rc.1+sha-b11a939/, version);
     }
   }
 }
