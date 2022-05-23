4d967d3c95962e7adae7e4b6b5ec796ab3c9987c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 17:21:15 +0000

    3172b4cc99 fix(core): produce proper error message for unknown props on `<ng-template>`s (#46068)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.1+sha-b11a939/, version);
+      banner = banner.replace(/14.0.0-rc.1+sha-3172b4c/, version);
     }
   }
 }
