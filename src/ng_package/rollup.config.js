09d7c24cf290789479691714aab4b53a34195e6b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-24 18:10:59 +0000

    4a7e724238 refactor(compiler-cli): fix "for to" typo in code comment (#46113)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.9+13.sha-123782b/, version);
+      banner = banner.replace(/13.3.9+17.sha-4a7e724/, version);
     }
   }
 }
