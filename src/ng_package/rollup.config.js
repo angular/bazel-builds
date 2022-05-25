fe2ec2b9befca457e9b12a7b0151e27a9fd6b8e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 18:42:32 +0000

    a12372a66d docs: update the Angular roadmap as of May 2022 (#46117)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.9+17.sha-4a7e724/, version);
+      banner = banner.replace(/13.3.9+18.sha-a12372a/, version);
     }
   }
 }
