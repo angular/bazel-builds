15a0abeaa771d384e84882bfda7f49b653fd8464
Andrew Scott <atscott01@gmail.com>
2022-05-26 18:44:18 +0000

    3fdcf991c7 build(docs-infra): be able to resolve root-relative image URLs in `getImageDimensions()` (#46138)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-bd6d82b/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-3fdcf99/, version);
     }
   }
 }
