6ed96565641442b714dce9c6a0596d67131e8284
Andrew Scott <atscott01@gmail.com>
2022-05-27 01:00:10 +0000

    8c9051eac9 docs: close lightbox div to fix page display (#46155)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.10+1.sha-286f4d6/, version);
+      banner = banner.replace(/13.3.10+3.sha-8c9051e/, version);
     }
   }
 }
