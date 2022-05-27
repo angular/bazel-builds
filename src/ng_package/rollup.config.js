35cd5489884f2bd21c46541fbdc13c4a365d1b9f
Andrew Scott <atscott01@gmail.com>
2022-05-27 16:47:12 +0000

    2863dff7fa docs: change wrong alert to callout (#46145)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-bb16b19/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-2863dff/, version);
     }
   }
 }
