f1bbcbe4cbcaa02c5dc2ed0a16d8f8b55faaa2b5
Andrew Scott <atscott01@gmail.com>
2022-05-27 17:34:56 +0000

    12b4ec3d85 docs: create a dedicated page for the NG0203 error (#46166)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-f87aa2d/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-12b4ec3/, version);
     }
   }
 }
