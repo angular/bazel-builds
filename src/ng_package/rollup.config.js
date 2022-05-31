71856451762f1313ee30c06c990084de2cf263bf
Andrew Scott <atscott01@gmail.com>
2022-05-31 21:15:49 +0000

    c90193475f docs: add the Standalone guide link to API docs of component/directive/pipe decorators (#46184)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.2+sha-ba9f30c/, version);
+      banner = banner.replace(/14.0.0-rc.2+sha-c901934/, version);
     }
   }
 }
