88dde102bc8e5eccf951c2b01707088fd0591f12
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:26:39 +0000

    536939e138 docs(core): improve standalone-related docs and add more usage examples (#46202)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0-rc.3+sha-763c5f8/, version);
+      banner = banner.replace(/14.0.0-rc.3+sha-536939e/, version);
     }
   }
 }
