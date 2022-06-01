3eff169c3920d8dc38474d86ddfa7de5a3efcf90
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:25:35 +0000

    94c2cfbe25 docs(router): Adjust example for scrollPositionRestoration (#46201)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.10+10.sha-ce2f149/, version);
+      banner = banner.replace(/13.3.11+2.sha-94c2cfb/, version);
     }
   }
 }
