df0f191941dde9d644ad6cfdf41ac291c1844e9c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 22:03:24 +0000

    ed570453da build(docs-infra): update to 14.0.0 (#46227)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0+sha-36983a4/, version);
+      banner = banner.replace(/14.0.0+sha-ed57045/, version);
     }
   }
 }
