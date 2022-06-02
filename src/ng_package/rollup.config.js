1c89bb450082f533652025941f80e0c9b498dabc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 21:00:55 +0000

    0206c10f8e refactor(core): minor ComponentDef improvements (#46093)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-8fb737c/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-0206c10/, version);
     }
   }
 }
