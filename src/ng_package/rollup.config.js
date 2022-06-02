38f6770a6f211cf2fe3d105bd644e6b9ce3e3523
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 19:52:29 +0000

    127e794885 fix(docs-infra): correct developer preview link (#46225)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0+sha-6280a98/, version);
+      banner = banner.replace(/14.0.0+sha-127e794/, version);
     }
   }
 }
