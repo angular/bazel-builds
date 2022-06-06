c4a3f6b1c20705118a2a9d87d2960b1b9c54df4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 19:00:59 +0000

    385b5ec92c docs: removed outdated migration documentation (#46257)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-aa2e8ac/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-385b5ec/, version);
     }
   }
 }
