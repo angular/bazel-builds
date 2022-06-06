dc7d1b232b5ca98394b27a3697a2313a9ded4742
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 23:31:03 +0000

    b20e470704 test: add an internal helper to populate document.head before a test (#46250)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.0+sha-9f5558c/, version);
+      banner = banner.replace(/14.0.0+sha-b20e470/, version);
     }
   }
 }
