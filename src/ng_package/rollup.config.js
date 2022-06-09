39e1da2371f939a754569e7d0a8694ea348354c7
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-09 16:24:33 +0000

    ef5cba3df7 fix(common): allow null in ngComponentOutlet (#46280)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.1+sha-16c71fc/, version);
+      banner = banner.replace(/14.0.1+sha-ef5cba3/, version);
     }
   }
 }
