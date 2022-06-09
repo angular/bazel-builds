87eef0de4b6b20c50b8cd1988d000d45d33a780d
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-09 21:04:33 +0000

    8a42f27d1e docs: update example hero ID (#46304)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-8b9c32d/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-8a42f27/, version);
     }
   }
 }
