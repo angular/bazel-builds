3db002e251c80c5a2b144d0afda9e89a2515a1a9
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 20:59:48 +0000

    f35f4751b5 fix(compiler-cli): use inline type-check blocks for components outside `rootDir` (#46096)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-71b6b9e/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-f35f475/, version);
     }
   }
 }
