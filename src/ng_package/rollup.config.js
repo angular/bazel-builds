e43f726f434a57899e219ae9d7affc0c1c5bc730
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 15:42:43 +0000

    77ae8e1c20 refactor(router): clean up internal hooks (#46321)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.1+sha-8ecfd71/, version);
+      banner = banner.replace(/14.0.1+sha-77ae8e1/, version);
     }
   }
 }
