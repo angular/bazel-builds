d896d28c10b1d977fde1871ecf4a72f62d52069b
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 18:17:29 +0000

    71b6b9e48b docs: Add kevindaviladev to contributors (#46330)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-adb21a2/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-71b6b9e/, version);
     }
   }
 }
