8b9097e64f14557899c05d7fd6c1d329bb4bd88c
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 21:17:10 +0000

    9ab32616aa docs:  Adding a book "Angular from Scratch" (#46352)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-d6880c9/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-9ab3261/, version);
     }
   }
 }
