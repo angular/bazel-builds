e530cb4bbf904af751b45372f18ffca8de22bfc1
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 17:18:17 +0000

    3fd8948b4a fix(core): Resolve forwardRef declarations for jit (#46334)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.1+sha-b92c1a6/, version);
+      banner = banner.replace(/14.0.1+sha-3fd8948/, version);
     }
   }
 }
