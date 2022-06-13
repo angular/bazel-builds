46ba8e247203ca5297f33aa3779c80e8771c6b28
Andrew Kushnir <akushnir@google.com>
2022-06-13 22:18:48 +0000

    5af8e46501 refactor(common): update peer deps version of `@angular/core`

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.11+sha-9dd43f2/, version);
+      banner = banner.replace(/13.3.11+sha-5af8e46/, version);
     }
   }
 }
