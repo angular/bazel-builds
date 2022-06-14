2603afc5f8aba9eae3a2a6d387f84a9fb4416afb
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-14 20:14:53 +0000

    d2470cc568 docs: fix code style (#45527)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.1+sha-ce74bea/, version);
+      banner = banner.replace(/14.0.1+sha-d2470cc/, version);
     }
   }
 }
