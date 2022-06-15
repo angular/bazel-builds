1f46f7accb57318c9115503af4a6a9219acbd9ad
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-15 16:16:34 +0000

    7a376466b3 feat(docs-infra): show dark scrollbars in dark mode (#46363)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-736b545/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-7a37646/, version);
     }
   }
 }
