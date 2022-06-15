27a56783e5448609f04977821caff61ad9bf7627
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-06-15 06:22:42 +0000

    485c6a7169 Revert "fix(common): sanitize `rawSrc` and `rawSrcset` values in NgOptimizedImage directive"

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-8afc623/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-485c6a7/, version);
     }
   }
 }
