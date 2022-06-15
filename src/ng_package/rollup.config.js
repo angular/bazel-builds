ad33ae0f77ec15c0858faf4e799330c2c671d4c0
Andrew Kushnir <akushnir@google.com>
2022-06-15 21:23:55 +0000

    14b96287e9 refactor(common): align tree shakable error messages to new format (#46370) Align tree shakable error messages are simplified with the new format and errorMessage variables are removed.

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.2+sha-88e1655/, version);
+      banner = banner.replace(/14.0.2+sha-14b9628/, version);
     }
   }
 }
