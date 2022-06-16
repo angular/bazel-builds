2cc37edfcac6a100aaf630157ab46b8d76c236d9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-16 16:45:21 +0000

    2b93a74f4f refactor(common): throw an error if an absolute URL is passed to Image Loaders

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.0+sha-485c6a7/, version);
+      banner = banner.replace(/14.1.0-next.0+sha-2b93a74/, version);
     }
   }
 }
