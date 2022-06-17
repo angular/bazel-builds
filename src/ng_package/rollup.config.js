bdb86ac9aa37ee320180f13cd1507c6004ec0d2c
Andrew Kushnir <akushnir@google.com>
2022-06-17 16:38:54 +0000

    f94c6f433d fix(router): Expose CanMatchFn as public API (#46394)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.1+sha-bb7c804/, version);
+      banner = banner.replace(/14.1.0-next.1+sha-f94c6f4/, version);
     }
   }
 }
