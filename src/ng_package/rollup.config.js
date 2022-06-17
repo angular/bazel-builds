810a27ac6bbe77a273dab9dd8d779b4d345a1875
Andrew Kushnir <akushnir@google.com>
2022-06-17 16:36:19 +0000

    bb7c80477b fix(core): make parent injector argument required in `createEnvironmentInjector` (#46397)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.1+sha-82acbf9/, version);
+      banner = banner.replace(/14.1.0-next.1+sha-bb7c804/, version);
     }
   }
 }
