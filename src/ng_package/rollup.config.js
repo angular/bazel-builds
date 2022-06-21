03715a61659e7fb9f154d09ca49a3382fba90c4c
Andrew Kushnir <akushnir@google.com>
2022-06-21 21:39:26 +0000

    7a084c8134 docs: add dany paredes to GDE resources (#46411)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.2+sha-c086653/, version);
+      banner = banner.replace(/14.0.2+sha-7a084c8/, version);
     }
   }
 }
