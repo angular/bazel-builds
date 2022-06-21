1903edc946abd1e50c5d0bc30bce53750bc5e943
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:04:53 +0000

    1aa27ebbad docs: fix base href haser tag (#46431) I add '>' end of header tag Fixes #46430

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.1+sha-88f53dc/, version);
+      banner = banner.replace(/14.1.0-next.1+sha-1aa27eb/, version);
     }
   }
 }
