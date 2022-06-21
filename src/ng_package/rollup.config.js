2942b8d6a4bd89a55c02b484684d4c6c060d866f
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:06:21 +0000

    ffedcdec08 build: replace copied `spec_bundle` rule in devtools with dev-infra rule (#46437)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.1+sha-e8a33e7/, version);
+      banner = banner.replace(/14.1.0-next.1+sha-ffedcde/, version);
     }
   }
 }
