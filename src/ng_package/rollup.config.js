6ba3988f3e83c264916d5d8f5b45225acbd4e741
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:15:51 +0000

    6ad7b40a6f fix(platform-server): invalid style attribute being generated for null values (#46433)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.2+sha-8fac299/, version);
+      banner = banner.replace(/14.0.2+sha-6ad7b40/, version);
     }
   }
 }
