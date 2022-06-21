01bca659813761fbaa048bd6c29a848149a2763a
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:59:56 +0000

    ddd6d66203 fix(core): deduplicate imports of standalone components in JIT compiler (#46439)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.1+sha-342effe/, version);
+      banner = banner.replace(/14.1.0-next.1+sha-ddd6d66/, version);
     }
   }
 }
