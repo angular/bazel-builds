aba7001a2d0d4f8fbf2f9e085c80beaefc46df2c
Andrew Kushnir <akushnir@google.com>
2022-06-22 22:11:20 +0000

    63396a1ea7 build: avoid use of `ts-node` wrapper script as it breaks child process forking (#46456)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.1+sha-2d713f5/, version);
+      banner = banner.replace(/14.1.0-next.1+sha-63396a1/, version);
     }
   }
 }
