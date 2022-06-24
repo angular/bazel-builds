7fef401577f3a45d4d7a760482e5951b55b29ea6
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:29:52 +0000

    7558c558e0 feat(docs-infra): redesign contributors page (#46347)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-7da389a/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-7558c55/, version);
     }
   }
 }
