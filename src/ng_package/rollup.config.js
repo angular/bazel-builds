d92810a21a50f0852de6a3274a2a3942d089dc08
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:46:42 +0000

    0c66032608 docs(forms): add usage notes (#46472)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.3+sha-be43177/, version);
+      banner = banner.replace(/14.0.3+sha-0c66032/, version);
     }
   }
 }
