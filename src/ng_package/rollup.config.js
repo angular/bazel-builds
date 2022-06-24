02c0f89cd3db1e4767d01dbf657f41d8135e650b
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:28:19 +0000

    dedbc2c5d5 refactor(forms): remove unused internal function (#46479)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-14e24ac/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-dedbc2c/, version);
     }
   }
 }
