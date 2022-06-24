bc01038bafa75d9c57fea965183bc0c5567df04c
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:34:29 +0000

    1c39aace0e fix(common): handle CSS custom properties in NgStyle (#46451)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-0782852/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-1c39aac/, version);
     }
   }
 }
