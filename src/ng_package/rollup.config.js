cabdf40bfcbdd80e3d4df05b0a67bd4ff5fdabd0
Dylan Hunn <dylhunn@gmail.com>
2022-06-25 00:22:15 +0000

    e9e1d8595f docs(core): add circleci insights metrics widget (#46496)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.3+sha-f930fc2/, version);
+      banner = banner.replace(/14.0.3+sha-e9e1d85/, version);
     }
   }
 }
