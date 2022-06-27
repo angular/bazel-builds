b767ef6c0c285bbcb169e7824922c1eec75f8958
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 16:24:41 +0000

    0360cde473 build: remove unused script from legacy build scripts (#46506)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-05254f5/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-0360cde/, version);
     }
   }
 }
