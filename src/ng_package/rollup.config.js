6d2b8d5c7ab5d428e4f7e689c0ab178c5167b6a8
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 22:34:20 +0000

    acf6dc9031 test(core): update `di_spec.ts` to use TestBed (#46522)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-8ed502a/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-acf6dc9/, version);
     }
   }
 }
