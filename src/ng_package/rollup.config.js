6aa093d570ec3c500e616512eeb6b0a98547a5a7
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 20:14:42 +0000

    a7597dd080 feat(common): make the CommonModule directives standalone (#46469)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-ee39cf6/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-a7597dd/, version);
     }
   }
 }
