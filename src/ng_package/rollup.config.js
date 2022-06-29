c289167efa57fe603602d3a3fbfd7551aa2d8f40
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 18:41:50 +0000

    97110674f3 release: cut the v14.1.0-next.3 release (#46622)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-66baa8c/, version);
+      banner = banner.replace(/14.1.0-next.3+sha-9711067/, version);
     }
   }
 }
