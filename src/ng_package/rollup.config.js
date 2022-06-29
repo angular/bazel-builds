23ca342609d247350fcc6194792071f058eb9b8f
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:25:47 +0000

    b1ac3d9968 refactor: clean-up pre-CLDR 39 safety check (#46606)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-957744f/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-b1ac3d9/, version);
     }
   }
 }
