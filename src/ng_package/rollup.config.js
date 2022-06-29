5ab3d3bf3910cc342bbb09f3c7fd695bf3cdc61a
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:27:31 +0000

    2758a30a34 build: update dependency @rollup/plugin-commonjs to v22 (#46556) PR Close #46556

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-a33d4ee/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-2758a30/, version);
     }
   }
 }
