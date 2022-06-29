6a79d1566eedd6de4e33ac8e7e4b0cf092e2f56b
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 08:12:45 +0000

    1289425cf8 test: update golden symbols (#46582)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.2+sha-6a53034/, version);
+      banner = banner.replace(/14.1.0-next.2+sha-1289425/, version);
     }
   }
 }
