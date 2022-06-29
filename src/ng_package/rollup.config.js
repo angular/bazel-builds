f333e2591a1f26475f72641535d78124c783803b
Andrew Scott <atscott01@gmail.com>
2022-06-29 22:38:20 +0000

    a3bd65e2b8 fix(router): Ensure `APP_INITIALIZER` of `enabledBlocking` option completes (#46634)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.4+sha-88298ca/, version);
+      banner = banner.replace(/14.0.4+sha-a3bd65e/, version);
     }
   }
 }
