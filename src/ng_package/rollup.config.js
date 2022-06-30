768021debd05fa4d0ade37953f6951eed3648bf4
Andrew Scott <atscott01@gmail.com>
2022-06-30 20:03:26 +0000

    1bd0b5665d refactor(core): remove experimental renderComponent function (#46646)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.4+sha-a065b6e/, version);
+      banner = banner.replace(/14.0.4+sha-1bd0b56/, version);
     }
   }
 }
