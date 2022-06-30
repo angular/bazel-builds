09f20b31f966ab47558d112d621e71502d357dbe
Andrew Scott <atscott01@gmail.com>
2022-06-30 21:26:27 +0000

    6f11a58040 feat(compiler): Add extended diagnostic to warn when text attributes are intended to be bindings (#46161)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.3+sha-5dd9d4a/, version);
+      banner = banner.replace(/14.1.0-next.3+sha-6f11a58/, version);
     }
   }
 }
