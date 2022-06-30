7eb4061ca42654b787dec57fd9d084623b33989b
Andrew Scott <atscott01@gmail.com>
2022-06-30 16:15:10 +0000

    a065b6e418 build: update dependency husky to v8 (#46616)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.4+sha-a3bd65e/, version);
+      banner = banner.replace(/14.0.4+sha-a065b6e/, version);
     }
   }
 }
