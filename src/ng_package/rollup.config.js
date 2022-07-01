0a2ccf04dc7b15966626e7556f097cd3379bfe69
Andrew Scott <atscott01@gmail.com>
2022-07-01 17:37:31 +0000

    ee2e4917e6 docs: Update runGuardsAndResolvers documentation (#46671)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.3+sha-a7a14df/, version);
+      banner = banner.replace(/14.1.0-next.3+sha-ee2e491/, version);
     }
   }
 }
