3c73bf39d94f0700a949211678a90788f90927e4
Andrew Scott <atscott01@gmail.com>
2022-07-01 20:43:49 +0000

    dd3e0968ec refactor(router): Remove custom error message for invalid guards (#46678)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.3+sha-915e82d/, version);
+      banner = banner.replace(/14.1.0-next.3+sha-dd3e096/, version);
     }
   }
 }
