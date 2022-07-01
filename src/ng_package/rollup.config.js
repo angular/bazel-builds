bdc4e2528460876c3cf876a0cc5ef7115120dca8
Andrew Scott <atscott01@gmail.com>
2022-07-01 17:41:00 +0000

    853b1c1bc9 docs: move section on subscribing to observable (#46655)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.4+sha-7fe81a9/, version);
+      banner = banner.replace(/14.0.4+sha-853b1c1/, version);
     }
   }
 }
