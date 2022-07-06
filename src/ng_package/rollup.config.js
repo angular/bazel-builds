c1c6477ea126d0948b32a1d93ffdbd3497e45da8
Andrew Scott <atscott01@gmail.com>
2022-07-06 14:49:12 +0000

    e96ece901c build: update dependency concurrently to v7 (#46698)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.3+sha-5fd3bb6/, version);
+      banner = banner.replace(/14.1.0-next.3+sha-e96ece9/, version);
     }
   }
 }
