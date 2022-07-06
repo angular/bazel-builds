b5e40a9df6535f6fab6f11f9dd01dbc5a805f7f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-06 23:13:07 +0000

    ad2721a149 build: update cross-repo angular dependencies (#46607)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.4+sha-42b6d23/, version);
+      banner = banner.replace(/14.1.0-next.4+sha-ad2721a/, version);
     }
   }
 }
