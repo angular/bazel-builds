679a6e85591c9acae0ad053530bd472a3a1b5035
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-06 23:06:06 +0000

    392c56749a fix(localize): add `--project` option to `ng-add` schematic (#46664)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.4+sha-a588b2f/, version);
+      banner = banner.replace(/14.1.0-next.4+sha-392c567/, version);
     }
   }
 }
