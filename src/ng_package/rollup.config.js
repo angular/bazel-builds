22ccc4581ab7bd8f1e9c4dcabf3970daaa4bc4ca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-07 21:09:32 +0000

    72d1affbdd docs(animations): add section about animations and view encapsulation (#46738)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.5+sha-ae1264b/, version);
+      banner = banner.replace(/14.0.5+sha-72d1aff/, version);
     }
   }
 }
