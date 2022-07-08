a261d7e8f19129aa6a50ce4a1507832ffbf5bcf1
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 22:17:34 +0000

    28bd6dd3d8 build: update eslint dependencies to v5.30.5 (#46705)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.5+sha-50126af/, version);
+      banner = banner.replace(/14.0.5+sha-28bd6dd/, version);
     }
   }
 }
