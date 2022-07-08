6fb910333682c6b0486d9d68babf3ac9f6e9b2eb
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 16:36:32 +0000

    a4ce273e50 feat(router): Add the target `RouterStateSnapshot` to `NavigationError` (#46731)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.4+sha-bd92723/, version);
+      banner = banner.replace(/14.1.0-next.4+sha-a4ce273/, version);
     }
   }
 }
