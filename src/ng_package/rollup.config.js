503dffc7b91f2fc87664e8414b28432734f109d7
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 18:50:45 +0000

    9e836c232f feat(compiler): warn when style suffixes are used with attribute bindings (#46651)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.4+sha-ea0f07a/, version);
+      banner = banner.replace(/14.1.0-next.4+sha-9e836c2/, version);
     }
   }
 }
