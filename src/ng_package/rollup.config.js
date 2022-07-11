b86d60806f9884d1351c1010ecaf7fc6dbeda9bf
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 20:20:35 +0000

    d51e0f27d7 docs(core): add full example of directive with typed context (#46689)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.5+sha-ec1ff66/, version);
+      banner = banner.replace(/14.0.5+sha-d51e0f2/, version);
     }
   }
 }
