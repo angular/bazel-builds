8ee18ab71fc6792306211dd826a6b6f94421d999
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 23:47:05 +0000

    98b41bf399 docs: add nicobytes to GDE resources and Platzi to Online Training (#46668)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.5+sha-023c218/, version);
+      banner = banner.replace(/14.0.5+sha-98b41bf/, version);
     }
   }
 }
