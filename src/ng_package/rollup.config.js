9a64986fd95548d8fece614b1e34267578b3d19b
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 21:13:29 +0000

    7b9269c5e4 build: account for integration test rule change (#46778)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.4+sha-19e6d9c/, version);
+      banner = banner.replace(/14.1.0-next.4+sha-7b9269c/, version);
     }
   }
 }
