0193820abde2a2a1153660040a6e516423e3e2ce
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 19:26:56 +0000

    10495a8405 docs: Add missing period (#46799)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-next.4+sha-f6859e8/, version);
+      banner = banner.replace(/14.1.0-next.4+sha-10495a8/, version);
     }
   }
 }
