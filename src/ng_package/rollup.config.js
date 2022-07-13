8c75c66bb93614a09875f3c6484cb22542013e2e
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 21:43:35 +0000

    3f2d16c823 fix(compiler): inputs/outputs incorrectly parsed in jit mode (#46813)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-rc.0+sha-4dca184/, version);
+      banner = banner.replace(/14.1.0-rc.0+sha-3f2d16c/, version);
     }
   }
 }
