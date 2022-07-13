d89abe7fa2f7590fb2b34e30ad63b3949085132e
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 00:21:53 +0000

    fd15744fca docs: add warning about lack of testing used by TOH (#46811)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.5+sha-862dd03/, version);
+      banner = banner.replace(/14.0.5+sha-fd15744/, version);
     }
   }
 }
