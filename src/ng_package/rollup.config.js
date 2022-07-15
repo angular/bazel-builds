58c21880ad1b28367df4e628b7daf178a64177b0
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-15 21:57:38 +0000

    60d08b78aa Revert "Revert "build: update cross-repo angular dependencies to 84dd092 (#46848)" (#46849)" (#46852)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-b2a7446/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-60d08b7/, version);
     }
   }
 }
