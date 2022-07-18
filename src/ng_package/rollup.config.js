496a0d1f6afb5626ef3d7edc714424a02c0945fc
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 22:02:34 +0000

    65a28a9ea3 fix(animations): make sure falsy values are added to _globalTimelineStyles (#46863)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-7882746/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-65a28a9/, version);
     }
   }
 }
