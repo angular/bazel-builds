d64b4ff0637ef78edbe37cc59d213a621e327427
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 16:59:49 +0000

    a0b4a92552 build: fix forms size tracking numbers (#46871)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-557cf7d/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-a0b4a92/, version);
     }
   }
 }
