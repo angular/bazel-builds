04b0e2546ea1660043ad7a9f2779110354a234aa
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-07-18 17:36:30 +0000

    5004b6ebf1 test: update tests to not run jasmine `done` function in sync-test zone from `describe` block

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-rc.0+sha-2ca404d/, version);
+      banner = banner.replace(/14.1.0-rc.0+sha-5004b6e/, version);
     }
   }
 }
