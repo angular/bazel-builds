e6ea3c87010027f694fcf6bee09da6ee6108a345
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-19 16:21:27 +0000

    88003e27db build: update github/codeql-action action to v2.1.16 (#46885)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-rc.0+sha-e95ee96/, version);
+      banner = banner.replace(/14.1.0-rc.0+sha-88003e2/, version);
     }
   }
 }
