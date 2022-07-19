bb3ae2726da84d6042b544e9d8f3acaff5701f12
Andrew Kushnir <akushnir@google.com>
2022-07-19 16:54:27 +0000

    14081dc48d fix(core): Fix `runInContext` for `NgModuleRef` injector (#46877)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-e736f95/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-14081dc/, version);
     }
   }
 }
