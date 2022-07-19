1c8494bf6e50a23268393a1a4724d856f7a56dad
Andrew Kushnir <akushnir@google.com>
2022-07-19 16:59:01 +0000

    b431b5c6d6 test: convert `source-map` core tests to use `async/await` instead of `fakeAsync` (#46888)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0-rc.0+sha-88003e2/, version);
+      banner = banner.replace(/14.1.0-rc.0+sha-b431b5c/, version);
     }
   }
 }
