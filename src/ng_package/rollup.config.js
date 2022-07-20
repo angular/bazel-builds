b455013ffd5617e9056b20d9a12db09c71ad5d40
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-07-20 21:47:05 +0000

    b5afd62a0b feat(common): explain why width/height is required

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-186245a/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-b5afd62/, version);
     }
   }
 }
