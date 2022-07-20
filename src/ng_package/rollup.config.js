ad8cee8def699855ab3acaac0a717bcb422d5c50
Andrew Kushnir <akushnir@google.com>
2022-07-20 16:08:59 +0000

    3c43d42dac refactor(bazel): remove `experimental_extended_template_diagnostics` flag (#46898)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.0.6+sha-d361eaa/, version);
+      banner = banner.replace(/14.0.6+sha-3c43d42/, version);
     }
   }
 }
