1eb73713d8f64ea0692826ff81187053f8ded8a6
Pawel Kozlowski <pkozlowski@google.com>
2022-07-21 10:41:08 +0000

    24f63fa4e3 build: cut `zone.js` v0.11.7 (#46911)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-10289f1/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-24f63fa/, version);
     }
   }
 }
