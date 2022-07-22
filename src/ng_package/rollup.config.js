44476dbf43f790fcce5836e7458d7b9b59f9e7e9
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 08:43:17 +0000

    1eb77a2f4f docs: add simona cotin to contributors (#46922)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0+sha-55f485b/, version);
+      banner = banner.replace(/14.1.0+sha-1eb77a2/, version);
     }
   }
 }
