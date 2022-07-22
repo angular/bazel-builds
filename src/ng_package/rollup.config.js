b89457a82e709478f01acb1e02a81373c65fd514
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 15:56:06 +0000

    52bbf7fd60 build: update doc approvers (#46939)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-d4c9523/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-52bbf7f/, version);
     }
   }
 }
