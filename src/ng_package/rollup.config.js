5790bb25e5c550da60654cd01b6e319a6a3bd2b8
Andrew Kushnir <akushnir@google.com>
2022-08-01 20:12:35 +0000

    e2ab99b95e fix(common): allow null/undefined to be passed to ngClass input (#39280) (#46906)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-8600732/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-e2ab99b/, version);
     }
   }
 }
