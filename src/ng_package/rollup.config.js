983fb0dcefdd6911066ba56785e0c97443e5f7ab
Andrew Kushnir <akushnir@google.com>
2022-08-01 18:35:23 +0000

    4ac2196695 docs: fixed typos (#47008)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-af4f5df/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-4ac2196/, version);
     }
   }
 }
