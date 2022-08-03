15dd0dccd42c0fbcf4c63b44b1d9bd7acdb767a4
Andrew Kushnir <akushnir@google.com>
2022-08-03 01:23:35 +0000

    efb8e3e431 docs(core): Update inject error documentation (#47020)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.0+sha-8085fe3/, version);
+      banner = banner.replace(/14.1.0+sha-efb8e3e/, version);
     }
   }
 }
