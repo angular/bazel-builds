c08f7ce985d8225d5385867c61f69b766a52f94b
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 20:24:47 +0000

    de1e28006b build: update io_bazel_rules_sass digest to 70ffe01 (#47023)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-c360b43/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-de1e280/, version);
     }
   }
 }
