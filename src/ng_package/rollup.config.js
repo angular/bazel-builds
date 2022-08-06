d9fbe2085f3c6bffdcc22f3a26105c0c4baeb98b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-08-06 00:02:35 +0000

    860fee8425 docs: add description and usage notes to the NgOptimizedImage directive

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-6174151/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-860fee8/, version);
     }
   }
 }
