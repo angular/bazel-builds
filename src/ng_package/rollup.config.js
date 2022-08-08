941f0879feebe407cfba82b1ecec36110d7a667d
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:51:00 +0000

    340fa465ce docs: add more documentation about lint messages (#47019)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-621c388/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-340fa46/, version);
     }
   }
 }
