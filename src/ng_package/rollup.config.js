017268cecc7244764928b23783c999449fdaf8f9
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 16:43:49 +0000

    d14235cfb1 ci: invalidate circleci caches to prune nested `node_modules` (#46707)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.1+sha-387f4a7/, version);
+      banner = banner.replace(/14.1.1+sha-d14235c/, version);
     }
   }
 }
