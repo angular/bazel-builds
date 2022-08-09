5fd2f7a6b8aeb28f0c42d8de8ce1cfc7ce89485c
Dylan Hunn <dylhunn@gmail.com>
2022-08-09 17:19:44 +0000

    58e604ceea release: cut the zone.js-0.11.8 release (#47084)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-next.0+sha-b59fe01/, version);
+      banner = banner.replace(/14.2.0-next.0+sha-58e604c/, version);
     }
   }
 }
