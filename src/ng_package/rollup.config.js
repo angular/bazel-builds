7b87047dd7a552fefe3dbdaac21db39798718279
Joey Perrott <josephperrott@gmail.com>
2022-08-15 15:35:34 +0000

    3d9455ee8b build(docs-infra): upgrade cli command docs sources to 07471d230 (#47149)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.2+sha-970e68a/, version);
+      banner = banner.replace(/14.1.2+sha-3d9455e/, version);
     }
   }
 }
