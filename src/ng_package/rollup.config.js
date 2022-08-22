0dbc7933d934660b6d1eb3c6cefdd5660254b2d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:09:48 +0000

    05f5e8ad86 fix(animations): fix stagger timing not handling params (#47208)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.3.0-next.0+sha-876ba77/, version);
+      banner = banner.replace(/14.3.0-next.0+sha-05f5e8a/, version);
     }
   }
 }
