0780d9ea69c742f9b6ca83bafc0fb67bb1c84617
Joey Perrott <josephperrott@gmail.com>
2022-08-22 13:22:04 +0000

    b13c991ce7 fix(docs-infra): avoid distorting contributor images (#47215)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.3.0-next.0+sha-a360309/, version);
+      banner = banner.replace(/14.3.0-next.0+sha-b13c991/, version);
     }
   }
 }
