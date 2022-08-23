e0df98d3912226a26df5a443a252bd68a5ff8ad8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 23:37:11 +0000

    09834f9478 docs: add Angular & JavaScript Master Podcast to resources list (#47227) PR Close #47227

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.1.3+sha-16855bd/, version);
+      banner = banner.replace(/14.1.3+sha-09834f9/, version);
     }
   }
 }
