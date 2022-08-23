0b766031ed472ad395e2e0aa94543aa853067dc6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 16:40:04 +0000

    4780f1bf26 ci: do not match dev-infra group for pullapprove owner changes (#47216)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.3.0-next.0+sha-c5b3f1c/, version);
+      banner = banner.replace(/14.3.0-next.0+sha-4780f1b/, version);
     }
   }
 }
