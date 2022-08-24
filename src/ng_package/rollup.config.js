120504717bbcbfe1f5d564eb9e07702f3b34584a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-24 19:38:20 +0000

    c5db867ddc fix(common): remove duplicate deepForEach (#47189)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0-rc.0+sha-9e02af8/, version);
+      banner = banner.replace(/14.2.0-rc.0+sha-c5db867/, version);
     }
   }
 }
