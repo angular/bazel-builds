5706a2ff1b10b390ca568663cfb9db7ddab7d6aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-25 21:12:39 +0000

    3d7deabca6 build: update ng-dev version (#47258)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.3.0-next.0+sha-0afb895/, version);
+      banner = banner.replace(/14.3.0-next.0+sha-3d7deab/, version);
     }
   }
 }
