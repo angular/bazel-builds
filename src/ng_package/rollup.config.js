df712beaf5ce839b357bcc6b88d19709e21729c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-26 00:50:38 +0000

    9dcfeb2dff release: bump the next branch to v15.0.0-next.0 (#47267)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.3.0-next.0+sha-d037423/, version);
+      banner = banner.replace(/15.0.0-next.0+sha-9dcfeb2/, version);
     }
   }
 }
