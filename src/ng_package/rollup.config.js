6c8a32eea59f172957bdc66702714e89c3d56788
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-26 16:38:25 +0000

    32e3081b5d docs(common): fix code examples in image directive guide (#47269)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.0+sha-46f5021/, version);
+      banner = banner.replace(/15.0.0-next.0+sha-32e3081/, version);
     }
   }
 }
