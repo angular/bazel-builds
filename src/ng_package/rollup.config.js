39bb5deaa5f372c938387be9a409abcc96771961
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-02 02:37:23 +0000

    af975d5bef docs(http): replace http method return description (#47322)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0+sha-4ac5ea3/, version);
+      banner = banner.replace(/14.2.0+sha-af975d5/, version);
     }
   }
 }
