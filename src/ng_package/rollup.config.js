b422a2a9e65728315b3fa232d1c2f07631acc0d8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-03 18:18:50 +0000

    626340b2a6 docs(http): Update HttClient "JSON object" to "JavaScript object" (#47325)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.0+sha-af975d5/, version);
+      banner = banner.replace(/14.2.0+sha-626340b/, version);
     }
   }
 }
