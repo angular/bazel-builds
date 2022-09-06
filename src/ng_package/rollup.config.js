1d30beb81969eeeb7a0fb691022c27d13fde0069
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:42:16 +0000

    d077d58139 docs(service-worker): add note regarding assets and CORS (#47262)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.0+sha-33bb34b/, version);
+      banner = banner.replace(/15.0.0-next.0+sha-d077d58/, version);
     }
   }
 }
