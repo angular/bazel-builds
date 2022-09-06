549d48d6f4684bc1adb29bb605b1f357c53c68cb
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:06:54 +0000

    9fbf810744 build: add zone.js to bug-report template for github (#47213)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.0+sha-385471d/, version);
+      banner = banner.replace(/15.0.0-next.0+sha-9fbf810/, version);
     }
   }
 }
