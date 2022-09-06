228680547fd4e71ea929684009ca38cbb7ab81b0
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:15:43 +0000

    35f93bd409 refactor(router): remove unnecesary `null` (#47238)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.0+sha-d36fd3d/, version);
+      banner = banner.replace(/15.0.0-next.0+sha-35f93bd/, version);
     }
   }
 }
