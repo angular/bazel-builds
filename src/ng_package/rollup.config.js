5bf881b54c1b5a6e1fb8a3ad8c8818100ed33b6a
Dylan Hunn <dylhunn@gmail.com>
2022-09-08 16:53:05 +0000

    a3e1303f04 fix(core): imply @Optional flag when a default value is provided (#47242)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.1+sha-16ba9aa/, version);
+      banner = banner.replace(/14.2.1+sha-a3e1303/, version);
     }
   }
 }
