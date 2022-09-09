b9b95bb95827531475043c5295e02fe5df154624
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:46:41 +0000

    4a13210ecd fix(forms): don't prevent default behavior for forms with method="dialog" (#47308)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.1+sha-ca7bf65/, version);
+      banner = banner.replace(/15.0.0-next.1+sha-4a13210/, version);
     }
   }
 }
