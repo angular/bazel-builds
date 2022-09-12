2875228f2477b8297ac1ea70d30d98076d9070b3
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 20:43:40 +0000

    c26854b733 docs: update prompts in the doc issue template (#47389)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.1+sha-f0f809c/, version);
+      banner = banner.replace(/15.0.0-next.1+sha-c26854b/, version);
     }
   }
 }
