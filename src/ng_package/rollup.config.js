05c1dc1a2c538f586de0ccb02ed7dd6d7f00392c
Dylan Hunn <dylhunn@gmail.com>
2022-09-13 16:38:07 +0000

    bba5abd050 docs(core): fix viewProviders & providers typos in NodeInjectorFactory (#47408)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.1+sha-6c7cd6b/, version);
+      banner = banner.replace(/15.0.0-next.1+sha-bba5abd/, version);
     }
   }
 }
