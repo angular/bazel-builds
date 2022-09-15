fae0d520e16a5359b4b8094fed7333ef07fb19ba
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 03:09:00 +0000

    d4e1691921 docs: release notes for the v14.2.2 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.1+sha-0a5b65d/, version);
+      banner = banner.replace(/15.0.0-next.1+sha-d4e1691/, version);
     }
   }
 }
