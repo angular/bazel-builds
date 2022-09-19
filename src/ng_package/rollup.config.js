9431d0a1717d49142c8af6b8ce8262bf23a9d07d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:17:26 +0000

    b6c90e8e0b fix(devtools): fix positioning of property explorer expansion icon (#47446)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.2+sha-a192c2d/, version);
+      banner = banner.replace(/15.0.0-next.2+sha-b6c90e8/, version);
     }
   }
 }
