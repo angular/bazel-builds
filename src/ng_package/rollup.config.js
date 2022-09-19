45c0d2a3f8fa1152fece7135c1e59d8eb667573c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 17:15:23 +0000

    150dd3092b docs: add fatima amzil to contributors (#47097)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.2+sha-8a3054c/, version);
+      banner = banner.replace(/15.0.0-next.2+sha-150dd30/, version);
     }
   }
 }
