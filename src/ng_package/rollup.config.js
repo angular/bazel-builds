2e8c1478d5cb49e49fc17e00190df1ac11bb9eb1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:38:58 +0000

    c9991ca49d build: update dependency google-closure-compiler to v20220905 (#47437)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.2+sha-53db8ab/, version);
+      banner = banner.replace(/15.0.0-next.2+sha-c9991ca/, version);
     }
   }
 }
