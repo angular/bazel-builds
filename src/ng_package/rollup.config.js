82c28945a9c291f1e6e9d5401a0d6987fe4e1a3d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 08:00:34 +0000

    3f4cbe4ca7 docs: update contribution docs for serving a local library/project (#47421)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.2+sha-db3b532/, version);
+      banner = banner.replace(/14.2.2+sha-3f4cbe4/, version);
     }
   }
 }
