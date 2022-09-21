0ed2aa0cb996cb01fe25f9c18bed195d887fb986
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 14:21:25 +0000

    8cef5dd7c1 build: update all non-major dependencies (#47487)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.2+sha-06ca0dc/, version);
+      banner = banner.replace(/15.0.0-next.2+sha-8cef5dd/, version);
     }
   }
 }
