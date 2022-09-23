0627414448bff186d733a56fe64140b20f75b39a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:18:17 +0000

    aef8d0bb02 fix(core): correctly check for `typeof` of undefined in `ngDevMode` check (#47480)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.3+sha-ddef448/, version);
+      banner = banner.replace(/15.0.0-next.3+sha-aef8d0b/, version);
     }
   }
 }
