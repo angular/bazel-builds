1856ad3b5861b14ef1bbbf721d4c14f0ca7af50d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:17:45 +0000

    85330f3fd9 fix(core): update `isDevMode` to rely on `ngDevMode` (#47475)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.3+sha-3fe21a6/, version);
+      banner = banner.replace(/15.0.0-next.3+sha-85330f3/, version);
     }
   }
 }
