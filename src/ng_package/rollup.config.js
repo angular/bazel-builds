558b2a5f75f8ecd6dd2bb1ba72e49462f3b59487
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:15:55 +0000

    3fe21a67ca refactor(core): expose host directives to their host through DI (#47476)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.3+sha-7a5ba93/, version);
+      banner = banner.replace(/15.0.0-next.3+sha-3fe21a6/, version);
     }
   }
 }
