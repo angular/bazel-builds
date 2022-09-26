5ec4ce86239264b6a35d4e8e5bf734a968bd6260
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-26 21:19:33 +0000

    85b5d125be build: fix rxjs 7 build failure (#47548)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.3+sha-f9e21d3/, version);
+      banner = banner.replace(/15.0.0-next.3+sha-85b5d12/, version);
     }
   }
 }
