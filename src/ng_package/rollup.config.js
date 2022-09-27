58845327e5a2ca2d60cc62a356fa256ace13bca0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 18:11:31 +0000

    002ee3226c refactor(core): support applying directives to the root component (#47530)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.3+sha-6a88bad/, version);
+      banner = banner.replace(/15.0.0-next.3+sha-002ee32/, version);
     }
   }
 }
