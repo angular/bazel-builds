7498d816e49cba4518a7e85630edd68a4a4fc637
Andrew Kushnir <akushnir@google.com>
2022-09-29 23:06:34 +0000

    76a8c68cc1 refactor(core): add input and output filtering for host directives (#47536)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.4+sha-26ad19e/, version);
+      banner = banner.replace(/15.0.0-next.4+sha-76a8c68/, version);
     }
   }
 }
