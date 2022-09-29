f8ae664b26266a397181f73087f49fc7786d23ab
Andrew Kushnir <akushnir@google.com>
2022-09-29 18:13:21 +0000

    26ad19e81a docs(common): update reference url to ICU docs (#47518) PR Close #47518

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.4+sha-c39a3a1/, version);
+      banner = banner.replace(/15.0.0-next.4+sha-26ad19e/, version);
     }
   }
 }
