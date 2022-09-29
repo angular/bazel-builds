ee93d8478a17e7ff5f2308dbee6d3a6e51ba47c4
Andrew Kushnir <akushnir@google.com>
2022-09-29 18:06:47 +0000

    5a17858c5a refactor(router): unify the `RouterLink` and `RouterLinkWithHref` class logic (#47500)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.4+sha-bcc4a95/, version);
+      banner = banner.replace(/15.0.0-next.4+sha-5a17858/, version);
     }
   }
 }
