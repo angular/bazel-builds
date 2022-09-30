c831da52b01cf223a25e1642e92a776e2dcd168a
Andrew Kushnir <akushnir@google.com>
2022-09-30 00:01:11 +0000

    752e2245f4 docs: add oxford commas and rephrase sentences (#47540)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.4+sha-89006b1/, version);
+      banner = banner.replace(/15.0.0-next.4+sha-752e224/, version);
     }
   }
 }
