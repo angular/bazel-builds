83c01c237bae67dc00b6adc179d8a985f118519b
Andrew Kushnir <akushnir@google.com>
2022-10-04 21:57:15 +0000

    29194638c1 refactor(compiler): Rename `PipeInScope` -> `PotentialPipe` (#47561)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.4+sha-e3cef4a/, version);
+      banner = banner.replace(/15.0.0-next.4+sha-2919463/, version);
     }
   }
 }
