de8abebecad4f51a40298663c5643ae4c467fea8
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 21:42:07 +0000

    e3ef401c25 docs: add Marko to GDE resources (#47595)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.5+sha-197e8b4/, version);
+      banner = banner.replace(/14.2.5+sha-e3ef401/, version);
     }
   }
 }
