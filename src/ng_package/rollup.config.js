0e0fcf18a115f85ffa01044334e5863db362ac7f
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 17:12:46 +0000

    e0dd2c53c2 build: update cross-repo angular dependencies (#47600)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.5+sha-fcea8fe/, version);
+      banner = banner.replace(/15.0.0-next.5+sha-e0dd2c5/, version);
     }
   }
 }
