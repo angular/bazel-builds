cc3f4f2c452ce419db9c33a034a9d5705c96bf97
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 23:43:20 +0000

    4620fbdf04 refactor(docs-infra): add new Input for better description of close button in notification component (#47681)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.5+sha-e3ef401/, version);
+      banner = banner.replace(/14.2.5+sha-4620fbd/, version);
     }
   }
 }
