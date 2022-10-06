755532735a593ce0089ddb37caf00efcb060f821
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 20:48:35 +0000

    1095dc5fd1 docs: fix documentation linting errors in ajs-quick-reference.md (#47526)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.5+sha-5782d63/, version);
+      banner = banner.replace(/14.2.5+sha-1095dc5/, version);
     }
   }
 }
