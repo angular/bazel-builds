a9392507a3c182d79497f6a035bfdb3edc7a46a8
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-07 19:03:21 +0000

    deb4cabe2e docs: update spelling dictionary (#47697)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.5+sha-33dc1e6/, version);
+      banner = banner.replace(/15.0.0-next.5+sha-deb4cab/, version);
     }
   }
 }
