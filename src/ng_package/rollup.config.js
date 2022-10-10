49e6fbe50d475ae7463de63184c6e591a2aaf614
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 20:22:59 +0000

    f467c9eabb build: update scorecard action dependencies (#47385)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.5+sha-6bcde0e/, version);
+      banner = banner.replace(/15.0.0-next.5+sha-f467c9e/, version);
     }
   }
 }
