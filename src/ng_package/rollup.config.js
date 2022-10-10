7621c0785fffe354e7f66b8a1d6278ff399bfa6d
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 21:53:23 +0000

    48b354a83e fix(compiler): update element schema (#47552)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.5+sha-bebef5f/, version);
+      banner = banner.replace(/15.0.0-next.5+sha-48b354a/, version);
     }
   }
 }
