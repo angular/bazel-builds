3d38c78dfb5cbc9e48dcc72ac096dde5e0cd6414
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 16:17:33 +0000

    1818c54b62 docs: fix linter errors in prerendering.md (#47724)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.5+sha-a41ff94/, version);
+      banner = banner.replace(/15.0.0-next.5+sha-1818c54/, version);
     }
   }
 }
