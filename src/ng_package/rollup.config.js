c5599061b9f566ef547a367490b19164c1fe81c2
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 23:32:30 +0000

    8921c12619 build: update all non-major dependencies (#47551)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.5+sha-547b45a/, version);
+      banner = banner.replace(/15.0.0-next.5+sha-8921c12/, version);
     }
   }
 }
