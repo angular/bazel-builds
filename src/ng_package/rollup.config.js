4b9c879985a0c9288d63fd48cdd04c8181050282
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-10-13 10:03:09 +0000

    ec97331c8e build: wire up new github action for identifying g3-affecting PRs (#47735)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-195a8d8/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-ec97331/, version);
     }
   }
 }
