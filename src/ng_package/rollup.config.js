74357aee7550644a32f01718b8dd4fabeb40cbf0
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:43:38 +0000

    4d18ba1af7 release: cut the v14.2.7 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.6+sha-1504473/, version);
+      banner = banner.replace(/14.2.7+sha-4d18ba1/, version);
     }
   }
 }
