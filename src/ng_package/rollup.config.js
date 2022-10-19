b211cb371f2a1f0876fedc2cca4a950fc531a78f
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:42:50 +0000

    64d6a4fc49 docs: Linting suggested corrections made. Restructuring or rewording sentences to be shorter and clearer. Inserted an introductory paragraph under level 1 heading. (#47788)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.6+sha-c62246c/, version);
+      banner = banner.replace(/14.2.6+sha-64d6a4f/, version);
     }
   }
 }
