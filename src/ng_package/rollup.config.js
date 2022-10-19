357a3a2207bfd92fd4ac6474caafa5ec08a7e36d
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 18:31:27 +0000

    716352c1b0 docs: fix linter errors for accessibility.md (#47725)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-next.6+sha-420d6b3/, version);
+      banner = banner.replace(/15.0.0-next.6+sha-716352c/, version);
     }
   }
 }
