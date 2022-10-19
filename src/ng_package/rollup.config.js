2ae45588c0e8d7d6349505bc207ee72589775fac
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:41:20 +0000

    bdd4d14db9 refactor(common): drop deprecated selector from the NgOptimizedImage directive (#47798)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-ce5880f/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-bdd4d14/, version);
     }
   }
 }
