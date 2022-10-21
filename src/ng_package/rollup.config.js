60081f9b9f477599dd4f995fb355fce163acecd6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 15:51:05 +0000

    fb273f6461 docs: fix minor typo in hierarchical-dependency-injection.md (#47812)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.0+sha-3a18398/, version);
+      banner = banner.replace(/15.0.0-rc.0+sha-fb273f6/, version);
     }
   }
 }
