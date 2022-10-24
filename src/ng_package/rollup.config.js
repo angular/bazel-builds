f2bf5c782f4dbac82b5eabfab7b1ebb14d7fb543
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 09:01:09 +0000

    6471eafde6 build: update recommended launch vscode config to use proper bazel dist path (#47831)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.0+sha-a014ca1/, version);
+      banner = banner.replace(/15.0.0-rc.0+sha-6471eaf/, version);
     }
   }
 }
