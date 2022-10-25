9004f4b7cece497949641217f063fe4a8ee6cca5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 13:52:17 +0000

    ed48887068 build: update cross-repo angular dependencies (#47871)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.0+sha-2908eba/, version);
+      banner = banner.replace(/15.0.0-rc.0+sha-ed48887/, version);
     }
   }
 }
