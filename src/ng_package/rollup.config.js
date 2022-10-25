371ed6c040805c02fe699147c304d56d016abb62
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 08:23:14 +0000

    c0903acbd4 Revert "fix(forms): don't mutate validators array (#47830)" (#47845)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.0+sha-588cd3e/, version);
+      banner = banner.replace(/15.0.0-rc.0+sha-c0903ac/, version);
     }
   }
 }
