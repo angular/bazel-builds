1ada02fdf7fb39de30d38ad1cf1b270448829c3f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:52:08 +0000

    f24da6baad release: cut the v15.0.0-rc.1 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.0+sha-ea7ff35/, version);
+      banner = banner.replace(/15.0.0-rc.1+sha-f24da6b/, version);
     }
   }
 }
