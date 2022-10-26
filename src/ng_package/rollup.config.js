768e24c777cac49c7f5389695b49382bf2cf3045
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 14:00:50 +0000

    d75e3da429 build: update bazel setup to v5.7.0 (#47436)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.0+sha-ebd2ddb/, version);
+      banner = banner.replace(/15.0.0-rc.0+sha-d75e3da/, version);
     }
   }
 }
