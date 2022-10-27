5f4a210971af48b08d92cc048e4715810733e312
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-27 07:48:20 +0000

    96c0e42e61 fix(core): allow readonly arrays for standalone imports (#47851)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.1+sha-f24da6b/, version);
+      banner = banner.replace(/15.0.0-rc.1+sha-96c0e42/, version);
     }
   }
 }
