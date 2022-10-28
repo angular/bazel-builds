177bf6f37bc424c17eb75d1b4fd137cd511894ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-10-28 09:59:21 +0000

    65a3338e54 docs: Linting corrections.  Restructuring or rewording sentences to be shorter and clearer. (#47897)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.8+sha-8707475/, version);
+      banner = banner.replace(/14.2.8+sha-65a3338/, version);
     }
   }
 }
