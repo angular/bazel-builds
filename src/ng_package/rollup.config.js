8c6f284756e9110e60105a24226d75412c11d82d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-01 12:01:58 +0000

    0d49fe01ea docs(core): update standalone docs for `provideRouter` (#47902)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-11288f2/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-0d49fe0/, version);
     }
   }
 }
