f8e9b06111be33421919c14f76fa694c5a5956dc
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:13:46 +0000

    e7b48da713 fix(http): rename `withLegacyInterceptors` to `withInterceptorsFromDi` (#47901)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.1+sha-173dde7/, version);
+      banner = banner.replace(/15.0.0-rc.2+sha-e7b48da/, version);
     }
   }
 }
