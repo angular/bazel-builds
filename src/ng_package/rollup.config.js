b00639090416f678304ebdf566dbdf821dc3962d
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:03:10 +0000

    32135b27a1 docs: update examples to reflect changes in Angular CLI version 15. (#47904)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-c9a6f95/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-32135b2/, version);
     }
   }
 }
