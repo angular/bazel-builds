32d182cf1775a4f2571e991f7fc6f255b915de85
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:13:35 +0000

    6d4f75931b build: add `--keep_going` to components tests. (#47958)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.9+sha-bab5f9a/, version);
+      banner = banner.replace(/14.2.9+sha-6d4f759/, version);
     }
   }
 }
