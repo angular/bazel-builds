3c7bc9e6c88e2f7e1c0b37371502c866063e6b4d
Andrew Kushnir <akushnir@google.com>
2022-11-07 21:41:56 +0000

    c3bc035f5d refactor(core): remove duplicate createRenderer call (#47981)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.9+sha-db867fe/, version);
+      banner = banner.replace(/14.2.9+sha-c3bc035/, version);
     }
   }
 }
