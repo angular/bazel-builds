064fefc06d3e138f4cef12db10aa1a57a6cca04f
Andrew Kushnir <akushnir@google.com>
2022-11-09 18:13:05 +0000

    a4312e1be5 fix(core): add` zone.js` version `0.12.x` as a valid peer dependency (#48002)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/14.2.9+sha-28efb06/, version);
+      banner = banner.replace(/14.2.9+sha-a4312e1/, version);
     }
   }
 }
