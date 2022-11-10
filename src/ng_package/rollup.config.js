609c8e1d18eea9a55f6a915175ba047d1f90c216
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-10 17:54:19 +0000

    449d29b701 fix(common): Add fetchpriority to ngOptimizedImage preloads (#48010)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.3+sha-6a9b1e1/, version);
+      banner = banner.replace(/15.0.0-rc.3+sha-449d29b/, version);
     }
   }
 }
