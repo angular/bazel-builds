8527e7717fa166b1d60c2fdba8be4496ac484711
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-11 19:46:53 +0000

    154ca82f7b docs(core): add an error details page for unsafe <iframe> bindings (#48027)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.3+sha-0c0550b/, version);
+      banner = banner.replace(/15.0.0-rc.3+sha-154ca82/, version);
     }
   }
 }
