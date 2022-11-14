33bdbd6a33b08293c18dbde432d9ea91d68d1175
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-14 20:22:26 +0000

    4cfb06086b release: cut the v15.0.0-rc.4 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.3+sha-933d481/, version);
+      banner = banner.replace(/15.0.0-rc.4+sha-4cfb060/, version);
     }
   }
 }
