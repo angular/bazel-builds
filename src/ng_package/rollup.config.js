7cca6f8e4bb18c6a32e6bea84e91ddd2c1516d51
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-15 20:57:27 +0000

    bc97322641 docs: add host directives guide (#48057)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0-rc.4+sha-4cfb060/, version);
+      banner = banner.replace(/15.0.0-rc.4+sha-bc97322/, version);
     }
   }
 }
