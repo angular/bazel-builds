a9c394fe781e22c6d875207cb41d2149e861e6cd
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-16 18:11:22 +0000

    893ec32726 docs: release notes for the v15.0.0 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-174c0db/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-893ec32/, version);
     }
   }
 }
