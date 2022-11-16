e947eddf2e864d32ec4af620ea7ee68b43d97a54
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-16 22:53:38 +0000

    bc063b2f86 docs: Add v15 release blog post link to changelog (#48094)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0+sha-b79ad22/, version);
+      banner = banner.replace(/15.0.0+sha-bc063b2/, version);
     }
   }
 }
