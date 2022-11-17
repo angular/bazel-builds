59368f67062a69f521d0de17402f7c4e4d381593
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:44:13 +0000

    0a8b8a66cd docs(router): Deprecate public members of Router that are meant to be configured elsewhere (#48006)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-6732618/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-0a8b8a6/, version);
     }
   }
 }
