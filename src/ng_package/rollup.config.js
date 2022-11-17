b54c991066c1705c1d0e91aacaf25e38db63b528
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:49:00 +0000

    79bd0f1e96 build(docs-infra): upgrade cli command docs sources to 6ccbc6b4c (#48076)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-80e3c28/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-79bd0f1/, version);
     }
   }
 }
