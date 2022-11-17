f3b4b7316b7c76ae28bf971cc3244a98b00afc12
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-17 01:14:43 +0000

    2551c78cc5 docs: add info about v15 changes and update process (#48095)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.0+sha-bc063b2/, version);
+      banner = banner.replace(/15.0.0+sha-2551c78/, version);
     }
   }
 }
