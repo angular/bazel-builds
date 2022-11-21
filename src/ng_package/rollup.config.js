f56778ba3eb096ae43d7e93f86a2833a8bbde1cc
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 21:30:49 +0000

    ec58266eab release: cut the v13.3.12 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/13.3.11+sha-4ea399a/, version);
+      banner = banner.replace(/13.3.12+sha-ec58266/, version);
     }
   }
 }
