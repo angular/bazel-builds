0654a0606900af5a39fb2da3ff2fa27b1c159cc1
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 18:04:49 +0000

    fd2eea5961 fix(language-service): correctly handle host directive inputs/outputs (#48147)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-c5d176c/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-fd2eea5/, version);
     }
   }
 }
