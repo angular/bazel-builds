8df22c43b756d51f969a3ae19af8046ea2769c65
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:51:14 +0000

    73f03ad2d2 feat(router): Add new NavigationSkipped event for ignored navigations (#48024)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-ffc427b/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-73f03ad/, version);
     }
   }
 }
