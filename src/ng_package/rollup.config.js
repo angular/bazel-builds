18080c6e99af05b5d6ff406f5312bc6e5a64a402
Joey Perrott <josephperrott.github@gmail.com>
2022-11-23 22:14:54 +0000

    12db492233 build(bazel): fix pwa test metrics

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-b258b32/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-12db492/, version);
     }
   }
 }
