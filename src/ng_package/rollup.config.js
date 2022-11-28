7b24283eed38c71f75db541815ce817f3e89da99
Andrew Scott <atscott01@gmail.com>
2022-11-28 19:11:01 +0000

    afc668acd5 refactor(router): Move navigationId handling to the transition manager (#48202)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-fcf8572/, version);
+      banner = banner.replace(/15.1.0-next.0+sha-afc668a/, version);
     }
   }
 }
