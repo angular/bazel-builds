fef08b4acc368b43bbf30a42d35653791e8184e6
Andrew Scott <atscott01@gmail.com>
2022-11-29 20:45:58 +0000

    2067643896 build: update eslint dependencies (#48216)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.1+sha-bec0007/, version);
+      banner = banner.replace(/15.0.1+sha-2067643/, version);
     }
   }
 }
