25b40fade9efc14d91a65498c35f34e71dea53c1
Andrew Scott <atscott@google.com>
2022-11-29 01:15:03 +0000

    a192dea677 Revert "refactor(router): remove malformedUriHandler from interface passed to transitions (#48257)" (#48270)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.1+sha-c0601db/, version);
+      banner = banner.replace(/15.0.1+sha-a192dea/, version);
     }
   }
 }
