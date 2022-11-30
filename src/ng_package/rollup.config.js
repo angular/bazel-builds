df58395dc13d9114e93ad8b002a296ce22250fd9
Andrew Scott <atscott01@gmail.com>
2022-11-30 16:46:27 +0000

    2a445923ee refactor(router): Remove DeprecatedLoadChildren (#48292)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.1+sha-82756e8/, version);
+      banner = banner.replace(/15.0.1+sha-2a44592/, version);
     }
   }
 }
