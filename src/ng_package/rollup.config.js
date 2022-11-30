462ff66d44eea25265c2eb4c80545a78866ed58e
Andrew Scott <atscott01@gmail.com>
2022-11-30 20:55:14 +0000

    4d398a0bab release: cut the v15.1.0-next.1 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.0+sha-33fcb0f/, version);
+      banner = banner.replace(/15.1.0-next.1+sha-4d398a0/, version);
     }
   }
 }
