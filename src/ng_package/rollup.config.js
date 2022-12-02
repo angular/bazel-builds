0d4aaa14b3b006eda00e1840d88ebf5704552458
Andrew Kushnir <akushnir@google.com>
2022-12-02 23:14:40 +0000

    2352c2f143 build: attempt to avoid nodejs version flakiness in `test_win` job (#48326)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.1+sha-7b7d5e1/, version);
+      banner = banner.replace(/15.1.0-next.1+sha-2352c2f/, version);
     }
   }
 }
