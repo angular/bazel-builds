16e127ba0014e2b63cedada3941ceca31484bb6c
Andrew Kushnir <akushnir@google.com>
2022-12-02 17:19:31 +0000

    6706fab017 perf(docs-infra): remove usage of the NgClass directive (#48312)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.1+sha-21952bb/, version);
+      banner = banner.replace(/15.1.0-next.1+sha-6706fab/, version);
     }
   }
 }
