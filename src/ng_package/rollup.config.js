25d45255edb157fe38a83e3185d4bc97d0fec1f7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-03 16:27:44 +0000

    6f79c2b7fd build: update actions/checkout action to v3 (#48337)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-59f2d46/, version);
+      banner = banner.replace(/15.0.2+sha-6f79c2b/, version);
     }
   }
 }
