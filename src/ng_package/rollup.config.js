b17b62492aca1067ff1245c0c910eb71a9779114
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 20:44:24 +0000

    226441fc3a build: set target firebase project when setting deploy target (#48352)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-38701ce/, version);
+      banner = banner.replace(/15.0.2+sha-226441f/, version);
     }
   }
 }
