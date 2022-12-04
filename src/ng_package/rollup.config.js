bb6e8a1b3faa2ca54d783318f040e02af627df04
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 16:32:48 +0000

    38701ce07b build: check out project before setting AIO preview targets (#48347)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-09388eb/, version);
+      banner = banner.replace(/15.0.2+sha-38701ce/, version);
     }
   }
 }
