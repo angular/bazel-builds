5f28b37d3e0a2369da9f24c4cee73baff6cb2daf
Andrew Kushnir <akushnir@google.com>
2022-12-05 23:18:54 +0000

    45cc5f49e4 build: switch new AIO preview action to use `aio: preview` label (#48345)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-50b1c2b/, version);
+      banner = banner.replace(/15.0.2+sha-45cc5f4/, version);
     }
   }
 }
