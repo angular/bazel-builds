329e1cdba5a8486e404bf2bb8c92e74c9226912a
Andrew Kushnir <akushnir@google.com>
2022-12-05 20:41:11 +0000

    50b1c2bf52 fix(common): Don't generate srcsets with very large sources (#47997)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-1b37306/, version);
+      banner = banner.replace(/15.0.2+sha-50b1c2b/, version);
     }
   }
 }
