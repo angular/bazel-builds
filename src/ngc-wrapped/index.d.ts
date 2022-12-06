ee140c799400a322f4a646b24f8c17df4eaf7772
Andrew Kushnir <akushnir@google.com>
2022-12-06 19:07:03 +0000

    dd42974b07 feat(core): support TypeScript 4.9 (#48005)

--- src/ngc-wrapped/index.d.ts
+++ src/ngc-wrapped/index.d.ts
@@ -9,7 +9,7 @@
 import { BazelOptions as ExternalBazelOptions, CompilerHost } from '@bazel/concatjs/internal/tsc_wrapped';
 import type { CompilerHost as NgCompilerHost, Program, CompilerOptions } from '@angular/compiler-cli';
 import ts from 'typescript';
-declare type CompilerCliModule = typeof import('@angular/compiler-cli') & typeof import('@angular/compiler-cli/private/bazel');
+type CompilerCliModule = typeof import('@angular/compiler-cli') & typeof import('@angular/compiler-cli/private/bazel');
 interface BazelOptions extends ExternalBazelOptions {
     allowedInputs?: string[];
     unusedInputsListPath?: string;
