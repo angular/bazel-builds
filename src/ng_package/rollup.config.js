aacb9ee5cec36a32c73526d6e2161dfc54f7d4f5
Andrew Kushnir <akushnir@google.com>
2022-12-06 20:02:07 +0000

    5e2cd07a0b build: update dependency jasmine-spec-reporter to v7 (#48367)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-4c76f94/, version);
+      banner = banner.replace(/15.0.2+sha-5e2cd07/, version);
     }
   }
 }
