c1590a689082b9f06b88899f053f64c1a3777ff7
Andrew Kushnir <akushnir@google.com>
2022-12-06 00:16:24 +0000

    ac51e5c107 build: update dependency jasmine-core to v4 (#48366)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-45cc5f4/, version);
+      banner = banner.replace(/15.0.2+sha-ac51e5c/, version);
     }
   }
 }
