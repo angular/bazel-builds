b53066e4b5a3864d213adba213b546b42643c2f4
Andrew Kushnir <akushnir@google.com>
2022-12-07 22:27:33 +0000

    b55d2dab5d fix(compiler-cli): evaluate const tuple types statically (#48091)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.2+sha-bf44dc2/, version);
+      banner = banner.replace(/15.0.3+sha-b55d2da/, version);
     }
   }
 }
