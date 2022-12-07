49c6509990d7ab9d77a6100c8a4e12666aea7e22
Andrew Kushnir <akushnir@google.com>
2022-12-07 17:30:07 +0000

    146d2ee246 refactor(compiler): replace `flatten` and `map` with `flatMap`. (#48378)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.1+sha-11a24c3/, version);
+      banner = banner.replace(/15.1.0-next.1+sha-146d2ee/, version);
     }
   }
 }
