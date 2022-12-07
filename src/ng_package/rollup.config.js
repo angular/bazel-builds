0909d76f0c7a34b22d00b4ba1a9c75dae31852a7
Andrew Kushnir <akushnir@google.com>
2022-12-07 19:01:11 +0000

    f8ecc194e9 fix(common): Update `Location` to support base href containing `origin` (#48327)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.1+sha-146d2ee/, version);
+      banner = banner.replace(/15.1.0-next.1+sha-f8ecc19/, version);
     }
   }
 }
