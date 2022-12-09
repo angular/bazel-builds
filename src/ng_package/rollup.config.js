1e96f12e05aed0323977f178bf250b8e0ad7191b
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 21:36:37 +0000

    126e798edf docs(common): Add note to location about investigating base href handling (#48422)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.3+sha-85d3f6f/, version);
+      banner = banner.replace(/15.0.3+sha-126e798/, version);
     }
   }
 }
