561088bdd56bb08eb387d4bd4dc56dff7e749375
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 18:03:17 +0000

    caa1ad000e docs(router): Update ROUTES docs to not point to provideRoutes (#48417)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.2+sha-e362214/, version);
+      banner = banner.replace(/15.1.0-next.2+sha-caa1ad0/, version);
     }
   }
 }
