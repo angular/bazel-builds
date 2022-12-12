084f038553f3a454d1b97c6619270bf2058f2d42
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 20:27:11 +0000

    1a318155e5 docs: move tour of heroes tutorial to a subdirectory (#48162)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.3+sha-f30d18a/, version);
+      banner = banner.replace(/15.0.3+sha-1a31815/, version);
     }
   }
 }
