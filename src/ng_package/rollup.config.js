b8797fa4398b91a15dbd60cb5c8857529554ed5d
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 18:04:33 +0000

    f30d18a942 fix(common): Fix TestBed.overrideProvider type to include multi (#48424)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.3+sha-6279fec/, version);
+      banner = banner.replace(/15.0.3+sha-f30d18a/, version);
     }
   }
 }
