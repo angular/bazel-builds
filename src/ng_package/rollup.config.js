1abf6cf10721058f52ece5547cc674ecaf5a7934
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 00:45:47 +0000

    70b2508d22 docs: add docgeni to tooling of resources list (#48061)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.3+sha-6c1064c/, version);
+      banner = banner.replace(/15.0.3+sha-70b2508/, version);
     }
   }
 }
