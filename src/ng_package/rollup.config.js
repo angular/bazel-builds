e9b47a452a771fbb892ea750ba4a0ee6e19ae6fa
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-21 13:34:28 +0000

    d768fb7fe5 docs: updated docs removed unnecessary imports and empty methods (#48513)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.4+sha-a3958a2/, version);
+      banner = banner.replace(/15.0.4+sha-d768fb7/, version);
     }
   }
 }
