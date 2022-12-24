2c8537effb7a2a4dfe6a38630974b8b55efa2d34
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-24 11:45:09 +0000

    125b3df884 ci: remove redundant aio dependencies installation step (#48574)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -142,7 +142,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.4+sha-ab13c96/, version);
+      banner = banner.replace(/15.0.4+sha-125b3df/, version);
     }
   }
 }
