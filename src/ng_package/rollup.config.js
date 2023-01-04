d363e54d759e8da7683a98992d053a4c0abaed26
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:45:24 +0000

    2d3c98ddf3 docs: missing closing parenthesis in withHashLocation() example (#48507)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.3+sha-33f35b0/, version);
+      banner = banner.replace(/15.1.0-next.3+sha-2d3c98d/, version);
     }
   }
 }
