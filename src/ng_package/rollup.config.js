2bcbfd45b8eef4cc4ac48b42deb12f94074874f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:05:44 +0000

    2f4f0638c7 fix(common): Add data attribtue to NgOptimizedImage (#48497)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.3+sha-aa51e9b/, version);
+      banner = banner.replace(/15.1.0-next.3+sha-2f4f063/, version);
     }
   }
 }
