ee9adbf4d2ac36e2169428b09511244700482214
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:15:43 +0000

    b9577adeb2 refactor(forms): cleanup type any in forms tests (#48624)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-15eccef/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-b9577ad/, version);
     }
   }
 }
