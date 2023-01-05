769255c3f0ccab72d2c12e29dc9a3cd92edc31b5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-05 16:55:26 +0000

    5d1fbbbf83 ci: fix snapshot publish github credential preparation (#48648)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.3+sha-fc1c03e/, version);
+      banner = banner.replace(/15.1.0-next.3+sha-5d1fbbb/, version);
     }
   }
 }
