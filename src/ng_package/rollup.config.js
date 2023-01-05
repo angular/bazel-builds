e238e92998bbd230e0f6afd5ffc07236ac2a27cd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 21:30:00 +0000

    28ea114d54 release: cut the v15.1.0-rc.0 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0-next.3+sha-5d1fbbb/, version);
+      banner = banner.replace(/15.1.0-rc.0+sha-28ea114/, version);
     }
   }
 }
