82119e794965da2f86eb74c796707bd1d7f0e94a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:01:55 +0000

    0c5db1a26e refactor(router): Remove files no longer in use (#48500)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-009b0d0/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-0c5db1a/, version);
     }
   }
 }
