682e3b1ff8770890697c44220d756388b78ef624
GitHub <noreply@github.com>
2023-01-06 09:02:37 +0000

    b6cc5f7eac build: update `SECURITY.md` to match the content of `angular/.github`

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-bdf288d/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-b6cc5f7/, version);
     }
   }
 }
