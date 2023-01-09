1523d94540253dbc4908b1e3147f44996144d388
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-09 18:27:48 +0000

    993217e192 fix(language-service): ship `/api` entry-point (#48670)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.0.4+sha-433dd5c/, version);
+      banner = banner.replace(/15.0.4+sha-993217e/, version);
     }
   }
 }
