95c8468e52e35dc5629264de6f73bc9f7e9bc823
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 17:29:08 +0000

    489243bbb8 fix(language-service): expose `package.json` for vscode extension resolution (#48678)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-7243ae6/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-489243b/, version);
     }
   }
 }
