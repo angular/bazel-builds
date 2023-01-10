03a235d5b788aa40fb19bd10321383fb62baa58e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 19:24:50 +0000

    d4a56721a4 ci: use readonly github token for querying CLI snapshot repo (#48681)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-45029f1/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-d4a5672/, version);
     }
   }
 }
