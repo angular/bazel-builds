f88f9db56b80e6c655ff88c16afeef74856a86f7
Andrew Kushnir <akushnir@google.com>
2023-01-12 20:02:38 +0000

    0b57e24eb6 docs: add first-app-lesson-13 example (#48512)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume BUILD_SCM_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-64967da/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-0b57e24/, version);
     }
   }
 }
