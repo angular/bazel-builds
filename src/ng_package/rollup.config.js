3a963849e78ca148d5b304a674eaf4dcd9b04ac2
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:48:23 +0000

    914e7b052e build: update actions/cache digest to 58c146c (#48564)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-5b1c7c1/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-914e7b0/, version);
     }
   }
 }
