9722782bc9ca344c70d0cf560643e727e92830da
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:46:19 +0000

    d47bd480c9 build: update scorecard action dependencies (#48465)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-f03f20c/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-d47bd48/, version);
     }
   }
 }
