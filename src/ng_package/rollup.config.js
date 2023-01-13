9d18368fcbc6598996a8736d49d743a9ccfcd6a9
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:45:11 +0000

    34dda890ed build: update io_bazel_rules_sass digest to 57ade09 (#48407)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-13f10de/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-34dda89/, version);
     }
   }
 }
