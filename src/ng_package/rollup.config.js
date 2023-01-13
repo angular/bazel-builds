de73a5f71c7c55833af31be8981440cc77f90cd3
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:59:12 +0000

    032b2bd689 perf(common): avoid excessive DOM mutation in NgClass (#48433)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0+sha-5978eed/, version);
+      banner = banner.replace(/15.1.0+sha-032b2bd/, version);
     }
   }
 }
