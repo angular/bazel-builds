b0285da024f2596da672cf856f4e94e564a6e6fc
Andrew Kushnir <akushnir@google.com>
2023-01-17 19:02:20 +0000

    9130635eac test: update size golden to reflect Angular cross dependency updates (#48747)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-fb7b8a9/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-9130635/, version);
     }
   }
 }
