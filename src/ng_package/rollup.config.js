ab472ac24e9076414ebbfbe31ecc263ac4ec4c72
Andrew Kushnir <akushnir@google.com>
2023-01-17 17:42:23 +0000

    6686eacb90 docs: Fix example in breaking changes to apply to correct sentence (#48758)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0+sha-032b2bd/, version);
+      banner = banner.replace(/15.1.0+sha-6686eac/, version);
     }
   }
 }
