7e1a1e9df14195014eed3b46c6d8f7a453788c74
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-01-18 18:28:29 +0000

    e3a8a5cc42 release: cut the v15.1.1 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.0+sha-0f8a4f9/, version);
+      banner = banner.replace(/15.1.1+sha-e3a8a5c/, version);
     }
   }
 }
