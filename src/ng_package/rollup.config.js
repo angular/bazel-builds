be25c332cf96efb3169ac31f84ad6c4cd97a7c66
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:04:44 +0000

    fe6a2bd9e2 build: update dependency rimraf to v4 (#48726)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.1+sha-3a688cb/, version);
+      banner = banner.replace(/15.2.0-next.1+sha-fe6a2bd/, version);
     }
   }
 }
