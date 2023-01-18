793960cba72bc204dcc4a1441452ae959409c6af
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-01-18 18:28:58 +0000

    318d94d4e1 docs: release notes for the v15.1.1 release

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.0+sha-9130635/, version);
+      banner = banner.replace(/15.2.0-next.0+sha-318d94d/, version);
     }
   }
 }
