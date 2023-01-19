3998cdc9b12df3dabf5dcbede7eca1df01a1e881
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-19 17:53:43 +0000

    60a7214ab2 refactor(bazel): Extract the helper patchNgHostWithFileNameToModuleName to its own file for 1P use (#48739)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.1.1+sha-97f6535/, version);
+      banner = banner.replace(/15.1.1+sha-60a7214/, version);
     }
   }
 }
