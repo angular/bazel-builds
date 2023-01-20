4db478f81988e8a439efefe16488fec04a2216fd
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 17:25:24 +0000

    840530ffd8 docs: update supported Chrome version description (#48782)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.1+sha-2d0ca73/, version);
+      banner = banner.replace(/15.2.0-next.1+sha-840530f/, version);
     }
   }
 }
