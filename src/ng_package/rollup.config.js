031237d210723eb65562e9f0c2e9878df4a38215
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 01:34:35 +0000

    2d0ca73d73 docs: Document extracted from the original Introduction document, which is to be retired. Linting suggested corrections made.  Restructuring or rewording sentences to be shorter and clearer. (#47907)

--- src/ng_package/rollup.config.js
+++ src/ng_package/rollup.config.js
@@ -143,7 +143,7 @@ if (bannerFile) {
     // Don't assume STABLE_PROJECT_VERSION exists
     if (versionTag) {
       const version = versionTag.split(' ')[1].trim();
-      banner = banner.replace(/15.2.0-next.1+sha-bca76ed/, version);
+      banner = banner.replace(/15.2.0-next.1+sha-2d0ca73/, version);
     }
   }
 }
