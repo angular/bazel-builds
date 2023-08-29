23af8716d145a7dd8ddcad81c0ef9b1b4b325e6d
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 19:18:49 +0000

    0b6aae8d6d build: use pin for devcontainer base image (#51556)

---
+++ src/ng_package/cross_entry_points_imports.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { BazelFileInfo, PackageMetadata } from './api';
+/**
+ * Analyzes the given JavaScript source file and checks whether there are
+ * any relative imports that point to different entry-points or packages.
+ *
+ * Such imports are flagged and will be returned in the failure list. Cross
+ * entry-point or package imports result in duplicate code and therefore are
+ * forbidden (unless explicitly opted out via comment - {@link skipComment}).
+ */
+export declare function analyzeFileAndEnsureNoCrossImports(file: BazelFileInfo, pkg: PackageMetadata): string[];
